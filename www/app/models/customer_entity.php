e0ec5245877b9a64586cb7ad3f5641765541bb08
lo-svn <lo-svn>
2012-10-16 19:09:59 +0000

    37851499
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/trunk@41 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- www/app/models/customer_entity.php
+++ www/app/models/customer_entity.php
@@ -1,7 +1,7 @@
 <?php
 class core_model_customer_entity extends core_model_base_customer_entity
 {
-	
+
 	function init_fields()
 	{
 		global $core;
@@ -24,34 +24,34 @@ class core_model_customer_entity extends core_model_base_customer_entity
 
 	function loadrow_by_email($username)
 	{
-		$this->import(core_db::row('select * from customer_entity where lower(email)=lower(\''.mysql_escape_string($username).'\')'));
+		$this->import(core_db::row('select * from customer_entity inner join organizations on customer_entity.org_id = organizations.org_id where organizations.is_deleted = 0 and organizations.is_enabled = 1 and customer_entity.is_deleted = 0 and customer_entity.is_enabled = 1 and lower(email)=lower(\''.mysql_escape_string($username).'\')'));
 		return $this;
 	}
-	
+
 	function authenticate($username,$password)
 	{
 		global $core;
 		core::load_library('crypto');
-		
+
 		$sql = '
 			select ce.*,d.hostname,d.domain_id,
 			o.name,o.buyer_type,o.allow_sell,o.is_active as org_is_active,
 			o.is_enabled as org_is_enabled,otd.orgtype_id,
 			d.name as hub_name,d.detailed_name as hub_detailed_name,
 			tz.offset_seconds,tz_name
-			
+
 			from customer_entity ce
 			inner join organizations o on (ce.org_id=o.org_id)
 			inner join organizations_to_domains otd on (o.org_id=otd.org_id)
 			inner join domains d on (d.domain_id=otd.domain_id and otd.is_home=1)
 			inner join timezones tz using (tz_id)
 			where trim(lower(ce.email))=lower(\''.mysql_escape_string($username).'\')
-			and ce.is_deleted=0 
+			and ce.is_deleted=0
 			and o.is_deleted=0
 		';
 		$row = core_db::row($sql);
-	
-		
+
+
 		if(!$row)
 		{
 			return array(
@@ -78,7 +78,7 @@ class core_model_customer_entity extends core_model_base_customer_entity
 			}
 		}
 /*
-		
+
 		$sql = '
 			select ce.entity_id, cev1.value as first_name,cev2.value as last_name,ce.email,
 			ce.group_id,ce.store_id,d.hostname
@@ -86,7 +86,7 @@ class core_model_customer_entity extends core_model_base_customer_entity
 			inner join customer_entity_varchar cev1 on (cev1.entity_id=ce.entity_id and cev1.attribute_id=5)
 			inner join customer_entity_varchar cev2 on (cev2.entity_id=ce.entity_id and cev2.attribute_id=7)
 			inner join OrbitUser ou on ou.EMAIL=ce.email
-			inner join core_store cs on ce.website_id=cs.website_id 
+			inner join core_store cs on ce.website_id=cs.website_id
 			inner join domains d on cs.store_id=d.mag_store
 			where (ce.email=\''.mysql_escape_string($username).'\' or ou.LOGIN_NAME=\''.mysql_escape_string($username).'\')
 			and ou.PASSWORD=\''.mysql_escape_string($password).'\'
@@ -104,7 +104,7 @@ class core_model_customer_entity extends core_model_base_customer_entity
 			return $row;
 		}
 	}
-	
+
 	function get_domain_permissions($org_id)
 	{
 		global $core;
@@ -115,10 +115,10 @@ class core_model_customer_entity extends core_model_base_customer_entity
 				->to_hash('domain_id',false);
 		$row[0] = array_values(array_map(function ($var) {return $var['domain_id'];}, array_filter($domain_ids, function ($var) { return $var['is_home']; })));
 		$row[0] = $row[0][0];
-		
+
 		$row[1] = array_values(array_map(function ($var) { return $var['domain_id'];}, $domain_ids));
 		$domains = array();
-		foreach ($domain_ids as $domain) 
+		foreach ($domain_ids as $domain)
 		{
 			$domains[$domain['orgtype_id']] = isset($domains[$domain['orgtype_id']]) ? $domains[$domain['orgtype_id']] : array();
 			$domains[$domain['orgtype_id']][] = $domain['domain_id'];
@@ -126,14 +126,14 @@ class core_model_customer_entity extends core_model_base_customer_entity
 		$row[2] = $domains;
 		return $row;
 	}
-	
+
 	function force_auth($entity_id)
 	{
 		$sql = '
 			select ce.entity_id, cev1.value as first_name,cev2.value as last_name,ce.email,
 			ce.group_id,ce.store_id,d.hostname
 			from customer_entity ce
-			inner join core_store cs on ce.website_id=cs.website_id 
+			inner join core_store cs on ce.website_id=cs.website_id
 			inner join domains d on cs.store_id=d.mag_store
 			where ce.entity_id='.intval($entity_id).'
 		';
