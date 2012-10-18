c972165b093b9c866c3000b23be2b04f654fd7b0
lo-svn <lo-svn>
2012-10-18 16:21:00 +0000

    23685379
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/trunk@48 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- db/patches/3.8/23685379.sql
+++ db/patches/3.8/23685379.sql
@@ -5,7 +5,7 @@ drop table if exists x_invoices_payments;
 drop table if exists invoice_send_dates;
 
 create table payables (
-	payables_id int(10) unsigned NOT NULL AUTO_INCREMENT,
+	payables_id int(10) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	payables_date datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
 	from_org_id bigint(20) NOT NULL,
 	to_org_id bigint(20) NOT NULL,
@@ -14,7 +14,7 @@ create table payables (
 );
 
 create table invoices (
-	invoice_id int(10) unsigned NOT NULL AUTO_INCREMENT,
+	invoice_id int(10) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	invoice_date datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
 	due_date datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
 	from_org_id bigint(20) NOT NULL,
@@ -24,20 +24,20 @@ create table invoices (
 );
 
 create table payments (
-	payment_id int(10) unsigned NOT NULL AUTO_INCREMENT,
+	payment_id int(10) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	amount decimal(10,2) NOT NULL,
 	from_org_id bigint(20) NOT NULL,
 	to_org_id bigint(20) NOT NULL
 );
 
 create table x_invoices_payments (
-	x_invoices_payments_id int(10) unsigned NOT NULL AUTO_INCREMENT,
+	x_invoices_payments_id int(10) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	invoice_id int(10) unsigned NOT NULL,
 	payment_id int(10) unsigned NOT NULL
 );
 
 create table  invoice_send_dates (
-	invoice_send_date_id int(10) unsigned NOT NULL AUTO_INCREMENT,
+	invoice_send_date_id int(10) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	invoice_id int(10) unsigned NOT NULL,
 	send_date datetime NOT NULL DEFAULT '0000-00-00 00:00:00'
 );
