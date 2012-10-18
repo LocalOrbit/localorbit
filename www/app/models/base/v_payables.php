bd034c9ed0a86ae3a3cd683d567a8a33ff68f0ed
lo-svn <lo-svn>
2012-10-18 20:14:00 +0000

    adding changes to payables
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/trunk@66 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- www/app/models/base/v_payables.php
+++ www/app/models/base/v_payables.php
@@ -3,18 +3,22 @@ class core_model_base_v_payables extends core_model
 {
 	function init_fields()
 	{
-		$this->add_field(new core_model_field(0,'payable_amount','float',10,'2','v_payables'));
-		$this->add_field(new core_model_field(1,'is_invoiced','int',8,'','v_payables'));
-		$this->add_field(new core_model_field(2,'invoicable','int',8,'','v_payables'));
-		$this->add_field(new core_model_field(3,'from_org_id','int',8,'','v_payables'));
-		$this->add_field(new core_model_field(4,'from_org_name','string',-4,'','v_payables'));
-		$this->add_field(new core_model_field(5,'to_org_id','int',8,'','v_payables'));
-		$this->add_field(new core_model_field(6,'to_org_name','string',-4,'','v_payables'));
-		$this->add_field(new core_model_field(7,'payable_type','string',-4,'','v_payables'));
-		$this->add_field(new core_model_field(8,'buyer_order_identifier','string',-4,'','v_payables'));
-		$this->add_field(new core_model_field(9,'seller_order_identifier','string',-4,'','v_payables'));
-		$this->add_field(new core_model_field(10,'amount_paid','float',10,'2','v_payables'));
-		$this->add_field(new core_model_field(11,'amount_due','float',10,'2','v_payables'));
+		$this->add_field(new core_model_field(0,'payable_id','int',8,'','v_payables'));
+		$this->add_field(new core_model_field(1,'payable_amount','float',10,'2','v_payables'));
+		$this->add_field(new core_model_field(2,'creation_date','timestamp',4,'','v_payables'));
+		$this->add_field(new core_model_field(3,'is_invoiced','int',8,'','v_payables'));
+		$this->add_field(new core_model_field(4,'invoicable','int',8,'','v_payables'));
+		$this->add_field(new core_model_field(5,'domain_name','string',-4,'','v_payables'));
+		$this->add_field(new core_model_field(6,'from_org_id','int',8,'','v_payables'));
+		$this->add_field(new core_model_field(7,'from_org_name','string',-4,'','v_payables'));
+		$this->add_field(new core_model_field(8,'to_org_id','int',8,'','v_payables'));
+		$this->add_field(new core_model_field(9,'to_org_name','string',-4,'','v_payables'));
+		$this->add_field(new core_model_field(10,'payable_type','string',-4,'','v_payables'));
+		$this->add_field(new core_model_field(11,'buyer_order_identifier','string',-4,'','v_payables'));
+		$this->add_field(new core_model_field(12,'seller_order_identifier','string',-4,'','v_payables'));
+		$this->add_field(new core_model_field(13,'description','string',-4,'','v_payables'));
+		$this->add_field(new core_model_field(14,'amount_paid','float',10,'2','v_payables'));
+		$this->add_field(new core_model_field(15,'amount_due','float',10,'2','v_payables'));
 		$this->init_data();
 	}
 }
