0500c7bbe94d8c27bda92c00ef1f82ca15259af5
lo-svn <lo-svn>
2012-10-18 17:25:55 +0000

    23685379
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/trunk@49 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- db/patches/3.8/23685379.sql
+++ db/patches/3.8/23685379.sql
@@ -3,6 +3,7 @@ drop table if exists invoices;
 drop table if exists payments;
 drop table if exists x_invoices_payments;
 drop table if exists invoice_send_dates;
+drop table if exists payment_methods;
 
 create table payables (
 	payables_id int(10) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -27,7 +28,8 @@ create table payments (
 	payment_id int(10) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	amount decimal(10,2) NOT NULL,
 	from_org_id bigint(20) NOT NULL,
-	to_org_id bigint(20) NOT NULL
+	to_org_id bigint(20) NOT NULL,
+	payment_method_id int(11) unsigned not null 
 );
 
 create table x_invoices_payments (
@@ -40,4 +42,9 @@ create table  invoice_send_dates (
 	invoice_send_date_id int(10) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	invoice_id int(10) unsigned NOT NULL,
 	send_date datetime NOT NULL DEFAULT '0000-00-00 00:00:00'
+);
+
+create table payment_methods (
+	payment_method_id int(11) unsigned not null auto_increment PRIMARY KEY,
+	payment_method varchar(255) not null
 );
