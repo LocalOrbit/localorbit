7ae9b509d843eddc4ebf4c8b39bb7557b3c3466e
lo-svn <lo-svn>
2012-10-18 19:25:28 +0000

    adding updated views
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/trunk@62 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- db/patches/3.8/23685379.sql
+++ db/patches/3.8/23685379.sql
@@ -98,6 +98,10 @@ insert into payment_methods (payment_method) values ('purchaseorder');
 insert into payment_methods (payment_method) values ('ACH');
 insert into payment_methods (payment_method) values ('check');
 
+
+
+
+
 drop view  if exists v_payables;
 
 CREATE VIEW v_payables AS 
@@ -154,7 +158,7 @@ CREATE VIEW v_invoices AS
 	) as amount_paid,
 	
 	(
-		select (iv.amount  - if(sum(xip.amount_paid)  is null,0,sum(xip.amount_paid)))
+		select iv.amount  - if(sum(xip.amount_paid)  is null,0,sum(xip.amount_paid))
 		from x_invoices_payments xip
 		where xip.invoice_id=iv.invoice_id
 	) as amount_due,
