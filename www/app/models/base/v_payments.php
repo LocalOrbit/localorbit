dd5be25b046cddc65d84aeb56f7fd54ed6f32191
lo-svn <lo-svn>
2012-10-22 13:46:35 +0000

    23685379
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/trunk@75 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

---
+++ www/app/models/base/v_payments.php
@@ -0,0 +1,16 @@
+<?php
+class core_model_base_v_payments extends core_model
+{
+	function init_fields()
+	{
+		$this->add_field(new core_model_field(0,'payment_id','int',8,'','v_payments'));
+		$this->add_field(new core_model_field(1,'amount','float',10,'2','v_payments'));
+		$this->add_field(new core_model_field(2,'creation_date','timestamp',4,'','v_payments'));
+		$this->add_field(new core_model_field(3,'from_org_id','int',8,'','v_payments'));
+		$this->add_field(new core_model_field(4,'from_org_name','string',-4,'','v_payments'));
+		$this->add_field(new core_model_field(5,'to_org_id','int',8,'','v_payments'));
+		$this->add_field(new core_model_field(6,'to_org_name','string',-4,'','v_payments'));
+		$this->init_data();
+	}
+}
+?>
