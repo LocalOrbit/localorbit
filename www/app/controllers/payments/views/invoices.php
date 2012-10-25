1c62aa8025adabb65d64ea132c231b77f26ce0ff
lo-svn <lo-svn>
2012-10-25 21:19:53 +0000

    23685379
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/trunk@101 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- www/app/controllers/payments/views/invoices.php
+++ www/app/controllers/payments/views/invoices.php
@@ -1,7 +1,7 @@
 <?php
 $invoices = core::model('v_invoices')->collection()->filter('to_org_id' , $core->session['org_id']);
 $invoices->add_formatter('payable_info');
-$invoices_table = new core_datatable('invoices','payments/portal',$invoices);
+$invoices_table = new core_datatable('invoices','payments/invoices',$invoices);
 $invoices_table->add(new core_datacolumn('creation_date','Date',true,'19%','{creation_date}','{creation_date}','{creation_date}'));
 $invoices_table->add(new core_datacolumn('from_domain_name','Hub',true,'19%','{from_domain_name}','{from_domain_name}','{from_domain_name}'));
 $invoices_table->add(new core_datacolumn('from_org_name','Organization',true,'19%','{from_org_name}','{from_org_name}','{from_org_name}'));
@@ -12,12 +12,32 @@ $invoices_table->columns[0]->autoformat='date-short';
 $invoices_table->columns[4]->autoformat='price';
 $invoices_table->columns[5]->autoformat='price';
 
+$invoices_table->add_filter(new core_datatable_filter('from_domain_id'));
+$invoices_table->filter_html .= core_datatable_filter::make_select(
+	'invoices',
+	'from_domain_id',
+	$items->filter_states['invoices__filter__from_domain_id'],
+	new core_collection('select distinct from_domain_id, from_domain_name from v_invoices where to_org_id = ' . $core->session['org_id']),
+	'from_domain_id',
+	'from_domain_name',
+	'Show from all domain',
+	'width: 270px;'
+);
+
+$org_sql = 'select distinct from_org_id, from_org_name from v_invoices where to_org_id = ' . $core->session['org_id'];
+
+$domain_id = $invoices_table->filter_states['invoices__filter__from_domain_id'];
+if(is_numeric($domain_id) && $domain_id > 0)
+{
+   $org_sql .= ' and from_domain_id='.$invoices_table->filter_states['invoices__filter__from_domain_id'];
+}
+
 $invoices_table->add_filter(new core_datatable_filter('from_org_id'));
 $invoices_table->filter_html .= core_datatable_filter::make_select(
 	'invoices',
 	'from_org_id',
 	$items->filter_states['invoices__filter__from_org_id'],
-	new core_collection('select distinct from_org_id, from_org_name from v_invoices where to_org_id = ' . $core->session['org_id'] . ';'),
+	new core_collection($org_sql),
 	'from_org_id',
 	'from_org_name',
 	'Show from all buyers',
