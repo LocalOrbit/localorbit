688aa3da4f86ffcb149fa47ca83f3d7daa99c4b0
lo-svn <lo-svn>
2012-11-09 17:49:39 +0000

    39293023
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/trunk@131 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

---
+++ db/patches/3.8/39293023.sql
@@ -0,0 +1,8 @@
+alter table domains add column payable_org_id int(10);
+
+update domains set domains.payable_org_id = (
+    select organizations_to_domains.org_id from organizations_to_domains 
+    left join organizations on organizations_to_domains.org_id = organizations.org_id 
+    where orgtype_id = 2 and is_home = 1 and organizations_to_domains.domain_id = domains.domain_id limit 1);
+
+select domain_id, payable_org_id from domains;
