a24a3db4dd7adb3e9857180a5c4b1803800461e2
lo-svn <lo-svn>
2012-11-12 15:16:07 +0000

    story 32473927
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/trunk@137 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

---
+++ www/app/models/base/organization_payment_methods.php
@@ -0,0 +1,19 @@
+<?php
+class core_model_base_organization_payment_methods extends core_model
+{
+	function init_fields()
+	{
+		$this->add_field(new core_model_field(0,'opm_id','int',8,'','organization_payment_methods'));
+		$this->add_field(new core_model_field(1,'org_id','int',8,'','organization_payment_methods'));
+		$this->add_field(new core_model_field(2,'payment_method_id','int',8,'','organization_payment_methods'));
+		$this->add_field(new core_model_field(3,'label','string',-4,'','organization_payment_methods'));
+		$this->add_field(new core_model_field(4,'name_on_account','string',-4,'','organization_payment_methods'));
+		$this->add_field(new core_model_field(5,'nbr1','string',-4,'','organization_payment_methods'));
+		$this->add_field(new core_model_field(6,'nbr1_last_4','string',-4,'','organization_payment_methods'));
+		$this->add_field(new core_model_field(7,'nbr2','string',-4,'','organization_payment_methods'));
+		$this->add_field(new core_model_field(8,'nbr2_last_4','string',-4,'','organization_payment_methods'));
+		$this->add_field(new core_model_field(9,'last_updated','timestamp',4,'','organization_payment_methods'));
+		$this->init_data();
+	}
+}
+?>
---
+++ www/app/models/organization_payment_methods.php
@@ -0,0 +1,5 @@
+<?php
+class core_model_organization_payment_methods extends core_model_base_organization_payment_methods
+{
+}
+?>
