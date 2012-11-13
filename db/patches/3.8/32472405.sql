50921f81839953085d0cebb28de25326530ab2db
lo-svn <lo-svn>
2012-11-13 16:08:21 +0000

    32472405
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/trunk@149 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- db/patches/3.8/32472405.sql
+++ db/patches/3.8/32472405.sql
@@ -1,1 +1,6 @@
-select org_id, count(*) from customer_entity where is_deleted = false and is_enabled = true and is_active = true group by org_id having count(*) > 1;
+alter table organizations add column payment_entity_id int(10) unsigned not null;
+
+update organizations set payment_entity_id = (
+	select entity_id from customer_entity where customer_entity.org_id = organizations.org_id order by is_deleted, is_enabled desc, is_active desc, entity_id limit 1
+) where is_deleted = false and is_active = true and is_enabled = true;
+
