e7c246ab0528bec5433d921453399f8b29307a40
lo-svn <lo-svn>
2012-11-15 18:43:21 +0000

    fixed total table
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/trunk@159 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- www/app/controllers/reports/views/totals_table.php
+++ www/app/controllers/reports/views/totals_table.php
@@ -1,6 +1,6 @@
 <?php
 $prefix = $core->view[0];
-$suffix1 = (isset($core->view[1]))?($core->view[1]):('Sales');
+$suffix1 = (isset($core->view[1]))?($core->view[1]):((lo3::is_market() || lo3::is_seller() || lo3::is_admin())?'Sales':'Purchases');
 $suffix2 = (isset($core->view[2]))?($core->view[2]):('Fees');
 ?>
 <br />
@@ -14,17 +14,31 @@ $suffix2 = (isset($core->view[2]))?($core->view[2]):('Fees');
 	<tr>
 		<th class="dt">Gross <?=$suffix1?></th>
 		<th class="dt">Discounts</th>
+		<?
+if(lo3::is_market() || lo3::is_seller() || lo3::is_admin())
+{
+	?>
 		<th class="dt">Hub <?=$suffix2?></th>
 		<th class="dt">LO <?=$suffix2?></th>
 		<th class="dt">Payment Processing <?=$suffix2?></th>
+	<?
+}
+	?>
 		<th class="dt">Net <?=$suffix1?></th>
 	</tr>
 	<tr class="dt">
 		<td class="dt" id="<?=$prefix?>gross">$0.00</td>
 		<td class="dt" id="<?=$prefix?>discount">$0.00</td>
+		<?
+if(lo3::is_market() || lo3::is_seller() || lo3::is_admin())
+{
+	?>
 		<td class="dt" id="<?=$prefix?>hub">$0.00</td>
 		<td class="dt" id="<?=$prefix?>lo">$0.00</td>
 		<td class="dt" id="<?=$prefix?>proc">$0.00</td>
+	<?
+}
+	?>
 		<td class="dt" id="<?=$prefix?>net">$0.00</td>
 	</tr>
 </table>
