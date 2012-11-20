30896f855d4cd34cdffa7ce4aea96a7159596978
lo-svn <lo-svn>
2012-11-20 17:39:34 +0000

    32473095
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/trunk@168 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- www/app/controllers/catalog/views/payment_purchaseorder.php
+++ www/app/controllers/catalog/views/payment_purchaseorder.php
@@ -1,13 +1,18 @@
 <?
+global $core;
 global $org;
 if($org['payment_allow_purchaseorder'] == 1)
 {
 	$style = ($core->view[0] > 1)?' style="display:none;"':'';
+	print_r($core->session);
 ?>
 <div id="payment_purchaseorder" class="payment_option form"<?=$style?>>
 	<h3>Purchase Order Information</h3>
 	<br />
 	<table class="form">
+		<!--<tr>
+			<td colspan="2">Payment should be made in x days.</td>
+		</tr>-->
 		<tr>
 			<td class="label">Purchase Order #</td>
 			<td class="value"><input type="text" style="width: 150px;" name="po_number" value="" /></td>
