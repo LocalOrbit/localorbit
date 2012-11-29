a3400371f94bc816ce6328f6cce39944c562ee18
lo-svn <lo-svn>
2012-11-29 15:20:45 +0000

    35919059
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/trunk@193 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- db/patches/3.8/35919059.sql
+++ db/patches/3.8/35919059.sql
@@ -1,7 +1,8 @@
 -- create buyer payables
 select lo_order.domain_id, 1, lo_oid, org_id, 
 	case payment_method when 'purchaseorder' then domains.payable_org_id else 1 end as to_org_id,
-	adjusted_total, 1 as invoicable
+	adjusted_total, 1 as invoicable,
+    order_date
 	from lo_order 
 	inner join domains on lo_order.domain_id = domains.domain_id
 	where ldstat_id != 1 and ldstat_id != 3 and ldstat_id != 6 and lbps_id != 5 and lbps_id != 6 
@@ -9,17 +10,58 @@ select lo_order.domain_id, 1, lo_oid, org_id,
 	order by lo_oid desc;
 
 -- create buyer invoices
-select lo_order.domain_id, 1, lo_oid, org_id, 
+select DATE_ADD(order_date, INTERVAL domains.po_due_within_days day), org_id, 
 	case payment_method when 'purchaseorder' then domains.payable_org_id else 1 end as to_org_id,
-	adjusted_total, 1 as invoicable
+	adjusted_total
 	from lo_order 
 	inner join domains on lo_order.domain_id = domains.domain_id
-	where ldstat_id != 1 and ldstat_id != 3 and ldstat_id != 6 and lbps_id != 5 and lbps_id != 6 
+	where ldstat_id != 1 and ldstat_id != 3 and ldstat_id != 6 and 
+    (lbps_id = 2 or lbps_id = 4 or lbps_id = 3)
 	and adjusted_total > 0
 	order by lo_oid desc;
+/*
+update payables, invoices set payables.invoice_id = invoices.invoice_id where 
+payables.from_org_id = invoices.from_org_id and 
+payables.to_org_id = invoices.to_org_id and 
+payables.amount = invoices.amount;
+*/
 
 -- create buyer payments
-
+select GREATEST(DATE_ADD(order_date, INTERVAL domains.po_due_within_days day), last_status_date) as creation_date, org_id, 
+	case lo_order.payment_method when 'purchaseorder' then domains.payable_org_id else 1 end as to_org_id,
+	amount_paid,
+    payment_method_id,
+payment_ref,
+admin_notes
+	from lo_order 
+	inner join domains on lo_order.domain_id = domains.domain_id
+    inner join payment_methods on lo_order.payment_method = payment_methods.payment_method
+	where ldstat_id != 1 and ldstat_id != 3 and ldstat_id != 6 and 
+    (lbps_id = 2 or lbps_id = 4)
+	and adjusted_total > 0
+	order by lo_oid desc;
+/*
+INSERT INTO `localorb_www_dev`.`payments`
+(`payment_id`,
+`from_org_id`,
+`to_org_id`,
+`amount`,
+`payment_method_id`,
+`ref_nbr`,
+`admin_note`,
+`creation_date`)
+VALUES
+(
+<{payment_id: }>,
+<{from_org_id: }>,
+<{to_org_id: }>,
+<{amount: }>,
+<{payment_method_id: }>,
+<{ref_nbr: }>,
+<{admin_note: }>,
+<{creation_date: CURRENT_TIMESTAMP}>
+);
+*/
 -- create seller payables
 
 -- create seller invoices
