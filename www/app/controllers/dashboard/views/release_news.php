2303df31f36e4c138c6739ee88e1432b6b308a5f
lo-svn <lo-svn>
2013-03-02 00:16:10 +0000

    45312563
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/trunk@1016 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- www/app/controllers/dashboard/views/release_news.php
+++ www/app/controllers/dashboard/views/release_news.php
@@ -3,35 +3,39 @@
 	#echo('<h2>hereeeee: '.$core->session['login_note_viewed'].'/'.$core->session['org_id'].'</h2>');
 	
 	// reset to test  devannarbor-mi.localorb.it/release_news.php?has_seen_release_news=0
-	if ($core->data["has_seen_release_news"] == 'yes') 
-	{
-		$user = core::model('customer_entity')->load($core->session['user_id']);
-		$user['login_note_viewed'] = 1;
-		$user->save();
-		$core->session['login_note_viewed'] = 1;
-			
-		core::js('$("#releaseNewsModal").modal("hide");');
-		core::deinit();
-	}	
-	else if ($core->data["news_remind_later"] == 'yes')
-	{
-		$core->session['login_note_viewed'] = 1;
-			
-		core::js('$("#releaseNewsModal").modal("hide");');
-		core::deinit();
-	}
-	else if ($core->session['login_note_viewed']  != 1 && $core->session['org_id'] < 1286)
+	if($core->config['stage'] == 'production')
 	{
 		
-		if(lo3::is_market())
+		if ($core->data["has_seen_release_news"] == 'yes') 
 		{
-			$this->release_news__market();
-		}
-		else
+			$user = core::model('customer_entity')->load($core->session['user_id']);
+			$user['login_note_viewed'] = 1;
+			$user->save();
+			$core->session['login_note_viewed'] = 1;
+				
+			core::js('$("#releaseNewsModal").modal("hide");');
+			core::deinit();
+		}	
+		else if ($core->data["news_remind_later"] == 'yes')
 		{
-			$this->release_news__buyerseller();
+			$core->session['login_note_viewed'] = 1;
+				
+			core::js('$("#releaseNewsModal").modal("hide");');
+			core::deinit();
 		}
-		core::js('$("#releaseNewsModal").modal();');
+		else if ($core->session['login_note_viewed']  != 1 && $core->session['org_id'] < 1286)
+		{
+			
+			if(lo3::is_market())
+			{
+				$this->release_news__market();
+			}
+			else
+			{
+				$this->release_news__buyerseller();
+			}
+			core::js('$("#releaseNewsModal").modal();');
+		}
 	}
 ?>
 	
