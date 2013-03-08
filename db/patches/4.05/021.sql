4f60969e22b8246fcc2573a0c9e8eef17eea811b
localorbit-chad <chad@localorb.it>
2013-03-08 01:39:12 +0000

    Revised sql query to timezone update
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/trunk@1079 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- db/patches/4.05/021.sql
+++ db/patches/4.05/021.sql
@@ -1,7 +1,7 @@
 INSERT INTO migrations (version_id, pt_ticket_no) 
 VALUES ('021', '45657971');
 
-UPDATE  `localorb_www_dev`.`timezones` SET  `offset_seconds` =  '-18000' WHERE  `timezones`.`tz_id` =1;
-UPDATE  `localorb_www_dev`.`timezones` SET  `offset_seconds` =  '-21600' WHERE  `timezones`.`tz_id` =2;
-UPDATE  `localorb_www_dev`.`timezones` SET  `offset_seconds` =  '-25200' WHERE  `timezones`.`tz_id` =3;
-UPDATE  `localorb_www_dev`.`timezones` SET  `offset_seconds` =  '-28800' WHERE  `timezones`.`tz_id` =4;
+UPDATE  `timezones` SET  `offset_seconds` =  '-18000' WHERE  `timezones`.`tz_id` =1;
+UPDATE  `timezones` SET  `offset_seconds` =  '-21600' WHERE  `timezones`.`tz_id` =2;
+UPDATE  `timezones` SET  `offset_seconds` =  '-25200' WHERE  `timezones`.`tz_id` =3;
+UPDATE  `timezones` SET  `offset_seconds` =  '-28800' WHERE  `timezones`.`tz_id` =4;
