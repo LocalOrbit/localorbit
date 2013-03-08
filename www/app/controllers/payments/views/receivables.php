43d6b3d2df399e9b77ac3bf1c1c64a11cf627cc1
lo-svn <lo-svn>
2013-03-08 19:19:37 +0000

    45805549  Financial UI: add new column to receivables tab: Aging
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/trunk@1105 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- www/app/controllers/payments/views/receivables.php
+++ www/app/controllers/payments/views/receivables.php
@@ -1,7 +1,8 @@
+=========
 <?php
 $invoices = core::model('v_invoices')
 	->collection()
-	->filter('amount_due', '>', 0);
+	->filter('amount_due', '=', 0);
 	
 if(lo3::is_admin())
 {
@@ -25,9 +26,10 @@ $invoices->add_formatter('payment_link_formatter');
 $invoices->add_formatter('payment_direction_formatter');
 $invoices_table = new core_datatable('receivables','payments/receivables',$invoices);
 $invoices_table = payments__add_standard_filters($invoices_table,'invoices');
-$invoices_table->add(new core_datacolumn('invoice_id','Description',true,'22%',			'<b>I-{invoice_id}</b><br />{description_html}','{description}','{description}'));
+$invoices_table->add(new core_datacolumn('invoice_id','Description',true,'17%',			'<b>I-{invoice_id}</b><br />{description_html}','{description}','{description}'));
 $invoices_table->add(new core_datacolumn('from_org_name','Payment Info',false,'40%','{direction_info}','{from_org_name}','{from_org_name}'));
-$invoices_table->add(new core_datacolumn('due_date','Due Date',true,'20%','{due_date}','{due_date}','{due_date}'));
+$invoices_table->add(new core_datacolumn('due_date','Due Date',true,'15%','{due_date}','{due_date}','{due_date}'));
+$invoices_table->add(new core_datacolumn('aging','Aging',true,'10%','{aging}','{aging}','{aging}'));
 $invoices_table->add(new core_datacolumn('amount_due','Amount Due',true,'14%',			'{amount_due}','{amount_due}','{amount_due}'));
 $invoices_table->add(new core_datacolumn('invoice_id',array(core_ui::check_all('invoices'),'',''),false,'4%',core_ui::check_all('invoices','invoice_id'),' ',' '));
 $invoices_table->columns[2]->autoformat='date-long';
