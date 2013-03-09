e6f8cd6ade1e7470219094a4fe45dfbb3479448a
lo-svn <lo-svn>
2013-03-09 21:19:44 +0000

    45805645
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/trunk@1121 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- www/app/controllers/payments/views/payables.php
+++ www/app/controllers/payments/views/payables.php
@@ -1,5 +1,6 @@
 <?php
 $payments_owed = core::model('v_invoices')
+	->add_custom_field('DATEDIFF(CURRENT_TIMESTAMP,due_date) as age')
 	->collection()
 	->filter('amount_due', '>', 0);
 	
@@ -19,17 +20,35 @@ else
 	$payments_owed->filter('from_org_id' , $core->session['org_id']);
 }
 
+
+function payable_age_formatter($data)
+{
+	if($data['age'] <= 0)
+	{
+		$data['age'] = 'Current';
+	}
+	else
+	{
+		$data['age'] = '<span class="text-error">'.$data['age'].'</span>';
+	}
+	return $data;
+}
+
 $payments_owed->add_formatter('payable_info');
 $payments_owed->add_formatter('payment_link_formatter');
 $payments_owed->add_formatter('payment_direction_formatter');
+$payments_owed->add_formatter('payable_age_formatter');
 $payments_table = new core_datatable('payables','payments/payables',$payments_owed);
 $payments_table = payments__add_standard_filters($payments_table,'payments');
-$payments_table->add(new core_datacolumn('invoice_id','Description',true,'22%',			'<b>I-{invoice_id}</b><br />{description_html}','{description}','{description}'));
-$payments_table->add(new core_datacolumn('from_org_name','Payment Info',false,'40%','{direction_info}','{to_org_name}','{to_org_name}'));
-$payments_table->add(new core_datacolumn('creation_date','Date',true,'20%','{creation_date}','{creation_date}','{creation_date}'));
-$payments_table->add(new core_datacolumn('amount_due','Amount',true,'14%',							'{amount_due}','{amount_due}','{amount_due}'));
-$payments_table->add(new core_datacolumn('payment_id',array(core_ui::check_all('payments'),'',''),false,'4%',core_ui::check_all('payments','invoice_id'),' ',' '));
-$payments_table->columns[2]->autoformat='date-long';
+$payments_table->add(new core_datacolumn('invoice_id','Reference',true,'20%',			'<b>I-{invoice_id}</b><br />{description_html}','{description}','{description}'));
+$payments_table->add(new core_datacolumn('from_org_name','Description',false,'40%','{direction_info}','{from_org_name}','{from_org_name}'));
+$payments_table->add(new core_datacolumn('creation_date','Invoice Date',true,'10%','{creation_date}','{creation_date}','{creation_date}'));
+$payments_table->add(new core_datacolumn('due_date','Due Date',true,'10%','{due_date}','{due_date}','{due_date}'));
+$payments_table->add(new core_datacolumn('DATEDIFF(due_date,CURRENT_TIMESTAMP)','Overdue',true,'10%',			'{age}','{age}','{age}'));
+$payments_table->add(new core_datacolumn('amount_due','Amount Due',true,'10%',			'{amount_due}','{amount_due}','{amount_due}'));
+$payments_table->columns[2]->autoformat='date-short';
+$payments_table->columns[3]->autoformat='date-short';
+
 #$payments_table->columns[3]->autoformat='price';
 $payments_table->sort_direction='desc';
 
--- www/app/controllers/payments/views/systemwide_payablesreceivables.php
+++ www/app/controllers/payments/views/systemwide_payablesreceivables.php
@@ -1,24 +1,28 @@
 <?php
-$invoices = core::model('v_invoices')
-	->collection()
-	->filter('amount_due', '>', 0);
+
 	
 if(lo3::is_admin())
 {
 	$invoices = new core_collection('
 		select *,
-		UNIX_TIMESTAMP(creation_date) as creation_date
+		UNIX_TIMESTAMP(creation_date) as creation_date,
+		UNIX_TIMESTAMP(due_date) as due_date,
+		DATEDIFF(CURRENT_TIMESTAMP,due_date) as age
 		from v_invoices vi
 		where vi.invoice_id > 0
+		and vi.amount_due >0
 	');
 }
 else if (lo3::is_market())
 {
 	$invoices = new core_collection('
 		select *,
-		UNIX_TIMESTAMP(creation_date) as creation_date
+		UNIX_TIMESTAMP(creation_date) as creation_date,
+		UNIX_TIMESTAMP(due_date) as due_date,
+		DATEDIFF(CURRENT_TIMESTAMP,due_date) as age
 		from v_invoices vi
-		where (
+		where vi.amount_due >0
+		and (
 			vi.from_org_id in (
 				select org_id
 				from organizations_to_domains 
@@ -38,28 +42,48 @@ else
 {
 	$invoices = new core_collection('
 		select *,
-		UNIX_TIMESTAMP(creation_date) as creation_date
+		UNIX_TIMESTAMP(creation_date) as creation_date,
+		UNIX_TIMESTAMP(due_date) as due_date,
+		DATEDIFF(CURRENT_TIMESTAMP,due_date) as age
 		from v_invoices vi
 		where (
 			vi.from_org_id = '.$core->session['org_id'].'
 			or
 			vi.to_org_id = '.$core->session['org_id'].'
 		)
+		and vi.amount_due >0
 	');
 }
 
+
+function systemwide_age_formatter($data)
+{
+	if($data['age'] <= 0)
+	{
+		$data['age'] = 'Current';
+	}
+	else
+	{
+		$data['age'] = '<span class="text-error">'.$data['age'].'</span>';
+	}
+	return $data;
+}
 	
 $invoices->add_formatter('payable_info');
 $invoices->add_formatter('payment_link_formatter');
 $invoices->add_formatter('payment_direction_formatter');
+$invoices->add_formatter('systemwide_age_formatter');
 $systemwide_table = new core_datatable('systemwide','payments/systemwide_payablesreceivables',$invoices);
 $systemwide_table = payments__add_standard_filters($systemwide_table,'systemwide');
-$systemwide_table->add(new core_datacolumn('invoice_id','Description',true,'22%',			'<b>I-{invoice_id}</b><br />{description_html}','{description}','{description}'));
-$systemwide_table->add(new core_datacolumn('from_org_name','Payment Info',false,'40%','{direction_info}','{from_org_name}','{from_org_name}'));
-$systemwide_table->add(new core_datacolumn('due_date','Due Date',true,'20%','{due_date}','{due_date}','{due_date}'));
-$systemwide_table->add(new core_datacolumn('amount_due','Amount Due',true,'14%',			'{amount_due}','{amount_due}','{amount_due}'));
+$systemwide_table->add(new core_datacolumn('invoice_id','Reference',true,'20%',			'<b>I-{invoice_id}</b><br />{description_html}','{description}','{description}'));
+$systemwide_table->add(new core_datacolumn('from_org_name','Description',false,'40%','{direction_info}','{from_org_name}','{from_org_name}'));
+$systemwide_table->add(new core_datacolumn('creation_date','Invoice Date',true,'10%','{creation_date}','{creation_date}','{creation_date}'));
+$systemwide_table->add(new core_datacolumn('due_date','Due Date',true,'10%','{due_date}','{due_date}','{due_date}'));
+$systemwide_table->add(new core_datacolumn('DATEDIFF(due_date,CURRENT_TIMESTAMP)','Aging',true,'10%',			'{age}','{age}','{age}'));
+$systemwide_table->add(new core_datacolumn('amount_due','Amount Due',true,'10%',			'{amount_due}','{amount_due}','{amount_due}'));
 #$systemwide_table->add(new core_datacolumn('invoice_id',array(core_ui::check_all('systemwide'),'',''),false,'4%',core_ui::check_all('systemwide','invoice_id'),' ',' '));
-$systemwide_table->columns[2]->autoformat='date-long';
+$systemwide_table->columns[2]->autoformat='date-short';
+$systemwide_table->columns[3]->autoformat='date-short';
 $systemwide_table->sort_direction='desc';
 
 
