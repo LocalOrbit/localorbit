f2abc0cca36e5fd48167b71dbeb9c3742d18fd6b
lo-svn <lo-svn>
2013-03-13 14:46:58 +0000

    45899969
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/trunk@1179 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- www/app/controllers/payments/views/receivables.php
+++ www/app/controllers/payments/views/receivables.php
@@ -2,42 +2,18 @@
 $invoices = core::model('v_invoices')
 	->add_custom_field('DATEDIFF(CURRENT_TIMESTAMP,due_date) as age')
 	->collection()
-	->filter('amount_due', '=', 0);
+	->filter('to_org_id' ,'=',$core->session['org_id'])
+	->filter('amount_due', '>', 0);
 	
-if(lo3::is_admin())
-{
-}
-else if (lo3::is_market())
-{
-	$invoices->filter('to_org_id' ,'in','(
-		select org_id
-		from organizations_to_domains 
-		where organizations_to_domains.domain_id in ('.implode(',',$core->session['domains_by_orgtype_id'][2]).')
-	)');
-}
-else
-{
-	$invoices->filter('to_org_id' ,'=',$core->session['org_id']);
-}
 
 
-function receivable_age_formatter($data)
-{
-	if($data['age'] <= 0)
-	{
-		$data['age'] = 'Current';
-	}
-	else
-	{
-		$data['age'] = '<span class="text-error">'.$data['age'].'</span>';
-	}
-	return $data;
-}
+
+
 
 $invoices->add_formatter('payable_info');
 $invoices->add_formatter('payment_link_formatter');
 $invoices->add_formatter('payment_direction_formatter');
-$invoices->add_formatter('receivable_age_formatter');
+$invoices->add_formatter('payments__age_formatter');
 if(lo3::is_market() || lo3::is_admin())
 	$invoices->add_formatter('lfo_accordion');
 	
@@ -68,7 +44,7 @@ $invoices_table->sort_direction='desc';
 		{
 		?>
 		<div class="pull-right" id="create_payment_form_toggler">
-			<input type="button" onclick="core.payments.makePayments('invoices');" class="btn btn-info" value="Make Payments" />
+			<input type="button" onclick="core.payments.makePayments('invoices');" class="btn btn-info" value="Enter Receipts" />
 		</div>
 		<?}?>
 		<br />&nbsp;<br />
