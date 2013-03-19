d1f401ce14c2726afc7816e6bbfdbc015f89669f
Mike Ratanatharathorn <mike@localorb.it>
2013-03-19 14:32:25 +0000

    45893817
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/trunk@1249 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

---
+++ www/homepage/company.php
@@ -0,0 +1,74 @@
+<? $title='About'; include('includes/page_start.php'); ?>	
+<table>
+	<col width="520" />
+	<col width="20" />
+	<col width="560" />
+	<tr>
+		<td>
+			<h1>Re-linking the Food Chain</h1>
+			Local Orbit is building flexible, localized tools designed to help people build healthier communities through better food.  
+			<br />&nbsp;<br />
+			Our web platform provides custom online marketplaces and back-office infrastructure for businesses and people working to increase local food distribution.
+			<br />&nbsp;<br />
+			Local Orbit has evolved through two years of conversations with farmers, food producers, chefs, eaters and people working to solve food distribution challenges. We've heard countless stories about disconnected markets and inefficient communications. 
+			<br />&nbsp;<br />
+			People are hungry for food that's produced by farmers, ranchers and artisans<br /> they know and trust. They want to spend a larger share of their food dollars in<br /> their own communities.
+			<br />&nbsp;<br />
+			Local Orbit's services create stronger, more efficient food networks that can meet the demand for food that’s produced closer to where we live. 
+			<br />&nbsp;<br />
+			
+		</td>
+		<td rowspan="2">&nbsp;</td>
+		<td>
+			<img src="/homepage/img/page_images/company.jpg" class="public_content_photo" />
+		</td>
+	</tr>
+	<tr>
+		<td>
+			<img src="/homepage/img/icon_scarecrow_small.png" class="home_icon" /><h3 class="home_header">Management Team</h3>
+			<div class="home_paragraph">
+				Local Orbit is the creation of people who are passionate about re-building local 
+				food systems. We know farmers don't want to spend their days online, but we also 
+				know demand for access to local food is very high, rapidly growing, and not going 
+				away. It's our goal to serve every link in this food chain beautifully, by providing
+				an elegant platform for multi-channel sales, streamlined purchasing and efficient 
+				business management.
+				<br />&nbsp;<br />
+				Our team brings an energetic mix of startup savvy, technical obsession, marketing 
+				prowess and food business to its work to power the New Food Economy.
+			</div>
+		</td>
+		<td>
+			<div>
+				<div class="faq_question" onclick="$(this).toggleClass('faq_question_expanded').parent().children('.faq_answer').toggle(300);">Erika Block | Founder &amp; CEO</div>
+				<div class="faq_answer">
+					Erika profile here.
+				</div>
+			</div>
+			<br />
+			<div>
+				<div class="faq_question" onclick="$(this).toggleClass('faq_question_expanded').parent().children('.faq_answer').toggle(300);">Ragan Erickson | Director of Product</div>
+				<div class="faq_answer">
+					Ragan profile here.
+				</div>
+			</div>
+			<br />
+			<div>
+				<div class="faq_question" onclick="$(this).toggleClass('faq_question_expanded').parent().children('.faq_answer').toggle(300);">Mike Thorn | Lead Developer</div>
+				<div class="faq_answer">
+					Mike profile here.
+				</div>
+			</div>
+			<br />
+			<div>
+				<div class="faq_question" onclick="$(this).toggleClass('faq_question_expanded').parent().children('.faq_answer').toggle(300);">Cory Van Horn | Director of Marketing</div>
+				<div class="faq_answer">
+					Cory profile here.
+				</div>
+			</div>			
+		</td>
+	</tr>
+</table>
+
+
+<? include('includes/page_end.php'); ?>
