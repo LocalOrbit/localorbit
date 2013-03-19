5b0b8b64214a72afcba06031dab0975fa1c3a5db
Mike Ratanatharathorn <mike@localorb.it>
2013-03-19 16:51:15 +0000

    45893817
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/trunk@1257 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- www/homepage/includes/page_end.php
+++ www/homepage/includes/page_end.php
@@ -1,31 +1,62 @@
-				</td>
-				<td>&nbsp;</td>
-			</tr>
-			<tr>
-				<td colspan="3" id="footer">
-					<table style="table-layout: fixed;">
-						<col width="145" />
-						<col width="195" />
-						<col width="195" />
-						<col width="195" />
-						<col width="195" />
-						<tr>
-							<td id="footer_logo">
+					</td>
+					<td>&nbsp;</td>
+				</tr>
+				<tr>
+					<td colspan="3" id="footer">
+						<br />
+						<table width="100%">
+							<colgroup>
+								<col width="4%">
+								<col width="16%">
+								<col width="20%">
+								<col width="20%">
+								<col width="20%">
+								<col width="20%">
+							</colgroup>
+							<tbody>
+								<tr>
+									<td>&nbsp;</td>
+									<td><a href="/index.php"><img src="/img/default/logo_gray.png"></a></td>
+									<td>
+										<img src="/img/default/footer/tools_gray.png"><br>
+										<b class="footer">Local Orbit</b>
+										<ul class="footer">
+											<li class="footer"><a class="footer" href="/homepage/company.php">Company</a></li>
+											<li class="footer"><a class="footer" href="/homepage/features.php">Features	</a></li>
+											<li class="footer"><a class="footer" href="/login.php">Customer Log in</a></li>
+										</ul>
+									</td>
+									<td>
+										<img src="/img/default/footer/scarecrow_gray.png"><br>
+										<b class="footer">Dig Deeper</b>
+										<ul class="footer">
+											<li class="footer"><a class="footer" href="/field-notes/">Field Notes: Our Blog</a></li>
+											<li class="footer"><a class="footer" href="#">Schedule a Demo</a></li>
+											<li class="footer"><a class="footer" href="#">Newsletter Sign Up</a></li>
+										</ul>
+									</td>
+									<td>
+										<img src="/img/default/footer/thumbsup_gray.png"><br>
+										
+										<b class="footer">Get Social</b>
+										<ul class="footer">
+											<li class="footer"><a class="footer" target="_blank" href="http://www.facebook.com/localorbit">Like On Facebook</a></li>
+											<li class="footer"><a class="footer" target="_blank" href="http://www.twitter.com/localorbit">Follow On Twitter</a></li>
+											<li class="footer"><a class="footer" target="_blank" href="#">Connect On Linkedin</a></li>
+										</ul>
+									</td>
 								
-							</td>
-							<td id="footer_links1">
-								
-							</td>
-							<td id="footer_links2">
-								
-							</td>
-							<td id="footer_links3">
-								
-							</td>
-							<td id="footer_links4">
-								
-							</td>
-						</tr>
+									<td>
+									<img src="/img/default/footer/phone_gray.png"><br>
+									<b class="footer">Contact</b>
+									<ul class="footer">
+										<li class="footer"><a class="footer" href="#">Call 734.418.8628</a></li>
+										<li class="footer"><a class="footer" href="#">Send Us A Note</a></li>
+										<li class="footer"><a class="footer" href="#">Customer Support</a></li>
+									</ul>
+								</td>
+							</tr>
+						</tbody>
 					</table>
 				</td>
 			</tr>
@@ -57,7 +88,7 @@
 			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
 			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 			})();
-			core.doRequest('/whitelabel/get_options',{'public':'yes'});
+			//core.doRequest('/whitelabel/get_options',{'public':'yes'});
 		</script>
 	</body>
 </html>
