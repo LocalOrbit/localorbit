2e0904ea8be4f5ecff26578ceceb80abb47e64f6
Mike Ratanatharathorn <mike@localorb.it>
2013-03-19 16:35:16 +0000

    45893817
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/trunk@1256 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- www/homepage/contact.php
+++ www/homepage/contact.php
@@ -13,8 +13,8 @@
 			<br />&nbsp;<br />&nbsp;<br />&nbsp;<br />&nbsp;<br />&nbsp;<br />&nbsp;<br />
 			<img src="/homepage/img/contact/phone_small.png" class="home_icon" /><span class="home_header" style="font-size:130%;">734.545.8100</span>
 			<div class="home_paragraph">&nbsp;</div>
-			<img src="/homepage/img/contact/phone_small.png" class="home_icon" />
-			<span class="home_header" style="line-height: 21px;font-size:130%;">300 East Liberty<br />Ann Arbor, MI 48104
+			<img src="/homepage/img/contact/envelope.png" class="home_icon" />
+			<span class="home_header" style="line-height: 21px;font-size:130%;margin-left:20px;">300 East Liberty<br />Ann Arbor, MI 48104
 			</span>
 			
 			<div style="padding-left:73px;clear:both;"><br />Send us a note</div>
@@ -22,8 +22,9 @@
 		<td>&nbsp;</td>
 		<td style="text-align: center;">
 			<div style="text-align: center;">
-				<img src="/homepage/img/contact/demo.png" />
-				<br />&nbsp;<br />
+				<div style="height: 130px;">
+					<img src="/homepage/img/contact/demo.png" />
+				</div>
 				<h3>Schedule a Demo?</h3>
 				We know every business is a little different
 				and that you're interested in seeing the tools 
@@ -34,7 +35,9 @@
 		<td>&nbsp;</td>
 		<td style="text-align: center;">
 			<div style="text-align: center;">
-				<br />&nbsp;<br />
+				<div style="height: 120px;padding-top: 10px;">
+					<img src="/homepage/img/contact/newsletter.png" />
+				</div>
 				<h3>Sign Up for Our Newsletter</h3>
 				Get the latest news and information about 
 				Local Orbit by signing up for our email list. 
@@ -45,8 +48,9 @@
 		<td>&nbsp;</td>
 		<td>
 			<div style="text-align: center;">
-				<img src="/homepage/img/contact/smiley.png" />
-				<br />&nbsp;<br />
+				<div style="height: 130px;">
+					<img src="/homepage/img/contact/smiley.png" />
+				</div>
 				<h3>Customer Support</h3>
 				Are you a current customer and have questions about 
 				your Local Orbit powered market? we are here to help. 
@@ -57,11 +61,11 @@
 	</tr>
 	<tr>
 		<td colspan="2">&nbsp;</td>
-		<td><input type="button" class="schedule_button" value="Schedule a Demo" /></td>
+		<td><input type="button" class="schedule_button" style="margin-top: 10px;" value="Schedule a Demo" /></td>
 		<td>&nbsp;</td>
-		<td><input type="button" class="schedule_button newsletter_button" value="Stay Informed" /></td>
+		<td><input type="button" class="schedule_button newsletter_button" style="margin-top: 10px;"  value="Stay Informed" /></td>
 		<td>&nbsp;</td>
-		<td><input type="button" class="schedule_button zendesk_button" value="Submit a Request" /></td>
+		<td><input type="button" class="schedule_button zendesk_button" style="margin-top: 10px;"  value="Submit a Request" /></td>
 </table>
 <br />&nbsp;<br />
 
