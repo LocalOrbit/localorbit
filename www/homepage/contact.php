a338ef00fc5b4d949c517b19828bd5a21da6af40
Mike Ratanatharathorn <mike@localorb.it>
2013-03-19 16:22:53 +0000

    45893817
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/trunk@1253 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

---
+++ www/homepage/contact.php
@@ -0,0 +1,67 @@
+<? $title='About'; include('includes/page_start.php'); ?>	
+<br />&nbsp;<br />
+<table width="100%">
+	<col width="34%" />
+	<col width="1%" />
+	<col width="21%" />
+	<col width="1%" />
+	<col width="21%" />
+	<col width="1%" />
+	<col width="21%" />
+	<tr>
+		<td>
+			<br />&nbsp;<br />&nbsp;<br />&nbsp;<br />&nbsp;<br />&nbsp;<br />&nbsp;<br />
+			<img src="/homepage/img/contact/phone_small.png" class="home_icon" /><span class="home_header" style="font-size:130%;">734.545.8100</span>
+			<div class="home_paragraph">&nbsp;</div>
+			<img src="/homepage/img/contact/phone_small.png" class="home_icon" />
+			<span class="home_header" style="line-height: 21px;font-size:130%;">300 East Liberty<br />Ann Arbor, MI 48104
+			</span>
+
+		</td>
+		<td>&nbsp;</td>
+		<td style="text-align: center;">
+			<div style="text-align: center;">
+				<img src="/homepage/img/contact/demo.png" />
+				<br />&nbsp;<br />
+				<h3>Schedule a Demo?</h3>
+				We know every business is a little different
+				and that you're interested in seeing the tools 
+				in action. No problem. Simply register for a demo 
+				time that best fits your schedule.
+			</div>
+		</td>	
+		<td>&nbsp;</td>
+		<td style="text-align: center;">
+			<div style="text-align: center;">
+				<br />&nbsp;<br />
+				<h3>Sign Up for Our Newsletter</h3>
+				Get the latest news and information about 
+				Local Orbit by signing up for our email list. 
+				We will send you a quarterly newsletter and a 
+				few extra updates along the way.
+			</div>
+		</td>
+		<td>&nbsp;</td>
+		<td>
+			<div style="text-align: center;">
+				<img src="/homepage/img/contact/smiley.png" />
+				<br />&nbsp;<br />
+				<h3>Customer Support</h3>
+				Are you a current customer and have questions about 
+				your Local Orbit powered market? we are here to help. 
+				Submit a request through our knowledge base and we 
+				will respond shortly.
+			</div>
+		</td>
+	</tr>
+	<tr>
+		<td colspan="2">&nbsp;</td>
+		<td><input type="button" class="schedule_button" value="Schedule a Demo" /></td>
+		<td>&nbsp;</td>
+		<td><input type="button" class="schedule_button newsletter_button" value="Stay Informed" /></td>
+		<td>&nbsp;</td>
+		<td></td>
+</table>
+<br />&nbsp;<br />
+
+<? include('includes/page_end.php'); ?>
