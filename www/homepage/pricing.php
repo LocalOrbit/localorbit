56fb3f67f242a2a5fd821105397f5632dfe12ec1
Mike Ratanatharathorn <mike@localorb.it>
2013-04-01 13:21:56 +0000

    45893817
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/trunk@1320 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- www/homepage/pricing.php
+++ www/homepage/pricing.php
@@ -1,4 +1,118 @@
 <? $title='Pricing'; include('includes/page_start.php'); ?>	
+<table width="100%">
+	<col width="30%" />
+	<col width="2%" />
+	<col width="68%" />
+	<tr>
+		<td colspan="3">
+			<div class="homepage_title" style="text-align: center;">
+				Affordable, beautifully designed sales &amp; business management tools to help<br />
+				any size organization work more efficiently, with room to grow.
+			</div>
+			<br />&nbsp;<br />
+		</td>
+	</tr>
+	<tr>
+		<td>
+			<h2>
+				Every plan includes the core<br />features you need to succeed.
+			</h2>
+			<div>
+				<div class="faq_question" onclick="$(this).toggleClass('faq_question_expanded').parent().children('.faq_answer').toggle(300);">Streamlined Ordering &amp; Payments</div>
+				<div class="faq_answer" style="padding-left: 27px;">
+					<ul>
+						<li>Ordering that ﬁts your business, whether you need a catalog with full e-commerce features and 24/7 ordering, or your buyers call in orders to your sales team </li>
+						<li>Payment Options: Credit cards, E-checks or Purchase Orders</li>
+					</ul>
+				</div>
+			</div>
+			<div>
+				<div class="faq_question" onclick="$(this).toggleClass('faq_question_expanded').parent().children('.faq_answer').toggle(300);">Flexible Product Management</div>
+				<div class="faq_answer" style="padding-left: 27px;">
+					<ul>
+						<li>Sellers can manage their own products &amp; stories, or Market Managers can control product management</li>
+						<li>Simple or Lot-based inventory optinos, with sell-by dates</li>
+						<li>Adjust pricing &amp; quantities on the fly</li>
+					</ul>
+				</div>
+			</div>
+			<div>
+				<div class="faq_question" onclick="$(this).toggleClass('faq_question_expanded').parent().children('.faq_answer').toggle(300);">Efficient Fulfillment Logistics</div>
+				<div class="faq_answer" style="padding-left: 27px;">
+					<ul>
+						<li>Pick lists that draw from oldest lots first</li>
+						<li>Packing slips for aggregation</li>
+						<li>Automatic delivery reminders</li>
+						<li>Track &amp; trace orders throughout the delivery cycle</li>
+					</ul>
+				</div>
+			</div>
+			<div>
+				<div class="faq_question" onclick="$(this).toggleClass('faq_question_expanded').parent().children('.faq_answer').toggle(300);">Complete Financial Management</div>
+				<div class="faq_answer" style="padding-left: 27px;">
+					<ul>
+						<li>Tools for invoicing, tracking receivables, and paying your sellers</li>
+						<li>Robust sales &amp; financial reporting</li>
+					</ul>
+				</div>
+			</div>
+			<div>
+				<div class="faq_question" onclick="$(this).toggleClass('faq_question_expanded').parent().children('.faq_answer').toggle(300);">Integrated Marketing</div>
+				<div class="faq_answer" style="padding-left: 27px;">
+					<ul>
+						<li>Fresh sheets</li>
+						<li>Newsletters</li>
+						<li>Featured promotions</li>
+						<li>Micro-blogs</li>
+						<li>Twitter &amp; Facebook integration</li>
+						<li>Rich product and producer stories</li>
+					</ul>
+				</div>
+			</div>
+
+			<div>
+				<div class="faq_question" onclick="$(this).toggleClass('faq_question_expanded').parent().children('.faq_answer').toggle(300);">Quick set up &amp; fabulous support</div>
+				<div class="faq_answer" style="padding-left: 27px;">
+					<ul>
+						<li>One-hour launch training</li>
+						<li>Phone &amp; live chat</li>
+						<li>Online forums, tutorials &amp; webinars</li>
+					</ul>
+				</div>
+			</div>
 
 
+		</td>
+		<td>&nbsp;</td>
+		<td>
+			<div class="pricing">
+				<span class="pricing">Start Up</span>&nbsp;&nbsp;$19/month
+				<br />
+			</div>
+			
+			<div class="pricing pricing_emphasis">
+				<span class="pricing">Grow</span>&nbsp;&nbsp;$49/month
+				<br />
+			</div>
+			<div class="pricing">
+				<span class="pricing">Automate</span>&nbsp;&nbsp;$99/month
+				<br />
+			</div>
+			<h3 style="clear:both;">14-day FREE trial. No risks. No start-up costs.</h3>
+		</td>
+	</tr>
+	<tr>
+		<td colspan="3">
+			<div class="homepage_title" style="text-align: center;">
+				Interested in <u>even more</u> services to simplify local sourcing, enabling institutions<br />
+				&amp; restaurant groups to expand their purchasing? We've got a solution for that.
+				<br />&nbsp;<br />
+				<a href="" class="btn btn_blue" style="margin-left: 420px;">Learn More</a>
+			</div>
+			<br />
+			
+		
+		</td>
+	</tr>
+</table>
 <? include('includes/page_end.php'); ?>
