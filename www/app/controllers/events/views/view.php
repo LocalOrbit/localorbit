4a473839535f4088cdc49cadc6065ea271dfe030
John Vavul <jonn@localorb.it>
2013-04-05 19:59:42 +0000

    format change to view ACH RQ RS
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/trunk@1363 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- www/app/controllers/events/views/view.php
+++ www/app/controllers/events/views/view.php
@@ -18,14 +18,23 @@ $data = core::model('events')->load();
 <?=core_form::value('Domain','<a href="#!market-edit--domain_id-'.$data['domain_id'].'">'.$data['domain_name'].'</a>')?>
 <?=core_form::value('IP Address',$data['ip_address'])?>
 <?=core_form::value('Date',core_format::date($data['creation_date'],'long'))?>
+
+<?=core_form::value('Object 1 ID',$data['obj_id1'])?>
+<?=core_form::value('Object 2 ID',$data['obj_id2'])?>
+<?=core_form::value('Text 1',$data['varchar1'])?>
+<?=core_form::value('Text 2',$data['varchar2'])?>
 <?
 switch($data['event_type'])
 {
+	case 'ACH RQ':
+		echo(core_form::value('API RQ',$data['text1']));
+		break;
+
+	case 'ACH RS':
+		echo(core_form::value('API RS',$data['text1']));
+		break;
+	
 	default:
-		echo(core_form::value('Object 1 ID',$data['obj_id1']));
-		echo(core_form::value('Object 2 ID',$data['obj_id2']));
-		echo(core_form::value('Text 1',$data['varchar1']));
-		echo(core_form::value('Text 2',$data['varchar2']));
 	break;
 }
 ?>
