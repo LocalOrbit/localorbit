6f175e7d16786f53486f32343011da9cae7919de
Mike Ratanatharathorn <mike@localorb.it>
2013-04-10 21:51:41 +0000

    47560267
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/trunk@1368 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- www/app/models/base/payments_ach_history.php
+++ www/app/models/base/payments_ach_history.php
@@ -5,11 +5,12 @@ class core_model_base_payments_ach_history extends core_model
 	{
 		$this->add_field(new core_model_field(0,'pah_id','int',8,'','payments_ach_history'));
 		$this->add_field(new core_model_field(1,'payment_id','int',8,'','payments_ach_history'));
-		$this->add_field(new core_model_field(2,'response_code','string',-4,'','payments_ach_history'));
-		$this->add_field(new core_model_field(3,'action_detail','string',-4,'','payments_ach_history'));
-		$this->add_field(new core_model_field(4,'effective_date','timestamp',4,'','payments_ach_history'));
-		$this->add_field(new core_model_field(5,'action_date','timestamp',4,'','payments_ach_history'));
-		$this->add_field(new core_model_field(6,'recorded_on','timestamp',4,'','payments_ach_history'));
+		$this->add_field(new core_model_field(2,'event_id','string',-4,'','payments_ach_history'));
+		$this->add_field(new core_model_field(3,'response_code','string',-4,'','payments_ach_history'));
+		$this->add_field(new core_model_field(4,'action_detail','string',-4,'','payments_ach_history'));
+		$this->add_field(new core_model_field(5,'effective_date','timestamp',4,'','payments_ach_history'));
+		$this->add_field(new core_model_field(6,'action_date','timestamp',4,'','payments_ach_history'));
+		$this->add_field(new core_model_field(7,'recorded_on','timestamp',4,'','payments_ach_history'));
 		$this->init_data();
 	}
 }
