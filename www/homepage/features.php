4f14fb72f47f623f14b928a88d7ae8927038d208
Mike Ratanatharathorn <mike@localorb.it>
2013-04-17 20:20:32 +0000

    45893817
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/trunk@1413 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- www/homepage/features.php
+++ www/homepage/features.php
@@ -17,29 +17,29 @@
 				<tr>
 			
 					<td rowspan="2">
-					<img src="/homepage/img/icon_pitchfork_shovel_blue.png" />
-			<br />&nbsp;<br />
+						<img style="margin-left: 80px;" src="/homepage/img/icon_pitchfork_shovel_blue.png" />
+						<br />&nbsp;<br />
 					
 						<h2 style="margin-top: 10px;">Smart. Secure. Efficient. </h2> 
 			
-			<span style="font-size:100%;">
+						<span style="font-size:100%;">
+							
+							Thoughtfully designed, flexible tools save you time and 
+							help grow your business. 
+							<br />&nbsp;<br />
+							Our cloud-based technology is scalable, reliable and worry-free,
+							so you can focus on your work - not on managing your tools.  
+									
+							<br />&nbsp;<br />
+							We offer live demos and keep these sessions small, so there's lots of time 
+							for questions about how Local Orbit can work for your business. 
+							<br />&nbsp;<br />
+							Reserve your place today.
+							<br />&nbsp;<br /> 
+					
 				
-				Thoughtfully designed, flexible tools save you time and 
-				help grow your business. 
-				<br><br>
- 				Our cloud-based technology is scalable, reliable and worry-free,
- 				so you can focus on your work - not on managing your tools.  
- 						
-				<br><br>
-				We offer live demos and keep these sessions small, so there's lots of time 
-				for questions about how Local Orbit can work for your business. 
-				<br><br>
-				Reserve your place today.
-				<br><br> 
-		
-	
 							<a href="http://www.localorb.it/field-notes/schedule-a-demo/" class="btn btn_blue">Sign Up for a Demo</a>
-			</div>	
+								</div>	
 						
 					<!-- <br />&nbsp;<br />
 						<a class="btn btn_green" href="http://localorb.us6.list-manage.com/subscribe?u=e0dc0b51636060c0278e6c377&id=9bfb25868c">Get Started</a>
@@ -48,7 +48,7 @@
 					<td rowspan="2">&nbsp;</td>
 					<td>
 						<h3>Streamlined Ordering</h3>
-						<br><br><br>REPLACE with THUMBNAIL<br>with pop-up to full size<br><br><br>
+						<br />&nbsp;<br /><br>REPLACE with THUMBNAIL<br>with pop-up to full size<br />&nbsp;<br /><br>
 						<div class="home_paragraph">
 							Full-featured e-commerce for buyers, 
 							or order entry by your sales team. Process orders the way you prefer to manage your business. </div>
@@ -57,14 +57,14 @@
 					<td rowspan="2">&nbsp;</td>
 					<td>
 						<h3>Financial Management </h3>
-						<br><br><br>REPLACE with THUMBNAIL<br>with pop-up to full size<br><br><br>
+						<br />&nbsp;<br /><br>REPLACE with THUMBNAIL<br>with pop-up to full size<br />&nbsp;<br /><br>
 						<div class="home_paragraph">
 					        One dashboard simplifies your day-to-day financials: track money in and out, issue invoices, make payments to your sellers. 					</div>
 					</td>
 					<td rowspan="2">&nbsp;</td>
 					<td>
 						<h3>Inventory Management</h3>
-						<br><br><br>REPLACE with THUMBNAIL<br>with pop-up to full size<br><br><br>
+						<br />&nbsp;<br /><br>REPLACE with THUMBNAIL<br>with pop-up to full size<br />&nbsp;<br /><br>
 						<div class="home_paragraph">
 							Real-time inventory. Adjust pricing and quantities on the fly. 
 							Flexible inventory options - simple or lot-based to ensure deep traceability.
@@ -74,20 +74,20 @@
 				<tr>
 					<td>
 						<h3>Logistics</h3>
-						<br><br><br>REPLACE with THUMBNAIL<br>with pop-up to full size<br><br><br>
+						<br />&nbsp;<br /><br>REPLACE with THUMBNAIL<br>with pop-up to full size<br />&nbsp;<br /><br>
 						<div class="home_paragraph">
 							Generate pick lists and packing slips. Automated emails remind buyers and sellers about upcoming deliveries. Producers maintain their brand throughout the process.  
 					</td>
 					<td>
 						<h3>Integrated Marketing</h3>
-					<br><br><br>REPLACE with THUMBNAIL<br>with pop-up to full size<br><br><br>
+					<br />&nbsp;<br /><br>REPLACE with THUMBNAIL<br>with pop-up to full size<br />&nbsp;<br /><br>
 						<div class="home_paragraph">
 						 Manage everything in one place: create discounts, promotions, <br>micro-blogs, newsletters and Fresh Sheets in minutes. Integrate your facebook or twitter feeds.
 						</div>
 					</td>
 					<td>
 						<h3>Reporting and Analysis</h3>
-						<br><br><br>REPLACE with THUMBNAIL<br>with pop-up to full size<br><br><br>
+						<br />&nbsp;<br /><br>REPLACE with THUMBNAIL<br>with pop-up to full size<br />&nbsp;<br /><br>
 						<div class="home_paragraph">
 							Sophisticated, easy-to-use reports enable you to track, learn from and grow your business - weekly, monthly and year-over-year.
 						</div>
