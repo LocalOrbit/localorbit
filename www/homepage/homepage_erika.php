e54d2805ce9cf9e1d894c981949f0dc673ab7244
localorbit-deploy <localorbit-deploy>
2013-04-17 20:12:36 +0000

    adding erika changes
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/trunk@1409 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

---
+++ www/homepage/homepage_erika.php
@@ -0,0 +1,254 @@
+<? include('includes/page_start.php'); ?>
+<table style="margin-top: 28px;">
+	<tr>
+		<td>
+			<div id="slides_homepage" class="slides">
+				<div class="slides_container">
+					<div class="slide">
+						<img src="/homepage/img/slideshow/slideshow-new-01.jpg" width="740" height="350" alt="[Local Orbit surpasses anything out there]" />
+						<div class="caption"><div style="height:10px;overflow:hidden;"><br /></div>Local Orbit surpasses anything out there.<br /><span class="caption_who">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;Lucy Norris, Northwest Agriculture Business Center</span></div>
+					</div>
+					<div class="slide">
+						<img src="/homepage/img/slideshow/slideshow-new-02.jpg" width="740" height="350" alt="Slide 2" />
+						<div class="caption">
+							The site is amazing! I really think your company has<br /> a shot at
+							changing the way food is sold
+							on a local level.<br />
+							<span class="caption_who">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;Brian Bruno, Apple Ridge Farm</span>
+						</div>
+					</div>
+					<div class="slide">
+						<img src="/homepage/img/slideshow/slideshow-new-03.jpg" width="740" height="350" alt="Slide 2" />
+						<div class="caption">
+							I am impressed with Local Orbit's capacity; the flexibility is
+							<br />essential for food markets across the country<br />
+							<span class="caption_who">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;Rich Pirog, MSU Center for Regional Food Systems</span>
+						</div>
+					</div>
+					<div class="slide">
+						<img src="/homepage/img/slideshow/slideshow-new-04.jpg" width="740" height="350" alt="Slide 2" />
+						<div class="caption">
+							Local Orbit enables me to work on my business' growth <br />rather than focus on the day to day functions.
+							<!--   I have always fantasized about a system with live inventory for my customers, that keeps producers up to date on market needs & simplifies bookkeeping & logistics. Local Orbit enables me to work on my business' growth.-->
+							<br />
+							<span class="caption_who">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;Gina Riccobono, FRESH</span>
+						</div>
+					</div>
+					<div class="slide">
+						<img src="/homepage/img/slideshow/slideshow-new-05.jpg" width="740" height="350" alt="Slide 2" />
+						<div class="caption">
+							Local Orbit provides the best overall package in terms<br /> of product quality, service and cost effectiveness.
+
+							<br />
+							<span class="caption_who">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;Natasha Lantz, Marquette Food Market</span>
+						</div>
+					</div>
+					<div class="slide">
+						<img src="/homepage/img/slideshow/slideshow-new-06.jpg" width="740" height="350" alt="Slide 2" />
+						<div class="caption">
+							We chose Local Orbit because of the individual support, <br />
+							the friendly staff and the user-friendly interfaces.
+							<br />
+							<span class="caption_who">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;Maya Kosok, Farm Alliance of Baltimore City</span>
+						</div>
+					</div>
+					<div class="slide">
+						<img src="/homepage/img/slideshow/slideshow-new-07.jpg" width="740" height="350" alt="Slide 2" />
+						<div class="caption">
+							<div style="height:10px;overflow:hidden;"><br /></div>
+
+							Local Orbit provides the information I need to minimize costs.
+							<br />
+							<span class="caption_who">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;Christine Quane, Detroit Eastern Market</span>
+						</div>
+					</div>
+				</div>
+				<script language="Javascript" type="text/javascript">
+				<!--
+				core.lo3.homepageStartup();
+				//-->
+				</script>
+				<a href="#!misc-home" class="slideshow_prev"><img src="/homepage/img/slideshow/library/arrow-prev_new.png" alt="Arrow Prev"></a>
+				<a href="#!misc-home" class="slideshow_next"><img src="/homepage/img/slideshow/library/arrow-next_new.png" alt="Arrow Next"></a>
+			</div>
+		</td>
+		<td>&nbsp;&nbsp;&nbsp;</td>
+		<td style="text-align: center !important;">
+			<div class="homepage_title">
+				Sales &amp; business management
+				tools for the entrepeneurs &amp; organizations that are 
+				creating a New Food Economy, connecting buyers &amp; sellers 
+				through shorter, transparent supply chains.
+			</div>
+			<br />
+			<img src="/homepage/img/icon_pitchfork_shovel_blue.png" />
+			<br />&nbsp;<br />
+			<div class="homepage_title">
+			We help people build profitable
+			businesses &amp; healthier communities.
+			
+				<br />&nbsp;<br />
+				<a class="btn btn_blue" style="margin-left: 55px;" href="/homepage/features.php">Learn More</a>
+			
+			</div>
+		</td>
+	</tr>
+</table>
+<br />
+
+<table width="1100" style="table-layout: fixed;">
+	<col width="24%" />
+	<col width="1%" />
+	<col width="23.5%" />
+	<col width="1%" />
+	<col width="23.5%" />
+	<col width="1%" />
+	<col width="24%" />
+	<tr>
+		<td colspan="7"><img src="/homepage/img/homepage_divider.png" /><br />&nbsp;<br /></td>
+	</tr>
+	<tr>
+		<td rowspan="2">
+			<h2 style="margin-top: 10px;">Why Choose Local Orbit? </h3> 
+			
+			Juggling spreadsheets, handwritten orders, quickbooks, emails and phone 
+			calls limits your ability to grow your business. There's a better way.
+			<br />&nbsp;<br />
+			Manage your marketing, sales and financials 
+			with from a single dashboard. Save time & increase profitability with 
+			real-time information, streamlined processes and powerful communications tools.
+			 
+			<br />&nbsp;<br />
+			
+			<a class="btn btn_blue" href="/field-notes/schedule-a-demo">Schedule a Demo</a>
+			
+			<br />&nbsp;<br />
+			
+			<br />&nbsp;<br />
+			
+		</td>
+		<td rowspan="2">&nbsp;</td>
+		<td>
+			<img src="/homepage/img/icon_smiley.png" class="home_icon" /><h3 class="home_header">No Risk, No Start Up Costs</h3>
+			<div class="home_paragraph">
+			Affordable tools that grow with you, at every stage of your business. No upfront investment. 14-day free trial. 
+			</div>
+		</td>
+		<td rowspan="2">&nbsp;</td>
+		<td>
+			<img src="/homepage/img/icon_wand.png" class="home_icon" /><h3 class="home_header">Unrivaled Flexibility</h3>
+			<div class="home_paragraph">
+				Custom branding, granular pricing & discounts, flexible delivery tools, 
+				multiple payment options.
+			</div>
+		</td>
+		<td rowspan="2">&nbsp;</td>
+		<td>
+			<img src="/homepage/img/icon_paperclip.png" class="home_icon" /><h3 class="home_header">Beautiful, Intuitive Design</h3>
+			<div class="home_paragraph">
+				Supports the management processes you already use. 
+				Incorporates best practices from successful businesses.
+			
+			</div>
+		</td>
+	</tr>
+	<tr>
+		<td>
+			<img src="/homepage/img/icon_clock.png" class="home_icon" /><h3 class="home_header">Operational Efficiency</h3>
+			<div class="home_paragraph">
+				Run your business from one dashboard, accessible from any device, wherever you need it.
+				
+			</div>
+		</td>
+		<td>
+			<img src="/homepage/img/icon_graph.png" class="home_icon" /><h3 class="home_header">Streamlined Financials</h3>
+			<div class="home_paragraph">
+				Track the status of every transaction. 
+				Collect & make payments for the cost of a stamp, with the click of a button.
+			</div>
+		</td>
+		<td>
+			<img src="/homepage/img/icon_magnifying_flipped.png" class="home_icon" /><h3 class="home_header">Transparency &amp; Flexibility</h3>
+			<div class="home_paragraph">
+				Tell customers the story of your products. Track the supply chain 
+				journey with just a few clicks.
+			</div>
+		</td>		
+	</tr>
+<tr>
+		<td colspan="7"><a name="whouses"></a><img src="/homepage/img/homepage_divider.png" /><br />&nbsp;<br /></td>
+	</tr>
+	<tr>
+		<td rowspan="2">
+			
+			<h2 style="margin-top: 10px;">Who Uses Local Orbit?</h3> 
+			
+			Local Orbit is designed with the flexibility to
+			support the innovative business models & regional diversity 
+			that are hallmarks of the New Food Economy. 
+			
+			<br />&nbsp;<br />
+			Our sweet spot: 
+			powering local marketplaces that enable efficient restaurant & institutional sales.  
+		
+			<br />&nbsp;<br />
+			<a class="btn btn_blue" href="/homepage/features.php">Learn More</a>
+			
+		</td>
+		<td rowspan="2">&nbsp;</td>
+		<td>
+			<h3>Market Makers</h3>
+			<div class="home_paragraph">
+				Food hubs, farmers markets and independent aggregators 
+				that facilitate direct sales.
+			</div>
+		</td>
+		<td rowspan="2">&nbsp;</td>
+		<td>
+			<h3>Producer Cooperatives</h3>
+			<div class="home_paragraph">
+				Groups of farmers and specialty producers who 
+				partner for marketing, sales and distribution.
+			</div>
+		</td>
+		<td rowspan="2">&nbsp;</td>
+		<td>
+			<h3>Independent Distributors</h3>
+			<div class="home_paragraph">
+				Create ordering systems for local products. 
+				They may also offer extra truck space for 3rd party delivery.
+			</div>
+			
+		</td>
+	</tr>
+	<tr>
+		<td>
+			<h3>Restaurants, Universities, Hospitals</h3>
+			<div class="home_paragraph">
+				Purchase through Local Orbit powered markets. For organizations with 
+				their own local supplier networks, our vendor management services simplify procurement.
+			</div>
+		</td>
+	
+		<td>
+			<h3>Farmers</h3>
+			<div class="home_paragraph">
+				Sell through Local Orbit powered marketplaces in their communities. They 
+				also create their own ordering systems & aggregate from other nearby producers. 
+			</div>
+			
+		</td>
+		<td>
+		<h3>Buying Groups</h3>
+			<div class="home_paragraph">
+				Create private, local ordering systems for members. Buying groups may also 
+				source through Local Orbit powered marketplaces in their regions.
+			</div
+			
+			
+		</td>		
+	</tr>
+
+</table>
+<br />&nbsp;<br />
+<? include('includes/page_end.php'); ?>
