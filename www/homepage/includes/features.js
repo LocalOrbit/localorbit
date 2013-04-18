2c30582fdbb910f19b59bd49312f07d952976393
Mike Ratanatharathorn <mike@localorb.it>
2013-04-18 18:48:52 +0000

    45893817
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/trunk@1426 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- www/homepage/includes/features.js
+++ www/homepage/includes/features.js
@@ -1,5 +1,5 @@
 function doFeaturePopup(src,width){
-	var src = '<img src="/homepage/img/features/'+src+'.png" />';
+	var src = '<img src="/homepage/img/features/'+src+'.png" class="thumb" />';
 	src += '<div class="feature_popup_closer" style="width: '+(width - 9)+'px;">';
 	src += '<input style=" margin-left: '+(Math.floor(( width - 230 ) / 2))+'px;" type="button" class="btn btn_blue" value="Close" onclick="$(\'#popup3,#overlay\').fadeOut(300);" /></div>';
 	$('#popup3').html(src);$('#popup3,#overlay').fadeIn(300);
