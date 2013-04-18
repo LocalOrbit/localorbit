e073dcc238cfa47d9f511c0a305dee88988a15e9
Mike Ratanatharathorn <mike@localorb.it>
2013-04-18 19:16:36 +0000

    45893817
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/trunk@1430 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- www/homepage/includes/features.js
+++ www/homepage/includes/features.js
@@ -1,8 +1,11 @@
 function doFeaturePopup(src,width){
 	var src = '<img src="/homepage/img/features/'+src+'.png" class="thumb" />';
 	
+	
+
 	src += '<div class="feature_popup_closer">';
 	src += '<img src="/homepage/img/icon_popup_close_grey.png" style="cursor: pointer;" onclick="$(\'#popup3,#overlay\').fadeOut(300);" /></div>';
-	
-	$('#popup3').html(src);$('#popup3,#overlay').fadeIn(300);
+	var pos = (new String(Math.floor(($('body').width() - width) / 2 )) + 'px');
+	//alert(pos);
+	$('#popup3').css({'margin-left':'0px','top':'40px','left':pos}).html(src);$('#popup3,#overlay').fadeIn(300);
 }
