0fa30afd09a066739f90d985f1bdaff5d5871911
Mike Ratanatharathorn <mike@localorb.it>
2013-04-26 14:59:34 +0000

    48771907
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/trunk@1537 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

---
+++ www/homepage/managed_services.php
@@ -0,0 +1,72 @@
+<? include('includes/page_start.php'); ?>
+<table width="1100" style="table-layout: fixed;">
+				<col width="28%" />
+				<col width="5%" />
+				<col width="32%" />
+				<col width="3%" />
+				<col width="32%" />
+
+				<tr>
+					<td rowspan="2">
+						
+						
+						<h2 style="margin-top: 10px;">Focus on What You Do Best</h2> 
+
+				
+						Customers want you to use locally sourced products - and they want 
+						the stories behind them. It's both a competitive and culinary advantage.
+						<br />&nbsp;<br /> 
+						But for large organizations, sourcing from lots of small vendors 
+						is an administrative headache.  
+						<br />&nbsp;<br />
+						Actually, we've got a couple of solutions. 
+						<br />&nbsp;<br />
+						<a href="https://docs.google.com/a/localorb.it/spreadsheet/viewform?formkey=dGN1aERlZ3Q2bnRHbDBDUkdqdEcwNnc6MQ" class="btn btn_blue" style="margin-left: 10px;" 
+						onclick="centeredPopup(this.href,'myWindow','860','860','yes');return false">Contact Us</a>
+				
+					</td>
+					<td rowspan="2">&nbsp;</td>
+					<td>
+						<img src="/homepage/img/icon_puzzle_piece_smaller.png" class="home_icon" /><h2 class="home_header">Managed Payments</h2>
+						<div class="home_paragraph">
+							For food hubs, farmers markets and aggregators.
+							
+							<br />&nbsp;<br />
+							Looking to simplify financial management and keep your staffing 
+							costs down? Focus on building your market and let us manage the payments. 
+							<br />&nbsp;<br />
+							Local Orbit invoices institutional buyers, collects 
+							payment and transfers funds directly to sellers, on 
+							terms you establish. Track the status of all money in 
+							and out from your account dashboard. 
+							<br />&nbsp;<br />
+							Secure, transparent, simple.
+							
+							
+							
+							 
+					
+							<br />&nbsp;<br />
+						</div>
+					</td>
+					<td rowspan="2">&nbsp;</td>
+					<td>
+						<img src="/homepage/img/icon_puzzle_piece_smaller.png" class="home_icon" /><h2 class="home_header">Vendor Management</h2>
+						<div class="home_paragraph">
+							For institutional food service and restaurants that 
+							source direct from many farms and independent suppliers. 
+							<br />&nbsp;<br />
+							Purchase from your favorite farms and artisan 
+							producers - without the hassle of managing dozens of 
+							weekly emails, calls, orders, invoices, payments or 
+							stories. Order from many vendors with a single click, and a 
+							single payment.
+							<br />&nbsp;<br />
+							Eliminate the administrative hassle and focus on building relationships.
+							
+							<br />&nbsp;<br />
+						</div>
+					</td>
+				</tr>
+			</table>
+<? include('includes/page_end.php'); ?>
