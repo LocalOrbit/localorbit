4256418f483fde8a6ed146d7ea771310b3b0fac3
Mike Ratanatharathorn <mike@localorb.it>
2013-04-26 14:59:18 +0000

    48771907
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/trunk@1532 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

---
+++ www/homepage/customers.php
@@ -0,0 +1,81 @@
+<? include('includes/page_start.php'); ?>
+<table width="1100" style="table-layout: fixed;">
+	<col width="22%" />
+	<col width="2%" />
+	<col width="23.5%" />
+	<col width="1.5%" />
+	<col width="23.5%" />
+	<col width="1.5%" />
+	<col width="23.5%" />
+	<tr>
+		<td rowspan="2">
+			
+			<h2 style="margin-top: 10px;">Who Uses Local Orbit?</h3> 
+			
+			Local Orbit is designed with the flexibility to
+			support the innovative business models &amp; regional diversity 
+			that are hallmarks of the New Food Economy. 
+			
+			<br />&nbsp;<br />
+			Our sweet spot: 
+			powering local marketplaces that enable efficient restaurant &amp; institutional sales.  
+		
+			<br />&nbsp;<br />
+			<a class="btn btn_blue" href="/homepage/features.php">Learn More</a>
+			
+		</td>
+		<td rowspan="2">&nbsp;</td>
+		<td>
+			<img src="/homepage/img/icon_person_network_smaller.png" class="home_icon" /><h3 class="home_header">Market Makers</h3>
+			<div class="home_paragraph">
+				Food hubs, farmers markets and independent aggregators 
+				that facilitate direct sales.
+			</div>
+		</td>
+		<td rowspan="2">&nbsp;</td>
+		<td>
+			<img src="/homepage/img/icon_handshake.png" class="home_icon" /><h3 class="home_header">Producer Cooperatives</h3>
+			<div class="home_paragraph">
+				Groups of farmers and specialty producers who 
+				partner for marketing, sales and distribution.
+			</div>
+		</td>
+		<td rowspan="2">&nbsp;</td>
+		<td>
+			<img src="/homepage/img/icon_truck.png" class="home_icon" /><h3 class="home_header">Independent Distributors</h3>
+			<div class="home_paragraph">
+				Create ordering systems for local products. 
+				They also provide 3rd party delivery services.
+			</div>
+			
+		</td>
+	</tr>
+	<tr>
+		<td>
+			<img src="/homepage/img/icon_house_4people.png" class="home_icon" /><h3 class="home_header" style="line-height: 21px;">Universities, Hospitals, <br/>Restaurants</h3>
+			<div class="home_paragraph">
+				Purchase through Local Orbit powered markets. For organizations with 
+				their own local supplier networks, our vendor management services simplify procurement.
+			</div>
+		</td>
+	
+		<td>
+			<img src="/homepage/img/icon_barn.png" class="home_icon" /><h3 class="home_header">Farmers</h3>
+			<div class="home_paragraph">
+				Sell through Local Orbit powered markets in their communities. They 
+				also create their own ordering systems &amp; aggregate from other nearby producers. 
+			</div>
+			
+		</td>
+		<td>
+		<img src="/homepage/img/icon_people_pyramid.png" class="home_icon" /><h3 class="home_header">Buying Groups</h3>
+			<div class="home_paragraph">
+				Create private, local ordering systems for members. Buying groups may also 
+				source through Local Orbit powered markets in their regions.
+			</div
+			
+			
+		</td>		
+	</tr>
+</table>
+<? include('includes/page_end.php'); ?>
