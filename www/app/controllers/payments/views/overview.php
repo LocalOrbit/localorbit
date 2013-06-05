e643db8801b49a6259d05a8a329b168f12018bc1
Mike Ratanatharathorn <mike@localorb.it>
2013-06-05 01:47:23 +0000

    refactoring overview tab for correct #s, improving speed by doing 1 less query
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/trunk@1789 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- www/app/controllers/payments/views/overview.php
+++ www/app/controllers/payments/views/overview.php
@@ -2,50 +2,56 @@
 global $core;
 
 # prepare the data for the top metrics table.
-$receivables_ov = core::model('v_payables')
-	->collection()
-	->filter('to_org_id','=',$core->session['org_id'])
-	->filter('(amount - amount_paid)', '>', 0)
-	->filter('invoiced','=','1');
-$payables_ov    = core::model('v_payables')
-	->collection()
-	->filter('from_org_id','=',$core->session['org_id'])
-	->filter('(amount - amount_paid)', '>', 0)
-	->filter('invoiced','=','1');
+$all = new core_collection("
+	select from_org_id,to_org_id,amount,amount_paid,days_left,invoice_id from v_payables 
+	where payment_status <> 'paid'
+	and payable_id in (select payable_id from payables where from_org_id=1 or to_org_id=1);
+");
 
-$payables_ov    = $payables_ov->load()->to_array();
-$receivables_ov = $receivables_ov->load()->to_array();
 
-$intervals = array('Overdue' => 0, 'Today' => 1, 'Next 7 days' => 7, 'Next 30 days' => 30);
+$intervals = array('Overdue' => 0, 'Today' => 1, 'Next 7 days' => 7, 'Next 30 days' => 30,'Purchase Orders'=>31);
+$data['payables']    = array_fill_keys(array_values($intervals), 0);
+$data['receivables'] = array_fill_keys(array_values($intervals), 0);
 
-$receivables_intervals = array_fill_keys(array_values($intervals), 0);
-$payables_intervals    = array_fill_keys(array_values($intervals), 0);
-
-$previousIndex = 0;
-foreach ($intervals as $val) 
+foreach($all as $item)
 {
-	for($index = 0; $index < count($receivables_ov); $index++)
-	{
-		if ($receivables_ov[$index]['days_left'] <= $val && ($val == $previousIndex || ($val != $previousIndex && $receivables_ov[$index]['days_left'] > 0)))
-		{
-			$receivables_intervals[$val] += $receivables_ov[$index]['amount'] -$receivables_ov[$index]['amount_paid'];
-		}
-	}
+	$type = (($item['from_org_id'] == 1)?'payables':'receivables');
+	$amount_due = floatval(($item['amount'] - $item['amount_paid']));
+	#print_r($item);
 	
-	for($index = 0; $index < count($payables_ov); $index++)
+	if(!is_numeric($item['invoice_id']))
 	{
-		if ($payables_ov[$index]['days_left'] <= $val && ($val == $previousIndex || ($val != $previousIndex && $payables_ov[$index]['days_left'] > 0)))
+		$data[$type][31] += $amount_due;
+	}
+	else
+	{
+		if($item['days_left'] < 0)
 		{
-			$payables_intervals[$val] += $payables_ov[$index]['amount'] -$payables_ov[$index]['amount_paid'];
+			$data[$type][0] += $amount_due;
+		}
+		else if($item['days_left'] == 0)
+		{
+			$data[$type][1] += $amount_due;
+		}
+		else if($item['days_left'] > 0 and $item['days_left'] < 7)
+		{
+			$data[$type][7] += $amount_due;
+		}
+		else
+		{
+			$data[$type][30] += $amount_due;
 		}
 	}
-	$previousIndex = $val;
 }
+
+$payables = $data['payables'];
+$receivables = $data['receivables'];
+
 ?>
 
 <div class="tabarea tab-pane active" id="paymentstabs-a<?=($core->view[0]+1)?>">
 	<div class="row row-top-margin-buffer">
-		<? if(count($receivables_ov) > 0 || lo3::is_seller()){?>
+		<? if(count($receivables) > 0 || lo3::is_seller()){?>
 		<div class="span4">
 			<h2>Money In</h2>
 			<?
@@ -54,9 +60,9 @@ foreach ($intervals as $val)
 				echo '<div class="overview-summary-list-item">
 						<div class="overview-summary-list-item-label">' . $key . '</div>
 						<div class="overview-summary-list-item-value">' . 
-							(($receivables_intervals[$value] <= 0)?'<div class="error">':'').
-							core_format::price($receivables_intervals[$value], false)
-							.(($receivables_intervals[$value] <= 0)?'</div>':'') . 
+							(($receivables[$value] <= 0)?'<div class="error">':'').
+							core_format::price($receivables[$value], false)
+							.(($receivables[$value] <= 0)?'</div>':'') . 
 						'</div></div>';
 			}				
 			?>
@@ -65,7 +71,7 @@ foreach ($intervals as $val)
 			</div>
 		</div>
 		<?}?>
-		<? if((count($payables_ov) > 0 && lo3::is_seller()) || (!lo3::is_seller())){?>
+		<? if((count($payables) > 0 && lo3::is_seller()) || (!lo3::is_seller())){?>
 		<div class="span4">
 				<h2>Money Out</h2>
 				<?
@@ -74,9 +80,9 @@ foreach ($intervals as $val)
 					echo '<div class="overview-summary-list-item">
 					<div class="overview-summary-list-item-label">' . $key . '</div>
 					<div class="overview-summary-list-item-value">' .
-					(($payables_intervals[$value] <= 0)?'<div class="error">':'').
-					core_format::price($payables_intervals[$value], false)
-					.(($payables_intervals[$value] <= 0)?'</div>':'') .
+					(($payables[$value] <= 0)?'<div class="error">':'').
+					core_format::price($payables[$value], false)
+					.(($payables[$value] <= 0)?'</div>':'') .
 					'</div></div>';
 					
 				}
