85925bd1722dddfe231825bb327a6140d69232ce
Mike Ratanatharathorn <mike@localorb.it>
2013-06-06 00:35:54 +0000

    changing drop to rename for old tables
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/trunk@1804 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- bin/migrations/4.0.7.1/sql/step3.sql
+++ bin/migrations/4.0.7.1/sql/step3.sql
@@ -5,10 +5,8 @@ drop table if exists payments;
 drop table if exists invoices;
 drop table if exists x_invoices_payments;
 
-
-RENAME TABLE new_invoices TO invoices;
-RENAME TABLE new_payments TO payments;
-RENAME TABLE new_payables TO payables;
-
-
-
+RENAME TABLE payables TO old_payables;
+RENAME TABLE payable_types TO old_payable_types;
+RENAME TABLE payments TO old_payments;
+RENAME TABLE invoices TO old_invoices;
+RENAME TABLE x_invoices_payments TO old_x_invoices_payments;
