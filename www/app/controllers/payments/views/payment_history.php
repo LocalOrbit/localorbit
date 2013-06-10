db2bfa2dcc60b8e045b4be4fcb255b3872a5559f
Mike Ratanatharathorn <mike@localorb.it>
2013-06-10 02:21:16 +0000

    51374735
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/trunk@1874 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- www/app/controllers/payments/views/payment_history.php
+++ www/app/controllers/payments/views/payment_history.php
@@ -10,16 +10,15 @@ $payments = new core_datatable('payments','payments/payment_history',$v_payments
 payments__add_standard_filters($payments,'transactions');
 $payments->add(new core_datacolumn('payment_date','Date Paid',true,'15%',			'{payment_date}','{payment_date}','{payment_date}'));
 
-$autoformat_offset = 5;
+$autoformat_offset = 4;
 if(lo3::is_admin() || lo3::is_market())
 {
 	$payments->add(new core_datacolumn('concat_ws(\' \',from_org_name,to_org_name)','From/To',true,'13%',			'{direction_html}','{direction}','{direction}'));
 	$autoformat_offset++;
 }
 
-$payments->add(new core_datacolumn('order_date','Ref #',false,'13%',			'{ref_nbr_html}','{ref_nbr_html}','{ref_nbr_html}'));
+$payments->add(new core_datacolumn('order_date','Ref #',false,'15%',			'{ref_nbr_html}','{ref_nbr_html}','{ref_nbr_html}'));
 $payments->add(new core_datacolumn('order_date','Description',false,'15%','{description_html}','{description_html}','{description_html}'));
-$payments->add(new core_datacolumn('order_date','Order Date',true,'10%','{order_date}','{order_date}','{order_date}'));
 $payments->add(new core_datacolumn('payment_method','Payment Method',true,'10%','{payment_method_html}','{payment_method_html}','{payment_method_html}'));
 $payments->add(new core_datacolumn('amount','Amount',true,'10%','{amount}','{amount}','{amount}'));
 
