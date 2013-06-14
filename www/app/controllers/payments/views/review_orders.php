c357f06a52ab0587d70a98c5f42ca289fe39442a
Mike Ratanatharathorn <mike@localorb.it>
2013-06-14 19:12:27 +0000

    fixing mm views where payables are NOT to/from them (such as paypal)
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/trunk@2020 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- www/app/controllers/payments/views/review_orders.php
+++ www/app/controllers/payments/views/review_orders.php
@@ -5,11 +5,7 @@ if(lo3::is_admin())
 {
 	
 }
-else if(lo3::is_self_managed())
-{
-	$v_payables->filter('from_org_id','=',$core->session['org_id']);
-}
-else if(lo3::is_fully_managed())
+else if(lo3::is_market())
 {
 	$v_payables->filter('from_org_id','in',array(1,$core->session['org_id']));
 	$v_payables->filter('domain_id','in',$core->session['domains_by_orgtype_id'][2]);
