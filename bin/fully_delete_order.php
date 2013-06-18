05625690ef7bbc24adb412c85544491b52d896bb
Mike Ratanatharathorn <mike@localorb.it>
2013-06-18 21:12:51 +0000

    adding script to fully delete orders
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/trunk@2038 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- bin/fully_delete_order.php
+++ bin/fully_delete_order.php
@@ -3,7 +3,7 @@
 
 # basic sectup
 define('__CORE_ERROR_OUTPUT__','exit');
-include(dirname(__FILE__).'/../../www/app/core/core.php');
+include(dirname(__FILE__).'/../www/app/core/core.php');
 core::init();
 ob_end_flush();
 
@@ -22,45 +22,42 @@ foreach($argv as $arg)
 
 $lo_oid = $config['lo-oid'];
 
-$sql = "
-	delete from payables 
-	where payable_type in ('buyer order','seller order','hub fees','lo fees') 
-	and parent_obj_id in (
-		select lo_liid 
-		from lo_order_line_item where lo_oid=$lo_oid
-	);
-	
-	delete from payables 
-	where payable_type in ('delivery fee') 
-	and parent_obj_id =$lo_oid;
-
-	delete from lo_fulfillment_order where lo_foid in (
+$sql = array(
+	"
+		delete from payables 
+		where payable_type in ('buyer order','seller order','hub fees','lo fees') 
+		and parent_obj_id in (
+			select lo_liid 
+			from lo_order_line_item where lo_oid=$lo_oid
+		);
+	",
+	"
+		delete from payables 
+		where payable_type in ('delivery fee') 
+		and parent_obj_id =$lo_oid;
+	",
+	"delete from lo_fulfillment_order where lo_foid in (
 		select lo_foid 
 		from lo_order_line_item where lo_oid=$lo_oid
-	);
-	
-	delete from lo_order_deliveries where lo_oid=$lo_oid;
-
-	delete from lo_order_line_item where lo_foid in (
-		select lo_foid 
-		from lo_order_line_item where lo_oid=$lo_oid
-	);
-	
-	delete from lo_order where lo_oid=$lo_oid;
-";
+	);",
+	"delete from lo_order_deliveries where lo_oid=$lo_oid;",
+	"delete from lo_order_line_item where lo_oid=$lo_oid;",
+	"delete from lo_order where lo_oid=$lo_oid;"
+);
 
 echo("preparing to delete $lo_oid\n");
-if($config['report-sql'] == 1)	echo("\n\n".$sql."\n\n");
+if($config['report-sql'] == 1)	echo("\n\n".print_r($sql,true)."\n\n");
 
 
 if($config['do-delete'] == 1)
 {
-	mysql_query($sql);
+	foreach($sql as $query)
+		mysql_query($query);
 	echo("DELETE COMPLETE\n");
 }
 else
 {
-	echo("NOT ACTUALLY DELETING\nIn order to actually delete the order, use this parameter: do-delete:1\n"):
+	echo("NOT ACTUALLY DELETING\nIn order to actually delete the order, use this parameter: do-delete:1\n");
 }
 
 exit();
