d445ff2945af9ee24d247ef14a91629603030271
Mike Ratanatharathorn <mike@localorb.it>
2013-06-18 19:24:25 +0000

    51925473
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/trunk@2032 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- www/app/models/core_model_lo_order___placeable.php
+++ www/app/models/core_model_lo_order___placeable.php
@@ -59,6 +59,7 @@ class core_model_lo_order___placeable extends core_model_base_lo_order
 				}
 				else
 				{
+					$this->reset_order_statuses();
 					$this->wipe_payables_for_order($this['lo_oid']);
 					$payment->delete();
 					unset($core->response['replace']['full_width']);
@@ -104,11 +105,12 @@ class core_model_lo_order___placeable extends core_model_base_lo_order
 				);
 
 
-				if(!$response['success'])
+				if(intval($response['success']) != 1)
 				{
 					core::log(print_r($response,true));
 					core::load_library('core_phpmailer');
-
+					$this->reset_order_statuses();
+					$this->wipe_payables_for_order($this['lo_oid']);
 					core_phpmailer::send_email(
 						'paypal fail',
 						'Response: '.print_r($response,true)."\n\n\n".str_replace($core->data['pp_cc_number'],'****-****-****-****',print_r($data,true)),
