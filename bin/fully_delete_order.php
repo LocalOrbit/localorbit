38101c9b14e7a89d87ad169e75267b40319ca7e5
Mike Ratanatharathorn <mike@localorb.it>
2013-06-18 21:04:20 +0000

    adding script to fully delete orders
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/trunk@2034 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

---
+++ bin/fully_delete_order.php
@@ -0,0 +1,67 @@
+#!/usr/bin/php
+<?php
+
+# basic sectup
+define('__CORE_ERROR_OUTPUT__','exit');
+include(dirname(__FILE__).'/../../www/app/core/core.php');
+core::init();
+ob_end_flush();
+
+$config = array(
+	'lo-oid'=>0,  # allows you to restrict the query to a single vendor
+	'report-sql'=>0,
+	'do-delete'=>0,
+);
+
+array_shift($argv);
+foreach($argv as $arg)
+{
+	$arg = explode(':',$arg);
+	$config[$arg[0]] = str_replace('"','',$arg[1]);
+}
+
+$lo_oid = $config['lo-oid'];
+
+$sql = "
+	delete from payables 
+	where payable_type in ('buyer order','seller order','hub fees','lo fees') 
+	and parent_obj_id in (
+		select lo_liid 
+		from lo_order_line_item where lo_oid=$lo_oid
+	);
+	
+	delete from payables 
+	where payable_type in ('delivery fee') 
+	and parent_obj_id =$lo_oid;
+
+	delete from lo_fulfillment_order where lo_foid in (
+		select lo_foid 
+		from lo_order_line_item where lo_oid=$lo_oid
+	);
+	
+	delete from lo_order_deliveries where lo_oid=$lo_oid;
+
+	delete from lo_order_line_item where lo_foid in (
+		select lo_foid 
+		from lo_order_line_item where lo_oid=$lo_oid
+	);
+	
+	delete from lo_order where lo_oid=$lo_oid;
+";
+
+echo("preparing to delete $lo_oid\n");
+if($config['report-sql'] == 1)	echo("\n\n".$sql."\n\n");
+
+
+if($config['do-delete'] == 1)
+{
+	mysql_query($sql);
+	echo("DELETE COMPLETE\n"):
+}
+else
+{
+	echo("NOT ACTUALLY DELETING\nIn order to actually delete the order, use this parameter: do-delete:1\n"):
+}
+
+exit();
+?>
