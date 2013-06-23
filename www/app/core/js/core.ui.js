2c936217e372d67e3eec99719b8ff8d2e236b69d
John Vavul <jonn@localorb.it>
2013-06-23 16:38:34 +0000

    fix for date issues
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/trunk@2122 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- www/app/core/js/core.ui.js
+++ www/app/core/js/core.ui.js
@@ -333,18 +333,18 @@ core.ui.dataTable.updateFilter=function(dtFilterName,filtValue,parseDate){
 	if(parseDate === true)
 	{
 		var months = {
-			'January':'01',
-			'February':'02',
-			'March':'03',
-			'April':'04',
+			'Jan':'01',
+			'Feb':'02',
+			'Mar':'03',
+			'Apr':'04',
 			'May':'05',
-			'June':'06',
-			'July':'07',
-			'August':'08',
-			'September':'09',
-			'October':'10',
-			'November':'11',
-			'December':'12'
+			'Jun':'06',
+			'Jul':'07',
+			'Aug':'08',
+			'Sep':'09',
+			'Oct':'10',
+			'Nov':'11',
+			'Dec':'12'
 		};
 		var parts = new String(filtValue).split(/\s/);
 		parts[1] = parseInt(new String(parts[1]).replace(',',''));
