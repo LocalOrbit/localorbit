4b9d1d075ddb9835c98bf0ff4c021df09b1d3061
Mike Ratanatharathorn <mike@localorb.it>
2013-06-24 20:35:13 +0000

    51747709
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/trunk@2138 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- db/patches/4.071/006.sql
+++ db/patches/4.071/006.sql
@@ -1,5 +1,5 @@
 INSERT INTO migrations (tag, version_id, pt_ticket_no) VALUES ('4.071', '006', '51747709');
 
+delete from phrases where label='note:totals_table_current_view';
 
-insert into phrases (pcat_id,label,edit_type,default_value) 
-values (5,'note:totals_table_current_view','textarea','The total below is a sum of the visible rows for the report you''ve selected. If you''d like to view the full report totals, select "Show all rows" in the drop down.');
+insert into phrases (pcat_id,label,edit_type,default_value) values (5,'note:totals_table_current_view','textarea','The total below is a sum of the visible rows for the report you''ve selected. If you''d like to view the full report totals, select &quot;Show all rows&quot; in the drop down.');
