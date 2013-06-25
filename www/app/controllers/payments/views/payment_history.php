123e9cf91a4207ccb5566d7f2bc77f91b7f82662
Mike Ratanatharathorn <mike@localorb.it>
2013-06-25 16:07:10 +0000

    51567409
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/trunk@2177 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- www/app/controllers/payments/views/payment_history.php
+++ www/app/controllers/payments/views/payment_history.php
@@ -38,28 +38,53 @@ $v_payments->add_formatter('format_payable_info');
 
 $payments = new core_datatable('payments','payments/payment_history',$v_payments);
 payments__add_standard_filters($payments,'transactions');
-$payments->add(new core_datacolumn('payment_date','Date Paid',true,'15%',			'{payment_date}','{payment_date}','{payment_date}'));
 
-$autoformat_offset = 4;
-if(lo3::is_admin() || lo3::is_market())
+core::log(print_r($core->data,true));
+
+if($core->data['format'] == 'csv')
 {
-	$payments->add(new core_datacolumn('concat_ws(\' \',from_org_name,to_org_name)','From/To',true,'13%',			'{direction_html}','{direction}','{direction}'));
-	$autoformat_offset++;
+	$payments->add(new core_datacolumn('payment_date','Date Paid',true,'15%',			'{payment_date}','{payment_date}','{payment_date}'));
+	$autoformat_offset = 3;
+	if(lo3::is_admin() || lo3::is_market())
+	{
+		$payments->add(new core_datacolumn('concat_ws(\' \',from_org_name,to_org_name)','From',true,'13%',			'{from_org_name}','{from_org_name}','{from_org_name}'));
+		$payments->add(new core_datacolumn('concat_ws(\' \',from_org_name,to_org_name)','To',true,'13%',			'{to_org_name}','{to_org_name}','{to_org_name}'));
+		$autoformat_offset+=2;
+	}
+
+	$payments->add(new core_datacolumn('order_date','Description',false,'15%','{description_html}','{description_unformatted}','{description_unformatted}'));
+	$payments->add(new core_datacolumn('payment_method','Payment Method',true,'10%','{payment_method_html}','{payment_method_html}','{payment_method_html}'));
+	$payments->add(new core_datacolumn('amount','Amount',true,'10%','{amount}','{amount}','{amount}'));
+	$payments->columns[0]->autoformat='date-short';
+	$payments->columns[($autoformat_offset)]->autoformat='price';
+	$payments->sort_column = 0;
+	$payments->sort_direction = 'desc';
+}
+else
+{
+	$payments->add(new core_datacolumn('payment_date','Date Paid',true,'15%',			'{payment_date}','{payment_date}','{payment_date}'));
+	$autoformat_offset = 4;
+	if(lo3::is_admin() || lo3::is_market())
+	{
+		$payments->add(new core_datacolumn('concat_ws(\' \',from_org_name,to_org_name)','From/To',true,'13%',			'{direction_html}','{direction}','{direction}'));
+		$autoformat_offset++;
+	}
+
+	$payments->add(new core_datacolumn('order_date','Ref #',false,'15%',			'{ref_nbr_html}','{ref_nbr_unformatted}','{ref_nbr_unformatted}'));
+	$payments->add(new core_datacolumn('order_date','Description',false,'15%','{description_html}','{description_unformatted}','{description_unformatted}'));
+	$payments->add(new core_datacolumn('payment_method','Payment Method',true,'10%','{payment_method_html}','{payment_method_html}','{payment_method_html}'));
+	$payments->add(new core_datacolumn('amount','Amount',true,'10%','{amount}','{amount}','{amount}'));
+	$payments->columns[0]->autoformat='date-short';
+	$payments->columns[($autoformat_offset)]->autoformat='price';
+	$payments->sort_column = 0;
+	$payments->sort_direction = 'desc';
+	
+	if(lo3::is_admin())
+	{
+		$payments->add(new core_datacolumn('payment_id',array(core_ui::check_all('payments'),'',''),false,'4%',core_ui::check_all('payments','payment_id'),' ',' '));
+	}
 }
 
-$payments->add(new core_datacolumn('order_date','Ref #',false,'15%',			'{ref_nbr_html}','{ref_nbr_html}','{ref_nbr_html}'));
-$payments->add(new core_datacolumn('order_date','Description',false,'15%','{description_html}','{description_html}','{description_html}'));
-$payments->add(new core_datacolumn('payment_method','Payment Method',true,'10%','{payment_method_html}','{payment_method_html}','{payment_method_html}'));
-$payments->add(new core_datacolumn('amount','Amount',true,'10%','{amount}','{amount}','{amount}'));
-if(lo3::is_admin())
-{
-	$payments->add(new core_datacolumn('payment_id',array(core_ui::check_all('payments'),'',''),false,'4%',core_ui::check_all('payments','payment_id'),' ',' '));
-}
-#$payments->add(new core_datacolumn('payable_info','Payable Info',true,'50%','{payable_info}','{payable_info}','{payable_info}'));
-$payments->columns[0]->autoformat='date-short';
-$payments->columns[($autoformat_offset)]->autoformat='price';
-$payments->sort_column = 0;
-$payments->sort_direction = 'desc';
 ?>
 <div class="tab-pane tabarea" id="paymentstabs-a<?=($core->view[0]+1)?>">
 	<?php
