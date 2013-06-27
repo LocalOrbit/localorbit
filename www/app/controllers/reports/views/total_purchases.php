539ad54b11647aebab374c95d84b0fd88b1ee497
Mike Ratanatharathorn <mike@localorb.it>
2013-06-27 21:09:25 +0000

    51747709
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/trunk@2212 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- www/app/controllers/reports/views/total_purchases.php
+++ www/app/controllers/reports/views/total_purchases.php
@@ -9,7 +9,7 @@ $col->__model->autojoin(
 	'left',
 	'lo_fulfillment_order',
 	'(lo_fulfillment_order.lo_foid=lo_order_line_item.lo_foid)',
-	array('UNIX_TIMESTAMP(lo_fulfillment_order.order_date) as order_date')
+	array('UNIX_TIMESTAMP(lo_fulfillment_order.order_date) as order_date','lo_fulfillment_order.lo3_order_nbr as lo3_lfo_order_nbr')
 );
 $col->__model->autojoin(
 	'left',
@@ -105,7 +105,31 @@ if(lo3::is_admin() || count($core->session['domains_by_orgtype_id'][2])>1)
 }
 
 # date, amount, status, order number
-$items->add(new core_datacolumn('order_date','Placed On',true,'13%','<a href="#!orders-view_sales_order--lo_foid-{lo_foid}">{order_date}</a>','{order_date}','{order_date}'));
+#$items->add(new core_datacolumn('order_date','Placed On',true,'13%','<a href="#!orders-view_sales_order--lo_foid-{lo_foid}">{order_date}</a>','{order_date}','{order_date}'));
+
+$offset = 0;
+if($core->data['format'] == 'csv')
+{
+	$items->add(new core_datacolumn('lo_fulfillment_order.order_date','Placed On',true,'15%','<a href="#!orders-view_order--lo_oid-{lo_oid}">{formatted_order_date}</a>','{formatted_order_date}','{formatted_order_date}'));
+	if(lo3::is_market() || lo3::is_admin())
+	{
+		$items->add(new core_datacolumn('lo_fulfillment_order.lo3_order_nbr','Seller Order Nbr',true,'15%','<a href="#!orders-view_sales_order--lo_foid-{lo_foid}">{formatted_order_date}</a>','{lo3_lfo_order_nbr}','{lo3_lfo_order_nbr}'));
+		$offset++;
+	}
+	$items->add(new core_datacolumn('lo_order.lo3_order_nbr','Buyer Order Nbr',true,'15%','<a href="#!orders-view_sales_order--lo_foid-{lo_foid}">{formatted_order_date}</a>','{lo3_order_nbr}','{lo3_order_nbr}'));
+		
+	$offset++;
+}
+else
+{
+	$order_link = '';
+	$order_link .= '<br /><a href="app.php#!orders-view_order--lo_oid-{lo_oid}">{lo3_order_nbr}</a>';
+	if(lo3::is_market() || lo3::is_admin())
+		$order_link .= '<br /><a href="app.php#!orders-view_sales_order--lo_foid-{lo_foid}">{lo3_lfo_order_nbr}</a>';
+	$items->add(new core_datacolumn('lo_fulfillment_order.order_date','Placed On',true,'15%','<a href="#!orders-view_order--lo_oid-{lo_oid}">{formatted_order_date}</a>'.$order_link,'{formatted_order_date}','{formatted_order_date}'));
+}
+
+
 if (lo3::is_seller()) {
 	$items->add(new core_datacolumn('product_name','Product',true,'25%','<a href="#!products-edit--prod_id-{prod_id}">{product_name}</a>','{product_name}','{product_name}'));
 } else {
@@ -123,11 +147,11 @@ $items->add(new core_datacolumn('buyer_payment_status','Buyer Payment Status',tr
 $items->add(new core_datacolumn('seller_payment_status','Seller Payment Status',true,'9%','{seller_payment_status}','{seller_payment_status}','{seller_payment_status}'));
 $items->add(new core_datacolumn('lo_oid','Order #',true,'10%','{lo3_order_nbr}','{lo3_order_nbr}','{lo3_order_nbr}'));
 
-$items->columns[0]->autoformat='date-short';
-$items->columns[3]->autoformat='price';
-$items->columns[4]->autoformat='price';
-$items->columns[5]->autoformat='price';
-$items->columns[6]->autoformat='price';
+
+$items->columns[3 + $offset]->autoformat='price';
+$items->columns[4 + $offset]->autoformat='price';
+$items->columns[5 + $offset]->autoformat='price';
+$items->columns[6 + $offset]->autoformat='price';
 $items->sort_direction = 'desc';
 $items->render();
 $this->totals_table('tp_');
