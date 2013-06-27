2b2546aaf6fab74107651f508a41b00edefa9caf
Mike Ratanatharathorn <mike@localorb.it>
2013-06-27 20:42:46 +0000

    51747709
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/trunk@2206 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- www/app/controllers/reports/views/sales_by_seller.php
+++ www/app/controllers/reports/views/sales_by_seller.php
@@ -15,7 +15,7 @@ $col->__model->autojoin(
 	'left',
 	'lo_fulfillment_order',
 	'(lo_fulfillment_order.lo_foid=lo_order_line_item.lo_foid)',
-	array('UNIX_TIMESTAMP(lo_fulfillment_order.order_date) as order_date')
+	array('UNIX_TIMESTAMP(lo_fulfillment_order.order_date) as order_date','lo_fulfillment_order.lo3_order_nbr as lo3_lfo_order_nbr')
 );
 $col->__model->autojoin(
 	'left',
@@ -45,7 +45,7 @@ $col->__model->autojoin(
 	'left',
 	'lo_order',
 	'(lo_order.lo_oid=lo_order_line_item.lo_oid)',
-	array('fee_percen_lo','fee_percen_hub','lo_order.payment_method','lo_order.paypal_processing_fee')
+	array('fee_percen_lo','fee_percen_hub','lo_order.payment_method','lo_order.paypal_processing_fee','lo_order.lo3_order_nbr')
 );
 $col->__model->autojoin(
 	'left',
@@ -175,8 +175,34 @@ else
 	);
 }
 
-# date, prod cat, item, amount, status (filter by producer)
-$items->add(new core_datacolumn('order_date','Placed On',true,'15%','<a href="#!orders-view_sales_order--lo_foid-{lo_foid}">{order_date}</a>','{order_date}','{order_date}'));
+#date, produ cat, item, amount, status, payment method (NOTE: If hub has only one payment type, this report tab does not show up)
+#relevant buyers by date, item, amount, status
+# if the user is trying to download the report in CSV format, 
+# then add 1 or 2 additional columns with the lo_oid and lo_foid
+# MMs get both, sellers get only foid.
+# $offset is used to keep track of what the column nbrs are for the 
+# autoformatters, since their position will change if it's a CSV download
+$offset = 0;
+if($core->data['format'] == 'csv')
+{
+	$items->add(new core_datacolumn('lo_fulfillment_order.order_date','Placed On',true,'15%','<a href="#!orders-view_sales_order--lo_foid-{lo_foid}">{formatted_order_date}</a>','{formatted_order_date}','{formatted_order_date}'));
+	if(lo3::is_market() || lo3::is_admin())
+	{
+		$items->add(new core_datacolumn('lo_order.lo3_order_nbr','Buyer Order Nbr',true,'15%','<a href="#!orders-view_order--lo_oid-{lo_oid}">{formatted_order_date}</a>','{lo3_order_nbr}','{lo3_order_nbr}'));
+		$offset++;
+	}
+	$items->add(new core_datacolumn('lo_fulfillment_order.lo3_order_nbr','Seller Order Nbr',true,'15%','<a href="#!orders-view_sales_order--lo_foid-{lo_foid}">{formatted_order_date}</a>','{lo3_lfo_order_nbr}','{lo3_lfo_order_nbr}'));
+	$offset++;
+}
+else
+{
+	$order_link = '';
+	if(lo3::is_market() || lo3::is_admin())
+		$order_link .= '<br /><a href="app.php#!orders-view_order--lo_oid-{lo_oid}">{lo3_order_nbr}</a>';
+	$order_link .= '<br /><a href="app.php#!orders-view_sales_order--lo_foid-{lo_foid}">{lo3_lfo_order_nbr}</a>';
+	$items->add(new core_datacolumn('lo_fulfillment_order.order_date','Placed On',true,'15%','<a href="#!orders-view_sales_order--lo_foid-{lo_foid}">{formatted_order_date}</a>'.$order_link,'{formatted_order_date}','{formatted_order_date}'));
+}
+
 $items->add(new core_datacolumn('category_ids','Category',true,'20%','<a href="#!products-edit--prod_id-{prod_id}">{parent_cat_name}</a>','{parent_cat_name}','{parent_cat_name}'));
 $items->add(new core_datacolumn('product_name','Product',true,'29%','<a href="#!products-edit--prod_id-{prod_id}">{product_name}</a> from {seller_name}','{product_name}','{product_name}'));
 $items->add(new core_datacolumn('qty_ordered','Quantity',true,'9%'));
@@ -188,11 +214,10 @@ $items->add(new core_datacolumn('delivery_status','Delivery Status',true,'9%','{
 $items->add(new core_datacolumn('buyer_payment_status','Buyer Payment Status',true,'9%','{buyer_payment_status}','{buyer_payment_status}','{buyer_payment_status}'));
 $items->add(new core_datacolumn('seller_payment_status','Seller Payment Status',true,'9%','{seller_payment_status}','{seller_payment_status}','{seller_payment_status}'));
 
-$items->columns[0]->autoformat='date-short';
-$items->columns[4]->autoformat='price';
-$items->columns[5]->autoformat='price';
-$items->columns[6]->autoformat='price';
-$items->columns[7]->autoformat='price';
+$items->columns[4 + $offset]->autoformat='price';
+$items->columns[5 + $offset]->autoformat='price';
+$items->columns[6 + $offset]->autoformat='price';
+$items->columns[7 + $offset]->autoformat='price';
 $items->sort_direction = 'desc';
 $items->render();
 
