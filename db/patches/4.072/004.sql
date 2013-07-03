eaa6c33a76e6674a93f5ec7994cd975f0a1fb89a
John Vavul <jonn@localorb.it>
2013-07-03 22:45:46 +0000

    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/trunk@2279 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- db/patches/4.072/004.sql
+++ db/patches/4.072/004.sql
@@ -2,7 +2,7 @@
 INSERT INTO migrations (tag, version_id, pt_ticket_no) VALUES ('4.072', '004', '51530693');
 
 ALTER TABLE domains
-ADD payment_allow_paypal_popup int DEFAULT '1';
+CHANGE payment_allow_paypal_express_checkout payment_allow_paypal_popup int DEFAULT '1';
 
 ALTER TABLE domains
-ADD payment_default_paypal_popup int DEFAULT '1';
+CHANGE payment_default_paypal_express_checkout payment_default_paypal_popup int DEFAULT '1';
