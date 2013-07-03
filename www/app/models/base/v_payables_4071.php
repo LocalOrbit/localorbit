488c82a9afd5a1b95d0d9891bfebd22d61686180
Mike Ratanatharathorn <mike@localorb.it>
2013-07-03 21:43:29 +0000

    adding missing models
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/trunk@2277 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

---
+++ www/app/models/base/v_payables_4071.php
@@ -0,0 +1,35 @@
+<?php
+class core_model_base_v_payables_4071 extends core_model
+{
+	function init_fields()
+	{
+		$this->add_field(new core_model_field(0,'payable_id','int',8,'','v_payables_4071'));
+		$this->add_field(new core_model_field(1,'domain_id','int',8,'','v_payables_4071'));
+		$this->add_field(new core_model_field(2,'from_org_id','int',8,'','v_payables_4071'));
+		$this->add_field(new core_model_field(3,'to_org_id','int',8,'','v_payables_4071'));
+		$this->add_field(new core_model_field(4,'payable_type','string',-4,'','v_payables_4071'));
+		$this->add_field(new core_model_field(5,'parent_obj_id','int',8,'','v_payables_4071'));
+		$this->add_field(new core_model_field(6,'amount','float',10,'2','v_payables_4071'));
+		$this->add_field(new core_model_field(7,'invoice_id','int',8,'','v_payables_4071'));
+		$this->add_field(new core_model_field(8,'creation_date','int',8,'','v_payables_4071'));
+		$this->add_field(new core_model_field(9,'is_deleted','int',8,'','v_payables_4071'));
+		$this->add_field(new core_model_field(10,'amount_paid','float',10,'2','v_payables_4071'));
+		$this->add_field(new core_model_field(11,'ldstat_id','int',8,'','v_payables_4071'));
+		$this->add_field(new core_model_field(12,'lbps_id','int',8,'','v_payables_4071'));
+		$this->add_field(new core_model_field(13,'lsps_id','int',8,'','v_payables_4071'));
+		$this->add_field(new core_model_field(14,'lo_oid','int',8,'','v_payables_4071'));
+		$this->add_field(new core_model_field(15,'lo_foid','int',8,'','v_payables_4071'));
+		$this->add_field(new core_model_field(16,'buyer_lo3_order_nbr','string',-4,'','v_payables_4071'));
+		$this->add_field(new core_model_field(17,'seller_lo3_order_nbr','string',-4,'','v_payables_4071'));
+		$this->add_field(new core_model_field(18,'po_number','string',-4,'','v_payables_4071'));
+		$this->add_field(new core_model_field(19,'buyer_org_id','int',8,'','v_payables_4071'));
+		$this->add_field(new core_model_field(20,'from_org_name','string',-4,'','v_payables_4071'));
+		$this->add_field(new core_model_field(21,'to_org_name','string',-4,'','v_payables_4071'));
+		$this->add_field(new core_model_field(22,'from_domain_id','int',8,'','v_payables_4071'));
+		$this->add_field(new core_model_field(23,'to_domain_id','int',8,'','v_payables_4071'));
+		$this->add_field(new core_model_field(24,'due_date','int',8,'','v_payables_4071'));
+		$this->add_field(new core_model_field(25,'invoice_date','int',8,'','v_payables_4071'));
+		$this->init_data();
+	}
+}
+?>
---
+++ www/app/models/v_payables_4071.php
@@ -0,0 +1,5 @@
+<?php
+class core_model_v_payables_4071 extends core_model_base_v_payables_4071
+{
+}
+?>
