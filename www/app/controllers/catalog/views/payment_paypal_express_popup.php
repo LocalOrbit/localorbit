b3a422c3fcea632dd8c1cbf06436ed9769a8c4f2
Mike Ratanatharathorn <mike@localorb.it>
2013-07-03 23:07:54 +0000

    updating lib for john
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/trunk@2282 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- www/app/controllers/catalog/views/payment_paypal_express_popup.php
+++ www/app/controllers/catalog/views/payment_paypal_express_popup.php
@@ -1,5 +1,7 @@
 <?php
+global $core;
 $this->update_fees('yes');
+include(__DIR__.'/../../../../bin/paypal/PayPalApi.php');
 $paypalUrl = $payPalApi->getExpressCheckoutRedirect();
 core::js('window.open("'.$paypalUrl.'");');
 core::deinit();
