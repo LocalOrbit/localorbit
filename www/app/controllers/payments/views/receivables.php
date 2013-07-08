69b3a46bb9e8c651a0989a888853cfba15624a34
Mike Ratanatharathorn <mike@localorb.it>
2013-07-08 12:03:02 +0000

    51747317
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/trunk@2286 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- www/app/controllers/payments/views/receivables.php
+++ www/app/controllers/payments/views/receivables.php
@@ -17,7 +17,7 @@ else
 }
 
 $receivables = new core_datatable('receivables','payments/receivables',$v_payables);
-$receivables->add(new core_datacolumn('ref_nbr_sortable','Ref Nbr',true,'10%','{ref_nbr_html}','{ref_nbr_nohtml}','{ref_nbr_nohtml}'));
+$receivables->add(new core_datacolumn('ref_nbr_sortable','Reference',true,'10%','{ref_nbr_html}','{ref_nbr_nohtml}','{ref_nbr_nohtml}'));
 
 $receivables->add(new core_datacolumn('description_sortable','Description',true,'10%','{description_html}','{description_nohtml}','{description_nohtml}'));
 
@@ -27,13 +27,108 @@ if(lo3::is_market() || lo3::is_admin())
 
 $receivables->add(new core_datacolumn('creation_date','Order Date',true,'10%','{creation_date}','{creation_date}','{creation_date}'));
 
-$receivables->add(new core_datacolumn('(amount - amount_paid)','Amount Owed',true,'10%','{amount_owed}','{amount_owed}','{amount_owed}'));
+$receivables->add(new core_datacolumn('amount_owed','Amount Owed',true,'10%','{amount_owed}','{amount_owed}','{amount_owed}'));
 
 $receivables->add(new core_datacolumn('delivery_status','Delivery',true,'10%','{delivery_status}','{delivery_status}','{delivery_status}'));
 $receivables->add(new core_datacolumn('buyer_payment_status','Buyer Pmt',true,'10%','{buyer_payment_status}','{buyer_payment_status}','{buyer_payment_status}'));
 $receivables->add(new core_datacolumn('seller_payment_status','Seller Pmt',true,'10%','{seller_payment_status}','{seller_payment_status}','{seller_payment_status}'));
+$receivables->add(new core_datacolumn('payable_id',array(core_ui::check_all('receivables'),'',''),false,'4%',core_ui::check_all('receivables','payable_id'),' ',' '));
 
 
+if(lo3::is_market() || lo3::is_admin())
+{
+	
+}
+else if(lo3::is_seller())
+{
+	$buyer_collection = new core_collection('
+		select org_id as id,name
+		from organizations
+		where org_id in (
+			select org_id
+			from lo_order
+			where lo_oid in (
+				select lo_oid
+				from lo_order_line_item
+				where seller_org_id='.$core->session['org_id'].'
+				and ldstat_id<>1
+			)
+		)
+		order by name
+	');
+	
+	$markets = new core_collection('
+		select domain_id as id,name
+		from domains
+		where domain_id in (
+			select distinct domain_id 
+			from payables
+			where to_org_id='.$core->session['org_id'].'
+		)
+		order by name
+	');
+	$markets->load();
+	
+	
+	$base = mktime(0, 0, 0, date('n'), date('j'));
+	$start =  $base - (86400*30) - intval($core->session['time_offset']);
+	$end = $base - intval($core->session['time_offset']) + 86399;
+	
+	$receivables->filter_html .= '<div style="float:right;width:410px;">'.get_inline_message($receivables->name,330).'</div>';
+	$receivables->filter_html .= '<div style="float:left;width:490px;">';
+
+	$receivables->add_filter(new core_datatable_filter('receivables_createdat1','creation_date','>','unix_date',null));
+	$receivables->add_filter(new core_datatable_filter('receivables_createdat2','creation_date','<','unix_date',null));
+	$receivables->filter_html .= core_datatable_filter::make_date($receivables->name,'receivables_createdat1',core_format::date($start,'short'),'Invoiced from ');
+	$receivables->filter_html .= core_datatable_filter::make_date($receivables->name,'receivables_createdat2',core_format::date($end,'short'),'Invoiced to ');	
+
+	$receivables->add_filter(new core_datatable_filter('payable_info','payable_info','~','search'));
+	$receivables->filter_html .= core_datatable_filter::make_text($receivables->name,'payable_info',$receivables->filter_states[$receivables->name.'__filter__payable_info'],'Search by name or ref #');
+
+	$receivables->filter_html .= '<br /><div class="clearfix">&nbsp;</div>';
+
+
+	$receivables->filter_html .= '<div style="float:left; width:215px;">';
+	
+	
+	$receivables->filter_html .= '<h4>Delivery Filters</h4>';
+	make_filter($receivables,'ldstat_id',array(
+			'2'=>'Pending',
+			'3'=>'Canceled',
+			'4'=>'Delivered',
+			'5'=>'Partially Delivered',
+			'6'=>'Contested',
+		),'Status','All',40);
+	make_filter($receivables,'buyer_org_id',$buyer_collection,'To','All Organizations',40);
+	
+	$receivables->filter_html .= '</div>';
+	$receivables->filter_html .= '<div style="float:left; width:250px;">';
+	
+	if($markets->__num_rows > 1)
+	{
+		$receivables->filter_html .= '<h4>Market Filters</h4>';
+		make_filter($receivables,'domain_id',$markets,'From','All Markets',90,130);
+		$receivables->filter_html .= '<br />';
+	}
+	
+	$receivables->filter_html .= '<h4>Payment Filters</h4>';
+	make_filter($receivables,'lbps_id',array(
+			'1'=>'Unpaid',
+			'2'=>'Paid',
+			'4'=>'Partially Paid',
+			'5'=>'Refunded',
+			'6'=>'Manual Review',
+	),'Buyer Payment','All',96,130);
+	make_filter($receivables,'lsps_id',array(
+			'1'=>'Unpaid',
+			'2'=>'Paid',
+			'3'=>'Partially Paid',
+	),'Seller Payment','All',96,130);
+	
+
+	$receivables->filter_html .= '</div>';
+	$receivables->filter_html .= '</div>';
+}
 
 ?>
 <div class="tab-pane tabarea" id="paymentstabs-a<?=($core->view[0]+1)?>">
