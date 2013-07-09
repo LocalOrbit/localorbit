4ff14da63270b1200d9e381313e5753264ff5457
Mike Ratanatharathorn <mike@localorb.it>
2013-07-09 18:09:47 +0000

    fixing js output
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/trunk@2329 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- www/app/controllers/catalog/views/payment_paypal_express_popup.php
+++ www/app/controllers/catalog/views/payment_paypal_express_popup.php
@@ -1,17 +1,22 @@
 <?php
 	// http://devz01.localorb.it/app.php#!catalog-payment_paypal_express_popup
 	global $core;
+	core::log('trying to load');
 
 	try {
 		$cart = $this->update_fees('yes');
 		
 		include(__DIR__.'/../../../../../bin/paypal/PayPalApi.php');
 		$paypalUrl = $payPalApi->getExpressCheckoutRedirect($cart);
+		ob_end_clean();
+		echo('<script>location.href="'.$paypalUrl.'";</script>');
+		exit();
 		//core::log('window.open("'.$paypalUrl.'");');
 		//core::js('window.open(\''.$paypalUrl.'\',\'123654786441\',\'width=960,height=800,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=1,left=0,top=0\');');
 		//core::js('document.location="'.$paypalUrl.'";');
 		//core::deinit(); 
 	} catch (Exception $e) {
-		echo "<strong>Error with Paypal</strong><br />".$e->getMessage();
+		core::log('paypal error: '.$e->getMessage());
+	
 	}
 ?>
