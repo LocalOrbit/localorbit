c350c02157fbac97d0b19fc52481364c0ae81536
Mike Ratanatharathorn <mike@localorb.it>
2013-07-12 00:03:23 +0000

    fixing window closing
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/trunk@2351 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- www/app/controllers/catalog/views/payment_paypal_express_popup_return.php
+++ www/app/controllers/catalog/views/payment_paypal_express_popup_return.php
@@ -2,21 +2,24 @@
 include($_SERVER['DOCUMENT_ROOT'].'/../bin/paypal/PayPalApi.php');	
 
 	
-	try {
-		$transactionId = $payPalApi->confirmTransaction();		
-		$core->session['paypal_popup_transaction_id'] = $transactionId;
-		core::js('window.opener.$("#payment_method_paypal_popup").attr("checked",true);window.opener.core.checkout.process();window.close();');
-		core::deinit();
-	} catch (Exception $e) {
-		core::log($e->getMessage());
-		echo "<script>";
-			echo "window.opener.core.ui.popup('','','<strong>Error with Paypal</strong><br />".$e->getMessage()."','close');";
-			echo "window.close();";
-		echo "</script>";
-	}
+try {
+	$transactionId = $payPalApi->confirmTransaction();		
+	$core->session['paypal_popup_transaction_id'] = $transactionId;
+	?>
 	
-	// dont send back json
-	ob_end_flush();
+	<script language="Javascript">
+		window.opener.$("#payment_method_paypal_popup").attr("checked",true);
+		window.opener.core.checkout.process();
+		window.close();
+	</script>
+	<?
 	exit();
+} catch (Exception $e) {
+	core::log($e->getMessage());
+	echo "<script>";
+		echo "window.opener.core.ui.popup('','','<strong>Error with Paypal</strong><br />".$e->getMessage()."','close');";
+		echo "window.close();";
+	echo "</script>";
+}
 ?>
 
