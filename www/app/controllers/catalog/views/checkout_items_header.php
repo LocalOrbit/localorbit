03e3460ad0eb62a34374a212ca0fc8d1db4b8c46
Mike Ratanatharathorn <mike@localorb.it>
2013-07-18 13:15:02 +0000

    fixing all delivery issues. EVER.
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/trunk@2478 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- www/app/controllers/catalog/views/checkout_items_header.php
+++ www/app/controllers/catalog/views/checkout_items_header.php
@@ -6,11 +6,16 @@
 			$all_addrs = $core->view[1];
 			$count = $core->view[2];
 			$total_delivs = $core->view[3];
-			$deliv = core::model('lo_order_deliveries')->load($lodeliv_id);
+			$deliv = core::model('lo_order_deliveries')
+				->autojoin('left','delivery_days','(lo_order_deliveries.dd_id=delivery_days.dd_id)',array(
+					'delivery_days.deliv_address_id as orig_deliv_address_id',
+					'delivery_days.pickup_address_id as orig_pickup_address_id'
+				))
+				->load($lodeliv_id);
 			
 			# this determines if the buyer picks up the items from a market location
 			
-			if(intval($deliv['pickup_address_id']) > 0 && !isset($all_addrs[$deliv['pickup_address_id']]))
+			if(intval($deliv['orig_pickup_address_id']) > 0)
 			{
 			#	echo('need to get a list of pikcup addresses');
 				$verb = 'Pickup';
@@ -29,10 +34,7 @@
 			else
 			{
 				$verb = 'Delivery';
-				#echo('need to get a list of delivery addresses');
 			}
-			#print_r($deliv->__data);
-			# if there's only one delivery, render it this way:
 			if($total_delivs == 1)
 			{
 				?>
@@ -59,13 +61,22 @@
 			//choose address
 			if($verb == 'Delivery')
 			{
+				$prefix = (intval($deliv['orig_deliv_address_id']) == 0)?'deliv_':'pickup_';
 				
+				$final_address = 0;
 				if(count($all_addrs) > 1)
 				{
-					echo '<select name="delivgroup-'.$deliv['dd_id_group'].'" style="margin-top:8px;width:390px;">';
+					echo '<select name="delivgroup-'.$deliv['dd_id_group'].'" style="margin-top:8px;width:390px;" onchange="core.checkout.updateDelivery('.$deliv['lo_oid'].','.$deliv['dd_id'].',this.options[this.selectedIndex].value,\''.$prefix.'\');">';
 					foreach($all_addrs as $address_id=>$address)
 					{
-						echo '<option value="'. $address[0]['address_id'].'">'.$address[0]['formatted_address'].'</option>';
+						if($final_address == 0)
+							$final_address = $address[0];
+						echo '<option value="'. $address[0]['address_id'].'"';
+						
+						if($deliv[$prefix.'address_id'] == $address_id)
+							echo(' selected="selected"');
+						
+						echo '>'.$address[0]['formatted_address'].'</option>';
 					}
 					echo '</select>';
 				}
@@ -73,10 +84,29 @@
 				{
 					foreach($all_addrs as $address_id=>$address)
 					{
+						if($final_address == 0)
+							$final_address = $address[0];
 						echo '<input name="delivgroup-'.$deliv['dd_id_group'].'" type="hidden" value="'.$address_id.'" />';
 						echo $address[0]['formatted_address'];
 					}
-				}				
+				}
+				
+				# if the pickup address hasn't actually been saved to the db, do it now.
+				if(intval($deliv[$prefix.'address_id']) == 0)
+				{
+					core::log('saving address now!! '.print_r($deliv->__data,true));
+					$deliv[$prefix.'address_id'] = $final_address['address_id'];
+					$deliv[$prefix.'org_id'] = $core->session['org_id'];
+					$deliv[$prefix.'address'] = $final_address['address'];
+					$deliv[$prefix.'city'] = $final_address['city'];
+					$deliv[$prefix.'region_id'] = $final_address['region_id'];
+					$deliv[$prefix.'postal_code'] = $final_address['postal_code'];
+					$deliv[$prefix.'telephone'] = $final_address['telephone'];
+					$deliv[$prefix.'fax'] = $final_address['fax'];
+					$deliv[$prefix.'longitude'] = $final_address['longitude'];
+					$deliv[$prefix.'latitude'] = $final_address['latitude'];
+					$deliv->save();
+				}		
 			}
 			else
 			{
