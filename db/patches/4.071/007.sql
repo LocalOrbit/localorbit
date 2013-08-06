e4a28a2d78f0ea2e8e39288f2ab023b6d6824a95
John Vavul <jonn@localorb.it>
2013-08-06 16:03:54 +0000

    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/trunk@2592 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- db/patches/4.071/007.sql
+++ db/patches/4.071/007.sql
@@ -1,13 +1,21 @@
 INSERT INTO migrations (tag, version_id, pt_ticket_no) VALUES ('4.071', '007', '51524895');
 
+
+
 UPDATE phrases
-SET default_value = '<table width=\"100%\"> 		<colgroup><col width=\"1%\"> 		<col width=\"1%\"> 		<col width=\"98%\"> 		</colgroup><tbody><tr> 			<td>{logo}</td> 			<td>&nbsp;&nbsp;</td> 			<td> 				<h2><a href="http://{hostname}/app.php#!orders-view_order--lo_oid-{lo_oid}">LO #: {order_nbr}</a></h2> 			</td> 		</tr> 	</tbody></table> 	<br>&nbsp;<br> 	You\'ve received a new order!<br>You can check the details of this and all your current orders by logging into your {hubname} account. <br>&nbsp;<br>	 	{items}  	<br><br>If you have any questions please respond to this email.<br>'
+SET default_value = '<span>{logo}&nbsp;&nbsp;<span><h2>Order #: <a href="http://{hostname}/app.php#!orders-view_order--lo_oid-{lo_oid}">LO #: {order_nbr}</a></h2></span></span>
+<br>&nbsp;<br>
+You\'ve received a new order!<br>You can check the details of this and all of your current orders by logging into your {hubname} account. 
+<br>&nbsp;<br>
+{items}<br><br>If you have any questions please respond to this email.<br>Thank you!<br>'
 WHERE label ='email:order_mm_notification';
+
+
 
 
 UPDATE phrases
 SET default_value = '<span>{logo}&nbsp;&nbsp;<span><h2>Order #: <a href="http://{hostname}/app.php#!orders-view_sales_order--lo_foid-{lo_foid}">LO #: {order_nbr}</a></h2></span></span>
-<br>&nbsp;<br>&nbsp;<br>
+<br>&nbsp;<br>
 You\'ve received a new order! <br>You can check the details of this and all of your current orders by logging into your {hubname} account. 
 <br>&nbsp;<br>
 {items}<br><br>If you have any questions please respond to this email. <br>Thank you!<br>'
