ceaa736a161c267029fbe404c810d36932734269
Mike Ratanatharathorn <mike@localorb.it>
2013-08-08 17:07:45 +0000

    54494112
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/trunk@2660 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- db/patches/4.075/001.sql
+++ db/patches/4.075/001.sql
@@ -2,4 +2,4 @@ INSERT INTO migrations (tag, version_id, pt_ticket_no) VALUES ('4.075', '001', '
 
 delete from phrases where label='note:inventory:optional';
 
-insert into phrases (pcat_id,label,edit_type,default_value) values (5,'note:inventory:optional','textarea','update this guys');
+insert into phrases (pcat_id,label,edit_type,default_value) values (5,'note:inventory:optional','textarea','If you use Lot Numbers or another identifier, you can enter whatever number works with your system. If your products have Good from and Expiration dates, you can include them here as well.');
