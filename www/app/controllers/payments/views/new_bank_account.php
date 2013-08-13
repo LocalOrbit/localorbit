e24fe53e2a272ceae09e36cbf2315613093b0873
Mike Ratanatharathorn <mike@localorb.it>
2013-08-13 20:35:33 +0000

    55044320
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/trunk@2702 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- www/app/controllers/payments/views/new_bank_account.php
+++ www/app/controllers/payments/views/new_bank_account.php
@@ -2,13 +2,16 @@
 global $core;
 core_ui::load_library('js','org.js');
 ?>
-<form id="organizationsForm" name="organizationsForm" class="form-horizontal" onsubmit="return core.submit('/organizations/save_payment_method',this);" action="/organizations/save_payment_method" >
+<form id="organizationsForm" name="organizationsForm" class="form-horizontal" onsubmit="return core.submit('/organizations/save_payment_method',this);" action="/organizations/save_payment_method">
 	<fieldset id="editInv">
 		<legend>Bank Account Info</legend>
 		<?=core_form::input_text('Account Nickname','pm_label')?>
 		<?=core_form::input_text('Name on Account','name_on_account')?>
 		<?=core_form::input_text('Account #','nbr1','',array('onfocus'=>"if(new String(this.value).indexOf('*')===0){this.value='';}"))?>
 		<?=core_form::input_text('Routing #','nbr2','',array('onfocus'=>"if(new String(this.value).indexOf('*')===0){this.value='';}"))?>
+		<input type="hidden" name="from_financials" value="yes" />
+		<input type="hidden" name="tab" value="<?=$core->data['tab']?>" />
+		<input type="hidden" name="group_key" value="<?=$core->data['group_key']?>" />
 		<input type="hidden" name="from_financials" value="yes" />
 		<input type="hidden" name="org_id" value="<?=$core->session['org_id']?>" />
 		<div class="form-actions pull-right">
