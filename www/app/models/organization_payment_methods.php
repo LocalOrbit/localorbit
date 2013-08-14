3700f92a7c54e6cd8e293efdece781bff7a3e827
Mike Ratanatharathorn <mike@localorb.it>
2013-08-14 17:30:18 +0000

    fixing ach transactioncode
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/trunk@2718 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- www/app/models/organization_payment_methods.php
+++ www/app/models/organization_payment_methods.php
@@ -52,17 +52,7 @@ class core_model_organization_payment_methods extends core_model_base_organizati
 		$transaction->CustomerRoutingNo  = core_crypto::decrypt($this['nbr2']);
 		$transaction->CustomerAcctNo     = core_crypto::decrypt($this['nbr1']);
 		$transaction->TransAmount   = $amount;
-		
-		if($CustTransType == 'D')
-		{
-			# if this is a debit, we can use the WEB trans code
-			$transaction->TransactionCode = 'WEB';
-		}
-		else
-		{
-			# if it's a credit ,we need to use either PPD or CCD
-			$transaction->TransactionCode = 'CCD';
-		}
+		$transaction->TransactionCode = 'CCD';
 		
 		$transaction->CustomerAcctType = 'C';
 		$transaction->OriginatorName  = $core->config['ach']['Company'];
