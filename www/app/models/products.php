9b52e9c2bdec564d7419f8392669d70688c57cc9
Mike Ratanatharathorn <mike@localorb.it>
2013-09-18 02:30:02 +0000

    55231900
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/trunk@2871 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- www/app/models/products.php
+++ www/app/models/products.php
@@ -111,7 +111,7 @@ class core_model_products extends core_model_base_products
 		return $col;
 	}
 
-	function get_catalog($domain_id=null,$org_id=-1,$check_inventory=true)
+	function get_catalog($domain_id=null,$org_id=-1,$check_inventory=true,$seller_id=null)
 	{
 		global $core;
 
@@ -137,7 +137,13 @@ class core_model_products extends core_model_base_products
 				and (product_prices.org_id = 0 or product_prices.org_id='.$org_id.')
 				and (product_prices.domain_id = 0 or product_prices.domain_id='.$domain_id.')
 			) as price_ids,
-			(select group_concat(dd_id) from product_delivery_cross_sells where product_delivery_cross_sells.prod_id=p.prod_id) as dd_ids,
+			(
+				select group_concat(product_delivery_cross_sells.dd_id) 
+				from product_delivery_cross_sells 
+				inner join delivery_days dd1 on (product_delivery_cross_sells.dd_id=dd1.dd_id)
+				where product_delivery_cross_sells.prod_id=p.prod_id 
+				and dd1.domain_id='.$domain_id.'
+			) as dd_ids,
 			(select sum(qty) from product_inventory inv where inv.prod_id=p.prod_id and (expires_on > now() or expires_on is null) and (good_from <= now() or good_from is null)) as inventory,
 			a.address,a.city,a.postal_code,dcr.code,a.latitude,a.longitude
 			from products p
@@ -176,6 +182,13 @@ class core_model_products extends core_model_base_products
 			';
 		}
 		
+		if(!is_null($seller_id))
+		{
+			$sql .= '
+				and o.org_id='.$seller_id.'
+			';
+		}
+		
 		$sql .= '
 			and p.unit_id is not null
 			and p.unit_id <> 0
@@ -210,6 +223,293 @@ class core_model_products extends core_model_base_products
 		$col->sort('p.category_ids');
 		$col->group('p.prod_id');
 		return $col;
+	}
+	
+	function get_final_catalog($domain_id=null,$seller_id=null,$prod_id=null)
+	{
+		global $core;
+		
+		# setup the structure that's going to be returned at the end of the function
+		$final = array(
+			'products'=>array(),
+			'deliveries'=>array(),
+			'sellers'=>array(),
+			'categories'=>array(),
+			'days'=>array(),
+			'addresses'=>array(),
+		);
+		
+		# setup some temp arrays 
+		$tmp_deliveries = array();
+		$tmp_addresses  = array(0); 
+		$tmp_sellers    = array();
+		$tmp_prices     = array();
+		$tmp_categories = array();
+		
+		# run the main query for all the products
+		$catalog = $this->get_catalog($domain_id,null,false,$seller_id)->load();
+		
+		# if no products were found, stop processing right here.
+		if($catalog->__num_rows == 0)
+		{
+			return $final;
+		}
+		
+		# next, extract the unique delivery days
+		# then, calculate the next time.
+		# 
+		# we need this in order to correctly check inventory
+		# also, get the list of unique address_ids used by the set of 
+		# delivery days.
+		$dd_ids    = $catalog->get_unique_values('dd_ids',true,true);
+		$deliveries    = core::model('delivery_days')
+			->collection()
+			->filter('delivery_days.dd_id','in',$dd_ids)
+			->filter('domain_id','=',$core->config['domain']['domain_id']);
+
+		foreach ($deliveries as $delivery)
+		{	
+			# this function calculates the final date/times for the delivery
+			$delivery->next_time();
+			
+			# add the addresses used by this delivery to our list of 
+			# all addresses
+			if($delivery['deliv_address_id'] != 0)
+				$tmp_addresses[] = $delivery['deliv_address_id'];
+			if($value['pickup_address_id'] != 0)
+				$tmp_addresses[] = $delivery['pickup_address_id'];
+	
+			$tmp_deliveries[$delivery['dd_id']] = array($delivery->__data);
+			
+			# we'll use this flag to only return delivs
+			# for which there is inventory in the date range. will be 
+			# flipped to true later.
+			$tmp_deliveries[$delivery['dd_id']]['has_products'] = false;
+		}
+		
+		# now that we have the final delivery times, we can calculate
+		# the actual inventory.
+		# load up the inventory for all the products.
+		$prod_ids  = $catalog->get_unique_values('prod_id');
+		$inventory = core::model('product_inventory')->collection()->filter('prod_id','in',$prod_ids)->to_hash('prod_id');
+		
+		# in order to properly sort, we'll need the text for each category
+		$cat_ids   = $catalog->get_unique_values('category_ids',true,true);
+		$cats  = core::model('categories')->load_for_products($cat_ids);
+		
+		$prods = $catalog->to_array();
+		for ($i = 0; $i < count($prods); $i++)
+		{
+			# get the categories for this product.
+			$prods[$i]['cat_list'] = explode(',',$prods[$i]['category_ids']);
+			# the first category is the catalog root, so just remove it
+			array_shift($prods[$i]['cat_list']);
+			
+			# create a new property called sort_col, then append on the text version
+			# of the first two categories
+			$prods[$i]['sort_col'] = '';
+			$prods[$i]['sort_col'] .= $cats->by_id[$prods[$i]['cat_list'][0]][0]['order_by'].'-';
+			$prods[$i]['sort_col'] .= $cats->by_id[$prods[$i]['cat_list'][0]][0]['cat_name'].'-';
+			$prods[$i]['sort_col'] .= $cats->by_id[$prods[$i]['cat_list'][1]][0]['cat_name'].'-';
+			$prods[$i]['sort_col'] .= $cats->by_id[$prods[$i]['cat_list'][2]][0]['cat_name'];
+			$prods[$i]['sort_col'] .= '-'.$prods[$i]['name'];
+			# lowercase the sort_col just to make sure we're comparing in a way that will
+			# make sense to the user
+			$prods[$i]['sort_col'] = strtolower($prods[$i]['sort_col']);
+			
+			# check the each delivery to make sure it's actually valid.
+			$prod_dds = array_unique(explode(',',$prods[$i]['dd_ids']));
+			
+			
+			
+			# setup an array per dd_id to store how much inventory is available 
+			# on this delivery
+			$prods[$i]['inventory_by_dd'] = array();
+			foreach($prod_dds as $dd_id)
+			{
+				$prods[$i]['inventory_by_dd'][$dd_id] = 0;
+			}
+			$valid_prod_dds = array();
+			
+			for($j=0;$j<count($inventory[$prods[$i]['prod_id']]);$j++)
+			{
+				# first, determine the date range that this lot is good for.
+				
+				# if this inventory lot has an good from, then calculate
+				# that date. if not, assume super far into the past. 
+				if(trim($inventory[$prods[$i]['prod_id']][$j]['good_from']) !='')
+				{
+					$good_from = core_format::parse_date($inventory[$prods[$i]['prod_id']][$j]['good_from'],'timestamp') - intval($core->session['time_offset']);
+				}
+				else
+				{
+					$good_from = 0;
+				}
+				
+				# if this inventory lot has an expires on, then calculate
+				# that date. if not, assume super far into the future. 
+				if(trim($inventory[$prods[$i]['prod_id']][$j]['expires_on']) !='')
+				{
+					$expires_on = core_format::parse_date($inventory[$prods[$i]['prod_id']][$j]['expires_on'],'timestamp') + 86400 - 1 - intval($core->session['time_offset']);
+				}
+				else
+				{
+					$expires_on = 99999999999999999;
+				}
+				
+				# now, loop through all of the deliveries and see if 
+				# that delivery is valid for the current inventory lot
+				for( $k=0; $k<count($prod_dds); $k++ )
+				{
+					# only check if the delivery is valid for this market
+					# since a product may have other deliveries on cross-selling
+					# markets.
+					#print_r($inventory[$prods[$i]['prod_id']][$j]);
+					#echo('checking deliv: '.$prod_dds[$k]."\n\n");
+					#print_r($tmp_deliveries[$prod_dds[$k]][0]);
+					#echo('good from: '.$good_from."\n\n");
+					#echo('expires on: '.$expires_on."\n\n");
+					if(isset($tmp_deliveries[$prod_dds[$k]]))
+					{
+						
+						# check all 3 conditions!
+						if(
+							$tmp_deliveries[$prod_dds[$k]][0]['delivery_end_time'] > $good_from
+							and
+							$tmp_deliveries[$prod_dds[$k]][0]['delivery_end_time'] < $expires_on
+							and
+							$inventory[$prods[$i]['prod_id']][$j]['qty'] > 0
+							
+						)
+						{
+							# if it matched, add this dd to the list of valid 
+							# deliveries for the product.
+							$valid_prod_dds[] = $prod_dds[$k];
+							$prods[$i]['inventory_by_dd'][$prod_dds[$k]] += $inventory[$prods[$i]['prod_id']][$j]['qty'];
+							$tmp_deliveries[$prod_dds[$k]]['has_products'] = true;
+						}
+					}
+				}
+			}
+			
+			#print_r($prods[$i]);
+			
+			# we now know which DDs have inventory for them. If the product
+			# has a valid dd, add this product to the final list of products 
+			# to return
+			if(count($valid_prod_dds) > 0)
+			{
+				$prods[$i]['deliveries'] = $valid_prod_dds;
+				$final['products'][] = $prods[$i];
+				$tmp_sellers[] = $prods[$i]['org_id'];
+				$tmp_prices = array_merge($tmp_prices,explode(',',$prods[$i]['price_ids']));
+				
+				if(is_numeric($prods[$i]['cat_list'][0]))
+					$tmp_categories[] = $prods[$i]['cat_list'][0];
+				if(is_numeric($prods[$i]['cat_list'][1]))
+					$tmp_categories[] = $prods[$i]['cat_list'][1];
+				if(is_numeric($prods[$i]['cat_list'][2]))
+					$tmp_categories[] = $prods[$i]['cat_list'][2];
+			}
+		}
+		
+		# we now have a final list of valid sellers. Query for their profiles
+		# and add to the final array.
+		if(count($tmp_sellers) > 0)
+		{
+			$final['sellers'] = core::model('organizations')
+				->collection()
+				->filter('organizations.org_id','in',array_unique($tmp_sellers))
+				->sort('name')
+				->to_hash('org_id');
+			
+			# now that we have the list of sellers,
+			# loop through them and find all their profile images
+			$org_model = core::model('organizations');
+			foreach($final['sellers'] as $key=>$seller)
+			{
+				 
+				list(
+					$final['sellers'][$key][0]['has_image'],
+					$final['sellers'][$key][0]['img_webpath'],
+					$final['sellers'][$key][0]['img_filepath']
+				) = $org_model->get_image($final['sellers'][$key][0]['org_id']);
+			}
+		}
+		
+		# get the very final list of categories
+		$final['categories'] = core::model('categories')->load_for_products(array_unique($tmp_categories));
+		
+		# we now have a final list of valid deliveries. copy
+		# their info into the final array.
+		foreach($tmp_deliveries as $tmp_delivery)
+		{
+			if($tmp_delivery['has_products'])
+			{
+				$final['deliveries'][] = $tmp_delivery;
+			}
+		}
+		
+		# get the final list of prices
+		$final['prices'] = core::model('product_prices')->get_valid_prices($tmp_prices, $core->config['domain']['domain_id'],$core->session['org_id']);
+		
+		# define a sort function that sorts by our defined sort 
+		# column created in this function
+		function final_prod_sort($a,$b)
+		{
+			$aArray = explode('-', $a['sort_col']);
+			$bArray = explode('-', $b['sort_col']);
+			
+			// compare orderby field
+			if ($aArray[0] == $bArray[0])
+			{
+				return strcmp($aArray[1]."-".$aArray[2]."-".$aArray[3], $bArray[1]."-".$bArray[2]."-".$bArray[3]);
+			}
+			else
+			{
+				return $aArray[0] > $bArray[0];
+			}
+			//return strcmp($a['sort_col'], $b['sort_col']);
+		}
+		
+		# apply the sort function to the final product list
+		usort($final['products'],'final_prod_sort');
+		
+		# prepare a much nicer formatted and sorted
+		# list of the delivery days, organized by the address
+		# combination
+		foreach($final['deliveries'] as $delivery)
+		{
+			$time = ((($delivery[0]['pickup_address_id'] == 0 || $delivery[0]['deliv_address_id']==0) ? 'Delivered' : 'Pick Up') . '-' . (($delivery[0]['deliv_address_id'] == 0)? $delivery[0]['delivery_end_time'] : $delivery[0]['pickup_end_time']));
+			
+			$time .= '-'.$delivery[0]['deliv_address_id'];
+			$time .= '-'.$delivery[0]['pickup_address_id'];
+
+			if (!array_key_exists($time, $final['days']))
+			{
+				$final['days'][$time] = array();
+			}
+			foreach ($delivery as $value)
+			{
+				$final['days'][$time][$delivery[0]['dd_id']] = $delivery;
+			}
+		}
+		
+		# define a sort function for the delivery options
+		function final_day_sort($a,$b)
+		{
+			list($type, $atime) = explode('-', $a);
+			list($type, $btime) = explode('-', $b);
+			return intval($atime) - intval($btime);
+		}
+		
+		# apply the sort function
+		uksort($final['days'],'final_day_sort');
+		
+		# get a collection of all the addresses used by the deliveries;
+		$final['addresses'] = core::model('addresses')->add_formatter('simple_formatter')->collection()->filter('address_id','in',array_unique($tmp_addresses))->to_hash('address_id');
+		
+		return $final;
 	}
 
 	function get_catalog_for_seller($org_id)
