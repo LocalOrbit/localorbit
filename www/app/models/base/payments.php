f2618f66d39aff79b7607d1aee7e423049959b2e
Mike Ratanatharathorn <mike@localorb.it>
2013-09-23 22:11:17 +0000

    55232730
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/trunk@2941 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- www/app/models/base/payments.php
+++ www/app/models/base/payments.php
@@ -9,6 +9,7 @@ class core_model_base_payments extends core_model
 		$this->add_field(new core_model_field(3,'admin_note','string',8000,'','payments'));
 		$this->add_field(new core_model_field(4,'ref_nbr','string',-4,'','payments'));
 		$this->add_field(new core_model_field(5,'creation_date','int',8,'','payments'));
+		$this->add_field(new core_model_field(6,'processing_status','string',-4,'','payments'));
 		$this->init_data();
 	}
 }
