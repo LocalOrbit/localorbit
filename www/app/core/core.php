473dcf197c5c055d77c2257858e0ce14fc490270
Mike Ratanatharathorn <mike@localorb.it>
2013-10-01 21:22:13 +0000

    55231900
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/trunk@2970 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- www/app/core/core.php
+++ www/app/core/core.php
@@ -635,7 +635,7 @@ class core
 		array_shift($p);
 		
 		$controller = core::controller($controller);
-		$data = $controller->$method($p[0],$p[1],$p[2],$p[3],$p[4],$p[5],$p[6],$p[7],$p[8],$p[9]);
+		$data = $controller->$method($p[0],$p[1],$p[2],$p[3],$p[4],$p[5],$p[6],$p[7],$p[8],$p[9],$p[10],$p[11]);
 		
 		if(!$return_data)
 		{
