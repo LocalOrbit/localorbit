ba41c38272eb1141f128d62b6b06d80045f88dfd
Mike Ratanatharathorn <mike@localorb.it>
2013-10-31 14:49:41 +0000

    revising code to use as example
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/trunk@3129 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- www/app/controllers/units/views/edit.php
+++ www/app/controllers/units/views/edit.php
@@ -4,6 +4,7 @@ core::ensure_navstate(array('left'=>'left_dashboard'), 'units-list','products-de
 core::head('Edit Unit','This page is used to edit product units');
 lo3::require_permission();
 lo3::require_login();
+core_ui::fullWidth();
 
 # only admins can edit units
 lo3::require_orgtype('admin');
@@ -15,18 +16,16 @@ $data = (is_numeric($core->data['UNIT_ID']))?core::model('Unit')->load():array()
 $this->save_rules()->js();
 
 # write out the form
-echo(
-	core_form::page_header('Editing '.$data['SINGLE'],'#!units-list','cancel').
-	core_form::form('unitsForm','/units/update','',
-		core_form::tab_switchers('unittabs',array('Unit')),
-		core_form::tab('unittabs',
-			core_form::table_nv(
-				core_form::input_text('Single','NAME',$data,array('required'=>true)),
-				core_form::input_text('Plural','PLURAL',$data,array('required'=>true))
-			)
-		),
-		core_form::input_hidden('UNIT_ID',$data),
-		core_form::save_buttons()
-	)
-);
+page_header('Editing '.$data['NAME'],'#!units-list','cancel','cancel');
 ?>
+<form name="unitsForm" class="form-horizontal" action="/units/update"
+ method="post" id="unitsForm"
+ onsubmit="return core.submit('/units/update',this);" enctype="multipart/form-data">
+	<?=core_form::tab_switchers('unittabs',array('Unit'))?>
+	<div class="tab-pane tabarea " id="unittabs-a1">
+		<?=core_form::input_text('Single','NAME',$data,array('required'=>true))?>
+		<?=core_form::input_text('Plural','PLURAL',$data,array('required'=>true))?>
+		<?=core_form::input_hidden('UNIT_ID',$data)?>
+	</div>
+	<?=core_form::save_buttons()?>
+</form>
