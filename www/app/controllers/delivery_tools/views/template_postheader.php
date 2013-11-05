3adf94db948c25e9edce19ff9619d12d467744ab
Mike Ratanatharathorn <mike@localorb.it>
2013-11-05 22:36:59 +0000

    59141162
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/trunk@3144 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- www/app/controllers/delivery_tools/views/template_postheader.php
+++ www/app/controllers/delivery_tools/views/template_postheader.php
@@ -2,6 +2,10 @@
 $org = $core->view[0];
 $do_buttons = ($core->view[1] === true);
 $addr_seller = $core->view[2];
+$override_address = $core->view[3];
+
+$final_address = (is_array($override_address))?$override_address:$org;
+
 #$org->dump();
 ?>				
 <div class="span4">
@@ -13,10 +17,10 @@ $addr_seller = $core->view[2];
 					<strong><?=$core->i18n['deliverytools:buyerlabel']?><br /><?=$org['name']?></strong>
 					<?}?>
 					<br/>
-					<?=$org['address']?><br />
-					<?=$org['city']?>, <?=$org['code']?> <?=$org['postal_code']?><br />
-					<?if($org['telephone'] != ''){?>
-					T: <?=$org['telephone']?><br />
+					<?=$final_address['address']?><br />
+					<?=$final_address['city']?>, <?=$final_address['code']?> <?=$final_address['postal_code']?><br />
+					<?if($final_address['telephone'] != ''){?>
+					T: <?=$final_address['telephone']?><br />
 					</address>
 					<?}?>
 					<? if($do_buttons){?>
