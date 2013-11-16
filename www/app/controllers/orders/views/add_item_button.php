16267430aae177eb088a6dafddd9a189c311c0f8
Mike Ratanatharathorn <mike@localorb.it>
2013-11-16 02:51:22 +0000

    fixing issues with adding items to order
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/trunk@3204 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- www/app/controllers/orders/views/add_item_button.php
+++ www/app/controllers/orders/views/add_item_button.php
@@ -4,6 +4,7 @@ if(lo3::is_market() || lo3::is_admin())
 	$lo_oid = $core->view[0];
 	$dd_id  = $core->view[1];
 	$start  = $core->view[2];
+	$order  = $core->view[3];
 	
 	$delivery = core::model('delivery_days')->load($dd_id);
 	$cutoff = $start - ($delivery['hours_due_before'] * 3600);
@@ -14,7 +15,13 @@ if(lo3::is_market() || lo3::is_admin())
 	<div class="text-error"><strong>Past delivery cutoff:</strong> Because the cutoff time has already passed for this delivery, you can no longer add items.</div>
 <?php
 	}
-	else
+	else if($order['payment_method'] == 'purchaseorder' && $order['lbps_id'] > 1)
+	{
+?>
+	<div class="text-error"><strong>Buyer has already paid:</strong> Because the buyer has already paid for this order, you can no longer add items.</div>		
+<?php
+	}
+	else if($order['payment_method'] == 'purchaseorder')
 	{
 ?>
 <div class="form-actions pull-right" style="margin-top: 0px;" id="new_item_button_dd_id_<?=$dd_id?>">
