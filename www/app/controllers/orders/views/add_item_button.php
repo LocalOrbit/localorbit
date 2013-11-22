85f1ba90d2d1b9d9bc5bbd8408c623b96aa3c558
Mike Ratanatharathorn <mike@localorb.it>
2013-11-22 23:06:52 +0000

    59593186
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/trunk@3228 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- www/app/controllers/orders/views/add_item_button.php
+++ www/app/controllers/orders/views/add_item_button.php
@@ -25,7 +25,7 @@ if(lo3::is_market() || lo3::is_admin())
 	{
 ?>
 <div class="form-actions pull-right" style="margin-top: 0px;" id="new_item_button_dd_id_<?=$dd_id?>">
-	<button class="btn btn-primary" onclick="core.checkout.addItemToOrder(<?=$lo_oid?>,<?=$dd_id?>);"><i class="icon icon-plus"></i>Add Item to Delivery</button>
+	<button class="btn btn-primary" onclick="core.checkout.addItemToOrder(<?=$lo_oid?>,<?=$dd_id?>);"><i class="icon icon-plus"></i>Edit/Add items</button>
 </div>
 <div style="clear:both;">&nbsp;</div>
 <div id="new_item_dd_id_<?=$dd_id?>" style="display: none;"><img src="<?=image('loading-progress')?>" /></div>
