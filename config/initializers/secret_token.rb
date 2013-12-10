3b688f8b13e1dcd7f92c39eee33cd722ea017ffb
David Genord II <david@collectiveidea.com>
2013-12-10 10:59:41 -0500

    Stock Rails 4.0.2
    Generated via:
    rails new local_orbit -d postgresql -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LocalOrbit::Application.config.secret_key_base = '4edd1c93887a120e04b118d93dc0dc0a79def610c8f3b04669891b7ce8b0c1cafbc0a3c5d6651f108e15257e1dc3fd689d049a4e23c648168ac432c3f03b9fe6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LocalOrbit::Application.config.session_store :cookie_store, key: '_local_orbit_session'
