0057ee62cf8bbd293af6a2688e9208390d5c3589
Chris Rittersdorf <chris.rittersdorf@collectiveidea.com>
2013-12-20 16:11:22 -0500

    Adds accounts with temporary passwords.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,49 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+#
+
+
+lo_market = Market.where(subdomain:"springfield").first || Market.create(
+  name:"Springfield Market",
+  subdomain:"springfield"
+)
+
+erika = User.create!(
+  email: "erika@localorb.it",
+  password: "KKBz2yVKg2F",
+  password_confirmation: "KKBz2yVKg2F",
+  role: "admin",
+  name: "Erika Block"
+)
+
+anna = User.create!(
+  email: "anna@localorb.it",
+  password: "Cau7t3vwW",
+  password_confirmation: "Cau7t3vwW",
+  role: "admin",
+  name: "Anna Richardson"
+)
+
+ragan = User.create!(
+  email: "ragan@localorb.it",
+  password: "T8gKxCCc6k",
+  password_confirmation: "T8gKxCCc6k",
+  role: "admin",
+  name: "Ragan Erickson"
+)
+
+kate = User.create!(
+  email: "kate@localorb.it",
+  password: "FH6sLbw2r",
+  password_confirmation: "FH6sLbw2r",
+  role: "admin",
+  name: "Kate Barker"
+)
+
+
+[erika, anna, ragan, kate].each do |u|
+  u.managed_markets << lo_market
+  u.save!
+end
