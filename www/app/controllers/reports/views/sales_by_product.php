309be9df1d73343b0f87e1741b7c0bec2faa4368
Mike Ratanatharathorn <mike@localorb.it>
2013-12-28 02:05:43 +0000

    51747709
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/trunk@3344 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- www/app/controllers/reports/views/sales_by_product.php
+++ www/app/controllers/reports/views/sales_by_product.php
@@ -145,6 +145,9 @@ $items->add_filter(new core_datatable_filter('sbpcreatedat2','lo_fulfillment_ord
 $items->filter_html .= core_datatable_filter::make_date('sales_by_product','sbpcreatedat1',core_format::date($start,'short'),'Placed on or after ');
 $items->filter_html .= core_datatable_filter::make_date('sales_by_product','sbpcreatedat2',core_format::date($end,'short'),'Placed on or before ');
 
+$items->add_filter(new core_datatable_filter('searchables','concat_ws(\' \',lo_order.lo3_order_nbr,lo_fulfillment_order.lo3_order_nbr)','~','search'));
+$items->filter_html .= core_datatable_filter::make_text('sales_by_product','searchables',$items->filter_states['sales_by_product__filter__searchables'],'Search');
+
 # add MM-only filters
 if(lo3::is_market())
 {
