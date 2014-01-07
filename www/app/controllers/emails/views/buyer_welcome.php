919ff11c0867d754de038707d25fe03010374785
Daniel Morrison <daniel@collectiveidea.com>
2014-01-07 21:35:53 +0000

    Update Buyers Welcome email to new style
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/trunk@3369 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- www/app/controllers/emails/views/buyer_welcome.php
+++ www/app/controllers/emails/views/buyer_welcome.php
@@ -11,8 +11,17 @@ core::log('tryign to send email from domain '.$values['domain_id']);
 $values['hub_name'] = core_db::col('select name from domains where domain_id='.$values['domain_id'],'name');
 core::log('domain name is '.$values['hub_name']);
 
-$body  = $this->email_start();
-$body .= $this->handle_source($core->session['i18n']['email:buyer_welcome'],$values);
+$body  = $this->email_start($values['domain_id']);
+$body .= $this->handle_source('<h1>Almost there!</h1>
+
+      <p>
+        Now that you\'ve verified your email address with {hub_name}, we still
+        need the Market Manager\'s activation to get you started. (Not just
+        anyone can shop or sell through {hub_name}!) This should happen shortly,
+        and we\'ll let you know as soon as it happens.
+      </p>
+
+      <p>Thank you for supporting your local food producers.</p>',$values);
 $body .= $this->footer();
 $body .= $this->email_end();
 
