62ba24d6e7c814626b1db42451e89df26e013d65
Daniel Morrison <daniel@collectiveidea.com>
2014-01-07 15:11:54 -0500

    Update Reset Password email to new style

--- www/app/controllers/emails/views/reset_password.php
+++ www/app/controllers/emails/views/reset_password.php
@@ -7,10 +7,26 @@ $values = array(
 core::log('tryign to send email from domain '.$values['domain_id']);
 $values['hub_name'] = core_db::col('select name from domains where domain_id='.$values['domain_id'],'name');
 core::log('domain name is '.$values['hub_name']);
-//core::log('new_password is '.$core->view[1]);
 
-$body  = $this->email_start();
-$body .= $this->handle_source($core->session['i18n']['email:reset_password'],$values);
+$body  = $this->email_start($values['domain_id']);
+$body .= $this->handle_source('<h1>Your New Password</h1>
+      <p>
+        Hello, you recently requested that your password be reset.
+      </p>
+      <p>
+        Your new password is: <strong>{new_password}</strong>
+      </p>
+      <p>
+        It is important that your log in and change your password soon.
+      </p>
+
+      <div class="lo_call_to_action">
+        <a href="{link}" class="lo_button lo_button_large">Log in to Your Account</a>
+        <p>
+          If clicking the button doesn\'t work, right click it and copy the link.<br>
+          After you\'ve copied it, paste it into a new browser window.
+        </p>
+      </div>',$values);
 $body .= $this->footer();
 $body .= $this->email_end();
 
