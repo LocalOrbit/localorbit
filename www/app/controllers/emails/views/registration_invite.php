5e8b95ded6f7c83cffdd6a2dcc2a6f659d82e0d5
Daniel Morrison <daniel@collectiveidea.com>
2014-01-07 21:36:59 +0000

    Update Registration Invitation email to new style
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/trunk@3378 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- www/app/controllers/emails/views/registration_invite.php
+++ www/app/controllers/emails/views/registration_invite.php
@@ -3,13 +3,26 @@ $to_email = $core->view[0];
 $values = array(
 	'hostname'=>$core->config['domain']['hostname'],
 	'link'=>$core->view[1],
-	'hubname'=>$core->view[2]
+	'hub_name'=>$core->view[2]
 );
 
 $body  = $this->email_start();
-$body .= $this->handle_source($core->session['i18n']['email:reg_invite'],$values);
+$body .= $this->handle_source('<h1>Join {hub_name} today!</h1>
+      <p>
+        Hello! You have been invited to join {hub_name} by a member of your organization.
+      </p>
+
+      <div class="lo_call_to_action">
+        <a href="{link}" class="lo_button lo_button_large">Join {hub_name}</a>
+        <p>
+          If clicking the button doesn\'t work, right click it and copy the link.<br>
+          After you\'ve copied it, paste it into a new browser window.
+        </p>
+      </div>
+
+      <p>Thank you for supporting Farm Fresh and your local food producers!</p>',$values);
 $body .= $this->footer();
 $body .= $this->email_end();
 
-$this->send_email('Join '.$values['hubname'].' today!',$to_email,$body);
+$this->send_email('Join '.$values['hub_name'].' today!',$to_email,$body);
 ?>
