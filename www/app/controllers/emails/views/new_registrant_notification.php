2004d043b394457f2c1c1da756240f08a4b85a88
Daniel Morrison <daniel@collectiveidea.com>
2014-01-07 21:36:08 +0000

    Update MM New Registration email to new style
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/trunk@3373 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- www/app/controllers/emails/views/new_registrant_notification.php
+++ www/app/controllers/emails/views/new_registrant_notification.php
@@ -12,15 +12,42 @@ $values = array(
 );
 core::log('tryign to send email from domain '.$values['domain_id']);
 $values['hub_name'] = core_db::col('select name from domains where domain_id='.$values['domain_id'],'name');
-$values['hubname'] = $values['hub_name'];
 core::log('domain name is '.$values['hub_name']);	
 
-$body  = $this->email_start();
-$body .= $this->handle_source($core->session['i18n']['email:reg_mm_notification'],$values);
+$body  = $this->email_start($values['domain_id']);
+$body .= $this->handle_source('<h1>Hooray!</h1>
+
+<p>A new organization has registered for your market! Their contact info is below:
+
+<dl>
+  <dt>Company:</dt>
+  <dd>{company}</dd>
+</dl>
+
+<dl>
+  <dt>Name:</dt>
+  <dd>{fullname}</dd>
+</dl>
+
+<dl>
+  <dt>Email:</dt>
+  <dd>{email}</dd>
+</dl>
+
+<p>To activate this organization:</p>
+
+<ul>
+  <li>log in to your market</li>
+  <li>click on Market Admin</li>
+  <li>click on Organizations - The new organization should be located near the top of the list.</li>
+  <li>click the Activate button</li>
+</ul>
+
+<p>Thank you!</p>',$values);
 $body .= $this->footer();
 $body .= $this->email_end();
 
-$this->send_email('hooray, a new user!',$to_email,$body,
+$this->send_email('Hooray!',$to_email,$body,
 	array(),
 	$core->config['mailer']['From'],
 	$values['hub_name']
