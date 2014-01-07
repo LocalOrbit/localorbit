ceff23826e3465ddab250cc425516b06a038384d
Daniel Morrison <daniel@collectiveidea.com>
2014-01-07 21:36:35 +0000

    Update Organization activated email to new style
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/trunk@3376 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- www/app/controllers/emails/views/org_activated_verified.php
+++ www/app/controllers/emails/views/org_activated_verified.php
@@ -8,8 +8,16 @@ $values['hub_name'] = core_db::col('select name from domains where domain_id='.$
 core::log('domain name is '.$values['hub_name']);
 
 
-$body  = $this->email_start();
-$body .= $this->handle_source($core->session['i18n']['email:org_activated_verified'],$values);
+$body  = $this->email_start($values['domain_id']);
+$body .= $this->handle_source('<h1>You\'re all set!</h1>
+
+      <p>
+        The {hub_name} Market Manager has activated your account and you are
+        officially ready to shop the market. Log in to quickly and easily buy
+        great food produced right near you.
+      </p>
+
+      <p>Thank you for supporting your local food producers.</p>',$values);
 $body .= $this->footer();
 $body .= $this->email_end();
 
