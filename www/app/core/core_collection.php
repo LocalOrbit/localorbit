LocalOrbit/localorbit

commit 7cf1d9437886c3cfc61fb54220ac08390e7b7dbd

Remove some notices from the error logs
git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/trunk@3405 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

www/app/core/core_collection.php
@@ -128,7 +128,7 @@ class core_collection implements Iterator,ArrayAccess
 		}
 		else
 		{
-			return $false;
+			return false;
 		}
 	}
 	

www/app/config.php
@@ -278,7 +278,7 @@ if(strpos(__FILE__,'/branches/')) {
 	);
 }
 
-if($_SERVER['SERVER_ENV'] == 'jvavul') {
+if(isset($_SERVER['SERVER_ENV']) && $_SERVER['SERVER_ENV'] == 'jvavul') {
 	$core->config['stage'] = 'dev';
 	$core->config['default_hostname'] = 'dev.localorb.it';
 	$core->config['session_domain']   = '.localorb.it';

www/app/core/core.php
@@ -38,7 +38,7 @@ class core
 		$this->data =& $_REQUEST;
 		
 		$this->config = array(
-			'no_base64'=>$_REQUEST['no_base64'],
+			'no_base64'=>(isset($_REQUEST['no_base64']) ? $_REQUEST['no_base64'] : null),
 			'app_page'=>'index.php',
 			'page'=>'default',
 			'layout'=>'default',
@@ -633,6 +633,11 @@ class core
 		$p = func_get_args();
 		array_shift($p);
 		array_shift($p);
+		for ($i = 0; $i <= 11; $i++) {
+		  if (!isset($p[$i])) {
+		    $p[$i] = null;
+		  }
+		}
 		
 		$controller = core::controller($controller);
 		$data = $controller->$method($p[0],$p[1],$p[2],$p[3],$p[4],$p[5],$p[6],$p[7],$p[8],$p[9],$p[10],$p[11]);

commit bb49b90783b0e3a6915f32ebdb1c20a7d4c2b177

Fix order bug introduced in r3405 (git 7cf1d9437886c3cfc61fb54220ac08390e7b7dbd) [Refs: #63651586]
git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/trunk@3482 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

www/app/core/core_collection.php
@@ -128,7 +128,7 @@ class core_collection implements Iterator,ArrayAccess
 		}
 		else
 		{
-			return false;
+			return null;
 		}
 	}
 	