3948846be9e215905572d46e8f3ac88e84e39c9a
Chris Rittersdorf <chris.rittersdorf@collectiveidea.com>
2014-01-21 17:05:05 -0500

    Finishes Market Manager editing an organization.
    [Finishes #64256114]

--- spec/features/market_manager/managing_organizations_spec.rb
+++ spec/features/market_manager/managing_organizations_spec.rb
@@ -8,24 +8,59 @@ describe "A Market Manager" do
     sign_in_as market_manager
   end
 
-  it "can add an organization" do
-    click_link 'Organizations'
-    click_link 'Add Organization'
-
-    fill_in 'Name', with: 'Famous Farm'
-    check 'Can sell product'
-    click_button 'Add Organization'
-
-    expect(page).to have_content('Famous Farm has been created')
-  end
-
-  context "adding an organization with a blank name" do
-    it "doesn't add the new organization" do
+  describe "Adding an organization" do
+    it "with valid information" do
       click_link 'Organizations'
       click_link 'Add Organization'
 
-      fill_in 'Name', with: ''
+      fill_in 'Name', with: 'Famous Farm'
+      check 'Can sell product'
       click_button 'Add Organization'
+
+      expect(page).to have_content('Famous Farm has been created')
+    end
+
+    context "with a blank name" do
+      it "doesn't add the new organization" do
+        click_link 'Organizations'
+        click_link 'Add Organization'
+
+        fill_in 'Name', with: ''
+        click_button 'Add Organization'
+
+        expect(page).to have_content("Name can't be blank")
+      end
+    end
+  end
+
+  describe "Editing an organization" do
+    let(:organization) { create(:organization, name: "Fresh Pumpkin Patch") }
+
+    before do
+      market.organizations << organization
+    end
+
+    it "allows updating all attributes" do
+      click_link 'Organizations'
+      click_link "Fresh Pumpkin Patch"
+      click_link "Edit Organization"
+
+      fill_in "Name", with: "SXSW Farmette"
+      uncheck "Can sell product"
+      click_button "Save Organization"
+
+      expect(page).to have_content("Saved SXSW Farmette")
+      expect(page).to have_content("Name SXSW Farmette")
+      expect(page).to have_content("Can sell products? false")
+    end
+
+    it "does not allow updates with a blank organization name" do
+      click_link 'Organizations'
+      click_link "Fresh Pumpkin Patch"
+      click_link "Edit Organization"
+
+      fill_in "Name", with: ""
+      click_button "Save Organization"
 
       expect(page).to have_content("Name can't be blank")
     end
