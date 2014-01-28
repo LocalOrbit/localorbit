fc233a8c40b71c0f7dd56a656fdb10f5f59cec13
David Genord II <david@collectiveidea.com>
2014-01-28 15:41:04 -0500

    Add a rake task to run the taxonomy import

--- lib/import_legacy_taxonomy.rb
+++ lib/import_legacy_taxonomy.rb
@@ -3,9 +3,10 @@ require 'csv'
 # Import the taxonomy tree from a csv export
 # of the legacy taxonomy data
 module ImportLegacyTaxonomy
-  def self.run(filename)
+  def self.run(filename, opts={})
     flattened  = {}
     base_nodes = []
+    original_count = Category.count
 
     CSV.foreach(filename, headers: true) do |row|
       next if row['cat_id'] == '1' || row['parent_id'] == '1'
@@ -22,6 +23,12 @@ module ImportLegacyTaxonomy
     end
 
     store_tree(base_nodes)
+
+    if opts[:verbose]
+      puts "#{original_count} Exisiting Categories"
+      puts "Created #{Category.count - original_count} new Categories."
+      puts "There were #{flattened.size} rows detected in the import file"
+    end
   end
 
   def self.store_tree(nodes, parent = nil)
---
+++ lib/tasks/import.rake
@@ -0,0 +1,8 @@
+namespace :import do
+  desc "imports legacy taxonomy categories from a CSV given by TAXONOMY_FILE"
+  task :taxonomy => [:environment] do
+    taxonomy_file = ENV['TAXONOMY_FILE']
+    raise "import:taxonomy requires a valid CSV" if taxonomy_file.nil?
+    ImportLegacyTaxonomy.run(taxonomy_file, verbose: true)
+  end
+end
