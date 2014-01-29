4034e3097bf02b66975d3b9242b075d1e99ebf4a
Chris Rittersdorf <chris.rittersdorf@collectiveidea.com>
2014-01-29 15:03:10 -0500

    Adds Category.for_select mapping the category structure to select options.

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,14 @@ class Category < ActiveRecord::Base
   has_many :children, class_name: 'Category', foreign_key: 'parent_id'
   has_many :products
 
+  def self.for_select(list = nil, parent_str = nil)
+    list ||= where(parent_id: nil)
+    output = []
+    list.each do |item|
+      name = [parent_str, item.name].compact.join(" > ")
+      output << [name, item.id]
+      output.concat(for_select(item.children, name))
+    end
+    output
+  end
 end
