0e1dad26785f05603bbb1b53998bddafdae1252d
David Genord II <david@collectiveidea.com>
2014-02-06 16:21:47 -0500

    Better organize taxonomy importer
    Because this will not kill our 4.0

--- lib/import_legacy_taxonomy.rb
+++ lib/import_legacy_taxonomy.rb
@@ -2,38 +2,52 @@ require 'csv'
 
 # Import the taxonomy tree from a csv export
 # of the legacy taxonomy data
-module ImportLegacyTaxonomy
+class ImportLegacyTaxonomy
   def self.run(filename, opts={})
-    flattened  = {}
-    base_nodes = []
-    original_count = Category.count
+    new(filename, opts).run
+  end
 
-    CSV.foreach(filename, headers: true) do |row|
-      next if row['cat_id'] == '1' || row['parent_id'] == '1'
+  def initialize(filename, opts = {})
+    @filename       = filename
+    @by_cat_id      = {}
+    @base_nodes     = []
+    @verbose        = opts[:verbose]
+    @original_count = Category.count if @verbose
+  end
 
-      flattened[row['cat_id']] = {name: row['cat_name'], children: [], parent_id: row['parent_id']}
-    end
+  def run
+    load_taxonomy
+    build_tree
+    store_tree(@base_nodes)
 
-    flattened.each_value do |node|
-      if parent = flattened[node[:parent_id]]
-        parent[:children] << node
-      else
-        base_nodes << node
-      end
-    end
-
-    base_nodes.reject! {|node| node[:parent_id] != "2" }
-
-    store_tree(base_nodes)
-
-    if opts[:verbose]
-      puts "#{original_count} Exisiting Categories"
-      puts "Created #{Category.count - original_count} new Categories."
-      puts "There were #{flattened.size} rows detected in the import file"
+    if @verbose
+      puts "#{@original_count} Exisiting Categories"
+      puts "Created #{Category.count - @original_count} new Categories."
+      puts "There were #{@by_cat_id.size} rows detected in the import file"
     end
   end
 
-  def self.store_tree(nodes, parent = nil)
+  def load_taxonomy
+    CSV.foreach(@filename, headers: true) do |row|
+      next if row['cat_id'] == '1' || row['parent_id'] == '1'
+
+      @by_cat_id[row['cat_id']] = {name: row['cat_name'], children: [], parent_id: row['parent_id']}
+    end
+  end
+
+  def build_tree
+    @by_cat_id.each_value do |node|
+      if parent = @by_cat_id[node[:parent_id]]
+        parent[:children] << node
+      else
+        @base_nodes << node
+      end
+    end
+
+    @base_nodes.reject! {|node| node[:parent_id] != "2" }
+  end
+
+  def store_tree(nodes, parent = nil)
     nodes.each do |node|
       scope = Category.where(name: node[:name], parent_id: parent.try(:id))
       obj = scope.first || scope.create
