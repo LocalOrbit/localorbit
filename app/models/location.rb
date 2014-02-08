78187083333e7d9da29d65491bde66eef2c9e1ca
Eric Milford <ericmilford@gmail.com>
2014-02-08 18:03:45 -0400

    Add validations to location

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,6 @@
 class Location < ActiveRecord::Base
   belongs_to :organization
+
+  validates :name, presence: true, uniqueness: true
+  validates :address, :city, :state, :zip, :organization_id, presence: true
 end
