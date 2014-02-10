0c7a7197c4301181ff4cfb7cff9be1cea2d54105
Eric Milford <ericmilford@gmail.com>
2014-02-10 15:25:59 -0400

    Scope name uniqueness to organization

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,7 +1,7 @@
 class Location < ActiveRecord::Base
   belongs_to :organization
 
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: { scope: :organization_id }
   validates :address, :city, :state, :zip, :organization_id, presence: true
 
   def self.alphabetical_by_name
