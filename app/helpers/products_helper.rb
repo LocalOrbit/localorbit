6aa234804457d8c8aca33e449ec458be72775f72
Chris Rittersdorf <chris.rittersdorf@collectiveidea.com>
2014-02-10 19:38:11 -0500

    Add clarity between the flash, field errors, and the product status message.

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,6 +1,6 @@
 module ProductsHelper
   def product_listing_disclaimer
-    content_tag(:div, "Your product will not appear in the Shop until all of these actions are complete", class: "alert")
+    content_tag(:div, "Your product will not appear in the Shop until all of these actions are complete", class: "product-status-alert")
   end
 
   def us_states
