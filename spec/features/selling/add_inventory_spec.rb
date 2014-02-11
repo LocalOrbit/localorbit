ec549e8ebe16e813d42af423ca4597894453f6b6
Chris Rittersdorf <chris.rittersdorf@collectiveidea.com>
2014-02-11 16:56:32 -0500

    Updates inventory tests to use products with advanced inventory.

--- spec/features/selling/add_inventory_spec.rb
+++ spec/features/selling/add_inventory_spec.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 
 describe "Adding advanced inventory" do
   let(:user) { create(:user) }
-  let(:product){ create(:product) }
+  let(:product){ create(:product, use_simple_inventory: false) }
 
   let(:empty_inventory_message) { "You don't have any inventory" }
 
