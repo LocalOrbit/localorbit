94ec38b2e9bdfe911390a38e83607b764022f3af
David Genord II <david@collectiveidea.com>
2014-02-12 13:57:58 -0500

    Fix an issue parsing initial dates selected for datepicker

--- spec/features/selling/add_inventory_spec.rb
+++ spec/features/selling/add_inventory_spec.rb
@@ -5,84 +5,111 @@ describe "Adding advanced inventory" do
   let(:product){ create(:product) }
   let(:empty_inventory_message) { "You don't have any inventory" }
 
-  before do
-    Timecop.freeze(Date.parse("February 24, 2014"))
-    product.organization.users << user
-    sign_in_as(user)
-    click_link "Products"
-    click_link product.name
-    click_link "Inventory"
-  end
-
-  after do
-    Timecop.return
-  end
-
-  it "adds the inventory lot to the table" do
-    expect(page).to have_content(empty_inventory_message)
-
-    within("#new_lot") do
-      fill_in "lot_number", with: "3"
-      fill_in "lot_good_from", with: "Tue, 25 Feb 2014"
-      fill_in "lot_expires_at", with: "Wed, 10 Dec 2014"
-      fill_in "lot_quantity", with: "12"
-      click_button "Save"
+  context "without js" do
+    before do
+      Timecop.freeze(Date.parse("February 24, 2014"))
+      product.organization.users << user
+      sign_in_as(user)
+      click_link "Products"
+      click_link product.name
+      click_link "Inventory"
     end
 
-    expect(page).to have_content("Successfully added a new lot")
-    expect(page).to_not have_content(empty_inventory_message)
-
-    lot_row = Dom::LotRow.first
-    expect(lot_row).to_not be_nil
-    expect(lot_row.number).to eql("3")
-    expect(lot_row.good_from).to eql("02/25/2014")
-    expect(lot_row.expires_at).to eql("12/10/2014")
-    expect(lot_row.quantity).to eql("12")
-  end
-
-  it "shows an error when adding incomplete information" do
-    within("#new_lot") do
-      fill_in "lot_number", with: ""
-      fill_in "lot_quantity", with: ""
-      click_button "Save"
+    after do
+      Timecop.return
     end
 
-    expect(page).to have_content("Could not save lot")
-    expect(page).to have_content("Quantity is not a number")
+    it "adds the inventory lot to the table" do
+      expect(page).to have_content(empty_inventory_message)
 
-    expect(Dom::LotRow.first).to be_nil
-  end
+      within("#new_lot") do
+        fill_in "lot_number", with: "3"
+        fill_in "lot_good_from", with: "Tue, 25 Feb 2014"
+        fill_in "lot_expires_at", with: "Wed, 10 Dec 2014"
+        fill_in "lot_quantity", with: "12"
+        click_button "Save"
+      end
 
-  it "shows an error when adding an expired lot" do
-    within("#new_lot") do
-      fill_in "lot_number", with: "3"
-      fill_in "lot_good_from", with: "Tue, 25 Feb 2014"
-      fill_in "lot_expires_at", with: "Wed, 10 Dec 2012"
-      fill_in "lot_quantity", with: "12"
-      click_button "Save"
+      expect(page).to have_content("Successfully added a new lot")
+      expect(page).to_not have_content(empty_inventory_message)
+
+      lot_row = Dom::LotRow.first
+      expect(lot_row).to_not be_nil
+      expect(lot_row.number).to eql("3")
+      expect(lot_row.good_from).to eql("02/25/2014")
+      expect(lot_row.expires_at).to eql("12/10/2014")
+      expect(lot_row.quantity).to eql("12")
     end
 
-    expect(page).to have_content("Expires On must be in the future")
-  end
+    it "shows an error when adding incomplete information" do
+      within("#new_lot") do
+        fill_in "lot_number", with: ""
+        fill_in "lot_quantity", with: ""
+        click_button "Save"
+      end
 
-  it "shows an error when the good from date is beyond the expires on date" do
-    within("#new_lot") do
-      fill_in "lot_number", with: "3"
-      fill_in "lot_good_from", with: "Tue, 25 Feb 2015"
-      fill_in "lot_expires_at", with: "Tue, 25 Dec 2012"
-      fill_in "lot_quantity", with: "12"
-      click_button "Save"
+      expect(page).to have_content("Could not save lot")
+      expect(page).to have_content("Quantity is not a number")
+
+      expect(Dom::LotRow.first).to be_nil
     end
 
-    expect(page).to have_content("Good From cannot be after expires at date")
+    it "shows an error when adding an expired lot" do
+      within("#new_lot") do
+        fill_in "lot_number", with: "3"
+        fill_in "lot_good_from", with: "Tue, 25 Feb 2014"
+        fill_in "lot_expires_at", with: "Wed, 10 Dec 2012"
+        fill_in "lot_quantity", with: "12"
+        click_button "Save"
+      end
+
+      expect(page).to have_content("Expires On must be in the future")
+    end
+
+    it "shows an error when the good from date is beyond the expires on date" do
+      within("#new_lot") do
+        fill_in "lot_number", with: "3"
+        fill_in "lot_good_from", with: "Tue, 25 Feb 2015"
+        fill_in "lot_expires_at", with: "Tue, 25 Dec 2012"
+        fill_in "lot_quantity", with: "12"
+        click_button "Save"
+      end
+
+      expect(page).to have_content("Good From cannot be after expires at date")
+    end
+
+    it "user can navigate back to product from the inventory page" do
+      click_link "Product Info"
+      product_form = Dom::ProductForm.first
+
+      expect(product_form.organization_field.value).to eql(product.organization_id.to_s)
+      expect(product_form.name.value).to eql(product.name.to_s)
+      expect(product_form.category.value).to eql(product.category.id.to_s)
+    end
   end
 
-  it "user can navigate back to product from the inventory page" do
-    click_link "Product Info"
-    product_form = Dom::ProductForm.first
+  context "with js", js: true do
+    before do
+      product.organization.users << user
+      sign_in_as(user)
+      click_link "Products"
+      click_link product.name
+      click_link "Inventory"
+    end
 
-    expect(product_form.organization_field.value).to eql(product.organization_id.to_s)
-    expect(product_form.name.value).to eql(product.name.to_s)
-    expect(product_form.category.value).to eql(product.category.id.to_s)
+    it "populates the correct date on validation errors" do
+      expected_date = Time.now.change(day: 15).strftime("%a, %e %b %Y")
+      field = page.find_field('lot_expires_at')
+
+      field.click
+      click_link "15"
+
+      expect(field.value).to eq(expected_date)
+
+      click_button "Save"
+
+      expect(page).to have_content("Lot # can't be blank when 'Expiration Date' is present")
+      expect(field.value).to eq(expected_date)
+    end
   end
 end
