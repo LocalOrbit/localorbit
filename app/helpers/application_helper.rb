2c84ac660041046f29e1f37b9cd4ec404f46f2d5
Ryan Glover <ersatzryan@gmail.com>
2014-02-25 16:50:39 -0500

    Filter buy seller and category at the same time

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,11 @@
 module ApplicationHelper
-  def filter_link(link_text, link_path)
-    class_name = current_page?(link_path) ? "current" : ""
+  def filter_link(object, param_name)
+    params = request.query_parameters
+
+    class_name = params[param_name] == object.id.to_s ? "current" : ""
 
     content_tag(:li, class: class_name) do
-      link_to link_text, link_path
+      link_to object.name, url_for(params.merge(param_name => object.id))
     end
   end
 end
--- spec/features/buying/filtering_products_spec.rb
+++ spec/features/buying/filtering_products_spec.rb
@@ -5,8 +5,12 @@ feature "Filtering Products List" do
   let!(:org2) { create(:organization, name: "Funny Farm") }
   let!(:category1) { create(:category, name: "Vegetables") }
   let!(:category2) { create(:category, name: "Fruits") }
+
   let!(:product1) { create(:product, organization: org1, category: category1) }
-  let!(:product2) { create(:product, organization: org2, category: category2) }
+  let!(:product2) { create(:product, organization: org1, category: category2) }
+
+  let!(:product3) { create(:product, organization: org2, category: category1) }
+  let!(:product4) { create(:product, organization: org2, category: category2) }
 
   let!(:buyer_org) { create(:organization, :buyer) }
   let!(:user) { create(:user, organizations: [buyer_org]) }
@@ -20,37 +24,58 @@ feature "Filtering Products List" do
   scenario "by seller" do
     visit products_path
 
-    expect(Dom::Product.count).to eq(2)
+    expect(Dom::Product.count).to eq(4)
 
     Dom::ProductFilter.filter_by_seller(org1)
 
     expect(Dom::ProductFilter.current_seller).to eq(org1.name)
-    expect(Dom::Product.count).to eq(1)
-    expect(Dom::Product.first.name).to eq(product1.name)
+    expect(Dom::Product.count).to eq(2)
+    expect(Dom::Product.all.map(&:name)).to match_array([product1.name, product2.name])
 
     Dom::ProductFilter.filter_by_seller(org2)
 
     expect(Dom::ProductFilter.current_seller).to eq(org2.name)
-    expect(Dom::Product.count).to eq(1)
-    expect(Dom::Product.first.name).to eq(product2.name)
+    expect(Dom::Product.count).to eq(2)
+    expect(Dom::Product.all.map(&:name)).to match_array([product3.name, product4.name])
   end
 
   scenario "by category" do
     visit products_path
 
-    expect(Dom::Product.count).to eq(2)
+    expect(Dom::Product.count).to eq(4)
 
     Dom::ProductFilter.filter_by_category(category1)
 
+    expect(Dom::ProductFilter.current_category).to eq(category1.name)
+    expect(Dom::Product.count).to eq(2)
+    expect(Dom::Product.all.map(&:name)).to match_array([product1.name, product3.name])
+
+    Dom::ProductFilter.filter_by_category(category2)
+
+    expect(Dom::ProductFilter.current_category).to eq(category2.name)
+    expect(Dom::Product.count).to eq(2)
+    expect(Dom::Product.all.map(&:name)).to match_array([product2.name, product4.name])
+  end
+
+  scenario "by both category and seller" do
+    visit products_path
+
+    expect(Dom::Product.count).to eq(4)
+
+    Dom::ProductFilter.filter_by_category(category1)
+    Dom::ProductFilter.filter_by_seller(org1)
+
+    expect(Dom::ProductFilter.current_seller).to eq(org1.name)
     expect(Dom::ProductFilter.current_category).to eq(category1.name)
     expect(Dom::Product.count).to eq(1)
     expect(Dom::Product.first.name).to eq(product1.name)
 
     Dom::ProductFilter.filter_by_category(category2)
+    Dom::ProductFilter.filter_by_seller(org1)
 
+    expect(Dom::ProductFilter.current_seller).to eq(org1.name)
     expect(Dom::ProductFilter.current_category).to eq(category2.name)
     expect(Dom::Product.count).to eq(1)
     expect(Dom::Product.first.name).to eq(product2.name)
-
   end
 end
