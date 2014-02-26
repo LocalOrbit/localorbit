35f2ea21add3f44eda46d4cd828d36bf2691b770
Tim Bugai <tim@bugai.net>
2014-02-26 15:33:32 -0500

    Refactor MarketAddressesController to be less ugly

--- app/controllers/admin/market_addresses_controller.rb
+++ app/controllers/admin/market_addresses_controller.rb
@@ -1,12 +1,14 @@
 class Admin::MarketAddressesController < AdminController
-  before_action :require_admin_or_market_manager, except: [:new, :create]
+  before_action :require_admin_or_market_manager
+  before_action :find_current_market
+  before_action :find_address, only: [:edit, :update]
   
   def index
-    @addresses = current_market.addresses 
+    @addresses = @market.addresses 
   end
 
   def new
-    @address = MarketAddress.new(market: current_market)
+    @address = MarketAddress.new(market: @market)
   end
 
   def create
@@ -14,23 +16,19 @@ class Admin::MarketAddressesController < AdminController
     if @address.errors.any?
       render :new
     else
-      redirect_to admin_market_addresses_path(current_market)
+      redirect_to admin_market_addresses_path(@market)
     end
   end
 
   def edit
-    current_market
-    @address = MarketAddress.where(id: params[:id]).first
   end
 
   def update
-    @address = MarketAddress.where(id: params[:id]).first
     if @address.update_attributes(market_address_params)
-      redirect_to admin_market_addresses_path(current_market)
+      redirect_to admin_market_addresses_path(@market)
     else
       render :edit
     end
-
   end
 
   private
@@ -41,11 +39,15 @@ class Admin::MarketAddressesController < AdminController
       :address,
       :city,
       :state,
-      :zip,
+      :zip
     ).merge(market_id: params[:market_id])
   end
   
-  def current_market
+  def find_current_market
     @market = Market.where(id: params[:market_id]).first
+  end
+
+  def find_address 
+    @address = MarketAddress.where(id: params[:id]).first
   end
 end
