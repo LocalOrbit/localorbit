f5306b43e091dc400c6c08ae214d3ed56c32716a
David Genord II <david@collectiveidea.com>
2014-02-27 12:13:56 -0500

    Correct view_sorted to use market and buyer name
    Also make sure all market and all buyer prices show first

--- app/models/price.rb
+++ app/models/price.rb
@@ -3,7 +3,10 @@ class Price < ActiveRecord::Base
   belongs_to :market
   belongs_to :organization
 
-  scope :view_sorted, lambda { order('market_id, organization_id, min_quantity') }
+  scope :view_sorted, lambda {
+    joins("LEFT JOIN markets ON prices.market_id = markets.id LEFT JOIN organizations ON prices.organization_id = organizations.id").
+    order('markets.name NULLS FIRST, organizations.name NULLS FIRST, min_quantity')
+  }
 
   validates :min_quantity, :sale_price, numericality: { greater_than: 0 }
   validates :min_quantity, uniqueness: { scope: [:market_id, :organization_id] }
--- spec/models/price_spec.rb
+++ spec/models/price_spec.rb
@@ -14,4 +14,27 @@ describe Price do
       expect(subject.net_price).to eq(3.87) # 3% fees
     end
   end
+
+  describe 'view_sorted' do
+    let(:product) { create(:product) }
+
+    it 'has all buyers before a buyer specific price' do
+      p2 = product.prices.create(min_quantity: 5, sale_price: 2)
+      p3 = product.prices.create(organization: create(:organization), sale_price: 3)
+      p1 = product.prices.create(sale_price: 2)
+
+      expect(product.prices.view_sorted).to eq([p1, p2, p3])
+    end
+
+    it 'has buyer specific pricing ordered by buyer name' do
+      org1 = create(:organization, name: 'Organic Farms')
+      org2 = create(:organization, name: 'Better Farms')
+
+      p2 = product.prices.create(organization: org2, sale_price: 2)
+      p3 = product.prices.create(organization: org1, sale_price: 2)
+      p1 = product.prices.create(sale_price: 2)
+
+      expect(product.prices.view_sorted).to eq([p1, p2, p3])
+    end
+  end
 end
