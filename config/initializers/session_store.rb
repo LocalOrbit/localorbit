cd4a6b24ef979ec0999a55f336fae53fa7469dc9
David Genord II <david@collectiveidea.com>
2014-03-03 16:34:17 -0500

    localhost doesn't behave in Chrome

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,4 +2,4 @@
 
 Rails.application.config.session_store :cookie_store,
   key: '_local_orbit_session',
-  domain: Rails.env.test? ? :all : ".#{Figaro.env.domain}"
+  domain: (Rails.env.test? || Figaro.env.domain == 'localhost') ? :all : ".#{Figaro.env.domain}"
