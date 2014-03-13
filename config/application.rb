79caa6fcc1746e6a9f9765df9c8cbd08fa6ed690
Daniel Morrison <daniel@collectiveidea.com>
2014-03-13 16:00:23 -0400

    Make staging & production configs more equal. Move assets to precompile into application.rb

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,8 @@ module LocalOrbit
 
     config.autoload_paths += %W(#{config.root}/lib)
 
+    config.assets.precompile += %w(chosen-sprite.png chosen-sprite@2x.png)
+
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,11 +1,11 @@
-LocalOrbit::Application.configure do
+Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
   config.cache_classes = true
 
   # Eager load code on boot. This eager loads most of Rails and
-  # your application in memory, allowing both thread web servers
+  # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
@@ -78,6 +78,9 @@ LocalOrbit::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # Do not dump schema after migrations.
+  config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = {protocol: 'https', host: Figaro.env.domain}
 end
