fef1e48e214856f5a73f38c7515c320921f3b011
Tim Bugai <tim@bugai.net>
2014-03-27 13:34:45 -0400

    Helper method for getting the correct unit value

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -33,6 +33,14 @@ class CartItem < ActiveRecord::Base
     super(methods: [:total_price, :unit_sale_price, :valid?])
   end
 
+  def unit
+    if quantity == 0 || quantity > 1
+      product.unit.plural
+    else
+      product.unit.singular
+    end
+  end
+
   protected
 
   def quantity_is_available
