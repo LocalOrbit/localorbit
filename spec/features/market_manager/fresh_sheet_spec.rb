c4966699c000e6fda4fe0b0776e09489b43812c9
David Genord II <david@collectiveidea.com>
2014-04-02 15:11:27 -0400

    Correct double sign in

--- spec/features/market_manager/fresh_sheet_spec.rb
+++ spec/features/market_manager/fresh_sheet_spec.rb
@@ -4,12 +4,9 @@ feature "A Market Manager sending a weekly Fresh Sheet" do
   let!(:user) { create(:user, :market_manager) }
   let!(:market) { user.managed_markets.first }
 
-  before do
+  scenario "navigating to the page" do
     switch_to_subdomain(market.subdomain)
     sign_in_as user
-  end
-
-  scenario "navigating to the page" do
     click_link "Marketing"
     click_link "Fresh Sheet"
     expect(page).to have_content("Fresh Sheet")
@@ -26,23 +23,30 @@ feature "A Market Manager sending a weekly Fresh Sheet" do
     expect(page).to have_css("iframe[src='#{preview_admin_fresh_sheet_path}']")
   end
 
-  scenario "previewing" do
-    visit admin_fresh_sheet_path
-    expect(page).to have_content("Fresh Sheet")
-    expect(page).to have_css("iframe[src='#{preview_admin_fresh_sheet_path}']")
-  end
+  context "signed in" do
+    before do
+      switch_to_subdomain(market.subdomain)
+      sign_in_as user
+    end
 
-  scenario "sending a test" do
-    expect(MarketMailer).to receive(:fresh_sheet).with(market, user.email).and_return(double(:mailer, deliver: true))
-    visit admin_fresh_sheet_path
-    click_button "Send Test"
-    expect(page).to have_content("Successfully sent a test to #{user.email}")
-  end
+    scenario "previewing" do
+      visit admin_fresh_sheet_path
+      expect(page).to have_content("Fresh Sheet")
+      expect(page).to have_css("iframe[src='#{preview_admin_fresh_sheet_path}']")
+    end
 
-  scenario "sending to everyone" do
-    expect(MarketMailer).to receive(:fresh_sheet).with(market).and_return(double(:mailer, deliver: true))
-    visit admin_fresh_sheet_path
-    click_button "Send Now"
-    expect(page).to have_content("Successfully sent the Fresh Sheet")
+    scenario "sending a test" do
+      expect(MarketMailer).to receive(:fresh_sheet).with(market, user.email).and_return(double(:mailer, deliver: true))
+      visit admin_fresh_sheet_path
+      click_button "Send Test"
+      expect(page).to have_content("Successfully sent a test to #{user.email}")
+    end
+
+    scenario "sending to everyone" do
+      expect(MarketMailer).to receive(:fresh_sheet).with(market).and_return(double(:mailer, deliver: true))
+      visit admin_fresh_sheet_path
+      click_button "Send Now"
+      expect(page).to have_content("Successfully sent the Fresh Sheet")
+    end
   end
 end
