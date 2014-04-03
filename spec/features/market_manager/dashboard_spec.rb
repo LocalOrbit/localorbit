3df9e17a3e1cc76f71fa093e684eaef108c79972
Jonathan Pichot <jonathan@pichot.us>
2014-04-03 10:04:42 -0400

    Add notice when either product or sales table on dashboard is empty

--- spec/features/market_manager/dashboard_spec.rb
+++ spec/features/market_manager/dashboard_spec.rb
@@ -12,15 +12,13 @@ feature "a market manager viewing their dashboard" do
   end
 
   describe "Current Sales tables" do
-    before do
+    it "lists all sales for the currently managed market ordered by creation date" do
+      create(:order, order_number: "LO-14-TEST-2", market: market)
       create(:order, total_cost: 50, market: market, placed_at: DateTime.parse("2014-04-01 12:00:00"), order_number: "LO-14-TEST")
-      create(:order, market: market)
       create(:order)
 
       visit dashboard_path
-    end
 
-    it "lists all sales for the currently managed market" do
       expect(page).to have_content("Current Sales")
 
       expect(Dom::Dashboard::CurrentSaleRow.all.count).to eq(2)
@@ -31,31 +29,45 @@ feature "a market manager viewing their dashboard" do
       expect(order_row.total).to eq("$50.00")
       expect(order_row.delivery).to eq("Pending")
       expect(order_row.payment).to eq("Unpaid")
+
+      expect(Dom::Dashboard::CurrentSaleRow.all.last.order_number).to eq("LO-14-TEST-2")
+    end
+
+    it "displays a message if there are no orders" do
+      visit dashboard_path
+
+      expect(page).to have_content("Current Sales")
+      expect(page).to have_content("No orders have yet been created")
     end
   end
 
   describe "Products table" do
-    let!(:organization) { create(:organization, name: "Super Farm!", markets: [market]) }
-    let(:product) { create(:product, name: "Power Food", organization: organization) }
+    it "lists all products in the managed market by creation date" do
+      organization = create(:organization, name: "Super Farm!", markets: [market])
+      product = create(:product, name: "Power Food", organization: organization)
 
-    before do
       create(:price, product: product, market: market, organization: organization, sale_price: 20)
       create(:lot, product: product, quantity: 123)
+      create(:product, name: "Last Thing", organization: organization, created_at: 1.day.ago)
       create(:product)
 
       visit dashboard_path
-    end
-
-    it "lists all products in the managed market" do
       expect(page).to have_content("Products")
 
-      expect(Dom::Dashboard::ProductRow.all.count).to eq(1)
+      expect(Dom::Dashboard::ProductRow.all.count).to eq(2)
       seller_row = Dom::Dashboard::ProductRow.first
 
       expect(seller_row.seller).to eq("Super Farm!")
       expect(seller_row.name).to eq("Power Food")
       expect(seller_row.pricing).to eq("$20.00")
       expect(seller_row.stock).to eq("123")
+    end
+
+    it "displays a message if there are no products" do
+      visit dashboard_path
+
+      expect(page).to have_content("Products")
+      expect(page).to have_content("No products have yet been created")
     end
   end
 end
