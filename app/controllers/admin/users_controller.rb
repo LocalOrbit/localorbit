6e33481f686b10b9475de5c06f900c09e31a2147
Daniel Morrison <daniel@collectiveidea.com>
2014-04-03 13:46:32 -0500

    Tweak user id lookup for users page

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -4,8 +4,8 @@ module Admin
       if current_user.admin?
         @users = User.all
       else
-        ids = current_user.markets.map{|m| m.managers.pluck(:id)}.flatten &
-          current_user.managed_organizations.map{|o| o.users.pluck(:id) }
+        ids = current_user.managed_markets.map{|m| m.manager_ids }.flatten |
+          current_user.managed_organizations.map{|o| o.user_ids }.flatten
         @users = User.where(id: ids)
       end
 
