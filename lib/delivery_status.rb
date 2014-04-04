0516982a293826dec74881e6fd0bac4e3ce570cf
Jonathan Pichot <jonathan@pichot.us>
2014-04-04 14:14:50 -0400

    Test delivery_status on seller order

--- lib/delivery_status.rb
+++ lib/delivery_status.rb
@@ -1,17 +1,17 @@
 module DeliveryStatus
   def delivery_status
-    statuses = items.map(&:delivery_status).uniq.map(&:downcase)
+    statuses = items.pluck(:delivery_status).uniq.map(&:downcase)
 
     # TODO: Currently, canceled will only return if all items are canceled, this logic needs to be confirmed
     return statuses.first if statuses.size == 1
-    return "contested" if statuses_within(statuses, ["contested", "pending"])
-    return "partially delivered" if statuses_within(statuses, ["delivered", "pending"])
     return "contested, partially delivered" if statuses_within(statuses, ["pending", "delivered", "contested"])
+    return "contested" if statuses.include?("contested")
+    return "partially delivered" if statuses_within(statuses, ["delivered", "pending"])
   end
 
   private
 
   def statuses_within(statuses, query)
-    (statuses | query).sort == query.sort
+    (query & statuses) == query
   end
 end
