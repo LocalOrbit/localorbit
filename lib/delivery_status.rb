ca765eafe1ee922ddabe874c2bfed9eadc5d5242
Jonathan Pichot <jonathan@pichot.us>
2014-04-04 14:58:19 -0400

    Clarify branching logic in delivery_status

--- lib/delivery_status.rb
+++ lib/delivery_status.rb
@@ -3,10 +3,15 @@ module DeliveryStatus
     statuses = items.pluck(:delivery_status).uniq.map(&:downcase)
 
     # TODO: Currently, canceled will only return if all items are canceled, this logic needs to be confirmed
-    return statuses.first if statuses.size == 1
-    return "contested, partially delivered" if statuses_within(statuses, ["pending", "delivered", "contested"])
-    return "contested" if statuses.include?("contested")
-    return "partially delivered" if statuses_within(statuses, ["delivered", "pending"])
+    if statuses.size == 1
+      statuses.first
+    elsif statuses_within(statuses, ["pending", "delivered", "contested"])
+      "contested, partially delivered"
+    elsif statuses.include?("contested")
+      "contested"
+    elsif statuses_within(statuses, ["delivered", "pending"])
+      "partially delivered"
+    end
   end
 
   private
