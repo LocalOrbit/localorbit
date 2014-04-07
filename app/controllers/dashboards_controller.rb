be3fb8d43e46b9a9cbe857fdf38eb21d0653e5bf
David Genord II <david@collectiveidea.com>
2014-04-07 11:16:26 -0400

    Make the dashboard market specific

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,6 +1,6 @@
 class DashboardsController < ApplicationController
   def show
-    @presenter = DashboardPresenter.new(current_user)
+    @presenter = DashboardPresenter.new(current_user, current_market || current_user.markets.first)
   end
 
   def coming_soon
