3548d5eb767c9bb9053fb909cd390ba3ad1b70c4
Chris Rittersdorf <chris.rittersdorf@collectiveidea.com>
2014-04-08 11:31:34 -0400

    Fix style for calling interactor variable.

--- app/interactors/create_order.rb
+++ app/interactors/create_order.rb
@@ -2,7 +2,7 @@ class CreateOrder
   include Interactor
 
   def perform
-    context[:order] = Order.create_from_cart(order_params, cart, context[:buyer])
+    context[:order] = Order.create_from_cart(order_params, cart, buyer)
     context.fail! if context[:order].errors.any?
   end
 end
