b29c34582e2201f0ff65c528260b2218dd84212a
Daniel Morrison <daniel@collectiveidea.com>
2014-04-09 16:24:55 -0400

    Addressing more error cases with bank account verification

--- app/interactors/verify_bank_account.rb
+++ app/interactors/verify_bank_account.rb
@@ -9,8 +9,11 @@ class VerifyBankAccount
     verification.amount_1 = verification_params[:amount_1]
     verification.amount_2 = verification_params[:amount_2]
 
-    verification.save
-
+    begin
+      verification.save
+    rescue Balanced::BankAccountVerificationFailure
+      fail!
+    end
     fail! unless verified?
 
     bank_account.update_attribute(:verified, verified?)
--- spec/features/organizations/bank_accounts/verifying_spec.rb
+++ spec/features/organizations/bank_accounts/verifying_spec.rb
@@ -5,43 +5,53 @@ feature "Verifying a bank account", js: true do
   let!(:market) { market_manager.managed_markets.first }
   let!(:org) { create(:organization, markets: [market]) }
 
-  before do
-    CreateBalancedCustomerForEntity.perform(organization: org)
-  end
+  context "as a market manager" do
+    before do
+      CreateBalancedCustomerForEntity.perform(organization: org)
+      switch_to_subdomain(market.subdomain)
+      sign_in_as(market_manager)
 
-  scenario "as a market manager" do
-    switch_to_subdomain(market.subdomain)
-    sign_in_as(market_manager)
+      visit new_admin_organization_bank_account_path(org)
 
-    visit new_admin_organization_bank_account_path(org)
+      fill_in "Organization EIN", with: "20-1234567"
+      fill_in "Full Legal Name", with: "John Patrick Doe"
+      select "Sep", from: "representative_dob_month"
+      select "17", from: "representative_dob_day"
+      select "1990", from: "representative_dob_year"
 
-    fill_in "Organization EIN", with: "20-1234567"
-    fill_in "Full Legal Name", with: "John Patrick Doe"
-    select "Sep", from: "representative_dob_month"
-    select "17", from: "representative_dob_day"
-    select "1990", from: "representative_dob_year"
+      fill_in "Last 4 of SSN", with: "1234"
+      fill_in "Street Address (Personal)", with: "6789 Fake Dr"
+      fill_in "Zip Code (Personal)", with: "12345"
 
-    fill_in "Last 4 of SSN", with: "1234"
-    fill_in "Street Address (Personal)", with: "6789 Fake Dr"
-    fill_in "Zip Code (Personal)", with: "12345"
+      fill_in "Name", with: "Org Bank Account"
+      select("Checking", from: "Account Type")
+      fill_in "Routing Number", with: "021000021"
+      fill_in "Account Number", with: "9900000002"
 
-    fill_in "Name", with: "Org Bank Account"
-    select("Checking", from: "Account Type")
-    fill_in "Routing Number", with: "021000021"
-    fill_in "Account Number", with: "9900000002"
+      click_button "Save"
 
-    click_button "Save"
+      expect(page).to have_content("Bank Accounts")
 
-    expect(page).to have_content("Bank Accounts")
+      click_link "Verify"
+    end
 
-    click_link "Verify"
+    scenario "successfully verifying an account" do
+      fill_in "Amount 1", with: 1
+      fill_in "Amount 2", with: 1
 
-    fill_in "Amount 1", with: 1
-    fill_in "Amount 2", with: 1
+      click_button "Verify"
 
-    click_button "Verify"
+      expect(page).to have_content("Bank Accounts")
+      expect(Dom::BankAccount.first).to be_verfied
+    end
 
-    expect(page).to have_content("Bank Accounts")
-    expect(Dom::BankAccount.first).to be_verfied
+    scenario "unsuccessfully verifying an account" do
+      fill_in "Amount 1", with: 2
+      fill_in "Amount 2", with: 3
+
+      click_button "Verify"
+
+      expect(page).to have_content("Verify Your Account")
+    end
   end
 end
