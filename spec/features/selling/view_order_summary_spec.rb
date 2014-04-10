c294e3c92116daade2853fb637ac7134ad26e1ad
Chris Rittersdorf <chris.rittersdorf@collectiveidea.com>
2014-04-10 17:54:03 -0400

    Add tests for market manager and admin order summaries.

--- spec/features/selling/view_order_summary_spec.rb
+++ spec/features/selling/view_order_summary_spec.rb
@@ -1,6 +1,7 @@
 require "spec_helper"
 
 describe "Order summary" do
+  let!(:user) { create(:user, organizations: [sellers]) }
   let!(:admin) { create(:user, :admin) }
   let!(:market_manager) { create(:user) }
   let!(:market)  { create(:market, :with_addresses, managers:[market_manager]) }
@@ -36,15 +37,19 @@ describe "Order summary" do
   end
 
   context "as a seller" do
-    let!(:user) { create(:user, organizations: [sellers]) }
+    it "can be navigated to" do
+      switch_to_subdomain(market.subdomain)
+      sign_in_as(user)
 
-    before do
+      click_link "Order Summary"
+      expect(page).to have_content("Order Summary")
+    end
+
+    it "displays an order summary for the delivery" do
       switch_to_subdomain(market.subdomain)
       sign_in_as(user)
       visit admin_delivery_tools_order_summary_path(friday_delivery.id)
-    end
 
-    it "displays an order summary for the delivery", js: true do
       expect(page).to have_content("Order Summary")
 
       expect(page).to have_content(sellers.name)
@@ -65,6 +70,44 @@ describe "Order summary" do
   end
 
   context "display  a list of order summaries for a delivery" do
+    context "as a market manager" do
+      it "can be navigated to" do
+        switch_to_subdomain(market.subdomain)
+        sign_in_as(market_manager)
+        navigate_to_order_summary
+      end
+
+      it "lists order summary for the market" do
+        switch_to_subdomain(market.subdomain)
+        sign_in_as(market_manager)
+        visit admin_delivery_tools_order_summary_path(friday_delivery.id)
+        see_orders_for_entire_market
+      end
+    end
+
+    context "as an admin" do
+      it "can be navigated to" do
+        switch_to_subdomain(market.subdomain)
+        sign_in_as(admin)
+        navigate_to_order_summary
+      end
+
+      it "lists order summary for the market" do
+        switch_to_subdomain(market.subdomain)
+        sign_in_as(admin)
+        visit admin_delivery_tools_order_summary_path(friday_delivery.id)
+        see_orders_for_entire_market
+      end
+    end
+
+    def navigate_to_order_summary
+      click_link "Orders & Delivery"
+      click_link "Upcoming Deliveries"
+      click_link "Order Summary"
+
+      expect(page).to have_content("Order Summary")
+    end
+
     def see_orders_for_entire_market
       expect(page).to have_content("Order Summary")
 
@@ -87,20 +130,6 @@ describe "Order summary" do
 
       expect(page).to have_content(others_order.order_number)
       expect(page).to have_content(others_order_item.name)
-    end
-
-    it "as a market manager" do
-      switch_to_subdomain(market.subdomain)
-      sign_in_as(market_manager)
-      visit admin_delivery_tools_order_summary_path(friday_delivery.id)
-      see_orders_for_entire_market
-    end
-
-    it "as an admin" do
-      switch_to_subdomain(market.subdomain)
-      sign_in_as(admin)
-      visit admin_delivery_tools_order_summary_path(friday_delivery.id)
-      see_orders_for_entire_market
     end
   end
 end
