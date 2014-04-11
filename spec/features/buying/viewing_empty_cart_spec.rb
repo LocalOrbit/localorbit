d51eee4432f6bbbe6f9bbefb4ab49721f2b0ef07
Chris Rittersdorf <chris.rittersdorf@collectiveidea.com>
2014-04-11 10:26:28 -0400

    Prevent users from checking out by explicitly visiting the "/cart" URL.
    [Fixes #69319604]

--- spec/features/buying/viewing_empty_cart_spec.rb
+++ spec/features/buying/viewing_empty_cart_spec.rb
@@ -7,10 +7,13 @@ describe "Viewing an empty cart" do
   let!(:market_manager) { create(:user, :market_manager, managed_markets: [market]) }
   let!(:delivery) { create(:delivery_schedule, market: market) }
 
+  before do
+    switch_to_subdomain(market.subdomain)
+  end
+
   context "as a buyer" do
-    it "shows the cart" do
+    it "shows the cart", js: true do
       sign_in_as(user)
-      switch_to_subdomain(market.subdomain)
       expect(Dom::CartLink.first.node).to have_content("0")
       click_link "Cart", match: :first
       expect(page).to have_content("Your cart is empty.")
@@ -18,10 +21,9 @@ describe "Viewing an empty cart" do
     end
   end
 
-  context "as a market manager" do
+  context "as a market manager", js: true  do
     it "shows the cart" do
       sign_in_as(market_manager)
-      switch_to_subdomain(market.subdomain)
       expect(Dom::CartLink.first.node).to have_content("0")
       click_link "Cart"
       expect(page).to have_content("Your cart is empty.")
@@ -29,4 +31,10 @@ describe "Viewing an empty cart" do
     end
   end
 
+  it "prevents a user from checking out without cart items" do
+    sign_in_as(user)
+    visit "/cart"
+
+    expect(page).to have_content("Your cart is empty. Please add items to your cart before checking out.")
+  end
 end
