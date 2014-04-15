aa274049648a83d9da5c48bdfe8acb3b3e97397a
Tim Bugai <tim@bugai.net>
2014-04-15 08:59:39 -0400

    Suggested refactor from David

--- app/controllers/admin/financials_controller.rb
+++ app/controllers/admin/financials_controller.rb
@@ -1,7 +1,7 @@
 class Admin::FinancialsController < AdminController
   def index
     if current_user.seller? && !(current_user.admin? || current_user.market_manager?)
-      @payments = Payment.where(payee_type: "Organization", payee_id: current_organization.id).order("updated_at DESC")
+      @payments = Payment.where(payee: current_organization).order("updated_at DESC")
 
       render "payment_history"
     end
