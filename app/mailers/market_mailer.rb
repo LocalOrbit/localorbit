464a20d83684ec6f708b96c4636c33496b374265
David Genord II <david@collectiveidea.com>
2014-04-15 14:35:04 -0400

    Only load categories we need for products

--- app/mailers/market_mailer.rb
+++ app/mailers/market_mailer.rb
@@ -3,12 +3,12 @@ class MarketMailer < ActionMailer::Base
   default from: "service@localorb.it"
 
   def fresh_sheet(market, recipients=nil, preview=false)
-    @categories = Category.where(depth: 2)
     @preview        = preview
     @market         = market
     @delivery       = market.next_delivery.decorate
     @product_groups = Product.available_for_market(market).available_for_sale(market).
-      group_by {|p| p.category.self_and_ancestors.find_by(depth: 2).id }
+                              group_by {|p| p.category.self_and_ancestors.find_by(depth: 2).id }
+    @categories     = Category.where(depth: 2, id: @product_groups.keys)
 
     recipients ||= market.organizations.buying.joins(:users).
       where("users.send_freshsheet = ?", true).pluck(:name, :email).
