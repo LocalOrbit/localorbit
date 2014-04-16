cf0e94041ce5d00ce266587c56b34ae0df821a03
Daniel Morrison <daniel@collectiveidea.com>
2014-04-16 17:33:16 -0400

    Mock geocoding in specs

---
+++ spec/support/geocoder.rb
@@ -0,0 +1,29 @@
+class Graticule::Geocoder::Canned
+  class_attribute :next_response
+  self.next_response = nil
+  class_attribute :default
+  
+  def locate(address)
+    response = next_response || LOCATIONS[address.to_s] || default
+    self.next_response = nil
+    response
+  end
+end
+
+Geocode.geocoder = Graticule::Geocoder::Canned.new
+
+unless defined?(LOCATIONS)
+  LOCATIONS = {}
+  [
+     # name/query       locality        region country   precision      latitude        longitude       postal_code
+    [ 'San Francisco',  "San Francisco", "CA",  "US",   :street,        37.775206,      -122.419209,    94110 ],
+    [ 'Detroit',        "Detroit",       "MI",  "US",   :street,        42.3316,        -83.0475,       48201 ],
+    [ '49423',          "Holland",       "MI",  "US",   :postal_code,   42.767645,      -86.109469,     49423 ],
+  ].each do |row|
+    LOCATIONS[ row[0] ] = Graticule::Location.new(locality: row[1], region: row[2], country: row[3],
+                                                  precision: row[4], latitude: row[5], longitude: row[6],
+                                                  postal_code: row[7])
+  end
+end
+
+Geocode.geocoder.default = LOCATIONS['49423']
