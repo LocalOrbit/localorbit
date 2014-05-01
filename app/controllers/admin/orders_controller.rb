c7f6d76696deea31c20c8d982b42d837d7ce1f97
Tim Bugai <tim@bugai.net>
2014-05-01 15:11:17 -0400

    Paginate the orders list

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,6 +1,6 @@
 class Admin::OrdersController < AdminController
   def index
-    @orders = Order.orders_for_seller(current_user)
+    @orders = Order.orders_for_seller(current_user).page(params[:page]).per(params[:per_page])
   end
 
   def show
