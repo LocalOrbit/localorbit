5c3504273f3305f91ce47643e4e1593e2c0fe9b7
Tim Bugai <tim@bugai.net>
2014-05-07 15:23:55 -0400

    Add active flag to organizations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140507152304) do
+ActiveRecord::Schema.define(version: 20140507192333) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -365,6 +365,7 @@ ActiveRecord::Schema.define(version: 20140507152304) do
     t.boolean  "allow_ach"
     t.integer  "legacy_id"
     t.boolean  "show_profile",          default: true
+    t.boolean  "active",                default: true
   end
 
   create_table "payments", force: true do |t|
