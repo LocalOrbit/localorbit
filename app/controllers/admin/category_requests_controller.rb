08b3a55cafbe92ade18eeaadc990a9af04a799ce
David Genord II <david@collectiveidea.com>
2014-05-07 16:05:37 -0400

    Fix unpermitted params

--- app/controllers/admin/category_requests_controller.rb
+++ app/controllers/admin/category_requests_controller.rb
@@ -1,16 +1,10 @@
 module Admin
   class CategoryRequestsController < AdminController
     def create
-      ZendeskMailer.request_category(current_user.email, current_user.name, new_category_params[:product_category]).
+      ZendeskMailer.request_category(current_user.email, current_user.name, params[:product_category]).
                     deliver
 
       redirect_to new_admin_product_path
-    end
-
-    private
-
-    def new_category_params
-      params.permit([:product_category])
     end
   end
 end
