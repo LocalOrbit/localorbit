0b84a1a6fc23d934688d058f95d1bd2ecac1000b
Daniel Morrison <daniel@collectiveidea.com>
2014-05-08 17:07:42 -0400

    Fix sorting issues. [Fixes #70976142]

--- app/models/concerns/sortable.rb
+++ app/models/concerns/sortable.rb
@@ -1,9 +1,11 @@
 module Sortable
   extend ActiveSupport::Concern
 
-  def column_and_direction(string)
-    column, direction = string.split(":").map(&:to_sym)
-    direction = :asc unless direction.include?(:asc, :desc)
-    [column, direction]
+  module ClassMethods
+    def column_and_direction(string)
+      column, direction = string.split(":").map(&:to_sym)
+      direction = :asc unless [:asc, :desc].include?(direction)
+      [column, direction]
+    end
   end
 end
