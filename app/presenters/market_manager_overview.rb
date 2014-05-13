40e2fe9989a82d5e7031e99ba23aaaf3593741cc
Chris Rittersdorf <chris.rittersdorf@collectiveidea.com>
2014-05-13 16:06:11 -0400

    Refactors market manager financial overview logic.

--- app/presenters/market_manager_overview.rb
+++ app/presenters/market_manager_overview.rb
@@ -4,9 +4,12 @@ class MarketManagerOverview < FinancialOverview
     @calculation_method = :gross_total
   end
 
+  def overdue
+    sum_order_total(@po_orders.delivered.where("invoice_due_date < ?", @time.beginning_of_day))
+  end
+
   def money_out_next_seven
-    orders = []
-    orders += @po_orders.delivered.paid_between(
+    orders = @po_orders.delivered.paid_between(
       next_seven_days(offset: -7)
     )
 
@@ -14,31 +17,25 @@ class MarketManagerOverview < FinancialOverview
   end
 
   def money_in_today
-    orders = []
-
-    orders += @po_orders.due_between(today)
-
-    sum_seller_items(orders)
+    orders = @po_orders.due_between(today)
+    sum_order_total(orders)
   end
 
   def money_in_next_seven
-    orders = []
-
-    orders += @po_orders.delivered.due_between(next_seven_days)
-
-    sum_seller_items(orders)
+    orders = @po_orders.delivered.due_between(next_seven_days)
+    sum_order_total(orders)
   end
 
   def money_in_next_thirty
-    orders = []
-    orders += @po_orders.delivered.due_between(next_thirty_days)
-
-    sum_seller_items(orders)
+    # Don't include the "next 7 days" total
+    start = (next_seven_days.end + 1.day).beginning_of_day
+    orders = @po_orders.delivered.due_between(start..next_thirty_days.end)
+    sum_order_total(orders)
   end
 
   def money_in_purchase_orders
     orders = @po_orders.uninvoiced
-    orders.map(&:total_cost).reduce(:+)
+    sum_order_total(orders)
   end
 
   def lo_fees_next_seven_days
@@ -47,6 +44,10 @@ class MarketManagerOverview < FinancialOverview
   end
 
   private
+  def sum_order_total(orders)
+    orders.map(&:total_cost).reduce(:+)
+  end
+
   def sum_money_to_sellers(orders)
     orders.inject(0) do |total, order|
       total + order.items.map(&:seller_net_total).reduce(:+)
