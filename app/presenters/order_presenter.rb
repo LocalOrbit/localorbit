0893002fd83c75f2c54856dc02317adb4f4ec83b
Tim Bugai <tim@bugai.net>
2014-05-14 16:05:39 -0400

    Display market fees

--- app/presenters/order_presenter.rb
+++ app/presenters/order_presenter.rb
@@ -39,13 +39,18 @@ module OrderPresenter
     totals[:transaction]
   end
 
+  def market_fees
+    totals[:market]
+  end
+
   def totals
-    @totals ||= items.inject(discount: 0, gross: 0, net: 0, payment: 0, transaction: 0) do |totals, item|
+    @totals ||= items.inject(discount: 0, gross: 0, net: 0, payment: 0, transaction: 0, market: 0) do |totals, item|
       totals[:discount]    += item.discount
       totals[:gross]       += item.quantity * item.unit_price
       totals[:transaction] += item.local_orbit_seller_fee + item.market_seller_fee
       totals[:net]         += item.seller_net_total
       totals[:payment]     += item.payment_seller_fee
+      totals[:market]      += item.market_seller_fee
       totals
     end
   end
