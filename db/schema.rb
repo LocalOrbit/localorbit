be80e060645282ce70206b8f724cfdf2b6478b74
David Genord II <david@collectiveidea.com>
2014-05-20 16:35:14 -0400

    Update existing order items with the correct quantity_delivered

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140514195658) do
+ActiveRecord::Schema.define(version: 20140520203136) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
