839ee632b2444ff9d44c682bfc0011ad014f7143
Chris Rittersdorf <chris.rittersdorf@collectiveidea.com>
2014-05-21 12:28:00 -0400

    Refactor financial overview class structure.

--- app/presenters/financial_overview.rb
+++ app/presenters/financial_overview.rb
@@ -1,53 +1,55 @@
-class FinancialOverview
+module FinancialOverview
   def self.build(user, market)
     klass = if user.can_manage_market?(market)
-      MarketManagerOverview
+      MarketManager
     else
-      SellerOverview
+      Seller
     end
 
     klass.new(seller: user, market: market)
   end
 
-  def initialize(opts={})
-    @seller = opts[:seller]
-    @market = opts[:market]
-    @time = Time.current
+  class Base
+    def initialize(opts={})
+      @seller = opts[:seller]
+      @market = opts[:market]
+      @time = Time.current
 
-    base_order_scope = Order.orders_for_seller(@seller).where(market: @market)
-    @cc_ach_orders = base_order_scope.paid_with(["credit card", "ach"])
-    @po_orders = base_order_scope.paid_with("purchase order")
-  end
+      base_order_scope = Order.orders_for_seller(@seller).where(market: @market)
+      @cc_ach_orders = base_order_scope.paid_with(["credit card", "ach"])
+      @po_orders = base_order_scope.paid_with("purchase order")
+    end
 
-  def next_seven_days(offset: 0)
-    range_start = (@time + 1.day).beginning_of_day + offset.days
-    range_end = (range_start + 6.days).end_of_day
+    def next_seven_days(offset: 0)
+      range_start = (@time + 1.day).beginning_of_day + offset.days
+      range_end = (range_start + 6.days).end_of_day
 
-    range_start..range_end
-  end
+      range_start..range_end
+    end
 
-  def next_thirty_days(offset: 0)
-    range_start = (@time + 1.day).beginning_of_day + offset.days
-    range_end = (range_start + 29.days).end_of_day
+    def next_thirty_days(offset: 0)
+      range_start = (@time + 1.day).beginning_of_day + offset.days
+      range_end = (range_start + 29.days).end_of_day
 
-    range_start..range_end
-  end
+      range_start..range_end
+    end
 
-  def today(offset: 0)
-    start_of_day = (@time).beginning_of_day + offset.days
-    end_of_day = start_of_day.end_of_day
+    def today(offset: 0)
+      start_of_day = (@time).beginning_of_day + offset.days
+      end_of_day = start_of_day.end_of_day
 
-    start_of_day..end_of_day
-  end
+      start_of_day..end_of_day
+    end
 
-  def overdue
-    sum_seller_items(@po_orders.delivered.where("invoice_due_date < ?", @time.beginning_of_day))
-  end
+    def overdue
+      sum_seller_items(@po_orders.delivered.where("invoice_due_date < ?", @time.beginning_of_day))
+    end
 
-  private
-  def sum_seller_items(orders)
-    orders.inject(0) do |total, order|
-      total + order.items.for_user(@seller).map(&@calculation_method).reduce(:+)
+    private
+    def sum_seller_items(orders)
+      orders.inject(0) do |total, order|
+        total + order.items.for_user(@seller).map(&@calculation_method).reduce(:+)
+      end
     end
   end
 end
---
+++ app/presenters/financial_overview/seller.rb
@@ -0,0 +1,26 @@
+module FinancialOverview
+  class Seller < FinancialOverview::Base
+    def initialize(opts={})
+      super
+      @calculation_method = :seller_net_total
+    end
+
+    def money_in_today
+      orders = []
+
+      orders += @cc_ach_orders.paid.delivered_between(today(offset: -7))
+      orders += @po_orders.delivered.paid_between(today(offset: -7))
+
+      sum_seller_items(orders)
+    end
+
+    def money_in_next_seven
+      orders = []
+
+      orders += @cc_ach_orders.paid.delivered_between(next_seven_days(offset: -7))
+      orders += @po_orders.delivered.paid_between(next_seven_days(offset: -7))
+
+      sum_seller_items(orders)
+    end
+  end
+end
