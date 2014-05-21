3b636ea19d696e8018c3320cfd5783647cf8d711
Chris Rittersdorf <chris.rittersdorf@collectiveidea.com>
2014-05-21 18:23:32 -0400

    Adds pagination to PaymentHistoryPresenter.

--- app/controllers/admin/financials/payments_controller.rb
+++ app/controllers/admin/financials/payments_controller.rb
@@ -1,7 +1,7 @@
 module Admin::Financials
   class PaymentsController < AdminController
     def index
-      @payment_history = PaymentHistoryPresenter.build(current_user, current_organization)
+      @payment_history = PaymentHistoryPresenter.build(current_user, current_organization, params[:page], params[:per_page])
     end
   end
 end
