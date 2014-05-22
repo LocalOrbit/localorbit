34f5892d195d0e06584664cd4df0cc8e8cae00ac
David Genord II <david@collectiveidea.com>
2014-05-22 14:37:49 -0400

    Fix random token auth failure

--- spec/features/token_auth_spec.rb
+++ spec/features/token_auth_spec.rb
@@ -4,7 +4,7 @@ feature "User can use an auth_token" do
   let!(:user) { create(:user, :admin) }
 
   scenario "can see a single page" do
-    visit "/dashboard?auth_token=#{user.auth_token}"
+    visit dashboard_path(auth_token: user.auth_token)
 
     within ".l-app-header" do
       expect(page).to have_content("Sign Out")
@@ -13,7 +13,7 @@ feature "User can use an auth_token" do
   end
 
   scenario "does not carry to the next request" do
-    visit "/dashboard?auth_token=#{user.auth_token}"
+    visit dashboard_path(auth_token: user.auth_token)
 
     click_link "Markets", match: :first
 
@@ -21,13 +21,13 @@ feature "User can use an auth_token" do
   end
 
   scenario "does not authorize invalid tokens" do
-    visit "/dashboard?auth_token=let-me-in"
+    visit dashboard_path(auth_token: 'let-me-in')
 
     expect(page).to have_content("Please Sign In")
   end
 
   scenario "does not authorize expired tokens" do
-    visit "/dashboard?auth_token=#{user.auth_token(-10.minutes)}"
+    visit dashboard_path(auth_token: user.auth_token(-10.minutes))
 
     expect(page).to have_content("Please Sign In")
   end
