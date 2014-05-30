10e39efb7ec9aca30395c2802ae646aabdde5296
Tim Bugai <tim@bugai.net>
2014-05-30 16:50:32 -0400

    Add class override to the migration

--- db/migrate/20140530195231_update_percentage_delivery_fees.rb
+++ db/migrate/20140530195231_update_percentage_delivery_fees.rb
@@ -1,4 +1,6 @@
 class UpdatePercentageDeliveryFees < ActiveRecord::Migration
+  class DeliverySchedule < ActiveRecord::Base; end
+
   def up
     DeliverySchedule.where(fee_type: 'percent').each do |ds|
       ds.update(fee: ds.fee * 100) if (0..1) === ds.fee
