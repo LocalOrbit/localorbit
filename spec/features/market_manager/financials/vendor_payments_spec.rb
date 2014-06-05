32930ff51326b929c9b3e986c153c56b01b7f706
Chris Rittersdorf <chris.rittersdorf@collectiveidea.com>
2014-06-05 10:33:36 -0400

    Updates test data to support multiple markets.

--- spec/features/market_manager/financials/vendor_payments_spec.rb
+++ spec/features/market_manager/financials/vendor_payments_spec.rb
@@ -1,34 +1,31 @@
 require "spec_helper"
 
 feature "Payments to vendors" do
-  let(:market) { create(:market, name: "Baskerville Co-op", po_payment_term: 14) }
-  let!(:delivery_schedule) { create(:delivery_schedule, market: market, day: 3.days.ago.wday) }
-  let!(:delivery) { Timecop.freeze(5.days.ago) { delivery_schedule.next_delivery } }
-  let!(:market_manager) { create :user, managed_markets: [market] }
+  let(:market1) { create(:market, name: "Baskerville Co-op", po_payment_term: 14) }
+  let!(:market_1_delivery_schedule) { create(:delivery_schedule, market: market1, day: 3.days.ago.wday) }
+  let!(:market1_delivery) { Timecop.freeze(5.days.ago) { market_1_delivery_schedule.next_delivery } }
+  let!(:market_manager) { create :user, managed_markets: [market1] }
 
-  let!(:seller1) { create(:organization, :seller, name: "Better Farms", markets: [market]) }
-  let!(:seller2) { create(:organization, :seller, name: "Great Farms", markets: [market]) }
-  let!(:seller3) { create(:organization, :seller, name: "Betterest Farms", markets: [market]) }
-  let!(:seller4) { create(:organization, :seller, name: "Greater Farms", markets: [market]) }
-  let!(:buyer)  { create(:organization, :buyer, name: "Money Bags", markets: [market]) }
+  let!(:market1_seller1) { create(:organization, :seller, name: "Better Farms", markets: [market1]) }
+  let!(:market1_seller2) { create(:organization, :seller, name: "Great Farms", markets: [market1]) }
+  let!(:market1_seller3) { create(:organization, :seller, name: "Betterest Farms", markets: [market1]) }
+  let!(:market1_seller4) { create(:organization, :seller, name: "Greater Farms", markets: [market1]) }
+  let!(:market1_buyer)  { create(:organization, :buyer, name: "Money Bags", markets: [market1]) }
 
-  let!(:product1) { create(:product, :sellable, organization: seller1) }
-  let!(:product2) { create(:product, :sellable, organization: seller2) }
-  let!(:product3) { create(:product, :sellable, organization: seller2) }
-  let!(:product4) { create(:product, :sellable, organization: seller3) }
+  let!(:market1_product1) { create(:product, :sellable, organization: market1_seller1) }
+  let!(:market1_product2) { create(:product, :sellable, organization: market1_seller2) }
+  let!(:market1_product3) { create(:product, :sellable, organization: market1_seller2) }
+  let!(:market1_product4) { create(:product, :sellable, organization: market1_seller3) }
 
-  let!(:order1) { create(:order, items:[create(:order_item, :delivered, product: product1, quantity: 4)], market: market, organization: buyer, delivery: delivery, payment_method: "purchase order", order_number: "LO-001", total_cost: 27.96, placed_at: 19.days.ago) }
-  let!(:order2) { create(:order, items:[create(:order_item, :delivered, product: product2, quantity: 3), create(:order_item, :delivered, product: product4, quantity: 7)], market: market, organization: buyer, delivery: delivery, payment_method: "purchase order", order_number: "LO-002", total_cost: 69.90, placed_at: 6.days.ago, payment_status: "paid") }
-  let!(:order3) { create(:order, items:[create(:order_item, :delivered, product: product3, quantity: 6)], market: market, organization: buyer, delivery: delivery, payment_method: "purchase order", order_number: "LO-003", total_cost: 41.94, placed_at: 4.days.ago) }
-  let!(:order4) { create(:order, items:[create(:order_item, :delivered, product: product2, quantity: 9), create(:order_item, :delivered, product: product3, quantity: 14)], market: market, organization: buyer, delivery: delivery, payment_method: "purchase order", order_number: "LO-004", total_cost: 160.77, placed_at: 3.days.ago) }
-
-  before do
-    switch_to_subdomain(market.subdomain)
-    sign_in_as market_manager
-    visit admin_financials_vendor_payments_path
-  end
+  let!(:market1_order1) { create(:order, items:[create(:order_item, :delivered, product: market1_product1, quantity: 4)], market: market1, organization: market1_buyer, delivery: market1_delivery, payment_method: "purchase order", order_number: "LO-001", total_cost: 27.96, placed_at: 19.days.ago) }
+  let!(:market1_order2) { create(:order, items:[create(:order_item, :delivered, product: market1_product2, quantity: 3), create(:order_item, :delivered, product: market1_product4, quantity: 7)], market: market1, organization: market1_buyer, delivery: market1_delivery, payment_method: "purchase order", order_number: "LO-002", total_cost: 69.90, placed_at: 6.days.ago, payment_status: "paid") }
+  let!(:market1_order3) { create(:order, items:[create(:order_item, :delivered, product: market1_product3, quantity: 6)], market: market1, organization: market1_buyer, delivery: market1_delivery, payment_method: "purchase order", order_number: "LO-003", total_cost: 41.94, placed_at: 4.days.ago) }
+  let!(:market1_order4) { create(:order, items:[create(:order_item, :delivered, product: market1_product2, quantity: 9), create(:order_item, :delivered, product: market1_product3, quantity: 14)], market: market1, organization: market1_buyer, delivery: market1_delivery, payment_method: "purchase order", order_number: "LO-004", total_cost: 160.77, placed_at: 3.days.ago) }
 
   scenario "displays the correct items" do
+    switch_to_subdomain(market1.subdomain)
+    sign_in_as market_manager
+    visit admin_financials_vendor_payments_path
     seller_rows = Dom::Admin::Financials::VendorPaymentRow.all
 
     expect(seller_rows.size).to eq(3)
@@ -46,6 +43,10 @@ feature "Payments to vendors" do
   end
 
   scenario "de-selecting orders", :js do
+    switch_to_subdomain(market1.subdomain)
+    sign_in_as market_manager
+    visit admin_financials_vendor_payments_path
+
     seller_row = Dom::Admin::Financials::VendorPaymentRow.for_seller("Great Farms")
     seller_row.review
 
@@ -53,15 +54,15 @@ feature "Payments to vendors" do
 
     expect(orders.size).to eq(3)
     expect(orders[0].order_number).to eq('LO-002')
-    expect(orders[0].placed_at).to have_content(order2.placed_at.strftime("%b %d, %Y"))
+    expect(orders[0].placed_at).to have_content(market1_order2.placed_at.strftime("%b %d, %Y"))
     expect(orders[0].total).to have_content('$20.97')
 
     expect(orders[1].order_number).to eq('LO-003')
-    expect(orders[1].placed_at).to have_content(order3.placed_at.strftime("%b %d, %Y"))
+    expect(orders[1].placed_at).to have_content(market1_order3.placed_at.strftime("%b %d, %Y"))
     expect(orders[1].total).to have_content('$41.94')
 
     expect(orders[2].order_number).to eq('LO-004')
-    expect(orders[2].placed_at).to have_content(order4.placed_at.strftime("%b %d, %Y"))
+    expect(orders[2].placed_at).to have_content(market1_order4.placed_at.strftime("%b %d, %Y"))
     expect(orders[2].total).to have_content('$160.77')
 
     expect(seller_row.selected_owed).to have_content("$223.68")
@@ -72,6 +73,9 @@ feature "Payments to vendors" do
   end
 
   scenario "mark all orders for seller paid", :js do
+    switch_to_subdomain(market1.subdomain)
+    sign_in_as market_manager
+    visit admin_financials_vendor_payments_path
     seller_row = Dom::Admin::Financials::VendorPaymentRow.for_seller("Great Farms")
     seller_row.pay_all_now
 
@@ -90,6 +94,9 @@ feature "Payments to vendors" do
   end
 
   scenario "mark selected orders for seller paid", :js do
+    switch_to_subdomain(market1.subdomain)
+    sign_in_as market_manager
+    visit admin_financials_vendor_payments_path
     seller_row = Dom::Admin::Financials::VendorPaymentRow.for_seller("Great Farms")
     seller_row.review
 
@@ -116,4 +123,13 @@ feature "Payments to vendors" do
     expect(seller_rows[2].order_count).to have_content(/\A1 order from Baskerville Co-op Review/)
     expect(seller_rows[2].owed).to have_content("$41.94")
   end
+
+  scenario "filtering by market" do
+    switch_to_subdomain(market1.subdomain)
+    sign_in_as market_manager
+    visit admin_financials_vendor_payments_path
+  end
+
+  scenario "filtering by organization"
+  scenario "filtering by payment type"
 end
