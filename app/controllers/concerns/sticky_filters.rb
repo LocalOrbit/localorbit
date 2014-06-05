fd3238efb5693e543776a462550a465c1b7f7705
Jonathan Pichot <jonathan@pichot.us>
2014-06-05 13:02:10 -0400

    Clear page params from sticky filters when not set (on page 1)

--- app/controllers/concerns/sticky_filters.rb
+++ app/controllers/concerns/sticky_filters.rb
@@ -3,6 +3,7 @@ module StickyFilters
     path = request.path
     session[:sticky_parameters] ||= {}
     existing_parameters = session[:sticky_parameters][path]
+    existing_parameters.delete("page") if parameters["page"].nil?
     session[:sticky_parameters][path] = existing_parameters.nil? ? parameters : existing_parameters.merge(parameters)
     session[:sticky_parameters][path].reject! {|key, value| value == "" || key == "clear" }
     session[:sticky_parameters][path]
