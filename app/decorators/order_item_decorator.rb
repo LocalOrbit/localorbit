59c362a64fd61fdcfc2cf7a89780070984729221
Tres Trantham <tres@trestrantham.com>
2014-06-06 18:03:23 -0400

    Add ReportsPresenter to manage report view content
    Updates OrderItemDecorator with view helpers derived from
    ReportsPresenter.

--- app/decorators/order_item_decorator.rb
+++ app/decorators/order_item_decorator.rb
@@ -4,14 +4,70 @@ class OrderItemDecorator < Draper::Decorator
   delegate_all
 
   def quantity_with_unit
-    "#{quantity} #{unit}"
-  end
-
-  def placed_at
-    order.placed_at.strftime("%m/%d/%Y")
+    "#{object.quantity} #{unit}"
   end
 
   def price_per_unit
-    "#{number_to_currency(unit_price)}/#{unit}"
+    "#{number_to_currency(object.unit_price)}/#{unit}"
+  end
+
+  def placed_on
+    order.placed_at.strftime("%m/%d/%Y")
+  end
+
+  def order_number
+    order.order_number
+  end
+
+  def category_name
+    product.category.name.to_s.titleize
+  end
+
+  def product_name
+    product.name.to_s.titleize
+  end
+
+  def seller_name
+    object.seller_name.to_s.titleize
+  end
+
+  def buyer_name
+    order.organization.name.to_s.titleize
+  end
+
+  def market_name
+    order.market.name.to_s.titleize
+  end
+
+  def unit_price
+    number_to_currency(object.unit_price)
+  end
+
+  def discount
+    number_to_currency(object.discount)
+  end
+
+  def quantity
+    number_with_delimiter(object.quantity)
+  end
+
+  def row_total
+    number_to_currency(object.gross_total)
+  end
+
+  def net_sale
+    number_to_currency(object.seller_net_total)
+  end
+
+  def delivery_status
+    object.delivery_status.to_s.titleize
+  end
+
+  def buyer_payment_status
+    order.payment_status.to_s.titleize
+  end
+
+  def seller_payment_status
+    object.seller_payment_status.to_s.titleize
   end
 end
---
+++ app/presenters/reports_presenter.rb
@@ -0,0 +1,51 @@
+class ReportsPresenter
+  attr_reader :report, :items, :fields, :q, :markets, :sellers
+
+  FIELD_MAP = {
+    placed_on:              { sort: :created_at,              display_name: "Placed On" },
+    category_name:          { sort: :product_category_name,   display_name: "Category" },
+    product_name:           { sort: :name,                    display_name: "Product" },
+    seller_name:            { sort: :seller_name,             display_name: "Seller" },
+    buyer_name:             { sort: :order_organization_name, display_name: "Buyer" },
+    market_name:            { sort: :order_market_name,       display_name: "Market" },
+    quantity:               { sort: :quantity,                display_name: "Quantity" },
+    unit_price:             { sort: :unit_price,              display_name: "Unit Price" },
+    discount:               { sort: :discount,                display_name: "Discount" },
+    row_total:              { sort: :nil,                     display_name: "Row Total" },
+    net_sale:               { sort: :nil,                     display_name: "Net Sale" },
+    delivery_status:        { sort: :delivery_status,         display_name: "Delivery" },
+    buyer_payment_status:   { sort: :order_payment_status,    display_name: "Buyer Payment Status" },
+    seller_payment_status:  { sort: nil,                      display_name: "Seller Payment Status" }
+  }.with_indifferent_access
+
+  REPORT_FIELD_MAP = {
+    total_sales: [
+      :placed_on, :product_name, :seller_name, :quantity, :unit_price, :discount,
+      :row_total, :net_sale, :delivery_status, :buyer_payment_status, :seller_payment_status
+    ],
+    sales_by_seller: [
+      :placed_on, :category_name, :product_name, :seller_name, :quantity, :unit_price, :discount,
+      :row_total, :net_sale, :delivery_status, :buyer_payment_status, :seller_payment_status
+    ]
+  }.with_indifferent_access
+
+  def self.reports
+    REPORT_FIELD_MAP.keys
+  end
+
+  def initialize(report:, user:, search: {}, paginate: {})
+    @report = report
+
+    # TODO: includes appropriate associations based on report
+    items = OrderItem.for_user(user).joins(:order)
+
+    # Initialize ransack and set a default sort order
+    @q = items.search(search)
+    @q.sorts = "created_at desc" if @q.sorts.empty?
+
+    @items = @q.result.page(paginate[:page]).per(paginate[:per_page])
+    @markets = Market.for_order_items(@items)
+    @sellers = @items.pluck(:seller_name).uniq
+    @fields = REPORT_FIELD_MAP[report]
+  end
+end
