2be8daedc3bb011c99cd50b3a780dfc3ab2a7a8a
Tres Trantham <tres@trestrantham.com>
2014-06-06 12:36:11 -0400

    Update specs for buyer only restriction

--- spec/support/shared_examples/restrict_buyer_only_action.rb
+++ spec/support/shared_examples/restrict_buyer_only_action.rb
@@ -1,10 +1,21 @@
-shared_examples "an action that restricts access admin, market manager, or seller only" do |run_action|
+shared_examples "an action that restricts access admin, market manager, or seller only" do |action|
+  let(:market)                    { create(:market) }
+  let(:market2)                   { create(:market) }
+  let(:organization)              { create(:organization, :buyer, markets: [market]) }
+  let(:seller_organization)       { create(:organization, :seller, markets: [market]) }
+  let(:admin)                     { create(:user, :admin) }
+  let(:market_manager_member)     { create(:user, managed_markets: [market]) }
+  let(:market_manager_non_member) { create(:user, managed_markets: [market2]) }
+  let(:member)                    { create(:user, organizations: [organization]) }
+  let(:non_member)                { create(:user) }
+  let(:market_seller)             { create(:user, organizations: [seller_organization]) }
+
   def meet_expected_expectation
-    %w[total_sales].include?(controller.action_name) ? be_a_success : be_a_redirect
+    %w[index total_sales].include?(controller.action_name) ? be_a_success : be_a_redirect
   end
 
   it "redirects to login given no user" do
-    instance_exec(&run_action)
+    instance_exec(&action)
 
     expect(response).to redirect_to(new_user_session_path)
   end
@@ -12,31 +23,39 @@ shared_examples "an action that restricts access admin, market manager, or selle
   it "renders 404 if logged in user is a buyer only" do
     sign_in create(:user)
 
-    instance_exec(&run_action)
+    instance_exec(&action)
 
     expect(response).to be_not_found
   end
 
-  it "runs successfully for an admin" do
-    sign_in create(:user, :admin)
+  it "prevents access to organization members" do
+    sign_in member
 
-    instance_exec(&run_action)
+    instance_exec(&action)
+
+    expect(response).to be_not_found
+  end
+
+  it "grants access to admins" do
+    sign_in admin
+
+    instance_exec(&action)
 
     expect(response).to meet_expected_expectation
   end
 
-  it "runs successfully for a market manager" do
-    sign_in create(:user, :market_manager)
+  it "grants access to market managers" do
+    sign_in market_manager_member
 
-    instance_exec(&run_action)
+    instance_exec(&action)
 
     expect(response).to meet_expected_expectation
   end
 
-  it "runs successfully for a seller" do
-    sign_in create(:user, :seller)
+  it "grants access to sellers" do
+    sign_in market_seller
 
-    instance_exec(&run_action)
+    instance_exec(&action)
 
     expect(response).to meet_expected_expectation
   end
