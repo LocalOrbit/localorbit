d906478673bf6ea91cbcf96cabc23185648e27a6
Tim Bugai <tim@bugai.net>
2014-06-10 08:15:14 -0400

    Merge the temporary credit card id into the order params

--- app/interactors/create_temporary_credit_card.rb
+++ app/interactors/create_temporary_credit_card.rb
@@ -4,11 +4,10 @@ class CreateTemporaryCreditCard
   def perform
     unless order_params["credit_card"]["id"].present?
       temp_card = cart.organization.bank_accounts.create(order_params["credit_card"])
-      temp_card.soft_delete
-
-      context[:temp_card] = temp_card
-
-      unless temp_card.valid?
+      if temp_card.valid?
+        temp_card.soft_delete
+        context[:order_params]["credit_card"]["id"] = temp_card.id
+      else
         context.fail!
       end
     end
