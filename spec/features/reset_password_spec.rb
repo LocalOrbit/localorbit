c23f5004840d0d112b4082061f71d90828c16699
Chris Rittersdorf <chris.rittersdorf@collectiveidea.com>
2014-06-17 16:28:59 -0400

    Sets the default password token timeout to 1 week.
    [Contributes #73317752]

--- spec/features/reset_password_spec.rb
+++ spec/features/reset_password_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 feature "Reset password:" do
   let!(:market) { create(:market) }
   let!(:market2) { create(:market) }
+  let!(:market3) { create(:market) }
   let!(:organization) { create(:organization, markets:[market]) }
   let!(:organization2) { create(:organization, markets:[market2]) }
 
@@ -34,6 +35,36 @@ feature "Reset password:" do
 
       expect(email).to have_subject("Reset password instructions")
       expect(email).to have_body_text(market.name) #Implies market branding
+    end
+
+    scenario "following email link before 1 week expiration" do
+      send_reset_email(from_subdomain: market2.subdomain)
+
+      Timecop.travel(6.days.from_now) do
+        open_last_email
+        visit_in_email("Change my password")
+
+        fill_in "New password", with: "password1"
+        fill_in "Confirm new password", with: "password1"
+        click_button "Change Password"
+
+        expect(page).to have_content("Welcome #{user.email}")
+      end
+    end
+
+    scenario "following email link after 1 week expiration" do
+      send_reset_email(from_subdomain: market2.subdomain)
+
+      Timecop.travel(8.days.from_now) do
+        open_last_email
+        visit_in_email("Change my password")
+
+        fill_in "New password", with: "password1"
+        fill_in "Confirm new password", with: "password1"
+        click_button "Change Password"
+
+        expect(page).to have_content("Reset password token has expired")
+      end
     end
   end
 end
