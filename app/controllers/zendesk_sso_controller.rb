0dc18dd7b29436098c8f635c97fc9760a06deea7
Daniel Morrison <daniel@collectiveidea.com>
2014-06-24 16:45:44 -0400

    Add user external ID to zendesk SSO

--- app/controllers/zendesk_sso_controller.rb
+++ app/controllers/zendesk_sso_controller.rb
@@ -18,10 +18,11 @@ class ZendeskSSOController < ApplicationController
     jti = "#{iat}/#{SecureRandom.hex(18)}"
 
     payload = JWT.encode({
-      :iat   => iat, # Seconds since epoch, determine when this token is stale
-      :jti   => jti, # Unique token id, helps prevent replay attacks
-      :name  => user.name,
-      :email => user.email,
+      iat: iat, # Seconds since epoch, determine when this token is stale
+      jti: jti, # Unique token id, helps prevent replay attacks
+      name: user.name,
+      email: user.email,
+      external_id: user.id.to_s,
     }, ZENDESK_SHARED_SECRET.to_s)
 
     redirect_to zendesk_sso_url(payload)
