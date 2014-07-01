5f44859a00ab16c0009f88b0aa2a9769f817b98c
Daniel Morrison <daniel@collectiveidea.com>
2014-07-01 13:49:46 -0400

    Use email if name is blank in Zendesk

--- app/controllers/zendesk_sso_controller.rb
+++ app/controllers/zendesk_sso_controller.rb
@@ -23,7 +23,7 @@ class ZendeskSSOController < ApplicationController
     payload = JWT.encode({
       iat: iat, # Seconds since epoch, determine when this token is stale
       jti: jti, # Unique token id, helps prevent replay attacks
-      name: user.name,
+      name: user.name || user.email, # ZD doesn't like blank names
       email: user.email,
       external_id: user.id.to_s,
     }, ZENDESK_SHARED_SECRET.to_s)
