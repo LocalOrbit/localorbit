20a6b119cd5a790acf395223beba9256093f6f35
Daniel Morrison <daniel@collectiveidea.com>
2014-07-11 17:02:28 -0400

    Flag delivery change if can't resolve location properly

--- app/interactors/update_order_delivery.rb
+++ app/interactors/update_order_delivery.rb
@@ -2,16 +2,56 @@ class UpdateOrderDelivery
   include Interactor
 
   def perform
+    if schedule_and_location_changed?
+      if new_delivery.requires_location?
+        if order.organization.locations.count == 1
+          address = order.organization.locations.first
+        else
+          fail_and_notify
+          return
+        end
+      else
+        address = new_delivery.delivery_schedule.buyer_pickup_location
+      end
+
+      order.delivery_address = address.address
+      order.delivery_city    = address.city
+      order.delivery_state   = address.state
+      order.delivery_zip     = address.zip
+      order.delivery_phone   = address.phone
+    end
+
     order.delivery_id = delivery_id
     if order.valid?
       order.save
+      UpdateBalancedPurchase.perform(order: order)
     else
-      Honeybadger.notify(
-          :error_class   => "Change Order Delivery",
-          :error_message => "Can't change delivery on an order: #{order.errors.full_messages}",
-          :parameters    => {order_id: order.id, delivery_id: delivery_id}
-        )
-      fail!
+      fail_and_notify
     end
+  end
+
+  def new_delivery
+    @new_delivery ||= Delivery.find(delivery_id)
+  end
+
+  def schedule_and_location_changed?
+    delivery_schedule_changed? && delivery_location_changed?
+  end
+
+  def delivery_schedule_changed?
+    new_delivery.delivery_schedule_id != order.delivery.delivery_schedule_id
+  end
+
+  def delivery_location_changed?
+    new_delivery.delivery_schedule.buyer_pickup_location_id != order.delivery.delivery_schedule.buyer_pickup_location_id
+  end
+
+  def fail_and_notify
+    Honeybadger.notify(
+        :error_class   => "Change Order Delivery",
+        :error_message => "Can't change delivery on an order: #{order.errors.full_messages}",
+        :parameters    => {order_id: order.id, delivery_id: delivery_id}
+      )
+    context.fail!
   end
 end
--- spec/interactors/update_order_delivery_spec.rb
+++ spec/interactors/update_order_delivery_spec.rb
@@ -1,13 +1,17 @@
 require "spec_helper"
 
 describe UpdateOrderDelivery do
-  let!(:order) { build(:order) }
+  let!(:market) { create(:market) }
+  let!(:delivery) { create(:delivery, delivery_schedule:
+    create(:delivery_schedule, buyer_pickup_location: create(:market_address, market: market)
+  )) }
+  let!(:order) { create(:order, delivery: delivery, market: market) }
 
   context "saving successfully" do
     it "saves the new delivery on the order" do
       expect(order).to receive(:valid?).and_return(true)
       expect(order).to receive(:save).and_return(true)
-      UpdateOrderDelivery.perform(order: order, delivery_id: 123)
+      UpdateOrderDelivery.perform(order: order, delivery_id: delivery.id)
     end
   end
 
@@ -16,7 +20,23 @@ describe UpdateOrderDelivery do
       expect(order).to receive(:valid?).and_return(false)
       expect(order).not_to receive(:save)
       expect(Honeybadger).to receive(:notify)
-      UpdateOrderDelivery.perform(order: order, delivery_id: 123)
+      UpdateOrderDelivery.perform(order: order, delivery_id: delivery.id)
+    end
+  end
+
+  context "trouble changing delivery location" do
+    let!(:delivery2) { create(:delivery, delivery_schedule:
+      create(:delivery_schedule, buyer_pickup_location: nil)
+    ) }
+
+    before do
+      2.times { create(:location, organization: order.organization) }
+    end
+
+    it "fails and notifies" do
+      expect(order).not_to receive(:save)
+      expect(Honeybadger).to receive(:notify)
+      UpdateOrderDelivery.perform(order: order, delivery_id: delivery2.id)
     end
   end
 end
