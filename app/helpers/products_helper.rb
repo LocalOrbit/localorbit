15c04a7df5a5b69b7212808ae2f5a1421c84cdd8
Daniel Morrison <daniel@collectiveidea.com>
2014-07-11 17:51:00 -0400

    Change logic for showing delivery change [Completes #73344994]

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -3,8 +3,8 @@ module ProductsHelper
     current_market.delivery_schedules.visible.count > 1 || current_organization.locations.count > 1 || current_user.managed_organizations.count > 1
   end
 
-  def can_change_delivery_on_placed_order?(user)
-    current_market.delivery_schedules.visible.count > 1
+  def can_change_delivery_on_placed_order?(order)
+    current_user.can_manage_market?(order.market)
   end
 
   def inventory_tab_complete?
