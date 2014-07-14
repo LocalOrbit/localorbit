375952655d5b6d1da10a5158534da5994a34eca6
Chris Rittersdorf <chris.rittersdorf@collectiveidea.com>
2014-07-14 15:41:31 -0400

    Re-factors remove organization interactor

--- app/interactors/remove_organization_from_markets.rb
+++ app/interactors/remove_organization_from_markets.rb
@@ -2,31 +2,22 @@ class RemoveOrganizationFromMarkets
   include Interactor
 
   def perform
-    if market_ids.blank?
+    if primary_market_organizations_to_remove.empty?
       fail!(error: "Please choose at least one market to remove #{organization.name} from.")
       return
     end
 
-    removed_from = {
-      markets: organization.market_organizations.not_cross_selling.where(market_id: market_ids),
-      cross_sells: organization.market_organizations.where(cross_sell_origin_market_id: market_ids)
-    }
+    context[:message] = "Removed #{organization.name} from #{primary_market_organizations_to_remove.map(&:market).map(&:name).to_sentence}"
 
-    # Guards against case where admin remove reported incorrect removal from market
-    if removed_from[:markets].empty?
-      fail!(error: "Please choose at least one market to remove #{organization.name} from.")
-      return
-    end
+    primary_market_organizations_to_remove.soft_delete_all
+    cross_sell_market_organizations_to_remove.soft_delete_all
+  end
 
-    postfix = if (removed_from[:markets].size > 1)
-      "market membership(s)"
-    else
-      removed_from[:markets].first.market.name
-    end
+  def primary_market_organizations_to_remove
+    organization.market_organizations.not_cross_selling.where(market_id: market_ids)
+  end
 
-    context[:message] = "Removed #{organization.name} from #{postfix}"
-
-    removed_from[:markets].soft_delete_all
-    removed_from[:cross_sells].soft_delete_all
+  def cross_sell_market_organizations_to_remove
+    organization.market_organizations.where(cross_sell_origin_market_id: market_ids)
   end
 end
