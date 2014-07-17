479eae9a68f0c45785a29db7132eb95d20944342
David Genord II <david@collectiveidea.com>
2014-07-17 16:51:42 -0400

    Avoid serializing full objects when possible
    [Fixes #75226028]

--- app/interactors/send_newsletter.rb
+++ app/interactors/send_newsletter.rb
@@ -3,12 +3,12 @@ class SendNewsletter
 
   def perform
     if commit == "Send Test"
-      MarketMailer.delay.newsletter(newsletter, market, email)
+      MarketMailer.delay.newsletter(newsletter.id, market.id, email)
       context[:notice] = "Successfully sent a test to #{email}"
     elsif commit == "Send Now"
       emails = newsletter.recipients.map(&:pretty_email)
       emails.each do |email|
-        MarketMailer.delay.newsletter(newsletter, market, email)
+        MarketMailer.delay.newsletter(newsletter.id, market.id, email)
       end
       context[:notice] = "Successfully sent this Newsletter"
     else
