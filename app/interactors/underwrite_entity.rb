76cdd3751f5177d0396d909d81bd18a162dacb66
Daniel Morrison <daniel@collectiveidea.com>
2014-07-21 11:45:06 -0400

    Refactor UnderwriteEntity

--- app/interactors/underwrite_entity.rb
+++ app/interactors/underwrite_entity.rb
@@ -1,27 +1,42 @@
 class UnderwriteEntity
   include Interactor
 
-  def perform
+  def setup
     representative_params.delete_if {|k, v| v.blank? }
-    if !entity.balanced_underwritten? && representative_params.any?
-      context[:balanced_customer] ||= Balanced::Customer.find(entity.balanced_customer_uri)
+  end
 
-      balanced_customer.name      = representative_params[:name]
-      balanced_customer.ssn_last4 = representative_params[:ssn_last4]
-      balanced_customer.address   = representative_params[:address]
+  def perform
+    return unless can_update_underwriting?
+    context[:balanced_customer] ||= Balanced::Customer.find(entity.balanced_customer_uri)
 
-      if representative_params[:dob].present?
-        balanced_customer.dob = "#{representative_params[:dob][:year]}-#{'%02d' % representative_params[:dob][:month].to_i}"
-      end
+    balanced_customer.name      = representative_params[:name]
+    balanced_customer.ssn_last4 = representative_params[:ssn_last4]
+    balanced_customer.address   = representative_params[:address]
 
-      if representative_params[:ein].present?
-        balanced_customer.ein = representative_params[:ein].gsub(/[^\d]/, "")
-        balanced_customer.business_name = entity.name
-      end
+    set_date_of_birth(representative_params[:dob])
+    set_business_fields(representative_params[:ein])
 
-      context[:balanced_customer] = balanced_customer.save
+    context[:balanced_customer] = balanced_customer.save
 
-      entity.update_attribute(:balanced_underwritten, balanced_customer.is_identity_verified?)
+    entity.update_attribute(:balanced_underwritten, balanced_customer.is_identity_verified?)
+  end
+
+  private
+
+  def can_update_underwriting?
+    !entity.balanced_underwritten? && representative_params.any?
+  end
+
+  def set_date_of_birth(date_params)
+    if date_params.present?
+      balanced_customer.dob = "#{date_params[:year]}-#{'%02d' % date_params[:month].to_i}"
+    end
+  end
+
+  def set_business_fields(ein)
+    if ein.present?
+      balanced_customer.ein = ein.gsub(/[^\d]/, "")
+      balanced_customer.business_name = entity.name
     end
   end
 end
