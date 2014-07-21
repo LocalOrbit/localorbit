9edc337d8b893d4c6e4f50eca7225d52969a1f3b
Matt Slack <slack@integernoun.com>
2014-07-21 16:34:47 -0400

    Hiding print buttons for Android [Finishes #73833362]

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,1 +1,1 @@
-Rails.application.config.assets.precompile += %w(balanced.js chosen-sprite.png chosen-sprite@2x.png wysiwyg.css invoices.css)
+Rails.application.config.assets.precompile += %w(balanced.js chosen-sprite.png chosen-sprite@2x.png wysiwyg.css invoices.css z-sniff.css)
