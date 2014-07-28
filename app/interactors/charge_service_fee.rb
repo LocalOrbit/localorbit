4b9e64679521f05990883b38b7875febc7413802
Daniel Morrison <daniel@collectiveidea.com>
2014-07-28 12:53:45 -0400

    Refactor ChargeServiceFee

--- app/interactors/charge_service_fee.rb
+++ app/interactors/charge_service_fee.rb
@@ -2,23 +2,10 @@ class ChargeServiceFee
   include Interactor
 
   def perform
-    payment = Payment.create({
-      payment_type:   "service",
-      market:         market,
-      payer:          market,
-      amount:         amount,
-      bank_account:   bank_account,
-      payment_method: bank_account.bank_account? ? "ach" : "credit card",
-      status:         bank_account.bank_account? ? "pending" : "paid"
-    })
+    payment = setup_payment
 
     begin
-      debit = market.balanced_customer.debit(
-        amount:                  (amount * 100).to_i,
-        source_uri:              bank_account.balanced_uri,
-        description:             "Local Orbit Service fee",
-        appears_on_statement_as: "Local Orbit"
-      )
+      debit = setup_debit
       payment.update_attributes(balanced_uri: debit.uri)
     rescue => e
       updates = {status: "failed"}
@@ -28,5 +15,28 @@ class ChargeServiceFee
       payment.update_attributes(updates)
       context.fail!
     end
+  end
+
+  private
+
+  def setup_debit
+    market.balanced_customer.debit(
+      amount:                  (amount * 100).to_i,
+      source_uri:              bank_account.balanced_uri,
+      description:             "Local Orbit Service fee",
+      appears_on_statement_as: "Local Orbit"
+    )
+  end
+
+  def setup_payment
+    Payment.create({
+      payment_type:   "service",
+      market:         market,
+      payer:          market,
+      amount:         amount,
+      bank_account:   bank_account,
+      payment_method: bank_account.bank_account? ? "ach" : "credit card",
+      status:         bank_account.bank_account? ? "pending" : "paid"
+    })
   end
 end
