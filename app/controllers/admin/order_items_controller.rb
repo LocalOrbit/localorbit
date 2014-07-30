9852023f905a17fe6e5f54d9fabb165752fdd392
Tim Bugai <tim@bugai.net>
2014-07-30 10:31:58 -0400

    Refactor OrderItemsController

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -4,17 +4,16 @@ module Admin
     include StickyFilters
 
     def index
-      @order_items = OrderItem.for_user(current_user).
-                       includes(order: :organization, product: :organization).
-                       preload(product: [:organization, :category], order: [:market, :organization])
-      prepare_filter_data(@order_items)
-
+      items = fetch_order_items
+      prepare_filter_data(items)
+      
       # initialize ransack and search
       @query_params = sticky_parameters(request.query_parameters)
       search = Search::QueryDefaults.new(@query_params[:q], :order_placed_at).query
 
-      @q = @order_items.search(search)
+      @q = items.search(search)
       @q.sorts = ["order_placed_at desc", "name"] if @q.sorts.empty?
+
       @order_items = @q.result
       @totals = OrderTotals.new(@order_items)
 
@@ -34,11 +33,37 @@ module Admin
 
     private
 
+    def fetch_order_items
+      OrderItem.for_user(current_user).
+        includes(order: :organization, product: :organization).
+        preload(product: [:organization, :category], order: [:market, :organization])
+    end
+
     def prepare_filter_data(order_items)
+      fetch_markets_list(order_items)
+      fetch_sellers_list(order_items)
+      fetch_buyers_list(order_items)
+      fetch_delivery_statuses(order_items)
+      fetch_buyer_payment_statuses(order_items)
+    end
+
+    def fetch_markets_list(order_items)
       @markets = Market.select(:id, :name).where(id: order_items.pluck("orders.market_id")).order(:name).uniq
+    end
+
+    def fetch_sellers_list(order_items)
       @sellers = Organization.select(:id, :name).where(id: order_items.joins(:product).pluck("products.organization_id")).order(:name).uniq
+    end
+
+    def fetch_buyers_list(order_items)
       @buyers = Organization.select(:id, :name).where(id: order_items.pluck("orders.organization_id")).order(:name).uniq
+    end
+
+    def fetch_delivery_statuses(order_items)
       @delivery_statuses = order_items.uniq.pluck(:delivery_status).sort
+    end
+
+    def fetch_buyer_payment_statuses(order_items)
       @buyer_payment_statuses = Order.joins(:items).uniq.merge(OrderItem.for_user(current_user)).pluck(:payment_status).sort
     end
   end
