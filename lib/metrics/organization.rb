e3e6b5d7ca0e08d91caca0feed6ec6f498d79a88
Tres Trantham <tres@trestrantham.com>
2014-07-30 15:26:18 -0400

    Refactor Metrics::Base

--- lib/metrics/organization.rb
+++ lib/metrics/organization.rb
@@ -1,19 +1,14 @@
 module Metrics
   class Organization < Base
-    BASE_SCOPE = ::Organization.where.not(id: MetricsPresenter::TEST_ORG_IDS).uniq
-    METRICS = {
+    cattr_accessor :base_scope, :metrics, :model_name
+
+    @@model_name = "Organization"
+    @@base_scope = ::Organization.where.not(id: MetricsPresenter::TEST_ORG_IDS).uniq
+    @@metrics = {
       total_buyer_only:   BASE_SCOPE.where(can_sell: false),
       total_sellers:      BASE_SCOPE.where(can_sell: true),
       total_buyers:       ::Organization.where(::Organization.arel_table[:id].in(BASE_SCOPE.select(:id).where(can_sell: false).union(BASE_SCOPE.select(:id).joins(:orders)))),
       total_buyer_orders: BASE_SCOPE.joins(:orders)
     }
-  end
-
-  def initialize
-    super(
-      base_scope: BASE_SCOPE,
-      metrics: METRICS,
-      model_name: "Organization"
-    )
   end
 end
