07007ae97b9f4d9c14efda4b463dc8a25e18d7af
Andrew Bredow <bredow@gmail.com>
2014-07-30 14:00:28 -0400

    Refactor the interface for metric calculations

--- lib/metrics/organization.rb
+++ lib/metrics/organization.rb
@@ -6,6 +6,14 @@ module Metrics
       total_sellers:      BASE_SCOPE.where(can_sell: true),
       total_buyers:       ::Organization.where(::Organization.arel_table[:id].in(BASE_SCOPE.select(:id).where(can_sell: false).union(BASE_SCOPE.select(:id).joins(:orders)))),
       total_buyer_orders: BASE_SCOPE.joins(:orders)
-    }.with_indifferent_access
+    }
+  end
+
+  def initialize
+    super(
+      base_scope: BASE_SCOPE,
+      metrics: METRICS,
+      model_name: "Organization"
+    )
   end
 end
