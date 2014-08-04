8d78c73fefd35fc75b36d5f6ae176927601735a3
Tres Trantham <tres@trestrantham.com>
2014-08-04 11:12:56 -0400

    Refactor metrics history to calc for current date
    This is idempotent so we can run it multiple times per day to get a
    running total for a given day. This allows historical metrics to be as
    current as possible as opposed to lagging one day behind "live" metrics.

--- spec/lib/metrics/base_spec.rb
+++ spec/lib/metrics/base_spec.rb
@@ -42,20 +42,23 @@ describe Metrics::TestMetrics do
       }.to change {
         Metric.count
       }.from(0).to(2)
+
       # Reminder: the metrics are calculated for the previous full day
-      expect(Metric.find_by(metric_code: "total_sellers", effective_on: 1.day.ago).model_ids).to eq([organization1.id])
-      expect(Metric.find_by(metric_code: "total_buyer_only", effective_on: 1.day.ago).model_ids).to eq([organization2.id])
+      expect(Metric.find_by(metric_code: "total_sellers", effective_on: Date.current).model_ids).to eq([organization1.id])
+      expect(Metric.find_by(metric_code: "total_buyer_only", effective_on: Date.current).model_ids).to eq([organization2.id])
 
       Timecop.freeze(1.day.from_now)
       organization1.update_attribute(:can_sell, false)
       organization2.update_attribute(:can_sell, true)
+
       expect {
         described_class.perform
       }.to change {
         Metric.count
       }.from(2).to(4)
-      expect(Metric.find_by(metric_code: "total_sellers", effective_on: 1.day.ago).model_ids).to eq([organization2.id])
-      expect(Metric.find_by(metric_code: "total_buyer_only", effective_on: 1.day.ago).model_ids).to eq([organization1.id])
+
+      expect(Metric.find_by(metric_code: "total_sellers", effective_on: Date.current).model_ids).to eq([organization2.id])
+      expect(Metric.find_by(metric_code: "total_buyer_only", effective_on: Date.current).model_ids).to eq([organization1.id])
     end
 
     context "when more than one calculation is run on the same day" do
@@ -65,6 +68,7 @@ describe Metrics::TestMetrics do
         }.to change {
           Metric.count
         }.from(0).to(2)
+
         expect {
           described_class.perform
         }.not_to change {
@@ -78,18 +82,21 @@ describe Metrics::TestMetrics do
         }.to change {
           Metric.count
         }.from(0).to(2)
-        expect(Metric.find_by(metric_code: "total_sellers", effective_on: 1.day.ago).model_ids).to eq([organization1.id])
-        expect(Metric.find_by(metric_code: "total_buyer_only", effective_on: 1.day.ago).model_ids).to eq([organization2.id])
+
+        expect(Metric.find_by(metric_code: "total_sellers", effective_on: Date.current).model_ids).to eq([organization1.id])
+        expect(Metric.find_by(metric_code: "total_buyer_only", effective_on: Date.current).model_ids).to eq([organization2.id])
 
         organization1.update_attribute(:can_sell, false)
         organization2.update_attribute(:can_sell, true)
+
         expect {
           described_class.perform
         }.not_to change {
           Metric.count
         }
-        expect(Metric.find_by(metric_code: "total_sellers", effective_on: 1.day.ago).model_ids).to eq([organization2.id])
-        expect(Metric.find_by(metric_code: "total_buyer_only", effective_on: 1.day.ago).model_ids).to eq([organization1.id])
+
+        expect(Metric.find_by(metric_code: "total_sellers", effective_on: Date.current).model_ids).to eq([organization2.id])
+        expect(Metric.find_by(metric_code: "total_buyer_only", effective_on: Date.current).model_ids).to eq([organization1.id])
       end
     end
   end
