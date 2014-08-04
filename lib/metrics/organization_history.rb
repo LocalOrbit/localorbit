f3c9b2550f3405d1c1451b4bad280e494f332478
Tres Trantham <tres@trestrantham.com>
2014-08-04 17:11:01 -0400

    Clean up total buyers calculation

--- lib/metrics/organization_history.rb
+++ lib/metrics/organization_history.rb
@@ -7,7 +7,7 @@ module Metrics
     @@metrics = {
       total_buyer_only:   { scope: self.base_scope.where(can_sell: false) },
       total_sellers:      { scope: self.base_scope.where(can_sell: true) },
-      total_buyers:       { scope: ::Organization.where(::Organization.arel_table[:id].in(self.base_scope.select(:id).where(can_sell: false).union(self.base_scope.select(:id).joins(:orders)))) },
+      total_buyers:       { scope: Organization.where(id: self.base_scope.select(:id).where(can_sell: false).union(self.base_scope.select(:id).joins(:orders)).pluck(:id)) },
       total_buyer_orders: { scope: self.base_scope.joins(:orders) },
       active_users:       { scope: self.base_scope.joins(products: :lots).where(Arel::Nodes::Group.new(Lot.arel_table[:expires_at].eq(nil).or(Lot.arel_table[:expires_at].gteq(Time.zone.now)))).union(self.base_scope.joins(:orders)).uniq }
     }
