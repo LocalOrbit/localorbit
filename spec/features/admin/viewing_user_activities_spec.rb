1a2466e3c70555b2c61b87a3d6765933b6c27f4f
Daniel Morrison <daniel@collectiveidea.com>
2014-08-07 12:30:18 -0400

    Turn off auditing for most tests for speed

--- spec/features/admin/viewing_user_activities_spec.rb
+++ spec/features/admin/viewing_user_activities_spec.rb
@@ -1,15 +1,17 @@
 require "spec_helper"
 
-feature "User Activities (User Event Log)", audit: true do
+feature "User Activities (User Event Log)" do
   let!(:market) { create(:market, name: "Foo Market", po_payment_term: 30, timezone: "Eastern Time (US & Canada)") }
   let!(:buyer)  { create(:organization, name: "Foo Buyer", markets: [market], can_sell: false) }
   let(:user)    { create(:user, :admin) }
 
   scenario "An admin can view user activities" do
+    Market.enable_auditing # auditing is turned off for test speed
     market.update!(name: "Bar Market", audit_comment: "This is a comment.")
+    Market.disable_auditing # turn it back off
 
     sign_in_as(user)
-    visit "/admin/activities?per_page=100"
+    visit "/admin/activities"
     expect(page).to have_content("User Event Log")
     expect(page).to have_content("Market update")
     expect(page).to have_content("Foo Market")
