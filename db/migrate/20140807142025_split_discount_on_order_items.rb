b3df3c4afd97abdc33e0e56fde265caf1f8933aa
Tim Bugai <tim@bugai.net>
2014-08-08 15:33:33 -0400

    Renamed the wrong field
    [ci skip]

--- db/migrate/20140807142025_split_discount_on_order_items.rb
+++ db/migrate/20140807142025_split_discount_on_order_items.rb
@@ -1,6 +1,6 @@
 class SplitDiscountOnOrderItems < ActiveRecord::Migration
   def change
-    add_column :order_items, :discount_seller, :decimal, precision: 10, scale: 2, null: false, default: 0
-    rename_column :order_items, :discount, :discount_market
+    add_column :order_items, :discount_market, :decimal, precision: 10, scale: 2, null: false, default: 0
+    rename_column :order_items, :discount, :discount_seller
   end
 end
