c0db64a323eeba5c5b06a25eac3f0f513a5c3086
Tres Trantham <tres@trestrantham.com>
2014-08-08 16:22:01 -0400

    Change Product calculations from window to history

--- lib/metrics/product_history.rb
+++ lib/metrics/product_history.rb
@@ -4,6 +4,7 @@ module Metrics
 
     @@model_name = BASE_SCOPE.name
     @@history_metrics = {
+      total_products:          { scope: BASE_SCOPE },
       total_products_simple:   { scope: BASE_SCOPE.where(use_simple_inventory: true) },
       total_products_advanced: { scope: BASE_SCOPE.where(use_simple_inventory: false) }
     }
