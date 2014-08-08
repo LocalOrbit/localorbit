9f890532f9944ba0d42242e24bb50a183b3fca42
Tres Trantham <tres@trestrantham.com>
2014-08-08 16:22:24 -0400

    Change Organization calcs from window to history

--- lib/metrics/organization_history.rb
+++ lib/metrics/organization_history.rb
@@ -4,10 +4,11 @@ module Metrics
 
     @@model_name = BASE_SCOPE.name
     @@history_metrics = {
-      total_buyer_only: { scope: BASE_SCOPE.where(can_sell: false) },
-      total_sellers:    { scope: BASE_SCOPE.where(can_sell: true) },
-      total_buyers:     { scope: Organization.where(id: BASE_SCOPE.select(:id).where(can_sell: false).union(BASE_SCOPE.select(:id).joins(:orders)).pluck(:id)) },
-      active_users:     { scope: BASE_SCOPE.joins(products: :lots).where(Arel::Nodes::Group.new(Lot.arel_table[:expires_at].eq(nil).or(Lot.arel_table[:expires_at].gteq(Time.zone.now)))).union(BASE_SCOPE.joins(:orders).where("orders.placed_at >= ?", Date.current.beginning_of_day)).uniq }
+      total_organizations:  { scope: BASE_SCOPE },
+      total_buyer_only:     { scope: BASE_SCOPE.where(can_sell: false) },
+      total_sellers:        { scope: BASE_SCOPE.where(can_sell: true) },
+      total_buyers:         { scope: Organization.where(id: BASE_SCOPE.select(:id).where(can_sell: false).union(BASE_SCOPE.select(:id).joins(:orders)).pluck(:id)) },
+      active_users:         { scope: BASE_SCOPE.joins(products: :lots).where(Arel::Nodes::Group.new(Lot.arel_table[:expires_at].eq(nil).or(Lot.arel_table[:expires_at].gteq(Time.zone.now)))).union(BASE_SCOPE.joins(:orders).where("orders.placed_at >= ?", Date.current.beginning_of_day)).uniq }
     }
   end
 end
