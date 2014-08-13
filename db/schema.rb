88acee389b88f430cdc5b3016a3ecb7bd67d333c
Chris Rittersdorf <chris.rittersdorf@collectiveidea.com>
2014-08-13 11:51:54 -0400

    Bump schema version.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140811204827) do
+ActiveRecord::Schema.define(version: 20140812212855) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
