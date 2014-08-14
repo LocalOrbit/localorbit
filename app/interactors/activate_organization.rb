35ff344f7e0e24cb486b176db2614c3fa8632b85
Chris Rittersdorf <chris.rittersdorf@collectiveidea.com>
2014-08-14 11:58:00 -0400

    Prevent an organization from receiving an email when it’s been auto-activated.

--- app/interactors/activate_organization.rb
+++ app/interactors/activate_organization.rb
@@ -6,7 +6,7 @@ class ActivateOrganization
     return if organization.can_sell?
 
     if market.auto_activate_organizations?
-      organization.update!(active: true)
+      organization.update!(active: true, needs_activated_notification: false)
     end
   end
 end
