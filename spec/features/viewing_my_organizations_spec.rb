a8db2092ba34168f8cb9793e021574ca374c0a0c
Chris Rittersdorf <chris.rittersdorf@collectiveidea.com>
2014-08-15 15:49:22 -0400

    Removes link to an organization for users who have been suspended.

--- spec/features/viewing_my_organizations_spec.rb
+++ spec/features/viewing_my_organizations_spec.rb
@@ -1,50 +1,49 @@
 require "spec_helper"
 
-feature "user viewing their organizations" do
-  let!(:market1) { create(:market) }
+feature "Viewing admin/organizations list" do
+  let!(:market1) { create(:market, managers: [market_manager]) }
+  let!(:market2) { create(:market) }
+
   let!(:org1) { create(:organization, markets: [market1]) }
   let!(:org2) { create(:organization, markets: [market1]) }
   let!(:org3) { create(:organization, markets: [market1]) }
+  let!(:org4) { create(:organization, markets: [market2]) }
+
   let!(:user) { create(:user, organizations: [org1, org2]) }
+  let!(:market_manager) { create(:user) }
 
-  context "as a user" do
-
+  context "as an organization member" do
     before do
-      u = user.user_organizations.find_by(organization: org1)
+      u = user.user_organizations.find_by(organization: org2)
       u.update_attributes(enabled: false)
+
       switch_to_subdomain(market1.subdomain)
       sign_in_as(user)
+      visit admin_organizations_path
     end
 
-    scenario "I can not view activity on organizations when im a suspended user" do
-      click_link "Account"
-      click_link "Your Organization"
+    scenario "I can see a list of organizations I belong to" do
+      expect(page).to have_link(org1.name)
+      expect(page).not_to have_link(org3.name)
+    end
 
-      expect(page).to have_content(org1.name)
+    scenario "I cannot click to edit an organization I've been suspended from" do
       expect(page).to have_content(org2.name)
-
-      expect(page).to_not have_content(org3.name)
-
-      expect(page).to have_link(org2.name)
-      expect(page).to_not have_link(org1.name)
+      expect(page).to_not have_link(org2.name)
     end
   end
 
   context "as a market manager" do
-
     before do
       switch_to_subdomain(market1.subdomain)
-      sign_in_as(user)
+      sign_in_as(market_manager)
+      visit admin_organizations_path
     end
 
-    scenario "I can view the organizations I am directly linked to" do
-      click_link "Account"
-      click_link "Your Organization"
-
+    scenario "I can click to edit any organization in my market" do
       expect(page).to have_content(org1.name)
       expect(page).to have_content(org2.name)
-
-      expect(page).not_to have_content(org3.name)
+      expect(page).to have_content(org3.name)
     end
   end
 end
