8722386e5e532ba60bc92aa51810a29737e36f97
Chris Rittersdorf <chris.rittersdorf@collectiveidea.com>
2014-08-21 12:19:14 -0400

    Whitespace fixes.

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -2,20 +2,19 @@ class UserDecorator < Draper::Decorator
   include Draper::LazyHelpers
   delegate_all
 
-
   def display_name
     name || email
   end
 
   def toggle_enabled_button
-
     return unless current_user.can_manage_user?(self)
+
     title = enabled_for_organization?(context[:org]) ? "Suspend" : "Enable"
     status = enabled_for_organization?(context[:org]) ? "alert" : "notice"
     
     link_to_opts = {
-        method: :patch,
-        class: "btn btn--small #{status}"
+      method: :patch,
+      class: "btn btn--small #{status}"
     }
 
     link_to(
