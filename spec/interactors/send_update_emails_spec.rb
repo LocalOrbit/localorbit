ed2f8701046bc092ac0b6c970d89cb3cf7931405
Daniel Morrison <daniel@collectiveidea.com>
2014-08-26 17:09:30 -0400

    Failing spec for overzealous order change emails [Refs #77644038]

--- spec/interactors/send_update_emails_spec.rb
+++ spec/interactors/send_update_emails_spec.rb
@@ -1,49 +1,52 @@
 require "spec_helper"
 
 describe SendUpdateEmails do
-  let!(:market)   { create(:market) }
+  let!(:market) { create(:market) }
 
-  let!(:seller1)  { create(:organization, :seller, markets: [market]) }
+  let!(:seller1) { create(:organization, :seller, markets: [market]) }
   let!(:product1) { create(:product, :sellable, organization: seller1) }
-  let!(:user1)    { create(:user, organizations: [seller1]) }
+  let!(:seller_user1)    { create(:user, organizations: [seller1]) }
 
-  let!(:seller2)  { create(:organization, :seller, markets: [market]) }
+  let!(:seller2) { create(:organization, :seller, markets: [market]) }
   let!(:product2) { create(:product, :sellable, organization: seller2) }
-  let!(:user2)    { create(:user, organizations: [seller2]) }
+  let!(:seller_user2) { create(:user, organizations: [seller2]) }
 
-  let!(:buyer)    { create(:organization, :buyer, markets: [market]) }
-  let!(:user3)    { create(:user, organizations: [buyer]) }
+  let!(:buyer) { create(:organization, :buyer, markets: [market]) }
+  let!(:buyer_user) { create(:user, organizations: [buyer]) }
 
   let!(:order)    { create(:order, market: market, organization: buyer) }
   let!(:item1)    { create(:order_item, product: product1, unit_price: 2.00, quantity: 4, order: order) }
   let!(:item2)    { create(:order_item, product: product2, unit_price: 4.00, quantity: 2, order: order) }
 
-  before do
-    Order.enable_auditing
-    OrderItem.enable_auditing
-    order.reload.update(updated_at: Time.current, items_attributes: {"0" => {id: item1.id, quantity: 5}})
-    OrderItem.disable_auditing
-    Order.disable_auditing
+  context "a update that should send an email" do
+    before do
+      Order.enable_auditing
+      OrderItem.enable_auditing
+      order.reload.update(updated_at: Time.current, items_attributes: {"0" => {id: item1.id, quantity: 5}})
+      OrderItem.disable_auditing
+      Order.disable_auditing
 
-    Audit.all.update_all(request_uuid: SecureRandom.uuid)
-  end
+      # Set all audits to be the same request
+      Audit.all.update_all(request_uuid: SecureRandom.uuid)
+    end
 
-  it "sends an email to users in the organization" do
-    expect_any_instance_of(OrderMailer).to receive(:buyer_order_updated).with(order)
+    it "sends an email to users in the organization" do
+      expect_any_instance_of(OrderMailer).to receive(:buyer_order_updated).with(order)
 
-    SendUpdateEmails.perform(order: order)
-  end
+      SendUpdateEmails.perform(order: order)
+    end
 
-  it "sends an email to sellers whose items have been updated" do
-    expect_any_instance_of(OrderMailer).to receive(:seller_order_updated).with(order, seller1)
+    it "sends an email to sellers whose items have been updated" do
+      expect_any_instance_of(OrderMailer).to receive(:seller_order_updated).with(order, seller1)
 
-    SendUpdateEmails.perform(order: order)
-  end
+      SendUpdateEmails.perform(order: order)
+    end
 
-  it "does not send an email to sellers whose items have not been updated" do
-    expect_any_instance_of(OrderMailer).to_not receive(:seller_order_updated).with(order, seller2)
+    it "does not send an email to sellers whose items have not been updated" do
+      expect_any_instance_of(OrderMailer).to_not receive(:seller_order_updated).with(order, seller2)
 
-    SendUpdateEmails.perform(order: order)
+      SendUpdateEmails.perform(order: order)
+    end
   end
 
   context "when an order item has been deleted" do
@@ -73,4 +76,28 @@ describe SendUpdateEmails do
     end
   end
 
+  context "a update that should not send emails" do
+    before do
+      Order.enable_auditing
+      OrderItem.enable_auditing
+      order.reload.update(updated_at: Time.current, items_attributes: {"0" => {id: item1.id, quantity_delivered: 4}})
+      OrderItem.disable_auditing
+      Order.disable_auditing
+
+      # Set all audits to be the same request
+      Audit.all.update_all(request_uuid: SecureRandom.uuid)
+    end
+
+    it "does not send an email to users in the organization" do
+      expect_any_instance_of(OrderMailer).not_to receive(:buyer_order_updated)
+
+      SendUpdateEmails.perform(order: order)
+    end
+
+    it "does not send an email to sellers whose items have not been updated" do
+      expect_any_instance_of(OrderMailer).not_to receive(:seller_order_updated)
+
+      SendUpdateEmails.perform(order: order)
+    end
+  end
 end
