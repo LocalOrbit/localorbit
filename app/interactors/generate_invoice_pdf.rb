d4c39ff1ce6b8b3a658b19873bd55d76aaeac203
Tim Bugai <tim@bugai.net>
2014-08-28 11:10:56 -0400

    Create interactor to generate a invoice pdf

---
+++ app/interactors/generate_invoice_pdf.rb
@@ -0,0 +1,24 @@
+class GenerateInvoicePdf
+  include Interactor
+
+  def perform
+    if context[:order].present?
+      invoice = BuyerOrder.new(order)
+
+      html = view.render( template: "admin/invoices/show.html.erb", locals: { invoice: invoice, user: nil } )
+
+      pdf = PDFKit.new(html, page_size: "letter")
+      order.invoice_pdf = pdf.to_file("/tmp/#{order.order_number}.pdf")
+      order.save
+    end
+  end
+
+  def view
+    action_view = ActionView::Base.new(ActionController::Base.view_paths, {})
+    action_view.extend ApplicationHelper
+    action_view.class_eval do
+      include Rails.application.routes.url_helpers
+    end
+    action_view
+  end
+end
