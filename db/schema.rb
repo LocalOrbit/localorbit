b8e9ef02bb1481a5b28b0aa6cd23840ed7412b29
Tim Bugai <tim@bugai.net>
2014-08-28 11:09:22 -0400

    Add fields for dragonfly to store the invoice

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140827130952) do
+ActiveRecord::Schema.define(version: 20140828145149) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -473,6 +473,8 @@ ActiveRecord::Schema.define(version: 20140827130952) do
     t.integer  "legacy_id"
     t.datetime "deleted_at"
     t.integer  "discount_id"
+    t.string   "invoice_pdf_uid"
+    t.string   "invoice_pdf_name"
     t.string   "delivery_status"
   end
 
@@ -539,6 +541,9 @@ ActiveRecord::Schema.define(version: 20140827130952) do
     t.boolean  "automatic_payments", default: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "promotions",         default: false, null: false
+    t.boolean  "advanced_pricing",   default: false, null: false
+    t.boolean  "advanced_inventory", default: false, null: false
   end
 
   create_table "prices", force: true do |t|
