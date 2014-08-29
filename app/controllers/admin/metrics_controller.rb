2c73d59347b497d69278ac5ec1e9a7c7dbba627f
Tres Trantham <tres@trestrantham.com>
2014-08-29 12:30:01 -0400

    Ensure deleted addresses are excluded from map

--- app/controllers/admin/metrics_controller.rb
+++ app/controllers/admin/metrics_controller.rb
@@ -18,7 +18,7 @@ class Admin::MetricsController < AdminController
   def map
     @mapbox_map_id = Figaro.env.mapbox_api_key
     markets = Market.where.not(id: Metrics::Base::TEST_MARKET_IDS).all.
-               joins("LEFT JOIN market_addresses ON (market_addresses.market_id = markets.id) AND (market_addresses.id = (SELECT market_addresses.id FROM market_addresses WHERE market_addresses.market_id = markets.id ORDER BY created_at ASC LIMIT 1))").
+               joins("LEFT JOIN market_addresses ON (market_addresses.market_id = markets.id) AND (market_addresses.id = (SELECT market_addresses.id FROM market_addresses WHERE market_addresses.market_id = markets.id AND market_addresses.deleted_at IS NULL ORDER BY created_at ASC LIMIT 1))").
                joins("INNER JOIN geocodings ON geocodings.geocodable_type = 'MarketAddress' AND geocodings.geocodable_id = market_addresses.id").
                includes(:plan, addresses: [:geocoding])
     @map_data = ActiveModel::ArraySerializer.new(markets, each_serializer: MarketMapSerializer).to_json
