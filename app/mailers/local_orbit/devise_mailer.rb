c18066459d8a47847a7ab8072c0e3dc05cbf8fc9
dcarpenter88 <dcarpenter88@mac.com>
2014-09-02 14:44:07 -0400

    Make invitations for buyers and sellers to join specific to the market.

--- app/mailers/local_orbit/devise_mailer.rb
+++ app/mailers/local_orbit/devise_mailer.rb
@@ -4,5 +4,23 @@ module LocalOrbit
       @token = token
       devise_mail(user, :reset_import_password_instructions, opts)
     end
+
+    def invitation_instructions(record, token, opts = {})
+      @token = token
+
+      if organization = record.organizations.first
+        @org_or_market_name = organization.name
+        @market = organization.original_market
+      elsif @market = record.primary_market
+        @org_or_market_name = @market.name
+      else
+        @org_or_market_name = "Local Orbit"
+      end
+
+      devise_mail(record, :invitation_instructions,
+                  opts.merge(
+                      reply_to: @market.try(:contact_email),
+                      subject: I18n.t("devise.mailer.invitation_instructions.subject", org_or_market_name: @org_or_market_name)))
+    end
   end
 end
