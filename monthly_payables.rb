b043016fd07187fa2714c5ad36bcf639b5304877
David Genord II <david@collectiveidea.com>
2014-09-03 12:36:40 -0400

    Tweek the output and add code to process the payments

--- monthly_payables.rb
+++ monthly_payables.rb
@@ -1,11 +1,12 @@
 # Payable LO fees
 
-orders = Order.payable_lo_fees.where('placed_at > ?', 2.months.ago).preload(:items); nil
+orders = Order.payable_lo_fees.where('placed_at > ?', 2.months.ago).preload(:items).joins(:market).except(:order).order("MAX(markets.name)", "orders.order_number"); nil
 grouped = orders.group_by {|o| o.market_id }; grouped.size
 lo_fees = Market.where(id: grouped.keys).inject({}) {|h,m| h[m.id] = m.local_orbit_seller_fee > m.local_orbit_market_fee ? m.local_orbit_seller_fee : m.local_orbit_market_fee; h }
 
 grouped.each do |market_id, orders|
   market = Market.find(market_id)
+  next if market.demo?
   if market.bank_accounts.any?(&:verified)
     name = market.name
   elsif market.bank_accounts.any? {|b| b.account_type == 'checking' || b.account_type == 'savings' }
@@ -18,21 +19,60 @@ grouped.each do |market_id, orders|
   orders.each do |order|
     order_total = order.delivery_fees * lo_fees[market_id] / 100 + order.items.each.sum {|i| i.local_orbit_seller_fee + i.local_orbit_market_fee }
     total += order_total
-    puts "\t#{order.order_number}: $#{'%.2f' % order_total}"
+    puts "\t#{order.order_number}: $#{'%.2f' % order_total}" if order_total > 0
   end
   puts "\tTotal: $#{'%.2f' % total}"
+end; nil
+
+# Process above payments
+grouped.each do |market_id, orders|
+  market = Market.find(market_id)
+  next if market.demo?
+
+  amount = orders.each.sum do |order|
+    order.delivery_fees * lo_fees[market_id] / 100 + order.items.each.sum {|i| i.local_orbit_seller_fee + i.local_orbit_market_fee }
+  end
+  if amount <= 10
+    puts "Total below $10 for #{market.name}"
+    next
+  end
+
+  if market.bank_accounts.where(verified: true).size != 1
+    puts "Could not select a bank account for #{market.name}"
+    next
+  end
+  bank_account = market.bank_accounts.where(verified: true).first
+  payment = Payment.create(
+    market_id: market_id,
+    payment_type: "lo fee",
+    amount: amount,
+    status: "pending",
+    payer: market,
+    payment_method: "ach",
+    bank_account_id: bank_account.id,
+    orders: orders
+  )
+
+  interactor = ProcessPaymentWithBalanced.perform(payment: payment, description: "Local Orbit transaction fees")
+  if interactor.success?
+    puts "Charged #{market.name} $#{"%0.2f" % amount}"
+  else
+    puts interactor.error.inspect
+  end
 end; nil
 
 
 
 # Payable Market fees
 
-orders = Order.payable_market_fees.preload(:items, :market); nil
+orders = Order.payable_market_fees.preload(:items, :market).joins(:market).except(:order).order("MAX(markets.name)", "orders.order_number"); nil
 grouped = orders.group_by {|o| o.market_id }; grouped.size
 lo_fees = Market.where(id: grouped.keys).inject({}) {|h,m| h[m.id] = m.local_orbit_seller_fee > m.local_orbit_market_fee ? m.local_orbit_seller_fee : m.local_orbit_market_fee; h }
 
 grouped.each do |market_id, orders|
-  puts Market.find(market_id).name
+  market = Market.find(market_id)
+  next if market.demo?
+  puts market.name
   delivery_total = BigDecimal.new('0')
   market_total = BigDecimal.new('0')
   orders.each do |order|
@@ -43,4 +83,62 @@ grouped.each do |market_id, orders|
     puts "\t#{order.order_number}: Delivery: $#{'%.2f' % delivery}  Market: $#{'%.2f' % market}"
   end
   puts "\tTotal: Delivery: $#{'%.2f' % delivery_total}  Market: $#{'%.2f' % market_total}"
+end; nil
+
+# Process payables
+
+grouped.each do |market_id, orders|
+  market = Market.find(market_id)
+  next if market.demo?
+
+  if market.bank_accounts.where(verified: true).size != 1
+    puts "Could not select a bank account for #{market.name}"
+    next
+  end
+
+  amount = orders.sum {|order| order.delivery_fees * (100 - lo_fees[market_id]) / 100 }
+  if amount > 0
+    payment = Payment.create(
+      market: market,
+      payee:  market,
+      payment_type: "delivery fee",
+      amount: amount,
+      status: "pending",
+      payment_method: "ach",
+      bank_account: market.bank_accounts.where(verified: true).first,
+      orders: orders
+    )
+
+    interactor = ProcessPaymentWithBalanced.perform(payment: payment, description: "Local Orbit delivery fees")
+    if interactor.success?
+      puts "Paid delivery fee to %s of $%.2f" % [market.name, amount]
+    else
+      puts interactor.error.inspect
+    end
+  else
+    puts "No payable delivery fees for #{market.name}"
+  end
+
+  amount = orders.sum {|order| order.items.each.sum {|i| i.market_seller_fee } }
+  if amount > 0
+    payment = Payment.create(
+      market: market,
+      payee:  market,
+      payment_type: "hub fee",
+      amount: amount,
+      status: "pending",
+      payment_method: "ach",
+      bank_account: market.bank_accounts.where(verified: true).first,
+      orders: orders
+    )
+
+    interactor = ProcessPaymentWithBalanced.perform(payment: payment, description: "Local Orbit market fees")
+    if interactor.success?
+      puts "Paid market fee to %s of $%.2f" % [market.name, amount]
+    else
+      puts interactor.error.inspect
+    end
+  else
+    puts "No payable market fees for #{market.name}"
+  end
 end; nil
