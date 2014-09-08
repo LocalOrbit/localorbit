2433221df68074c35b126b64c31ef2a35ca71d26
David Genord II <david@collectiveidea.com>
2014-09-08 15:57:54 -0400

    Updates to the service plan listing

--- app/controllers/admin/financials/service_payments_controller.rb
+++ app/controllers/admin/financials/service_payments_controller.rb
@@ -2,7 +2,7 @@ class Admin::Financials::ServicePaymentsController < AdminController
   before_action :require_admin
 
   def index
-    @markets = Market.active.sort_service_payment
+    @markets = Market.active.sort_service_payment.map(&:decorate)
   end
 
   def create
