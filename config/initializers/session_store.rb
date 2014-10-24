21a1cfa71aff8e9c48741addd87ad814a1d4ea72
David Crosby <crosby@atomicobject.com>
2014-10-24 16:44:51 -0400

    Session store using subdomain wildcard

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -3,4 +3,7 @@
 session_suffix = Figaro.env.deploy_env == 'production' ? '' : "_#{Figaro.env.deploy_env}"
 session_key = "_LocalOrbit_session_data#{session_suffix}"
 
-LocalOrbit::Application.config.session_store :cookie_store, key: session_key
+# Use .(domain) unless testing:
+domain = Rails.env.test? ? nil : ".#{Figaro.env.domain}"
+
+LocalOrbit::Application.config.session_store(:cookie_store, key: session_key, domain: domain)
