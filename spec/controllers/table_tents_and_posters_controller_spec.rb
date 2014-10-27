03cd244947d8dfff14ce5e1719abb9a45a565581
David Crosby <crosby@atomicobject.com>
2014-10-27 19:34:46 -0400

    TableTentsAndPosters controller spec for create is running.

--- spec/controllers/table_tents_and_posters_controller_spec.rb
+++ spec/controllers/table_tents_and_posters_controller_spec.rb
@@ -1,9 +1,12 @@
 require "spec_helper"
 
 describe TableTentsAndPostersController do
-  let(:buyer) { create(:user, :buyer) }
+
+  include_context "the mini market"
+
   before do
-    sign_in buyer
+    switch_to_subdomain mini_market.subdomain
+    sign_in barry
   end
 
   describe "#index" do
@@ -17,13 +20,43 @@ describe TableTentsAndPostersController do
     end
   end
 
-  describe "#create", :wip=>true do
-    let (:order) {create(:order, organization: buyer.organizations.first)}
+  [ "poster", "table_tent" ].each do |type_string|
+    describe "Creating #{type_string}s..." do
+      let(:printable_type) { type_string }
 
-    it "generates a pdf" do
-      expect(GenerateTableTentsOrPosters).to receive(:perform).with(order: order, type: "poster", include_product_names: false)
-      post :create, order_id: order.id, type: "poster", include_product_names: false
-      binding.pry
+      describe "#create", :wip=>true do
+        def expect_generate_pdf
+          expect(GenerateTableTentsOrPosters).to receive(:perform).
+            with(order: order, type: printable_type, include_product_names: false).
+            and_return(context)
+        end
+
+        let (:order) {create(:order, organization: buyer_organization)}
+        let (:pdf_result) { double "PDF Result", data: "the pdf data" }
+
+        describe "when PDF generation succeeds" do
+          let (:context) { double "Result Context", success?: true, pdf_result: pdf_result }
+
+          it "renders the PDF data" do
+            expect_generate_pdf
+            post :create, order_id: order.id, type: printable_type, include_product_names: false
+            expect(response.content_type).to eq "application/pdf"
+            expect(response.body).to eq pdf_result.data 
+          end
+        end
+
+        describe "when PDF generation fails" do
+          let (:context) { double "Result Context", success?: false, message: "Too much want" }
+
+          it "redirects to index and shows an error" do
+            expect_generate_pdf
+            post :create, order_id: order.id, type: printable_type, include_product_names: false
+            expect(response).to redirect_to([:order, :table_tents_and_posters, type: printable_type])
+
+            expect(flash[:alert]).to match(/generate.*#{printable_type}/i)
+          end
+        end
+      end
     end
   end
 end
