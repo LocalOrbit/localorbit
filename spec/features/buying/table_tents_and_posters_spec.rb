7372ba1aba8662ea151c71c8843c7f8313b23f59
David Crosby <crosby@atomicobject.com>
2014-10-30 21:48:09 -0400

    Spec for the poster pdf generation workflow

--- spec/features/buying/table_tents_and_posters_spec.rb
+++ spec/features/buying/table_tents_and_posters_spec.rb
@@ -11,11 +11,23 @@ feature "Downloading table tents or posters" do
     sign_in_as(user)
   end
 
-  scenario "lets users download a table tent for a placed order" do
+  scenario "lets users download a Table Tent for a placed order" do
     visit order_path(order)
     expect(page).to have_text "Download the table tents"
     find(".app-download-table-tents-btn").click
     expect(page).to have_text 'Table Tents (4" x 6")'
     expect(page).to have_text 'Why use table tents?'
+    click_on "Download the PDF"
+    expect(page.body).to match(/^%PDF-1.4/)
+  end
+
+  scenario "lets users download a Poster for a placed order" do
+    visit order_path(order)
+    expect(page).to have_text "Download the posters"
+    find(".app-download-posters-btn").click
+    expect(page).to have_text 'Posters (8.5" x 11")'
+    expect(page).to have_text 'Why use posters?'
+    click_on "Download the PDF"
+    expect(page.body).to match(/^%PDF-1.4/)
   end
 end
