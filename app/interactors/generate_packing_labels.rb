7f73d72777514875940bbe7c3e47d8de34c279c9
Mel Klimushyn <mklimushyn@gmail.com>
2014-11-07 16:33:45 -0500

    WIP. Test to work through function flow for GeneratePackingLabels. Changed qr_code to be nested in order. Unit test and implementation for make_order_infos.

--- app/interactors/generate_packing_labels.rb
+++ app/interactors/generate_packing_labels.rb
@@ -1,26 +1,28 @@
 class GeneratePackingLabels
+  include Interactor
+
   def perform
     delivery, request = require_in_context(:delivery, :request)
 
-    # order_infos = GeneratePackingLabels.make_order_infos(delivery)
-    # labels = GeneratePackingLabels.make_labels(order_infos)
-    # pages = GeneratePackingLabels.make_pages(labels)
-    #
-    # pdf_context = GeneratePdf.perform(
-    #   request: RequestUrlPresenter.new(request),
-    #   template: "avery_labels/labels", 
-    #   pdf_size: { page_size: "letter" },
-    #   params: {
-    #     pages: pages
-    #   })
-    #
-    # context[:pdf_result] = pdf_context.pdf_result
+    order_infos = GeneratePackingLabels.make_order_infos(delivery)
+    labels = GeneratePackingLabels.make_labels(order_infos)
+    pages = GeneratePackingLabels.make_pages(labels)
+
+    pdf_context = GeneratePdf.perform(
+      request: RequestUrlPresenter.new(request),
+      template: "avery_labels/labels",
+      pdf_size: { page_size: "letter" },
+      params: {
+        pages: pages
+      })
+
+    context[:pdf_result] = pdf_context.pdf_result
   end
 
-  # def self.make_order_infos(delivery)
-  #   raise "TODO"
-  # end
-  #
+  def self.make_order_infos(delivery)
+    delivery.orders.map {|order| make_order_info(order)}
+  end
+
   # def self.make_labels(order_infos)
   #   raise "TODO"
   # end
@@ -28,5 +30,36 @@ class GeneratePackingLabels
   # def self.make_pages(labels)
   #   raise "TODO"
   # end
-  #
+
+  private
+
+  def self.make_order_info(order)
+    market_logo_url = if(order.market.logo) then order.market.logo.url else "" end
+    order_info = {
+      deliver_on: order.delivery.deliver_on.strftime("%B %e, %Y"),
+      order_number: order.order_number,
+      buyer_name: order.organization.name,
+      market_logo_url: market_logo_url,
+      qr_code_url: get_qr_code(order),
+      products: order.items.map {|order_item| make_product_info(order_item)}
+    }
+  end
+
+  def self.make_product_info(order_item)
+    lot = order_item.lots.first
+    lot_desc = if(lot) then "Lot ##{lot.lot_id}" else nil end
+    product_info = {
+      product_name: order_item.name,
+      unit_desc: order_item.unit,
+      quantity: order_item.quantity,
+      lot_desc: lot_desc,
+      producer_name: order_item.seller_name
+    }
+  end
+
+  def self.get_qr_code(order)
+    ""
+  end
+
+
 end
--- spec/interactors/generate_packing_labels_spec.rb
+++ spec/interactors/generate_packing_labels_spec.rb
@@ -1,8 +1,68 @@
-describe GeneratePackingLabels do
+describe GeneratePackingLabels, wip:true do
+  let(:market) { create(:market) }
+
+  let!(:buyer) { create(:organization, :buyer, name: "Big Money", markets: [market]) }
+  let!(:seller) { create(:organization, :seller, name: "Good foodz", markets: [market]) }
+  let!(:product1) { create(:product, :sellable, name: "Giant Carrots", organization: seller) }
+  let!(:delivery_schedule) { create(:delivery_schedule, market: market) }
+  let!(:delivery) { create(:delivery, delivery_schedule: delivery_schedule, deliver_on: 2.days.from_now) }
+  let!(:order_items) do
+    [
+      create(:order_item, product: product1, seller_name: seller.name, name: product1.name, unit_price: 6.50, quantity: 5, quantity_delivered: 0, unit: "stuff", market_seller_fee: 0.75, delivery_status: "canceled", payment_status: "refunded"),
+    ]
+  end
+
+  let!(:order) { create(:order, items: order_items, organization: buyer, market: market, delivery: delivery, order_number: "LO-ADA-0000001", total_cost: order_items.sum(&:gross_total)) }
   subject { described_class }
 
-  it "works" do
+  product_info_object = {
+    product_name: "Giant Carrots",
+    unit_desc: "stuff",
+    quantity: 5,
+    lot_desc: "Lot #2",
+    producer_name: "Good foodz"
+  }
+
+  order_info = {
+    deliver_on: 2.days.from_now.strftime("%B %e, %Y"),
+    order_number: "LO-ADA-0000001",
+    buyer_name: "Big Money",
+    market_logo_url: "",
+    qr_code_url: "",
+  }
+
+  full_order_info = order_info.merge({products: [product_info_object]})
+
+  order_label_object = {
+    template: "avery_labels/order",
+    data: {
+      order: order_info,
+    }
+  }
+
+  product_label_object = {
+    template: "avery_labels/vertical_product",
+    data: {
+      order: order_info,
+      product: product_info_object
+    }
+  }
+
+  it "works by creating order infos, labels, and then pages" do
     s = subject
-    binding.pry
+    expect(s).to receive(:make_order_infos).and_return([full_order_info])
+    expect(s).to receive(:make_labels).and_return([order_label_object, product_label_object])
+    expect(s).to receive(:make_pages).and_return([{
+      a: order_label_object,
+      b: product_label_object
+    }])
+    context = s.perform(delivery: nil, request: double(:request, :base_url=>"/admin/delivery_tools/deliveries/1/packing_labels"))
+    expect(context.pdf_result.data.match(/^%PDF-1.4/)).to_not eq nil
+  end
+
+  describe "#make_order_info" do
+    it "creates an order_infos structure for a given order" do
+      expect(GeneratePackingLabels.make_order_infos(delivery)).to eq [full_order_info]
+    end
   end
 end
