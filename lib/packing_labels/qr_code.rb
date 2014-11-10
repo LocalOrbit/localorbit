5013bfd7796a9c4839fb929d265ceaefd9ab3a39
David Crosby <crosby@atomicobject.com>
2014-11-10 15:28:26 -0500

    WIP: qr code url creation

--- lib/packing_labels/qr_code.rb
+++ lib/packing_labels/qr_code.rb
@@ -1,10 +1,10 @@
 module PackingLabels
-   class QrCode
-     class << self
-       def make_qr_code(order)
-         raise "TODO"
-       end
+  class QrCode
+   class << self
+     def make_qr_code(order,host:)
+       raise "TODO"
+       # order_url = Rails.application.routes.url_helpers.order_path(host: host, order:order)
      end
    end
+  end
 end
-
--- spec/lib/packing_labels/qr_code_spec.rb
+++ spec/lib/packing_labels/qr_code_spec.rb
@@ -1,8 +1,20 @@
-describe PackingLabels::QrCode do
+require "spec_helper"
+
+describe PackingLabels::QrCode, wip:true do
   subject { described_class }
+  include_context "the mini market"
+  let(:order) {create(:order, organization: buyer_organization)}
 
   describe ".make_qr_code" do
-    it "works" 
+    # it "gets an order's url" do 
+    # end
+  end
+
+  describe ".get_order_url" do
+    it "gets and order's url" do
+      # expect(PackingLabels.get_order_url(order)).to eq order_path(order)
+       # order_url = Rails.application.routes.url_helpers.order_path(host: host, order:order)
+    end
   end
 
 end
