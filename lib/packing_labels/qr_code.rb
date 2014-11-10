b64e83a23526682e1a011422d2e827b560e654aa
David Crosby <crosby@atomicobject.com>
2014-11-10 17:49:52 -0500

    QrCode implemented to use Google Charts

--- lib/packing_labels/qr_code.rb
+++ lib/packing_labels/qr_code.rb
@@ -1,9 +1,23 @@
 module PackingLabels
+  # /orders/5
+  # /admin/orders/5 
   class QrCode
    class << self
      def make_qr_code(order,host:)
-       raise "TODO"
-       # order_url = Rails.application.routes.url_helpers.order_path(host: host, order:order)
+       order_url = Rails.application.routes.url_helpers.admin_order_url(host: host, id: order.id)
+       google_qr_code_url_for order_url
+     end
+
+     def google_qr_code_url_for(content)
+       query_string = {
+         cht: "qr",
+         chs: "300x300",
+         chl: Rack::Utils.escape(content),
+         chld: "H|0" # pixel padding control
+       }.map do |key,val| 
+         "#{key}=#{val}"
+       end.join("&")
+       "http://chart.apis.google.com/chart?#{query_string}"
      end
    end
   end
--- spec/lib/packing_labels/qr_code_spec.rb
+++ spec/lib/packing_labels/qr_code_spec.rb
@@ -11,9 +11,27 @@ describe PackingLabels::QrCode, wip:true do
   end
 
   describe ".get_order_url" do
+    let(:order) { create(:order) }
+    let(:host) { "http://the.webs.example.com" }
+    let(:order_url) { Rails.application.routes.url_helpers.admin_order_url(host:host, id:order.id) }
+
     it "gets and order's url" do
-      # expect(PackingLabels.get_order_url(order)).to eq order_path(order)
-       # order_url = Rails.application.routes.url_helpers.order_path(host: host, order:order)
+      qr_code_url = subject.make_qr_code(order, host:host)
+      expect(qr_code_url).to match(/chl=#{Rack::Utils.escape(order_url)}/)
+      expect(qr_code_url).to match(/http:\/\/chart.apis.google.com\/chart\?/)
+      puts qr_code_url
+    end
+  end
+
+
+  describe ".google_qr_code_url_for" do
+    let(:content_url) { "http://localorbit.com/admin/orders/0?thinger" }
+    let(:escaped_content_url) { Rack::Utils.escape(content_url) }
+
+    it "generates a Google Charts QR code URI by URL-escaping our content" do
+      qr_code_url = subject.google_qr_code_url_for(content_url)
+      expect(qr_code_url).to match(/chl=#{escaped_content_url}/)
+      expect(qr_code_url).to match(/http:\/\/chart.apis.google.com\/chart\?/)
     end
   end
 
