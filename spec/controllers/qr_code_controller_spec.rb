45228ad81d69ed911b34931d3241eed15e4baf42
David Crosby <crosby@atomicobject.com>
2014-11-11 13:14:35 -0500

    QR Code redirect logic is tested

--- spec/controllers/qr_code_controller_spec.rb
+++ spec/controllers/qr_code_controller_spec.rb
@@ -5,7 +5,7 @@ describe QrCodeController do
   let!(:order_item1) { create(:order_item, product: product1) }
   let!(:order) { create(:order, items: [order_item1], market: mini_market, organization: buyer_organization) }
 
-  let(:host) { "http://#{order.market.subdomain}.localtest.me" }
+  let(:host) { "http://#{order.market.subdomain}.#{Figaro.env.domain}" }
 
   before do
     switch_to_subdomain mini_market.subdomain
@@ -17,12 +17,21 @@ describe QrCodeController do
     get :order, id: order.id
   end
 
+  def see_redirected_to_buyer_order_url
+    expect(response).to redirect_to(order_url(host: host, id: order.id))
+  end
+
+  def see_redirected_to_admin_order_url
+    expect(response).to redirect_to(admin_order_url(host: host, id: order.id))
+  end
+
   describe "#order" do
     context "logged in as a buyer" do
       let(:user) { barry }
+
       it "redirects to the correct order url on the correct subdomain" do
         hit_qr_code_url
-        expect(response).to redirect_to(order_url(host: host, id: order.id))
+        see_redirected_to_buyer_order_url
       end
     end
 
@@ -31,34 +40,25 @@ describe QrCodeController do
 
       it "redirects to the correct order url on the correct subdomain" do
         hit_qr_code_url
-        expect(response).to redirect_to(admin_order_url(host: host, id: order.id))
+        see_redirected_to_admin_order_url
       end
     end
 
     context "logged in as a market manager" do
-      before do
-        switch_to_subdomain mini_market.subdomain
-        sign_in mary
-        switch_to_subdomain "app"
-      end
+      let(:user) { mary }
+
       it "redirects to the correct order url on the correct subdomain" do
-        get :order, id: order.id
-        host = "http://#{order.market.subdomain}.localtest.me"
-        expect(response).to redirect_to(admin_order_url(host: host, id: order.id))
+        hit_qr_code_url
+        see_redirected_to_admin_order_url
       end
     end
 
     context "logged in as an admin" do
-      before do
-        switch_to_subdomain mini_market.subdomain
-        sign_in aaron
-        switch_to_subdomain "app"
-      end
+      let(:user) { aaron }
 
       it "redirects to the correct order url on the correct subdomain" do
-        get :order, id: order.id
-        host = "http://#{order.market.subdomain}.localtest.me"
-        expect(response).to redirect_to(admin_order_url(host: host, id: order.id))
+        hit_qr_code_url
+        see_redirected_to_admin_order_url
       end
     end
   end
