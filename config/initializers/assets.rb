2a8fbede9ba586fc76dae42aae660a71a1e5049b
Mel Klimushyn <mklimushyn@gmail.com>
2014-11-12 10:44:18 -0500

    Add pdf_resizer and jquery to the precompiled assets.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,3 @@
-Rails.application.config.assets.precompile += %w(balanced.js chosen-sprite.png chosen-sprite@2x.png wysiwyg.css invoices.css z-sniff.css)
+Rails.application.config.assets.precompile += %w(balanced.js chosen-sprite.png chosen-sprite@2x.png wysiwyg.css invoices.css z-sniff.css pdf_resizer.js jquery.js)
 # Mapbox icons
 Rails.application.config.assets.precompile += %w(icons-000000.png icons-000000@2x.png)
