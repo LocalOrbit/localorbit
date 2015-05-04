2ebf41e8481d54cd631737b1f6a66cf0105b1ab3
David Crosby <crosby@atomicobject.com>
2015-05-04 18:47:02 -0400

    ids for providers

--- lib/payment_provider/stripe.rb
+++ lib/payment_provider/stripe.rb
@@ -1,6 +1,8 @@
 module PaymentProvider
   class Stripe
     class << self
+      def id; :stripe; end
+
       def place_order(buyer_organization:, user:, order_params:, cart:)
         PlaceStripeOrder.perform(payment_provider: :stripe, 
                                  entity: buyer_organization, 
