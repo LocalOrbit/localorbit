fdeec953b8118eba9f1cd0a3bb80938ecc2168de
David Crosby <crosby@atomicobject.com>
2015-05-05 17:30:02 -0400

    PaymentProvider in Admin::OrdersController

--- spec/factories.rb
+++ spec/factories.rb
@@ -241,6 +241,7 @@ FactoryGirl.define do
     organization
     market
     delivery
+    payment_provider 'balanced'
 
     sequence(:order_number) {|n| "LO-%s-%s-%07d" % [Time.now.strftime("%y"), market.try(:subdomain).to_s.upcase, n] }
     placed_at        { Time.current }
@@ -331,6 +332,7 @@ FactoryGirl.define do
     payment_method "purchase order"
     amount         199.99
     status         "paid"
+    payment_provider 'balanced'
 
     trait :checking do
       payment_type   "order"
