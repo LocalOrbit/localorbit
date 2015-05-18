b858db924aaa5789b2c9d095bc529e66abf669fc
David Crosby <crosby@atomicobject.com>
2015-05-18 08:25:45 -0400

    Clarifying transfer amount parameter in TransferPaid webhook handler to be amount_in_cents

--- lib/payment_provider/handlers/transfer_paid.rb
+++ lib/payment_provider/handlers/transfer_paid.rb
@@ -5,19 +5,23 @@ module PaymentProvider
       def self.extract_job_params(event)
         {
           transfer_id: event.data.object.id,
-          amount: event.data.object.amount,
+          amount_in_cents: event.data.object.amount,
           stripe_account_id: event.user_id
         }
       end
 
-      def self.handle(transfer_id:, stripe_account_id:, amount:)
+      def self.handle(transfer_id:, stripe_account_id:, amount_in_cents:)
         if stripe_account_id
           market = Market.where(stripe_account_id: stripe_account_id).first
           order_ids = PaymentProvider::Stripe.order_ids_for_market_payout_transfer(
-            transfer_id: transfer_id, stripe_account_id: stripe_account_id)
+            transfer_id: transfer_id, 
+            stripe_account_id: stripe_account_id)
           payment = PaymentProvider::Stripe.create_market_payment(
-            transfer_id: transfer_id, market: market, order_ids: order_ids, 
-            status: 'paid', amount: ::Financials::MoneyHelpers.cents_to_amount(amount))
+            transfer_id: transfer_id, 
+            market: market, 
+            order_ids: order_ids, 
+            status: 'paid', 
+            amount: ::Financials::MoneyHelpers.cents_to_amount(amount_in_cents))
           PaymentMailer.payment_received(market.managers.to_a, payment.id)
         end
       end
--- spec/lib/payment_provider/handlers/transfer_paid_spec.rb
+++ spec/lib/payment_provider/handlers/transfer_paid_spec.rb
@@ -3,17 +3,17 @@ require 'spec_helper'
 describe PaymentProvider::Handlers::TransferPaid do
   subject { described_class }
 
-  context '#extract_job_params' do
+  describe '.extract_job_params' do
     it 'extracts the account and transfer ids' do
       event = double(user_id: 'account id')
-      transfer = double(id: 'transfer id', amount: 'the amount')
+      transfer = double(id: 'transfer id', amount: 'the pennies')
       allow(event).to receive_message_chain('data.object') { transfer }
       expect(subject.extract_job_params(event)).
-        to eq(transfer_id: 'transfer id', stripe_account_id: 'account id', amount: 'the amount')
+        to eq(transfer_id: 'transfer id', stripe_account_id: 'account id', amount_in_cents: 'the pennies')
     end
   end
 
-  context '#handle' do
+  describe '.handle' do
     let!(:user) { create(:user) }
     let!(:market) { create(:market, stripe_account_id: 'account id', managers: [user]) }
     let!(:market2) { create(:market) }
@@ -21,7 +21,7 @@ describe PaymentProvider::Handlers::TransferPaid do
     it 'does nothing if no stripe_account_id is given' do
       expect(PaymentProvider::Stripe).to_not receive(:create_market_payment)
       expect(PaymentMailer).to_not receive(:payment_received)
-      subject.handle(transfer_id: 'transfer id', stripe_account_id: nil, amount: '1234')
+      subject.handle(transfer_id: 'transfer id', stripe_account_id: nil, amount_in_cents: '1234')
       expect(Payment.count).to eq(0)
     end
 
@@ -37,7 +37,7 @@ describe PaymentProvider::Handlers::TransferPaid do
       expect(PaymentMailer).to receive(:payment_received).with([user], 187)
 
       subject.handle(transfer_id: 'transfer id', stripe_account_id: 'account id', 
-                     amount: '1234')
+                     amount_in_cents: '1234')
     end
   end
 end
