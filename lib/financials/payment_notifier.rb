f7c1c0e2dd97361260619382c81c919687605c68
David Crosby <crosby@atomicobject.com>
2015-05-18 11:02:53 -0400

    Added async option to Financials::PaymentProvider

--- lib/financials/payment_notifier.rb
+++ lib/financials/payment_notifier.rb
@@ -1,24 +1,28 @@
 module Financials
   class PaymentNotifier
     class << self
-      def seller_payment_received(payment:)
+      def seller_payment_received(payment:, async:true)
         if payment and (seller_organization = payment.payee)
-          _notify_payment_received(payment, seller_organization.users)
+          _notify_payment_received(payment, seller_organization.users, async)
         end
       end
 
-      def market_payment_received(payment:)
+      def market_payment_received(payment:, async:true)
         if payment and (market = payment.payee)
-          _notify_payment_received(payment, market.managers)
+          _notify_payment_received(payment, market.managers, async)
         end
       end
 
       private
 
-      def _notify_payment_received(payment, users)
+      def _notify_payment_received(payment, users, async)
         if users.present?
           recipients = users.map(&:pretty_email)
-          ::PaymentMailer.delay.payment_received(recipients, payment.id) 
+          if async
+            ::PaymentMailer.delay.payment_received(recipients, payment.id) 
+          else
+            ::PaymentMailer.payment_received(recipients, payment.id).deliver
+          end
         end
         nil
       end
