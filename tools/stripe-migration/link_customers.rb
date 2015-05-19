ad0b7b1330c34461549bbd902a175bfc9962d5f4
David Crosby <crosby@atomicobject.com>
2015-05-18 20:59:57 -0400

    Initial market stripe customer id matchup

--- tools/stripe-migration/link_customers.rb
+++ tools/stripe-migration/link_customers.rb
@@ -1,9 +1,65 @@
+require 'yaml'
+require 'pry'
 module LinkCustomers
-  def link_market_customers(market)
+  extend self
+  def match_markets
+    markets = load_markets
+    # markets_by_bcid = index_markets(markets)
 
+    stripe_custs = load_stripe_customers
+    stripe_custs_by_bcid = index_stripe_custs(stripe_custs)
+
+    matched_markets = []
+    missed_markets = []
+
+    markets.each do |_,market|
+      bcid = market[:balanced_customer_id]
+      if bcid and sc = stripe_custs_by_bcid[bcid]
+        matched_markets << market.merge(stripe_customer_id: sc[:id])
+      else
+        missed_markets << market
+      end
+    end
+
+
+    write_yaml "tools/stripe-migration/matched_markets.yml", matched_markets
+    write_yaml "tools/stripe-migration/missed_markets.yml", missed_markets
+  end
+
+  def load_stripe_customers
+    YAML.load_file("tools/stripe-migration/downloaded_stripe_customers.yml")
+  end
+
+  def load_markets
+    YAML.load_file("#{lo_prod_dir}/markets.yml")
+  end
+
+  def index_markets(markets)
+    idx = {}
+    markets.each do |id,m|
+      bcid = m[:balanced_customer_id]
+      idx[bcid] = m if bcid
+    end
+    idx
+  end
+
+  def index_stripe_custs(stripe_custs)
+    idx = {}
+    stripe_custs.each do |sc|
+      bcid = sc[:metadata][:"balanced.customer_id"]
+      idx[bcid] = sc if bcid
+    end
+    idx
+  end
+
+  def lo_prod_dir
+    dir = "tools/stripe-migration/lo-prod-ids"
+  end
+
+  def write_yaml(fname, data)
+    File.write fname, YAML.dump(data)
+    puts "Wrote #{fname}"
   end
 end
 
-market_id = ARGV.shift
-market = Market.find(market_id)
-LinkCustomers.link_market_customers(market)
+LinkCustomers.match_markets
