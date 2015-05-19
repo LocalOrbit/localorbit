c085ad2cba5ed5396ea89e82d4bd66aea6b15220
David Crosby <crosby@atomicobject.com>
2015-05-19 12:18:11 -0400

    Code for discovering multiple references to same underlying bank account

--- tools/stripe-migration/link_accounts.rb
+++ tools/stripe-migration/link_accounts.rb
@@ -36,7 +36,44 @@ module LinkAccounts
     puts "  (of those, count of multi-matches): #{multi_count}"
     puts "Unmatched stripe accounts: #{miss_count}"
   end
+
+  def find_matching_in_list(bank_accounts, ba1, ignore:[])
+    bank_accounts.select do |ba0|
+      (
+        (!ignore.include?(ba0[:bank_account_id])) and (ba0[:last4] == ba1[:last4]) and (ba0[:bank_name] == ba1[:bank_name]) and (ba0[:routing_number] == ba1[:routing_number]) 
+      )
+    end
+  end
+
+  def dupes
+    bank_accounts = YAML.load_file('tools/stripe-migration/downloaded_stripe_bank_accounts.yml')
+    count = 0
+    accounted_for = []
+    bank_accounts.each do |ba|
+      matches = find_matching_in_list(bank_accounts,ba,ignore:accounted_for)
+      if matches.length == 1
+        # itself?
+        if matches.first == ba
+          # itself.
+          accounted_for << ba[:bank_account_id]
+        else
+          puts "HUH? 1 match but isn't us: ba=#{ba.inspect} matched:#{matches.first.inspect}"
+        end
+        
+      elsif matches.length == 0
+        # puts "WTF? 0 matches for #{ba.inspect}"
+      else
+        count += 1
+        puts "Matched #{ba[:bank_name]} #{ba[:last4]} #{matches.count} times"
+        matches.each do |m|
+          accounted_for << m[:bank_account_id]
+          puts "  #{m.inspect}"
+        end
+      end
+    end
+    puts "\nCases of dupes: #{count}"
+  end
 end
 
 
-LinkAccounts.go
+LinkAccounts.dupes
