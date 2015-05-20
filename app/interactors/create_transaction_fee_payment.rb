3f15ae1965d6b5847b4cdedfe0b9a2f6f612b05c
David Crosby <crosby@atomicobject.com>
2015-05-19 23:24:29 -0400

    .balanced filter when looking for orders to charge LO transactions fees for

--- app/interactors/create_transaction_fee_payment.rb
+++ app/interactors/create_transaction_fee_payment.rb
@@ -4,7 +4,7 @@ class CreateTransactionFeePayment
   def perform
     market       = Market.find(market_id)
     bank_account = market.bank_accounts.find(bank_account_id)
-    orders       = market.orders.find(order_ids)
+    orders       = market.orders.balanced.find(order_ids)
 
     context[:payment] = Payment.create(
       payment_provider: PaymentProvider::Balanced.id.to_s,
