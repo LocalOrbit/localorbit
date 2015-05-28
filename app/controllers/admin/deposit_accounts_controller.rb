a78938e2e927d3a757bc2ca6456f3632e86e5f0a
David Crosby <crosby@atomicobject.com>
2015-05-28 10:10:01 -0400

    More spiking of the Deposit Account feature

--- app/controllers/admin/deposit_accounts_controller.rb
+++ app/controllers/admin/deposit_accounts_controller.rb
@@ -2,25 +2,26 @@ class Admin::DepositAccountsController < AdminController
   before_action :load_market_and_set_payment_provider
 
   def index
-    @bank_accounts = [] #@market.bank_accounts.visible
+    # TODO: 
+    @bank_accounts = [] # @market.bank_accounts.visible
   end
 
   def new
-    @bank_account = @market.bank_accounts.build #TODO 
+    @bank_account = @market.bank_accounts.build #TODO something more abstract?
   end
 
   def create
-    redirect_to [:admin, @market, :bank_accounts], notice: "TODO: add payment method"
-    # results = PaymentProvider.add_payment_method(@payment_provider, 
+    redirect_to [:admin, @market, :deposit_accounts], notice: "TODO: add payment method"
+    # TODO:
+    # results = PaymentProvider.add_deposit_account(@payment_provider, 
     #                                              type: params[:type], 
-    #                                              entity: @entity, 
-    #                                              bank_account_params: bank_account_params,
-    #                                              representative_params: representative_params)
+    #                                              entity: @market, 
+    #                                              bank_account_params: bank_account_params)
     #
     # if results.success?
-    #   redirect_to [:admin, @entity, :bank_accounts], notice: "Successfully added a payment method"
+    #   redirect_to [:admin, @market, :deposit_accounts], notice: "Successfully added deposit account"
     # else
-    #   flash.now[:alert] = "Unable to save payment method"
+    #   flash.now[:alert] = "Unable to save deposit account"
     #   @bank_account = results.bank_account
     #   render :new
     # end
@@ -38,6 +39,10 @@ class Admin::DepositAccountsController < AdminController
     @market = Market.find(params[:market_id])
     @entity = @market
     @payment_provider = @market.payment_provider
+
+    if PaymentProvider.is_balanced?(@payment_provider)
+      redirect_to admin_market_path(id:@market.id)
+    end
   end
 
   def bank_account_params
