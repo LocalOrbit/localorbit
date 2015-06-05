a95c3cb67b50dbc1397fb0c6e8e035fd2e67c8c6
jczetta <jczetta@gmail.com>
2015-06-05 16:16:16 -0400

    Add migration for users table, tos recording refactor, specs for additional functionality

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150528141550) do
+ActiveRecord::Schema.define(version: 20150605170153) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -752,12 +752,12 @@ ActiveRecord::Schema.define(version: 20150528141550) do
   add_index "user_organizations", ["user_id"], name: "index_user_organizations_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
-    t.string   "email",                        default: "", null: false
-    t.string   "encrypted_password",           default: ""
+    t.string   "email",                          default: "", null: false
+    t.string   "encrypted_password",             default: ""
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                default: 0,  null: false
+    t.integer  "sign_in_count",                  default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -773,13 +773,14 @@ ActiveRecord::Schema.define(version: 20150528141550) do
     t.integer  "invited_by_id"
     t.string   "invited_by_type"
     t.string   "name"
-    t.integer  "invitations_count",            default: 0
+    t.integer  "invitations_count",              default: 0
     t.integer  "legacy_id"
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
     t.date     "accepted_terms_of_service_at"
+    t.string   "accepted_terms_of_service_from"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
