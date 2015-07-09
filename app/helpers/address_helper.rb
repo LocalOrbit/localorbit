1b90dc4c6464fb52a8fc3b4ded2bf838f04e443a
Mel Klimushyn <mel.klimushyn@atomicobject.com>
2015-07-09 10:59:57 -0400

    Remove state filtering on the AddressHelper.

--- app/helpers/address_helper.rb
+++ app/helpers/address_helper.rb
@@ -1,12 +1,12 @@
 module AddressHelper
-  ExcludedUsStates = %w(AA AK AE AP AS GU MP PR UM VI)
-  IncludedCaProvinces = %w(QC ON BC)
+  ExcludedUsStates = %w()
+  ExcludedCaProvinces = %w()
 
   def options_for_state_select
     return @options_for_state_select if @options_for_state_select
 
     us_states = Country["US"].states.reject do |s| ExcludedUsStates.include?(s) end
-    ca_provinces = Country["CA"].states.select do |s| IncludedCaProvinces.include?(s) end
+    ca_provinces = Country["CA"].states.reject do |s| ExcludedCaProvinces.include?(s) end
 
     to_option = -> ((key,state)) { [state["name"], key] }
     @options_for_state_select = us_states.map(&to_option) + ca_provinces.map(&to_option)
