386dde9f2aa2c6d8eef624f3f0b4533b8d7df765
Mel Klimushyn <mel.klimushyn@atomicobject.com>
2015-07-17 11:51:37 -0400

    WIP: Adding headers and footers to pdf.
    This reverts commit d6c5869947f36ff5361fdf22a8b964a86d472861.

--- app/controllers/admin/invoices_controller.rb
+++ app/controllers/admin/invoices_controller.rb
@@ -3,6 +3,8 @@ module Admin
     before_action :fetch_order
 
     def show
+
+      ClearInvoicePdf.perform(order: @order)
       if @order.invoice_pdf.present?
         redirect_to @order.invoice_pdf.remote_url
       else
@@ -11,6 +13,7 @@ module Admin
                                          request: RequestUrlPresenter.new(request))
         redirect_to action: :await_pdf
       end
+
     end
 
     def await_pdf
@@ -26,15 +29,41 @@ module Admin
         end
       end
     end
-    
+
     # Secret: peek at an HTML version of the Invoice
     def peek
       @invoice = BuyerOrder.new(@order)
       @market  = @invoice.market.decorate
       @needs_js = true
-      render "show", layout: false, locals: { invoice: @invoice, user: current_user }
-    end
 
+      market = @market
+      invoice = @invoice
+
+      @header_params = {
+        logo_stored: market.logo_stored?,
+        thumb_url: market.logo.thumb('200x150>').url,
+        name: market.name,
+        has_address: market.has_address?,
+        street_address: market.street_address,
+        city_state_zip: market.city_state_zip,
+        display_contact_phone: market.display_contact_phone,
+        contact_email: market.contact_email,
+        billing_organization_name: invoice.billing_organization_name,
+        billing_address: invoice.billing_address,
+        billing_city: invoice.billing_city,
+        billing_state: invoice.billing_state,
+        billing_zip: invoice.billing_zip,
+        order_number: invoice.order_number,
+        delivery_date: invoice.delivery_date,
+        delivery_date_present: invoice.delivery_date.present?,
+        payment_note: invoice.payment_note,
+        payment_note_present: invoice.payment_note.present?,
+        due_date: (invoice.invoice_due_date || market.po_payment_term.to_i.days.from_now).strftime("%-m/%-d/%Y"),
+        date: (invoice.invoiced_at || Date.current).strftime("%-m/%-d/%Y")
+      }.to_query
+
+      render "show", layout: false, locals: { invoice: @invoice, user: current_user, header_params: @header_params }
+    end
 
     private
 
---
+++ app/controllers/pdf_view_controller.rb
@@ -0,0 +1,39 @@
+class PdfViewController < ActionController::Base
+  
+  def header
+    market = header_params[:market]
+    invoice = header_params[:invoice]
+    render "header", locals: header_params
+  end
+
+  def footer
+    render "footer"
+  end
+
+  def header_params
+    params.permit(
+      :logo_stored,
+      :thumb_url,
+      :name,
+      :has_address,
+      :street_address,
+      :city_state_zip,
+      :display_contact_phone,
+      :contact_email,
+      :billing_organization_name,
+      :billing_address,
+      :billing_city,
+      :billing_state,
+      :billing_zip,
+      :order_number,
+      :delivery_date,
+      :delivery_date_present,
+      :payment_note,
+      :payment_note_present,
+      :due_date,
+      :date,
+      :page,
+      :topage
+    )
+  end
+end
