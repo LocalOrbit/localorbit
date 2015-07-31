801396ecdf6eef17a08aa3bfe036080a1108754f
Clara Nees <cnees@umich.edu>
2015-07-31 17:24:43 -0400

    Add GeneralProducts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150805123456) do
+ActiveRecord::Schema.define(version: 20150803182658) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -245,6 +245,26 @@ ActiveRecord::Schema.define(version: 20150805123456) do
   end
 
   add_index "fresh_sheets", ["market_id", "user_id"], name: "index_fresh_sheets_on_market_id_and_user_id", using: :btree
+
+  create_table "general_products", force: true do |t|
+    t.text     "name"
+    t.text     "who_story"
+    t.text     "how_story"
+    t.integer  "location_id"
+    t.string   "image_uid"
+    t.integer  "top_level_category_id"
+    t.datetime "deleted_at"
+    t.text     "short_description"
+    t.text     "long_description"
+    t.boolean  "use_all_deliveries",       default: true
+    t.string   "thumb_uid"
+    t.integer  "second_level_category_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "general_product_id"
+  end
+
+  add_index "general_products", ["general_product_id"], name: "index_general_products_on_general_product_id", using: :btree
 
   create_table "geocodes", force: true do |t|
     t.decimal "latitude",    precision: 15, scale: 12
@@ -700,6 +720,7 @@ ActiveRecord::Schema.define(version: 20150805123456) do
     t.integer  "second_level_category_id"
     t.string   "code"
     t.integer  "external_product_id"
+    t.integer  "general_product_id"
   end
 
   add_index "products", ["category_id"], name: "index_products_on_category_id", using: :btree
