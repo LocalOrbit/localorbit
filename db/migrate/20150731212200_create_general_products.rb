007fced668ba372e1c4fceefe39f05f4d13308dd
Clara Nees <cnees@umich.edu>
2015-07-31 17:24:43 -0400

    Add GeneralProducts

---
+++ db/migrate/20150731212200_create_general_products.rb
@@ -0,0 +1,19 @@
+class CreateGeneralProducts < ActiveRecord::Migration
+  def change
+    create_table :general_products do |t|
+      t.text :name
+      t.text :who_story
+      t.text :how_story
+      t.integer :location_id, index: true
+      t.string :image_uid
+      t.integer :top_level_category_id, index: true
+      t.datetime :deleted_at
+      t.text :short_description
+      t.text :long_description
+      t.boolean :use_all_deliveries, default: true
+      t.string :thumb_uid
+      t.integer :second_level_category_id
+      t.timestamps
+    end
+  end
+end
