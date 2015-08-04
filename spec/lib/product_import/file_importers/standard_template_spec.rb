5fcb4dbd2442c8a7477c899292c5033523d49fb1
Drew Colthorp <colthorp@atomicobject.com>
2015-08-04 10:43:44 -0400

    Fix standard template spec

--- spec/lib/product_import/file_importers/standard_template_spec.rb
+++ spec/lib/product_import/file_importers/standard_template_spec.rb
@@ -2,52 +2,6 @@ require 'spec_helper'
 
 describe ProductImport::FileImporters::StandardTemplate do
 
-  describe "extract stage" do
-    subject { described_class.new.transform_for_stages(:extract) }
-
-    let(:data) {
-      [
-        ["Seller Name" , "Product Name"                   , "Category Name" , "Short Description" , "Supplier  Product Number" , "Unit Name" , "Unit Description (optional)" , "Price" , "Customer Category" , "Customer Unit of Measure" , "Customer Original Price" , "Customer Unit of Measure"] , 
-        ["Bi-Rite"     , "DON JUAN CAKE FIG WITH ALMONDS" , "Miscellaneous" , "1 / 11# AVG"       , "31604"                    , "Each"      , "1 / 11# AVG"                 , "71.39" , "CONDIMENTS"        , "11# AVG"                  , "6.49"                    , "11# AVG"                   , "FALSE"] , 
-      ]
-    }
-
-    it "turns a table into hashes" do
-      success, fail = subject.transform_enum(data)
-      expect(fail).to eq([])
-
-      expect(success).to eq([
-        {
-          "Seller Name"=>"Bi-Rite",
-          "Product Name"=>"DON JUAN CAKE FIG WITH ALMONDS",
-          "Category Name"=>"Miscellaneous",
-          "Short Description"=>"1 / 11# AVG",
-          "Supplier  Product Number"=>"31604",
-          "Unit Name"=>"Each",
-          "Unit Description (optional)"=>"1 / 11# AVG",
-          "Price"=>"71.39",
-          "Customer Category"=>"CONDIMENTS",
-          "Customer Unit of Measure"=>"11# AVG",
-          "Customer Original Price"=>"6.49",
-          12=>"FALSE"
-        }
-      ])
-    end
-
-    described_class::REQUIRED_HEADERS.each do |required_key|
-      it "rejects if required field #{required_key} is missing" do
-        idx = data[0].index(required_key)
-        bad_data = data.deep_dup
-        bad_data.first[idx] = 'wrong'
-
-        success, fail = subject.transform_enum(bad_data)
-        expect(success.length).to eq(0)
-        expect(fail.length).to eq(2)
-      end
-    end
-  end
-
-
   describe "Processing a file" do
     it "parses and canonicalizes a standard_template file" do
       file = test_file("Bi-Rite Zynga Supplier Product Listing Import Template.xlsx")
