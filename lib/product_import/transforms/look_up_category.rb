eeeab299971d839fa0599754249d820c6cb1b4d2
Drew Colthorp <colthorp@atomicobject.com>
2015-08-06 12:38:14 -0400

    Support unambiguous categories

--- lib/product_import/transforms/look_up_category.rb
+++ lib/product_import/transforms/look_up_category.rb
@@ -2,8 +2,8 @@
 class ProductImport::Transforms::LookUpCategory < ProductImport::Framework::Transform
   def transform_step(row)
 
-    if category_map.key? row['category']
-      row['category_id'] = category_map[row['category']]
+    if cat = category_for_name(row['category'])
+      row['category_id'] = cat
       continue row
     else
       reject "Could not find category with name #{row['category']}"
@@ -17,5 +17,38 @@ class ProductImport::Transforms::LookUpCategory < ProductImport::Framework::Tran
         categories = Category.where(depth:2)
         Hash[categories.map{ |c| [c.name,c.id] }]
       end
+  end
+
+  def category_for_name(n)
+    if n.present? && !category_map.key?(n)
+      parts = n.split(/(?:\s*>\s*)+/).reject{|n| n.blank?}
+      category_map[n] = nil
+
+      # If we have a All > Foo > Bar spec, go and look it up and cache it.
+      if (2..3).include? parts.length
+
+        # All is optional, but length three must start with All
+        if parts.length == 3
+          if parts[0] == "All"
+            parts.shift
+          else
+            return nil
+          end
+        end
+
+        potential_matches = Category.where(depth: 2, name: parts.last).includes(:parent)
+
+        potential_matches.each do |m|
+          categories = [m.parent, m]
+          names = categories.map(&:name)
+          if parts == names
+            category_map[n] = m.id
+            break
+          end
+        end
+      end
+    end
+
+    return category_map[n]
   end
 end
--- spec/lib/product_import/transforms/look_up_category_spec.rb
+++ spec/lib/product_import/transforms/look_up_category_spec.rb
@@ -19,4 +19,20 @@ describe ProductImport::Transforms::LookUpCategory do
     expect(failures.size).to eq(2)
   end
 
+  it "sets the category id to the second level deep category with matching name" do
+    data = [
+      {"category" => "All > Fruits >> > >> Apples"},
+      {"category" => "Fruits > Apples"},
+      {"category" => "All"},
+      {"bad" => true},
+    ]
+
+    successes, failures = subject.transform_enum(data)
+
+    expect(successes.size).to eq(2)
+    expect(successes[0]).to eq({"category" => "All > Fruits >> > >> Apples", "category_id" => category1.id})
+    expect(successes[1]).to eq({"category" => "Fruits > Apples", "category_id" => category1.id})
+
+    expect(failures.size).to eq(2)
+  end
 end
