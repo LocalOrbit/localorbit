e2a43236820bc73d840c724e781525fe548570c4
jczetta <jczetta@gmail.com>
2015-08-20 13:31:46 -0400

    WIP for final pieces of admin file import adding spec files, necessary views, etc.

---
+++ spec/controllers/admin/upload_controller_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe UploadController do
+	it "accepts a file" do
+		
+	end
+
+	it "submits a form properly" do
+	end
+
+	it "renders errors to user" do
+	end
+
+	it "properly calls the script to upload data (see product import tests)" do
+	end
+end
