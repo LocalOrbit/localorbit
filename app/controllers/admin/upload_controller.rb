ff710f3e3a8fa63ceb0a1d5163811e5cca5e9760
jczetta <jczetta@gmail.com>
2015-08-24 15:52:20 -0400

    Add to Admin::UploadController tests, clarify code in controller

--- app/controllers/admin/upload_controller.rb
+++ app/controllers/admin/upload_controller.rb
@@ -1,16 +1,24 @@
 class Admin::UploadController < AdminController
 	require 'rubyXL'
 
-  def upload
-  	uploaded = params[:datafile] # gets the xlsx data (or should) from form upload post req
-  	filepath = Rails.root.join('tempfiles',uploaded.original_filename)
-	  File.open(filepath, 'wb') do |file|
-	  	file.write(uploaded.read) # writes that data to the open filestream in the tempfiles fldr
-	  end
+  def upload(test_file=nil)
+  	if not test_file
+	  	uploaded = params[:datafile] # Gets the xlsx data from form upload post request
+	  	filepath = Rails.root.join('tempfiles',uploaded.original_filename)
+		  File.open(filepath, 'wb') do |file|
+		  	file.write(uploaded.read) # Writes that data to the open filestream in the tempfiles fldr
+		  end
+		else # This seems...bad.
+			uploaded = File.open(test_file)
+			filepath = Rails.root.join('tempfiles',test_file)
+		  File.open(filepath, 'wb') do |file|
+		  	file.write(uploaded.read)
+		  end
+		end
 	end
 
   def index
-  	# TODO more general SQL such that you can reasonably select a domain...
+  	# TODO more general SQL such that you can reasonably select a domain
   	sql = "select subdomain, id from markets where id in (select destination_market_id from market_cross_sells where source_market_id=112);"
   	records = ActiveRecord::Base.connection.execute(sql)
   	@suppliers_available = Hash.new
@@ -20,46 +28,39 @@ class Admin::UploadController < AdminController
   end
 
   def check
-  	@errors = []
-  	@total_products_msg = "Loaded 0 products." # tmp, add later
+  	@total_products_msg = "0 products." # initial
   	if params.has_key?(:datafile)
   		profile = params[:profile]
-  		filepath_partial = params[:datafile].original_filename
-  		filepath = './tempfiles/' + filepath_partial.to_s
-  		error_file = "./tempfiles/#{profile}_errors_#{Date.today}.yml" # will cause file errors if run around midnight, TODO fix
-  		# would be nice to audit contents here (like headers). right now let's run it as we were doing before.
+  		filepath = './tempfiles/' + params[:datafile].original_filename.to_s
 			upload # call the upload method to write file to tempfiles
-			 # todo: make sure profiles possible are generated and available in form from controller instead of typed into the template
-			cli_call_result = `./bin/import_products standard_template -p #{profile} -f '#{filepath}' 2>&1` 
+			cli_call_result = `./bin/import_products standard_template -p #{profile} -f '#{filepath}' 2>&1` # Run CLI script with correct options, include STDERR with STDOUT, to parse both.
 
-    	@error_display = []
+    	@error_display = [] # initial
     	if cli_call_result.include?("Assuming file is invalid and bailing out")
-    		@errors = [":reason: Invalid file. No upload. Check your data file headers."] # array in case we want to add more information, easier to add
+    		@errors = [":reason: Invalid file. No upload. Check your data file headers."] # array in case we want to add more information, easier
     		@errors.each do |er|
     			reason = @errors.select{|ln| ln.start_with?(":reason:")} # selects from something different b/c no need to split the file error msg (this replaces file audit, poss permanently)
     			@error_display << {'name' => "<NONE>",'reason' => reason.first[8..-1]} 
     		end
-    		@total_products_msg = "0 products."
+    		@total_products_msg = "0 products." # 0 products have been uploaded if the file has bad headers. Could be factored out TODO.
     	elsif cli_call_result.include?("---")
 				@errors = cli_call_result.split("---")[1..-1]
 				@errors.each do |er|
-					er_data = er.split("\n")
+					er_data = er.split("\n") # Pull apart YAML in the huge text string we now have.
 					reason = er_data.select{|ln| ln.start_with?(":reason:")} 
 					if reason.empty?
-						reason = ["        No identifiable reason for error. Check your data file."]
+						reason = ["        No identifiable reason for error. Check your data file."] # 8 spaces for standard placement of text.
 					end
 					name = er_data.select{|ln| ln.include?(" name:")}
-					@error_display << {'name' => name.first[8..-1] ,'reason' => reason.first[8..-1]} # Product name, error reason (do we want other things, line number is more effort for moment)
+					@error_display << {'name' => name.first[8..-1] ,'reason' => reason.first[8..-1]} # Product name, error reason (do we want other things, line number is more effort for moment). 8..-1 because apparent standard char# inward for text from process.
 				end 
-	    	@total_products_msg = cli_call_result.split("Loaded").last
+	    	@total_products_msg = cli_call_result.split("Loaded").last # could be factored out TODO
     	else
-    		@errors = nil
     		@no_errors = "No errors! Hooray!"
     		@total_products_msg = cli_call_result.split("Loaded").last
     	end
 			return
 		end
   end
-
 
 end
--- spec/controllers/admin/upload_controller_spec.rb
+++ spec/controllers/admin/upload_controller_spec.rb
@@ -1,16 +1,42 @@
 require 'spec_helper'
 
-describe UploadController do
-	it "accepts a file" do
-		
+describe Admin::UploadController do
+
+	let!(:market)   { create(:market, subdomain:"birite", id:130) } # will this work with the test db?
+	let!(:seller_org) {create(:organization, name:"bi-rite") }
+
+	it "accepts a file and saves it to tempfiles directory" do
+		test_filename = "admin_upload_template_withPC_good.xlsx"
+		test_file = test_file(test_filename)
+		subject.upload(test_file)
+		expect(`ls #{Rails.root + "tempfiles"}`.split("\n")).to include(test_filename) # hmm
+	
+		it "handles upload messages" do
+
+			# go to path 
+
+			# check for "no errors!"
+		end
+
+		it "renders errors to user for bad headers" do
+
+			# upload a file with bad headers
+			# expect to see the Bad File error on loaded page
+			
+		end
+
+		it "renders correct errors to user for missing fields" do
+
+			# upload a bad file with known errors
+			# expect to see the errors on the loaded page
+
+		end
+
 	end
 
-	it "submits a form properly" do
-	end
-
-	it "renders errors to user" do
-	end
-
-	it "properly calls the script to upload data (see product import tests)" do
-	end
+	def test_file(fname)
+    path = Rails.root + "spec/lib/product_import/test_data" + fname
+    raise ArgumentError, "Unknown test file #{fname}" unless path.file?
+  	path
+  end
 end
