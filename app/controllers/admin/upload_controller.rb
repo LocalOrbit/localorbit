9c13659b79454197a9452fbd0fb2fa6738a0aebb
jczetta <jczetta@gmail.com>
2015-08-24 13:11:33 -0400

    Clarify errors, add clear msg for bad file.

--- app/controllers/admin/upload_controller.rb
+++ app/controllers/admin/upload_controller.rb
@@ -30,31 +30,34 @@ class Admin::UploadController < AdminController
   		# would be nice to audit contents here (like headers). right now let's run it as we were doing before.
 			upload # call the upload method to write file to tempfiles
 			 # todo: make sure profiles possible are generated and available in form from controller instead of typed into the template
-			cli_call_result = `./bin/import_products standard_template -p #{profile} -f '#{filepath}' 2>&1` # 2> error_file`#{}2> #{profile}_errors_#{DateTime.now}.yml` # saves a YAML file of errors which needs to be parsed and rendered
-    	#@total_products_msg = cli_call_result
+			cli_call_result = `./bin/import_products standard_template -p #{profile} -f '#{filepath}' 2>&1` 
 
-
-    	if cli_call_result.include?("---")
+    	@error_display = []
+    	if cli_call_result.include?("Assuming file is invalid and bailing out")
+    		@errors = [":reason: Invalid file. No upload. Check your data file headers."] # array in case we want to add more information, easier to add
+    		@errors.each do |er|
+    			reason = @errors.select{|ln| ln.start_with?(":reason:")} # selects from something different b/c no need to split the file error msg (this replaces file audit, poss permanently)
+    			@error_display << {'name' => "<NONE>",'reason' => reason.first[8..-1]} 
+    		end
+    		@total_products_msg = "0 products."
+    	elsif cli_call_result.include?("---")
 				@errors = cli_call_result.split("---")[1..-1]
-				# split, 2 and -3 indexes
-				@error_display = []
 				@errors.each do |er|
 					er_data = er.split("\n")
 					reason = er_data.select{|ln| ln.start_with?(":reason:")} 
-					reason ||= "Unspecified reason for error."
+					if reason.empty?
+						reason = ["        No identifiable reason for error. Check your data file."]
+					end
 					name = er_data.select{|ln| ln.include?(" name:")}
-					name ||= "Could not find product name/line with error. Check data file."
-					@error_display << {'name' => name.first[8..-1] ,'reason' => reason.first[8..-1]} # Product name, error reason (do we want other things)
-				end # this actually doesn't work depending on the errors. have to do actual validation of the rows by chars, which is better anyway. prod name col, reason? other things? todo decide, pick name and reason initially
-    		@total_products_msg = cli_call_result.split("Loaded").last#.scan(/(Loaded %d+ products!)/) # todo fix this
+					@error_display << {'name' => name.first[8..-1] ,'reason' => reason.first[8..-1]} # Product name, error reason (do we want other things, line number is more effort for moment)
+				end 
+	    	@total_products_msg = cli_call_result.split("Loaded").last
     	else
+    		@errors = nil
     		@no_errors = "No errors! Hooray!"
-    		@total_products_msg = cli_call_result.split("---").first
+    		@total_products_msg = cli_call_result.split("Loaded").last
     	end
-    	#.scan(/(Loaded %d+ products!)/)
-    	#ryan_string.scan(/(^.*)(:)(.*)/i)
-    	# then (maybe in another method) try to import it from the location and render errors + how many have been uploaded
-    	return
+			return
 		end
   end
 
