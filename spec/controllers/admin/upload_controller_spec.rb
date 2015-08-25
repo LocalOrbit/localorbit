3139ff805a2b0e5a1a24ceb6afe42fc8970a3a15
jczetta <jczetta@gmail.com>
2015-08-25 10:43:24 -0400

    Add feature test, remove other tests atop product import layer

--- spec/controllers/admin/upload_controller_spec.rb
+++ spec/controllers/admin/upload_controller_spec.rb
@@ -5,39 +5,8 @@ describe Admin::UploadController do
 	let!(:market)   { create(:market, subdomain:"birite", id:130) } # will this work with the test db?
 	let!(:seller_org) {create(:organization, name:"bi-rite") }
 
-	it "accepts a file and saves it to tempfiles directory" do
-		test_filename = "admin_upload_template_withPC_good.xlsx"
-		test_file = test_file(test_filename)
-
-		pending("Incomplete test")
-		#expect(`ls #{Rails.root + "tempfiles"}`.split("\n")).to include(test_filename) # hmm
-	
-		it "handles upload messages" do
-			pending("Incomplete test")
-			# go to path 
-
-			# check for "No errors! Hooray!"
-		end
-
-		it "renders errors to user for bad headers" do
-			pending("Incomplete test")
-			# upload a file with bad headers
-			# expect to see the Bad File error on loaded page
-
-		end
-
-		it "renders correct errors to user for missing fields" do
-			pending("Incomplete test")
-			# upload a bad file with known errors
-			# expect to see the errors on the loaded page
-
-		end
-
+	it "exists as a layer on top of the product import" do
+		expect(subject.index).to be 
 	end
 
-	def test_file(fname)
-    path = Rails.root + "spec/lib/product_import/test_data" + fname
-    raise ArgumentError, "Unknown test file #{fname}" unless path.file?
-  	path
-  end
 end
