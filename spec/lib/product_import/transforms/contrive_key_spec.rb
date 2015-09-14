85da841f8ef0b798b8a4b1bf0839b2fdb3394373
jczetta <jczetta@gmail.com>
2015-09-14 10:52:14 -0400

    Fix contrive_key_spec for alterations to reqs.

--- spec/lib/product_import/transforms/contrive_key_spec.rb
+++ spec/lib/product_import/transforms/contrive_key_spec.rb
@@ -5,45 +5,46 @@ describe ProductImport::Transforms::ContriveKey do
   describe "with a single from key" do
     subject do
       described_class.new(
-        from: ["foo","baz","moo","cluck"]
+        from: ["baz","moo","cluck","desc"]
       )
     end
     
     it "Contrives a key from the specified fields" do
       data = [
-        {"foo" => "bar", "baz" => "qux", "moo" => "quack","cluck" => "baa"},
+        {"foo" => "bar", "baz" => "qux", "moo" => "quack","cluck" => "baa", "desc" => "too"},
         {"bad" => true}
       ]
 
       successes, failures = subject.transform_enum(data)
 
       expect(successes.size).to eq(1)
+      expected_key = "cxOyvwVOOjlA6OQ7xiwr1J8eZ24" # == ExternalProduct.contrive_key(list_of_correct_data)
 
-      expected_key = "Ys23Ag/5IOWqZCw9QGaVDdHwH00" # == ExternalProduct.contrive_key(["bar"])
-
-      expect(successes[0]).to eq({"foo" => "bar", "baz" => "qux", "moo" => "quack","cluck" => "baa","contrived_key" => expected_key})
+      expect(successes[0]).to eq({"foo" => "bar", "baz" => "qux", "moo" => "quack","cluck" => "baa", "desc" => "too","contrived_key" => expected_key})
 
       expect(failures.size).to eq(1)
       expect(failures[0][:raw]).to eq({ "bad" => true, "contrived_key" => nil })
     end
 
-    it "Correctly manages products with no product code" do
-      data = [
-          {"foo" => "", "baz" => "qux", "moo" => "quack", "cluck" => "baa"},
-          {"bad" => true, "contrived_key" => nil}
-        ]
-      successes, failures = subject.transform_enum(data)
+    # TODO must regenerate contrived key field before merging this in
+    # This test goes away because there aren't two scenarios here anymore (product codes along for the ride)
+    # it "Correctly manages products with no product code" do
+    #   data = [
+    #       {"foo" => "", "baz" => "qux", "moo" => "quack", "cluck" => "baa"},
+    #       {"bad" => true, "contrived_key" => nil}
+    #     ]
+    #   successes, failures = subject.transform_enum(data)
 
-      expect(successes.size).to eq(1)
+    #   expect(successes.size).to eq(1)
 
-      expected_key = "09gRZ45j3+YRI8ZkNjvIx74AVVw" # == ExternalProduct.contrive_key(["qux","quack","baa"]) 
+    #   expected_key = "09gRZ45j3+YRI8ZkNjvIx74AVVw" # == ExternalProduct.contrive_key(["qux","quack","baa"]) 
 
-      expect(successes[0]).to eq({"foo" => "", "baz" => "qux", "moo" => "quack","cluck" => "baa","contrived_key" => expected_key})
+    #   expect(successes[0]).to eq({"foo" => "", "baz" => "qux", "moo" => "quack","cluck" => "baa","contrived_key" => expected_key})
 
-      expect(failures.size).to eq(1)
-      expect(failures[0][:raw]).to eq({ "bad" => true, "contrived_key" => nil })
+    #   expect(failures.size).to eq(1)
+    #   expect(failures[0][:raw]).to eq({ "bad" => true, "contrived_key" => nil })
 
-    end
+    # end
   end
 
   
