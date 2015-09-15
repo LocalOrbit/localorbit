b667234385489265dcc81b67ca45c6ad3d8a7558
jczetta <jczetta@gmail.com>
2015-09-15 09:51:33 -0400

    Clean up

--- lib/product_import/transforms/ensure_canonical_data.rb
+++ lib/product_import/transforms/ensure_canonical_data.rb
@@ -3,10 +3,10 @@
 class ProductImport::Transforms::EnsureCanonicalData < ProductImport::Framework::TransformPipeline
 
   transform :validate_keys_are_present,
-    keys: %w(organization name category price unit unit_description)
+    keys: %w(organization name category price unit unit_description) # require category even though it may change in future, prod to upload must have one
 
   transform :contrive_key,
-    from: %w(organization name unit unit_description), # don't need to validate product code anymore
+    from: %w(organization name unit unit_description), # don't need to validate product code now
     skip_if_present: true
 
   transform :move_non_canonical_fields_to_source_data
