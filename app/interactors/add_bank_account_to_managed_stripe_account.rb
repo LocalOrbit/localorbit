2351fe0320d78c43b83064f88ad819c3d0a6e7c3
Andy Bass <andy@localorb.it>
2015-09-18 14:25:30 -0400

    Fix to properly set new bank account as default, and remove old bank acocunts within Stripe

--- app/interactors/add_bank_account_to_managed_stripe_account.rb
+++ app/interactors/add_bank_account_to_managed_stripe_account.rb
@@ -8,7 +8,16 @@ class AddBankAccountToManagedStripeAccount
 
     # Create a Stripe bank account for the Stripe Account
     stripe_tok = bank_account_params[:stripe_tok]
-    stripe_bank_account = stripe_account.bank_accounts.create(bank_account: stripe_tok)
+    stripe_bank_account = stripe_account.bank_accounts.create(bank_account: stripe_tok, default_for_currency: true)
+
+    # Remove old external accounts
+    account = Stripe::Account.retrieve(stripe_account.id)
+    bank_accounts = account.external_accounts
+    bank_accounts.each do |a|
+      if !a.default_for_currency
+        del_result = a.delete
+      end
+    end
 
     # Update bank account w stripe id
     bank_account.update(stripe_id: stripe_bank_account.id, account_role: 'deposit')
