56f36c5d5971a5a80306b5a515b74941b9104f6c
jczetta <jczetta@gmail.com>
2015-09-22 15:23:07 -0400

    Update upload process, including audit migration. WIP.

--- app/controllers/admin/upload_controller.rb
+++ app/controllers/admin/upload_controller.rb
@@ -29,9 +29,10 @@ def check
       upload # call the upload method to write file to tempfiles
 
       @job_id = Time.now.to_i #send this to the audit
-
+      @user = current_user.id
+      #p @job_id, "JOB ID!"
       # system calls the wrapper, run in the background, passing through the job_id
-      system("./bin/import_wrapper #{@job_id} &") # first arg for import_wrapper
+      system("./bin/import_wrapper #{@job_id} #{profile} #{filepath} #{user} &") # first arg for import_wrapper
     end
   end
 
@@ -39,7 +40,7 @@ def check
     @job_id = params[:job_id] # access this from the post
     # try to find audit with job id
     aud = Audit.where(associated_id:@job_id)
-    binding.pry
+    #binding.pry
     content = aud['comment'].split("|*|")
     error_text = content.first
     products_loaded = content.last
@@ -48,35 +49,33 @@ def check
 
 
   def get_output(cli_call_result, products_loaded)
-    # while getting error, get url_with_import, throw that to cli_call_result
-    #cli_call_result = "" # the result of url_with_import string
-    if params.has_key?(:datafile)
-    	@error_display = [] # initial
-    	if cli_call_result.include?("Assuming file is invalid and bailing out")
-    		@errors = [":reason: Invalid file. No upload. Check your data file headers."] # array in case we want to add more information, easier
-    		@errors.each do |er|
-    			reason = @errors.select{|ln| ln.start_with?(":reason:")} # selects from something different b/c no need to split the file error msg (this replaces file audit, poss permanently)
-    			@error_display << {'name' => "<NONE>",'reason' => reason.first[8..-1]} 
-    		end
-    		@total_products_msg = "0 products." # 0 products have been uploaded if the file has bad headers. Could be factored out TODO.
-    	elsif cli_call_result.include?("---")
-				@errors = cli_call_result.split("---")[1..-1]
-				@errors.each do |er|
-					er_data = er.split("\n") # Pull apart YAML in the huge text string we now have.
-          reason = er_data.select{|ln| ln.start_with?(":reason:")} 
-					if reason.empty?
-						reason = ["        No identifiable reason for error. Check your data file."] # 8 spaces for standard placement of text.
-					end
-					name = er_data.select{|ln| ln.include?(" name:")}
-					@error_display << {'name' => name.first[8..-1] ,'reason' => reason.first[8..-1]} # Product name, error reason (do we want other things, line number is more effort for moment). 8..-1 because apparent standard char# inward for text from process.
-				end 
-	    	@total_products_msg = "#{products_loaded}" # could be factored out TODO
-    	else
-    		@no_errors = "No errors! Hooray!"
-    		@total_products_msg = "#{products_loaded}" #cli_call_result.split("Loaded").last
-    	end
-			return
-		end
+    #if params.has_key?(:datafile) # this is now handled elsewhere TBC
+  	@error_display = [] # initial
+  	if cli_call_result.include?("Assuming file is invalid and bailing out")
+  		@errors = [":reason: Invalid file. No upload. Check your data file headers."] # array in case we want to add more information, easier
+  		@errors.each do |er|
+  			reason = @errors.select{|ln| ln.start_with?(":reason:")} # selects from something different b/c no need to split the file error msg (this replaces file audit, poss permanently)
+  			@error_display << {'name' => "<NONE>",'reason' => reason.first[8..-1]} 
+  		end
+  		@total_products_msg = "0 products." # 0 products have been uploaded if the file has bad headers. Could be factored out TODO.
+  	elsif cli_call_result.include?("---")
+			@errors = cli_call_result.split("---")[1..-1]
+			@errors.each do |er|
+				er_data = er.split("\n") # Pull apart YAML in the huge text string we now have.
+        reason = er_data.select{|ln| ln.start_with?(":reason:")} 
+				if reason.empty?
+					reason = ["        No identifiable reason for error. Check your data file."] # 8 spaces for standard placement of text.
+				end
+				name = er_data.select{|ln| ln.include?(" name:")}
+				@error_display << {'name' => name.first[8..-1] ,'reason' => reason.first[8..-1]} # Product name, error reason (do we want other things, line number is more effort for moment). 8..-1 because apparent standard char# inward for text from process.
+			end 
+    	@total_products_msg = "#{products_loaded}" # could be factored out TODO
+  	else
+  		@no_errors = "No errors! Hooray!"
+  		@total_products_msg = "#{products_loaded}" #cli_call_result.split("Loaded").last
+  	end
+		return # want to send this data to the check tpl
+		#end
   end
 
 end
