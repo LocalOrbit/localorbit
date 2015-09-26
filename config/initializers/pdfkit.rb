6dfc4f3d824ac5856bf9c5c7164038769949d318
Andy Bass <abass@conpoto.com>
2015-09-25 23:20:28 -0400

    Updated tests

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -1,5 +1,5 @@
 PDFKit.configure do |config|
-  config.wkhtmltopdf = '/Users/andyb/.rbenv/shims/wkhtmltopdf'
+  # config.wkhtmltopdf = '/Users/andyb/.rbenv/shims/wkhtmltopdf'
   # config.default_options[:quiet] = false
   config.default_options[:load_error_handling] = 'ignore'
 end
