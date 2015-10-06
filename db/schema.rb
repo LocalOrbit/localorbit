157fd2272a34d4190053374b5906bd9359c106e3
jczetta <jczetta@gmail.com>
2015-10-06 13:59:05 -0400

    Fix rollback appropriately

--- db/schema.rb
+++ db/schema.rb
@@ -119,7 +119,6 @@ ActiveRecord::Schema.define(version: 20151006174218) do
     t.datetime "updated_at"
     t.integer  "user_id"
     t.integer  "discount_id"
-    t.integer  "delivery_notes",  array: true
   end
 
   add_index "carts", ["delivery_id"], name: "index_carts_on_delivery_id", using: :btree
@@ -173,15 +172,6 @@ ActiveRecord::Schema.define(version: 20151006174218) do
   add_index "deliveries", ["cutoff_time"], name: "index_deliveries_on_cutoff_time", using: :btree
   add_index "deliveries", ["deliver_on"], name: "index_deliveries_on_deliver_on", using: :btree
   add_index "deliveries", ["delivery_schedule_id"], name: "index_deliveries_on_delivery_schedule_id", using: :btree
-
-  create_table "delivery_notes", force: true do |t|
-    t.text     "note"
-    t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.datetime "deleted_at"
-    t.integer  "supplier_org_id"
-  end
 
   create_table "delivery_schedules", force: true do |t|
     t.integer  "market_id"
@@ -608,7 +598,6 @@ ActiveRecord::Schema.define(version: 20151006174218) do
     t.string   "invoice_pdf_uid"
     t.string   "invoice_pdf_name"
     t.string   "payment_provider"
-    t.integer  "delivery_notes",                                     array: true
   end
 
   add_index "orders", ["delivery_id"], name: "index_orders_on_delivery_id", using: :btree
