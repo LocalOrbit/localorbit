e9fb9aea4f103ce4b3181d10a2f67861fc3408ca
Andy Bass <andy@localorb.it>
2015-10-06 16:26:38 -0400

    Fix change notifications to only send on order quantity change, not delivered quantity change

--- app/models/order.rb
+++ app/models/order.rb
@@ -304,32 +304,40 @@ class Order < ActiveRecord::Base
 
   def sellers_with_changes
     uuid = audits.last.try(:request_uuid)
+    sellers = []
 
     if uuid
       Audit.where(request_uuid: uuid, auditable_type: "OrderItem").map do |audit|
         if audit.audited_changes["quantity"] && audit.audited_changes["quantity"].second >0 && audit.action != :destroy
           # If auditable is there, use the seller, or else find it from the product in the changes
-          audit.try(:auditable).try(:seller) || Product.find_by(id: audit.audited_changes["product_id"]).try(:organization)
+          sellers << audit.try(:auditable).try(:seller) || Product.find_by(id: audit.audited_changes["product_id"]).try(:organization)
         end
-      end.compact.uniq
+      end
     else
       []
     end
+
+    sellers.compact.uniq
+    sellers
   end
 
   def sellers_with_cancel
     uuid = audits.last.try(:request_uuid)
 
+    sellers = []
     if uuid
       Audit.where(request_uuid: uuid, auditable_type: "OrderItem").map do |audit|
         if (audit.audited_changes["quantity"] && audit.audited_changes["quantity"].second == 0) || audit.action == :destroy
           # If auditable is there, use the seller, or else find it from the product in the changes
-          audit.try(:auditable).try(:seller) || Product.find_by(id: audit.audited_changes["product_id"]).try(:organization)
+          sellers << audit.try(:auditable).try(:seller) || Product.find_by(id: audit.audited_changes["product_id"]).try(:organization)
         end
-      end.compact.uniq
+      end
     else
       []
     end
+
+    sellers.compact.uniq
+    sellers
   end
 
   def subtotal
