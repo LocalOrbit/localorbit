796bfa4cc71dc51d9802d1c5b5b5252a26352478
Andy Bass <andy@localorb.it>
2015-10-20 15:48:36 -0400

    Fix to display note HTML

--- app/controllers/admin/fresh_sheets_controller.rb
+++ app/controllers/admin/fresh_sheets_controller.rb
@@ -28,7 +28,7 @@ class Admin::FreshSheetsController < AdminController
     email = MarketMailer.fresh_sheet(
       market: current_market, 
       to: current_user.email, 
-      note: @note.to_s.html_safe,
+      note: @note.html_safe,
       preview: true, 
       unsubscribe_token: "XYZ987XYZ987XYZ987",
       port: request.port
