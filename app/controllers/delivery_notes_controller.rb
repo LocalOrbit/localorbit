113bf11e93b03585d466fd41c1ffd2514abf51bf
jczetta <jczetta@gmail.com>
2015-10-20 10:50:03 -0400

    WIP buyer note, accessible. TODOS null supplier; did not add mods in cart model or schema

--- app/controllers/delivery_notes_controller.rb
+++ app/controllers/delivery_notes_controller.rb
@@ -3,21 +3,25 @@ class DeliveryNotesController < ApplicationController
 	before_action :find_cart
 
 	def index
-		# TODO decide if there is an indexed display, and if so where
+		# TODO decide if there is an indexed display, and if so where (at first no)
 		#@delivery_notes = @cart.delivery_notes.visible.alphabetical_by_supplier_org
 	end
 
 	def new
-		@delivery_note = DeliveryNote.new(cart_id: find_cart)
+		#@delivery_note = DeliveryNote.new#(cart_id: find_cart)
+		# back and forth between parameter posting and stack level somewhere
+
 	end
 
 	def create
 		@cart = Cart.find(find_cart)
 		@buyer_org = current_organization
-		#@delivery_note = @cart.delivery_notes.build(delivery_note_params)
-		@delivery_note = DeliveryNote.build(delivery_note_params)
+		@supplier = params[:supplier_org] # hmmmm
+		params.require(:delivery_note).permit(:note) # tmp
+
+		@delivery_note = DeliveryNote.new(:note => params[:delivery_note][:note])
 		if @delivery_note.save
-			redirect_to cart_path(@cart) # hm
+			redirect_to "/cart" # tmp for cart path
 		else
 			render :new
 		end
@@ -29,9 +33,9 @@ class DeliveryNotesController < ApplicationController
 
 	def update
 		if @delivery_note.update_attributes(delivery_note_params) # does that exist here
-			redirect_to delivery_notes_path(@cart.id) # each cart has only one buyer org.
+			redirect_to delivery_notes_path(@cart.id) # each cart has only one buyer org. this could be a problem though, not set upu for it anymore
 		else
-			render :edit
+			render :edit # hm, may not use this ultimately
 		end
 	end
 
@@ -43,16 +47,10 @@ class DeliveryNotesController < ApplicationController
 	private
 
 	def delivery_note_params
-		params.require("delivery_note").permit(
-      :supplier_org,
-      :buyer_org,
-      :note,
-      :cart_id
-    )
+		params.require(:delivery_note).permit(:note,:supplier_org,:buyer_org,:cart_id)
 	end
 
 	def find_cart
-		@cart_id = current_cart.id # this exists, right?
+		@cart_id = current_cart.id # this should exist in valid session for notes
 	end
-
 end
