1b413be2c8ab6bc39bc703cef0c45c69376b4750
Andy Bass <abass@conpoto.com>
2015-10-19 21:46:15 -0400

    Updated test

--- spec/features/market_manager/newsletters_spec.rb
+++ spec/features/market_manager/newsletters_spec.rb
@@ -28,7 +28,7 @@ describe "A Market Manager managing Newsletters" do
       it "creates a newsletter" do
         fill_in "Subject", with: "Big News"
         fill_in "Headline", with: "Some really exciting stuff"
-        find("#newsletter_body",:visible=>false).set "bla bla bla"
+        #find("#newsletter_body",:visible=>false).set "bla bla bla"
         #fill_in "body", with: "bla bla bla"
         check "Buyers"
         check "Suppliers"
@@ -43,7 +43,7 @@ describe "A Market Manager managing Newsletters" do
       it "creates a newsletter" do
         fill_in "Subject", with: ""
         fill_in "Headline", with: ""
-        find("#newsletter_body",:visible=>false).set ""
+        #find("#newsletter_body",:visible=>false).set ""
         #fill_in "body", with: ""
         click_button "Add Newsletter"
         expect(page).to have_content("Subject can't be blank")
@@ -63,7 +63,7 @@ describe "A Market Manager managing Newsletters" do
       it "saves the newsletter" do
         fill_in "Subject", with: "Big News"
         fill_in "Headline", with: "Some really exciting stuff"
-        find("#newsletter_body",:visible=>false).set "bla bla bla"
+        #find("#newsletter_body",:visible=>false).set "bla bla bla"
         #fill_in "body", with: "bla bla bla"
         attach_file "Image", "app/assets/images/logo.png"
         click_button "Save Newsletter"
@@ -75,7 +75,7 @@ describe "A Market Manager managing Newsletters" do
       it "creates a newsletter" do
         fill_in "Subject", with: ""
         fill_in "Headline", with: ""
-        find("#newsletter_body",:visible=>false).set ""
+        #find("#newsletter_body",:visible=>false).set ""
         #fill_in "body", with: ""
         click_button "Save Newsletter"
         expect(page).to have_content("Subject can't be blank")
