8524b9e704d67775910d447d67b21f8edd94adba
Andy Bass <andy@localorb.it>
2015-10-21 23:44:32 -0400

    16up product labels

--- lib/packing_labels/page.rb
+++ lib/packing_labels/page.rb
@@ -3,14 +3,23 @@ module PackingLabels
     class << self
       def make_pages(labels, product_labels_only)
         if product_labels_only == "true"
-          labels.each_slice(6).map do |(a,b,c,d,e,f)|
+          labels.each_slice(15).map do |(a,b,c,d,e,f,g,h,i,j,k,l,m,n,o)|
             {
                 a: a,
                 b: b,
                 c: c,
                 d: d,
                 e: e,
-                f: f
+                f: f,
+                g: g,
+                h: h,
+                i: i,
+                j: j,
+                k: k,
+                l: l,
+                m: m,
+                n: n,
+                o: o
             }
             end
         else
