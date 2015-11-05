3f05665cd3ee1f1f16f09c698b27fb0d4adb2328
Andy Bass <andy@localorb.it>
2015-11-04 21:01:54 -0500

    Fix & clarify overview calculations

--- app/presenters/financial_overview/market_manager.rb
+++ app/presenters/financial_overview/market_manager.rb
@@ -11,42 +11,42 @@ module FinancialOverview
     end
 
     def overdue
-      sum_order_total(@po_orders.delivered.where("invoice_due_date < ?", @time.beginning_of_day))
+      sum_order_total(@po_orders.delivered.payment_overdue)
     end
 
     def money_out_next_seven
-      orders = @po_orders.delivered.paid_between(
-        next_seven_days(offset: -7)
-      )
-
+      orders = @po_orders.delivered.paid_between(next_seven_days(offset: -7))
       sum_money_to_sellers(orders)
     end
 
     def money_in_today
-      orders = @po_orders.due_between(today)
-      sum_order_total(orders)
+      #orders = @po_orders.invoiced.unpaid.due_between(today)
+      #sum_order_total(orders)
+      @po_orders.invoiced.unpaid.due_between(today).sum(:total_cost)
     end
 
     def money_in_next_seven
-      orders = @po_orders.invoiced.unpaid.due_between(next_seven_days)
-      sum_order_total(orders)
+      #orders = @po_orders.invoiced.unpaid.due_between(next_seven_days)
+      #sum_order_total(orders)
+      @po_orders.invoiced.unpaid.due_between(next_seven_days).sum(:total_cost)
     end
 
     def money_in_next_thirty
-      # Don't include the "next 7 days" total
-      start = (next_seven_days.end + 1.day).beginning_of_day
-      orders = @po_orders.invoiced.unpaid.due_between(start..next_thirty_days.end)
-      sum_order_total(orders)
+      #orders = @po_orders.invoiced.unpaid.due_between(next_thirty_days)
+      #sum_order_total(orders)
+      @po_orders.invoiced.unpaid.due_between(next_thirty_days).sum(:total_cost)
     end
 
     def money_in_purchase_orders
-      orders = @po_orders.uninvoiced
-      sum_order_total(orders)
+      #orders = @po_orders.uninvoiced
+      #sum_order_total(orders)
+      @po_orders.uninvoiced.sum(:total_cost)
     end
 
     def lo_fees_next_seven_days
-      orders = @po_orders.paid_between(next_seven_days(offset: -7))
-      sum_local_orbit_fees(orders)
+      #orders = @po_orders.paid_between(next_seven_days(offset: -7))
+      #sum_local_orbit_fees(orders)
+      @po_orders.paid_between(next_seven_days(offset: -7)).sum(:local_orbit_market_fee)
     end
 
     private
