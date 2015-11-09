b0512adac278b4fc1ed305e683b658ebe310e65d
jczetta <jczetta@gmail.com>
2015-11-09 12:45:15 -0500

    Remove extraneous lines.

--- app/controllers/admin/financials/payments_controller.rb
+++ app/controllers/admin/financials/payments_controller.rb
@@ -15,18 +15,10 @@ module Admin::Financials
       if @payment.update_attributes(payment_method:params[:payment][:payment_method], note:params[:payment][:note], amount:BigDecimal(params[:payment][:amount]))
         redirect_to "/admin/financials/payments"
       end
-
-
-      # @delivery_note = DeliveryNote.find(params[:id])
-      # if @delivery_note.update_attributes(payment_method:params[:payment][:payment_method
-      #   ], note:params[:payment][:note], amount:BigDecimal(params[:payment][:amount]))
-      #   redirect_to payment_history_path # well - the same path as the payments are shown on
-      # end
     end
 
     def edit
       @payment = Payment.find(params[:id].to_i)
-      #update
     end
   end
 end
