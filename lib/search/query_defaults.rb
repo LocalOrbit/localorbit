7a57412df7cb2f96edcb87e4e71e2c5256cfd227
Andy Bass <andy@localorb.it>
2015-12-02 13:22:41 -0500

    HTML format fix Buyer invoices show today + 30 by default

--- lib/search/query_defaults.rb
+++ lib/search/query_defaults.rb
@@ -5,8 +5,13 @@ module Search
       q = (q || {}).with_indifferent_access.deep_dup
       query = {}.with_indifferent_access
 
-      query["#{date_search_attr}_date_gteq"] = q.delete("#{date_search_attr}_date_gteq") || 30.days.ago.to_date.to_s
-      query["#{date_search_attr}_date_lteq"] = q.delete("#{date_search_attr}_date_lteq") || Date.today.to_s
+      if date_search_attr == :invoice_due_date
+        query["#{date_search_attr}_date_gteq"] = q.delete("#{date_search_attr}_date_gteq") || Date.today.to_s
+        query["#{date_search_attr}_date_lteq"] = q.delete("#{date_search_attr}_date_lteq") || 30.days.from_now.to_date.to_s
+      else
+        query["#{date_search_attr}_date_gteq"] = q.delete("#{date_search_attr}_date_gteq") || 30.days.ago.to_date.to_s
+        query["#{date_search_attr}_date_lteq"] = q.delete("#{date_search_attr}_date_lteq") || Date.today.to_s
+      end
       query["order_market_id_eq"] = q.delete("order_market_id_eq") unless q["order_market_id_eq"].nil?
 
       # HACK:
