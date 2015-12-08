72ee103735172e681c910cbb8b8cff069acc5a2b
jczetta <jczetta@gmail.com>
2015-12-08 15:32:47 -0500

    Fill in base of defaults for apiv2

---
+++ app/controllers/api/v2/defaults.rb
@@ -0,0 +1,35 @@
+module Api
+	module V2
+		module Defaults
+			extend ActiveSupport::Concern
+
+			included do
+				prefix "api"
+				version "v2", using: :path 
+				default_format :json
+				format :json
+				formatter :json,
+					Grape::Formatter::ActiveModeSerializers 
+
+				helpers do 
+					def permitted_params
+						# TODO fill in
+					end
+
+					def logger
+						Rails.logger
+					end
+				end
+
+				rescue_from ActiveRecord::RecordNotFound do |e|
+          error_response(message: e.message, status: 404)
+        end
+
+        rescue_from ActiveRecord::RecordInvalid do |e|
+          error_response(message: e.message, status: 422)
+        end
+
+			end
+		end
+	end
+end
