bffe6cbf683af605f6a70b3288182ff19506a980
Keith Meisel <keith@localorb.it>
2015-12-08 15:39:00 -0500

    Added context-sensitive delivery notes to master packing slip

--- app/controllers/admin/order_summaries_controller.rb
+++ app/controllers/admin/order_summaries_controller.rb
@@ -3,5 +3,6 @@ class Admin::OrderSummariesController < AdminController
     @delivery = Delivery.find(params[:id]).decorate
     order_items = OrderItem.for_delivery_and_user(@delivery, current_user)
     @summaries = OrdersBySellerPresenter.new(order_items)
+    @delivery_notes = DeliveryNote.joins(:order).where(orders: {delivery_id: @delivery.id})
   end
 end
