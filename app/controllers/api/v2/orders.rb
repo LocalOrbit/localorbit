a3e44b5ab7eed5a9418c9b28b9e90af647c5f0bc
jczetta <jczetta@gmail.com>
2015-12-11 17:17:39 -0500

    Add orders to seralizer, add outline of order endpoints

---
+++ app/controllers/api/v2/orders.rb
@@ -0,0 +1,20 @@
+module API
+	module V2
+		class Orders < Grape::API 
+			include API::V2::Defaults
+
+			resource orders: do 
+				desc "Get all orders"
+
+				desc "Get order by order number"
+
+				desc "Get order by supplier organization (included)"
+
+				desc "Get order by buyer organization (placed)"
+
+				desc "Get order within date range (optional end date)"
+			end
+
+		end
+	end
+end
