70e9305050bf12c9685beddebc5b1d03c24a31c8
Andy Bass <andy@localorb.it>
2015-12-15 09:24:46 -0500

    Audit filters WIP

--- app/models/audit.rb
+++ app/models/audit.rb
@@ -1,7 +1,17 @@
 require "audited/adapters/active_record/audit"
 
 class Audited::Adapters::ActiveRecord::Audit < ActiveRecord::Base
+  include PgSearch
+
   belongs_to :masquerader, class_name: "User"
+
+  scope_accessible :search, method: :for_search, ignore_blank: true
+
+  pg_search_scope :search_by_name, against: :name, using: {tsearch: {prefix: true}}
+
+  def self.for_search(query)
+    search_by_name(query)
+  end
 
   def self.decorator_class
     AuditDecorator
