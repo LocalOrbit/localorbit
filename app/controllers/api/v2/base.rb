3659d5bf07c442aae185ded455ae24416720bf56
jczetta <jczetta@gmail.com>
2015-12-22 16:13:01 -0500

    Basis for post routes on products, remove swagger docs setup for now

--- app/controllers/api/v2/base.rb
+++ app/controllers/api/v2/base.rb
@@ -4,17 +4,28 @@ module API
 	module V2
 		class Base < Grape::API
 			version 'v2'
-			# mount Products
-			#mount Api::V2::Products
-			#mount Products
+
 			mount API::V2::Products
 
-			add_swagger_documentation(
-        api_version: "v1",
-        hide_documentation_path: true,
-        mount_path: "/api/v2/swagger_doc",
-        hide_format: true
-      )
+			before do
+	      error!("401 Unauthorized", 401) unless authenticated
+	    end
+
+	    helpers do
+	      # def authenticated
+	      #   user = User.find_by_email(params[:email])
+	      #   user && user.valid_password?(params[:password])
+	      # end
+
+	      # def authenticated # or like, http://funonrails.com/2014/03/api-authentication-using-devise-token/
+	      # 	user_key = User.find_by_key(params:key) # TODO field for key, hash username + API key
+	      # 	user_key != nil # TODO check error response for this
+	      # end
+	      def authenticated
+	      	true # TMP
+	      end
+    	end
+
 		end
 	end
 end
--- app/controllers/api/v2/products.rb
+++ app/controllers/api/v2/products.rb
@@ -4,6 +4,7 @@ module API
 			include API::V2::Defaults
 
 			resource :products do # TODO Q: Should we allow this endpoint?
+				# get requests
 				desc "Return all products"
 				get "", root: :products do 
 					Product.all
@@ -33,6 +34,27 @@ module API
 					category_id = Category.find_by_name(permitted_params[:category]).id
 					Product.where(category_id: category_id)
 				end
+
+				# post requests
+
+				desc "Create a product"
+				params do
+					requires :name, :unit, :category_id, :unit_description,
+				end
+				post '/add-product' do
+					product = Product.create(params(:go,:here))
+				end
+
+				desc "Upload json"
+				params do
+					requires :param1, :param2
+				end
+				post '/add-products' do
+					# do stuff with posted json file here
+					# should be normal way of parsing it and then it will add by row
+					# sending responses in the order with the route redirections as mentioned in plan
+				end
+
 
 			end
 
