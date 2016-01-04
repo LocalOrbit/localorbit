2e0019e4e484d2293a6cb96562bd6524abebd95a
jczetta <jczetta@gmail.com>
2016-01-04 10:47:46 -0500

    Continued additions to product routes re: offline notes, beginning of api_helpers files in lib

---
+++ lib/api_helpers/post_routes_helpers.rb
@@ -0,0 +1,17 @@
+module API
+	module V2
+		extend self
+
+		# PRODUCT HELPERS
+
+		# Determines whether this product needs a new GenProd or belongs to an existing one
+		def identify_product_uniqueness(product_params) # takes hash of params
+		end
+
+		def get_category_from_name(category_name)
+		end
+
+		
+
+	end
+end
