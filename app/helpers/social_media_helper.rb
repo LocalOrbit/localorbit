6607e26b46b7633b97ab5dcc2cb5ec0361ace55e
Keith Meisel <keith@localorb.it>
2016-01-05 13:13:04 -0500

    Standardized facebook and twitter feeds across views.  I'm cool that way

--- app/helpers/social_media_helper.rb
+++ app/helpers/social_media_helper.rb
@@ -1,19 +1,18 @@
 module SocialMediaHelper 
   
-  def facebook_like(facebook_handle) 
-    if !(facebook_handle == '' || facebook_handle == nil) then 
+  def facebook_feed(facebook_handle, display = true) 
+    if display == true && !(facebook_handle == '' || facebook_handle == nil) then 
       massaged_handle = strip_detritus(facebook_handle, 'facebook.com/')
       prepared_url = "https://www.facebook.com/#{massaged_handle}"
       
       render partial: "shared/facebook", locals: { f: prepared_url }
-      
     else 
       # If facebook_handle is null or blank then skip it... 
     end 
   end 
   
-  def twitter_button(twitter_handle)
-    if !(twitter_handle == '' || twitter_handle == nil) then 
+  def twitter_feed(twitter_handle, display = true)
+    if display == true && !(twitter_handle == '' || twitter_handle == nil) then 
       massaged_handle = strip_detritus(twitter_handle, 'twitter')
       prepared_url = "https://www.twitter.com/#{massaged_handle}"
       
