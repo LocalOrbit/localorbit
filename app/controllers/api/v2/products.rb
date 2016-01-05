de76cd9f386ddcfb9a81bfb427d1551de2ce95dc
jczetta <jczetta@gmail.com>
2016-01-05 10:13:28 -0500

    Working on product creation in post route

--- app/controllers/api/v2/products.rb
+++ app/controllers/api/v2/products.rb
@@ -7,7 +7,7 @@ module API
 				# get requests
 				desc "Return all products"
 				get "", root: :products do 
-					Product.all
+					GeneralProduct.all # if you're actually looking for all products, this is what you want (how will it deal with units?)
 				end
 
 				desc "Return a product"
@@ -32,30 +32,44 @@ module API
 				end
 				get ":category", root: "product" do # This one does not really work that well, eg category "carrots" gets all the cat "Vegetables", TODO examine priorities
 					category_id = Category.find_by_name(permitted_params[:category]).id
-					Product.where(category_id: category_id)
+					Product.where(category_id: category_id) # possible this should be genprod
 				end
 
-				# # post requests
+				### post requests
 
-				# desc "Create a product"
-				# params do
-				# 	requires :name, :unit, :category_id, :unit_description,
-				# end
+				desc "Create a product"
+				params do
+					requires :name, :organization_name, :market_name, :unit, :category, :unit_description, :short_description, :long_description
+				end
 
-				# # singular in post request
-				# post '/add-product' do
-				# 	product = Product.create(params(:go,:here))
-				# end
+				# singular in post request
+				post '/add-product' do
+					product_name = permitted_params[:name]
+					possible_orgs = Organization.find_by_name(permitted_params[:organization_name])
+					
 
-				# desc "Upload json"
-				# params do
-				# 	requires :param1, :param2
-				# end
-				# post '/add-products' do
-				# 	# do stuff with posted json file here
-				# 	# should be normal way of parsing it and then it will add by row
-				# 	# sending responses in the order with the route redirections as mentioned in plan
-				# end
+					product = Product.create!(
+						        name: #product_name,
+						        organization_id: #seller_id,
+						        market_id: #market_id from code..
+						        unit_id: #unit_id,
+						        category_id: #category_id,
+						        code: #code,
+						        short_description: #short_description,
+						        long_description: #long_description,
+						        unit_description: #unit_description
+						      	)
+				end
+
+				desc "Upload json"
+				params do
+					requires :param1, :param2
+				end
+				post '/add-products' do
+					# do stuff with posted json file here
+					# should be normal way of parsing it and then it will add by row
+					# sending responses in the order with the route redirections as mentioned in plan
+				end
 
 
 			end
