8ef3717b2496e2956d879ac36ff5934e5a2f5b34
jczetta <jczetta@gmail.com>
2016-01-07 13:54:42 -0500

    Add to post_routes_helpers for upload data validations.

--- app/controllers/api/v2/products.rb
+++ app/controllers/api/v2/products.rb
@@ -39,36 +39,51 @@ module API
 
 				desc "Create a product"
 				params do
-					requires :name, :organization_name, :market_name, :unit, :category, :unit_description, :short_description, :long_description
+					requires :name, :organization_name, :market_name, :unit, :category, :unit_description, :short_description, :long_description, :price
 				end
 
 				# singular in post request
 				post '/add-product' do
 					product_name = permitted_params[:name]
 					possible_orgs = Organization.find_by_name(permitted_params[:organization_name])
-					
+					supplier_id = possible_orgs.first.id # TODO better accuracy
+					unit_id = Unit.find_by_name(permitted_params[:unit]).first.id
+					category_id = get_category_id_from_name(permitted_params[:category])
+					product_code = ""
+					if permitted_params[:code]
+						product_code = permitted_params[:code]
+					end
 
 					product = Product.create!(
-						        name: #product_name,
-						        organization_id: #seller_id,
-						        market_id: #market_id from code..
-						        unit_id: #unit_id,
-						        category_id: #category_id,
-						        code: #code,
-						        short_description: #short_description,
-						        long_description: #long_description,
-						        unit_description: #unit_description
+						        name: product_name,
+						        organization_id: supplier_id,
+						        market_name: permitted_params[:market_name], # TODO check, will this relationship hold up? see: where p is a Product,
+						    		## p.organization.markets.include?(Market.find_by_name(p.market_name))
+										## => true
+
+						        unit_id: unit_id,
+						        category_id: category_id,
+						        code: product_code,
+						        short_description: permitted_params[:short_description],
+						        long_description: permitted_params[:long_description],
+						        unit_description: permitted_params[:unit_description]
 						      	)
+					## To create inventory and price(s). TODO: probably no inventory, yes 1 sale price
+					# product.lots.create!(quantity: 999_999)
+     			product.prices.create!(sale_price: price, min_quantity: 1) ## TODO: min quantity default or option?
 				end
 
 				desc "Upload json"
 				params do
-					requires :param1, :param2
+					requires type: JSON # expects properly formatted JSON data
 				end
 				post '/add-products' do
 					# do stuff with posted json file here
 					# should be normal way of parsing it and then it will add by row
 					# sending responses in the order with the route redirections as mentioned in plan
+
+					# will need some of the helper functions here
+
 				end
 
 
