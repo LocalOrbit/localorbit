ac77a2c577c66b2913a7c0396c7931dbcd04a3bb
jczetta <jczetta@gmail.com>
2016-01-07 13:54:42 -0500

    Add to post_routes_helpers for upload data validations.

--- lib/api_helpers/post_routes_helpers.rb
+++ lib/api_helpers/post_routes_helpers.rb
@@ -5,12 +5,25 @@ module API
 		# PRODUCT HELPERS
 
 		# Determines whether this product needs a new GenProd or belongs to an existing one
+		# Returns true if new GenProd is necessary, returns GenProd id if there exists one (this seems bad though, maybe should return false)
 		def identify_product_uniqueness(product_params) # takes hash of params
 		end
 
-		def get_category_from_name(category_name)
+		# TODO: limitations?? this will be somewhat better when it is limited but perhaps should limit to a depth like in original prod upload
+		def get_category_id_from_name(category_name)
+			id = Category.find_by_name(category_name).first.id
+			# return nil if no possible one
+			id
 		end
 
+		def get_organization_id_from_name(organization_name)
+		end
+
+		## May not be necessary.
+		# def get_market_id_from_name(market_name)
+		# end
+
+		
 		
 
 	end
