1db92672a01d940279b4a14e3c0d1c64702e2adb
Andy Bass <andy@localorb.it>
2016-01-17 20:59:11 -0500

    Catalog promo backspace fix, delete button fix Show new catalog layout for supplier Filter disabled emails from supplier/buyer notifications Hide 'Add Lot' text from simple inventory Swap order CSV export columns (product code, product name) Hide non-seller fields if 'Is Seller' not checked Fix ' on Newsletters Fix upcoming deliveries layout on Firefox

--- app/assets/javascripts/product_catalog/components/product_catalog.js.jsx
+++ app/assets/javascripts/product_catalog/components/product_catalog.js.jsx
@@ -13,33 +13,53 @@
       selectedType: React.PropTypes.string.isRequired,
       orderCutoff: React.PropTypes.string.isRequired,
       buyerInfo: React.PropTypes.string.isRequired,
-      useTemplates: React.PropTypes.bool.isRequired
+      useTemplates: React.PropTypes.bool.isRequired,
+      supplierId: React.PropTypes.number
     },
 
       render: function() {
 
         var orderTemplates;
+        var productFilter;
+        var productTable;
+
         if(this.props.useTemplates)
             orderTemplates = (<lo.TemplatePicker baseUrl={this.props.baseUrl} cartUrl={this.props.cartUrl} />);
         else
             orderTemplates = ('');
 
+        if (this.props.supplierId > 0) {
+            window.lo.ProductActions.newFilters(null, this.props.supplierId);
+            productFilter = ('');
+            productTable = (<lo.ProductTable
+                limit={30}
+                filter={null}
+                cartUrl={this.props.cartUrl}
+                url={this.props.baseUrl + '/products'}
+                supplierOnly={true}
+            />);
+        }
+        else {
+            productFilter = (<lo.ProductFilter
+                deliveryDate={this.props.deliveryDate}
+                selectedType={this.props.selectedType}
+                orderCutoff={this.props.orderCutoff}
+                buyerInfo={this.props.buyerInfo}
+                useTemplates={this.props.useTemplates}
+            />);
+            productTable = (<lo.ProductTable
+                limit={30}
+                filter={null}
+                cartUrl={this.props.cartUrl}
+                url={this.props.baseUrl + '/products'}
+                supplierOnly={false}
+            />);
+        }
         return (
         <div>
           {orderTemplates}
-          <lo.ProductFilter
-            deliveryDate={this.props.deliveryDate}
-            selectedType={this.props.selectedType}
-            orderCutoff={this.props.orderCutoff}
-            buyerInfo={this.props.buyerInfo}
-            useTemplates={this.props.useTemplates}
-          />
-          <lo.ProductTable
-            limit={30}
-            filter={null}
-            cartUrl={this.props.cartUrl}
-            url={this.props.baseUrl + '/products'}
-          />
+          {productFilter}
+          {productTable}
         </div>
       );
     }
