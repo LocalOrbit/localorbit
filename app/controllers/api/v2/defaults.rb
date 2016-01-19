3557ef417f4a8a2f117c4e35ad91420aca073700
jczetta <jczetta@gmail.com>
2016-01-19 16:45:44 -0500

    Remove Raven line due to constant's problem

--- app/controllers/api/v2/defaults.rb
+++ app/controllers/api/v2/defaults.rb
@@ -35,7 +35,7 @@ module API
 	        if Rails.env.development?
 	          raise e
 	        else
-	          Raven.capture_exception(e)
+	          #Raven.capture_exception(e)
 	          error_response(message: "Internal server error", status: 500)
 	        end
 	      end
