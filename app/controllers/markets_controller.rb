769041d36784d7e4f12cc94b2a6816a17a794595
Keith Meisel <keith@localorb.it>
2016-01-19 21:11:41 -0500

    Refined controller assignment of default values; modified partial to reflect pre-assigned attributes

--- app/controllers/markets_controller.rb
+++ app/controllers/markets_controller.rb
@@ -13,11 +13,18 @@ class MarketsController < ApplicationController
   def new
   	# KXM This will likely be similar to the admin/registration piece, with a create action feeding the new action (or the other way around - cut me some slack, it's Sunday evening).
     @market = Market.new(payment_provider: PaymentProvider.for_new_markets.id)
+    @market.pending = true;
+
+    # KXM The hard-coded value is brittle, as is the expectation that the supplied parameter will match a plan
+    plan = Plan.find_by name: params[:plan] || "Start Up"
+    @market.plan_id = plan.id
+    #binding.pry
   	render layout: "website-bridge"
   end
 
   def create
-    results = RegisterStripeMarket.perform(market_params: market_params)
+    #results = RegisterStripeMarket.perform({:market_params => market_params, :billing_params => billing_params, :plan_params => plan_params})
+    results = CreateMarket.perform({:market_params => market_params, :billing_params => billing_params, :plan_params => plan_params})
 
     if results.success?
       #redirect_to [:admin, results.market]
@@ -38,28 +45,23 @@ class MarketsController < ApplicationController
 			:contact_phone,
 			:name,
 			:subdomain,
-      :pending => true
+      :pending,
+      :plan_id
   	)
   end
 
   # KXM Included here for reference until I know better what to do with it
+  # KXM "Wrong number of arguments"... probably the same for plan_params below.  It looks like it's choking on the array.... it's probably supposed to be a hash
   def billing_params
     params.permit(
-      :billing[
-        :address,
-        :city,
-        :state,
-        :zip,
-        :phone
-      ]
+      billing: [ :address, :city, :state, :zip, :phone ]
     )
   end
 
   # KXM Included here for reference until I know better what to do with it
   def plan_params
     params.permit(
-      :purchase[
-        :plan,
+      purchase: [
         :code,
         :name,
         :credit_card,
