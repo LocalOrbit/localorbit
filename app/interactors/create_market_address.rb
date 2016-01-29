3071d0d99837314baee5900508b81414194ab7b4
Keith Meisel <keith@localorb.it>
2016-01-29 09:34:58 -0500

    Restructured comments

--- app/interactors/create_market_address.rb
+++ app/interactors/create_market_address.rb
@@ -4,7 +4,6 @@ class CreateMarketAddress
     market = context[:market]
     address = MarketAddress.create(billing_params.merge(market_id: market.id, name: market.name, billing: true))
     context[:billing_address] = address
-    # KXM Context is failing here
     context.fail! if address.errors.any?
   end
 end
--- app/interactors/roll_your_own_market.rb
+++ app/interactors/roll_your_own_market.rb
@@ -1,10 +1,6 @@
 class RollYourOwnMarket
   include Interactor::Organizer
 
-  # KXM Interactor inspired by RegisterStripeMarket interactor, the following line coming directly from that
-  # organize CreateMarket, CreateManagedStripeAccountForMarket, CreateStripeCustomerForEntity
-
-  # Other interactors will be added here, once we have a better idea of what happens at what time
+  # KXM Other interactors will be added here, once we have a better idea of what happens at what time
   organize CreateMarket, CreateMarketAddress
-  # organize CreateMarket
 end
