a9217aba8685cf8c9bc490f5f0dde7e5e0757e3c
jczetta <jczetta@gmail.com>
2016-02-02 14:45:45 -0500

    Comment out post requests testing -- testing in Postman, etc for the moment. TODO: investigate, + ensure 404 for bad requests.

--- spec/controllers/api/v2/products_controller_spec.rb
+++ spec/controllers/api/v2/products_controller_spec.rb
@@ -21,32 +21,42 @@ let!(:product2) { create(:product, name:"Test Product 2") }
 
 	end
 
-	describe "POST /api/v2/add-product" do
+	## TODO: Testing in Postman, etc -  butcan you test in rspec this way? 
+	## JavaScript tests with jquery 
 
-		it "posts one product correctly with 201 response" do # todo market name problem - in controller
-			post "api/v2/products/add-product", {name:"Very Unusual Name",organization_name:"Boettcher Farm",price:2.34,unit:"Case",category:"Fruits",code:"hmmm-abc",short_description:"short",long_description:"GOES ON FOREVER long long long",unit_description:"unit description with new unit same product"}.to_json, {"Content-Type"=>"application/json"}
-			#expect(response.status).to eq(201)
-			expect(JSON.parse(response.body)).to eq("hi")
-		end
+############
 
-		it "returns a bad request response on malformed request data (no product name)" do
-			# remove product name and make sure it gives error
-		end
+	# describe "POST /api/v2/add-product" do
 
-		it "returns a bad request response on malformed request data (no org name)" do
-			# same for correct single product request with no org name
-		end
+	# 	it "posts one product correctly with 201 response" do # todo market name problem - in controller
+	# 		post "api/v2/products/add-product"
 
-		# TODO: test for mkt name behavior when route is adequately supported for markets
+	# 		#{}?organization_name=Boettcher+Farm&category=Fruits&code=hmmm-abc&name=Very+Unusual+Name&short_description=short&price=2.34&long_description=GOES+ON+FOREVER+long+long+long&unit=Case&unit_description=unit+description+with+new+unit+same+product"
 
-	end
+	# 		#, {name:"Very Unusual Name",organization_name:"Boettcher Farm",price:2.34,unit:"Case",category:"Fruits",code:"hmmm-abc",short_description:"short",long_description:"GOES ON FOREVER long long long",unit_description:"unit description with new unit same product"}.to_json, {"Content-Type"=>"application/json"}
+	# 		#expect(response.status).to eq(201)
+	# 		#binding.pry
+	# 		expect(JSON.parse(response.body)).to eq("hi")
+	# 	end
 
-	describe "POST /api/v2/add-products" do
+	# 	it "returns a bad request response on malformed request data (no product name)" do
+	# 		# remove product name and make sure it gives error
+	# 	end
 
-		it "correctly posts products when properly formatted JSON data file is posted" do
-		end
+	# 	it "returns a bad request response on malformed request data (no org name)" do
+	# 		# same for correct single product request with no org name
+	# 	end
 
-	end
+	# 	# TODO: test for mkt name behavior when route is adequately supported for markets
+
+	# end
+
+	# describe "POST /api/v2/add-products" do
+
+	# 	it "correctly posts products when properly formatted JSON data file is posted" do
+	# 	end
+
+	# end
 
 
 end
