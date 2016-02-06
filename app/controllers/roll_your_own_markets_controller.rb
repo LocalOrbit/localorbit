46330e8fd646e3f50ddd963f4371efb251006a16
Keith Meisel <keith@localorb.it>
2016-02-06 16:42:21 -0500

    Refactored RYO controller AJAX endpoints; refactored coupon and plan JavaScript

--- app/controllers/roll_your_own_markets_controller.rb
+++ app/controllers/roll_your_own_markets_controller.rb
@@ -1,26 +1,61 @@
 class RollYourOwnMarketsController < ApplicationController
+  skip_before_action :require_selected_market
+  skip_before_action :authenticate_user!
+  skip_before_action :ensure_market_affiliation
+  skip_before_action :ensure_active_organization
+  skip_before_action :ensure_user_not_suspended
+
+	# Cached data objects
+	@_plans  = {}
+	@_plan   = {}
+	@_coupon = {}
 
 	def get_stripe_plans
 		# Set secret key
     Stripe.api_key = Rails.configuration.stripe[:secret_key]
 
     requested_plan = params[:plan]
-    if(requested_plan == nil)
-	    plans = Stripe::Plan.all
-    else
-	    plans = Stripe::Plan.retrieve(requested_plan)
-    end
 
-    @plan_data = plans
-		render json: @plan_data
+    begin  	
+	    if(requested_plan == nil)
+	    	if(@_plans)
+	    		return @_plans
+
+	    	else
+			    @_plans = Stripe::Plan.all
+					render json: @_plans
+				end
+
+	    else
+	    	if(@_plan)
+	    		return @_plan
+
+	    	else
+			    @_plan = Stripe::Plan.retrieve(requested_plan)
+					render json: @_plan
+		    end
+	    end
+    rescue Exception => e
+			err = e.json_body[:error]
+		  render :status => e.http_status, :text => err[:message]
+    end
 	end
 
 	def get_stripe_coupon
-		# Set secret key
-    Stripe.api_key = Rails.configuration.stripe[:secret_key]
+		if(@_coupon)
+			return @_coupon
 
-    coupon = Stripe::Coupon.retrieve(params[:coupon])
+		else
+			# Set secret key
+	    Stripe.api_key = Rails.configuration.stripe[:secret_key]
 
-		render json: coupon
+	    begin
+		    @_coupon = Stripe::Coupon.retrieve(params[:coupon])
+				render json: @_coupon
+	    rescue Exception => e
+				err = e.json_body[:error]
+			  render :status => e.http_status, :text => err[:message]
+	    end
+	  end
 	end
 end
