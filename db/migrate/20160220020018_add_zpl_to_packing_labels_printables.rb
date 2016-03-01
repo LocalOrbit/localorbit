6641b7839843b1068d9248d03776f416c7985606
Andy Bass <andy@localorb.it>
2016-03-01 16:24:55 -0500

    Update column to json

--- db/migrate/20160220020018_add_zpl_to_packing_labels_printables.rb
+++ db/migrate/20160220020018_add_zpl_to_packing_labels_printables.rb
@@ -1,6 +1,6 @@
 class AddZplToPackingLabelsPrintables < ActiveRecord::Migration
   def change
-    add_column :packing_labels_printables, :zpl, :text
+    add_column :packing_labels_printables, :zpl, :json
     add_column :packing_labels_printables, :zpl_name, :string
   end
 end
