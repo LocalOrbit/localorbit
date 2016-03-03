1e3e028c49a0aba5b316e8cdb611827f5b6ce378
Andy Bass <andy@localorb.it>
2016-03-03 12:58:35 -0500

    Fix test Fix migration

--- db/migrate/20160123195225_create_role_actions_table.rb
+++ db/migrate/20160123195225_create_role_actions_table.rb
@@ -2,9 +2,10 @@ class CreateRoleActionsTable < ActiveRecord::Migration
   def change
     create_table :role_actions do |t|
       t.string :description
-      t.string :org_type
+      t.string :org_type, array: true, using: 'gin', default: '{}'
       t.string :section
       t.string :action
+      t.string :plan_ids, array: true, using: 'gin', default: '{}'
     end
   end
 end
