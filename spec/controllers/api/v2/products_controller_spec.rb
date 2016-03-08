587061a5343c07efbda81ecc1be67088fe5e83f0
jczetta <jczetta@gmail.com>
2016-03-08 15:24:27 -0500

    Fix defined var overriding such that not both tests would succeed. All pass.

--- spec/controllers/api/v2/products_controller_spec.rb
+++ spec/controllers/api/v2/products_controller_spec.rb
@@ -3,7 +3,7 @@ require "spec_helper"
 describe API::V2::Products, :type => :request do
 
 let!(:org) { create(:organization, name: "Organization 1") }
-let!(:org) { create(:organization, name: "Organization 2")}
+let!(:org2) { create(:organization, name: "Organization 2")}
 let!(:product1) { create(:product, name:"Test Product 1") }
 let!(:product2) { create(:product, name:"Test Product 2") }
 
@@ -26,13 +26,12 @@ let!(:product2) { create(:product, name:"Test Product 2") }
 
 ############
 
-	describe "POST /api/v2/add-product" do
-		# binding.pry
+	describe "POST /api/v2/add-product", :order => :defined do
+		sleep(2)
 		it "posts one product correctly with 201 response" do 
 			post "api/v2/products/add-product", {name:"Super Unusual Name",organization_name:"Organization 1",price:2.34,unit:"box",category:"Fruits",code:"hmmm-abc",short_description:"short",long_description:"GOES ON FOREVER long long long",unit_description:"unit description with new unit same product"}.to_json, {"Content-Type"=>"application/json"}
-			# binding.pry
-			# p response.body
-			# expect(response.status).to eq(201)
+	
+			expect(response.status).to eq(201)
 			expect(JSON.parse(response.body)).to eq({"result" => "product successfully created"})
 		end
 
@@ -50,12 +49,11 @@ let!(:product2) { create(:product, name:"Test Product 2") }
 			expect(response.status).to eq(400)
 		end
 
-		# TODO: test for mkt name, org name, behavior when route is adequately supported for markets (and orgs)
+		# Future TODO: test for org name, behavior when route is adequately supported for markets (and orgs)
 
 	end
 
 	describe "POST /api/v2/add-products" do
-
 		it "correctly posts products when properly formatted JSON data file is posted" do
 			post "api/v2/products/add-products", {"products_total"=>2,"products"=> [{"Organization"=>"Organization 1","Category"=>"Fruits","Product Name"=>"Test Product 45","Code"=>"abcdef-code1","Short Description"=>"look how short","Long Description"=>"look how long","Unit"=>"box","Unit Description"=>"5 pound boxcase of something lalala unique","Price"=>2.41,"Multiple Pack Sizes"=>"N"},{"Organization"=>"Organization 2","Category"=>"Fruits","Product Name"=>"Test Product 33","Code"=>"abcdef-code3","Short Description"=>"look how short descr","Long Description"=>"look how long very lengthy","Unit"=>"box","Unit Description"=>"5 pound boxcase more like box of something or something","Price"=>2.45,"Multiple Pack Sizes"=>"N"}]}
 			expect(response.status).to eq(201)
