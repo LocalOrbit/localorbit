15fe6b1532e51e8013b76b3d127950e709451a87
Keith Meisel <keith@localorb.it>
2016-03-16 22:02:09 -0400

    Initial attempts... stashed here for now

--- spec/controllers/markets_controller_spec.rb
+++ spec/controllers/markets_controller_spec.rb
@@ -7,6 +7,16 @@ describe MarketsController do
     expect(response).to redirect_to(new_user_session_path)
   end
 
+  context "when performing user-driven market creation" do
+    it "allows unauthenticated access" do
+      VCR.turn_off!
+      get :new
+
+      expect(response).to respond_with 200
+      VCR.turn_on!
+    end
+  end
+
   it "returns not found for markets I am not a member of" do
     market = create(:market)
     user = create(:user)
