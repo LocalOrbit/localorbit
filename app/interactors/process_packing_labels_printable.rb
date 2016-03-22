9134ddc3b555b6c1978806b8a3cec9237da1fbf7
Andy Bass <andy@localorb.it>
2016-03-21 22:54:35 -0400

    Update sort Make font size variable based on string size - buyer, supplier, product, order number

--- app/interactors/process_packing_labels_printable.rb
+++ app/interactors/process_packing_labels_printable.rb
@@ -6,7 +6,7 @@ class ProcessPackingLabelsPrintable
     delivery_printable = PackingLabelsPrintable.find packing_labels_printable_id
     delivery = delivery_printable.delivery
     user = delivery_printable.user
-    orders = delivery.orders.for_seller(user).sort_by(&:id)
+    orders = delivery.orders.for_seller(user).sort_by(&:billing_organization_name)
     seller_orders = orders.map do |o| SellerOrder.new(o,user) end
 
     # pdf_result = PackingLabels::Generator.generate(orders: orders, request: request)
