bd79973d39b214b9964e4fcb14b4963762a3af48
jczetta <jczetta@gmail.com>
2016-04-05 11:51:39 -0400

    Clean up upload controller, use Imports files

--- app/controllers/admin/upload_controller.rb
+++ app/controllers/admin/upload_controller.rb
@@ -2,6 +2,7 @@ class Admin::UploadController < AdminController
 	require 'rubyXL'
   require 'open3'
   include API::V2 
+  include Imports
 
   def index
     @plan = current_market.plan.name # check if LocalEyes plan on market
@@ -29,11 +30,11 @@ class Admin::UploadController < AdminController
   def upload
     if params.has_key?(:datafile)
       # pass the datafile to the method with the csv file
-      jsn = API::V2::SerializeProducts.get_json_data(params[:datafile]) # product stuff, row errors
+      jsn = ::Imports::SerializeProducts.get_json_data(params[:datafile]) # product stuff, row errors
       @num_products_loaded = 0
       unless jsn.include?("invalid")
         jsn[0]["products"].each do |p|
-          API::V2::ProductHelpers.create_product_from_hash(p)
+          ::Imports::ProductHelpers.create_product_from_hash(p)
           @num_products_loaded += 1
         end
         @errors = jsn[1]
@@ -41,27 +42,8 @@ class Admin::UploadController < AdminController
         @num_products_loaded = 0
         @errors = {"File"=>jsn}
       end
- 
-      # interface -> show number of products loaded and a link back to do whatever. plus, file errors and whatever
 
     end
   end
-
   
-
-  # def newjob
-  #   @job_id = params[:job_id] # access this from the post
-  #   # now try to find audit with job id
-  #   aud = Audit.where(associated_id:@job_id)
-  #   if not aud.first
-  #     raise ActiveRecord::RecordNotFound
-  #   end
-  #   content = aud.first['comment'].split("|*|")
-  #   error_text = content.first
-  #   products_loaded = content.last.match(/(Loaded \d+ products!)/).captures.first
-  #   get_output(error_text,products_loaded) # when this is complete, should render errors on check tpl page
-  #   render(:layout => false)
-  # end
-  
-
 end
