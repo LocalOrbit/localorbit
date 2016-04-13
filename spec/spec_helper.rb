e74d3bb2ba97b69f094099524d66d69f3ffd2d45
Keith Meisel <keith@localorb.it>
2016-04-13 13:49:14 -0400

    Config edit to address deadlock

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -86,7 +86,27 @@ RSpec.configure do |config|
 
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner.strategy = :transaction # KXM
     ImportLegacyTaxonomy.run(Rails.root.join("db/taxonomy_truncated.csv"))
+  end
+
+  config.around( :each ) do |spec|
+    if spec.metadata[:js]
+      # JS => doesn't share connections => can't use transactions
+      spec.run
+      DatabaseCleaner.clean_with :deletion
+    else
+      # No JS/Devise => run with Rack::Test => transactions are ok
+      DatabaseCleaner.start
+      spec.run
+      DatabaseCleaner.clean
+
+      # see https://github.com/bmabey/database_cleaner/issues/99
+      begin
+        ActiveRecord::Base.connection.send :rollback_transaction_records, true
+      rescue
+      end
+    end
   end
 
   config.before(:each) do
