84b60c4b7e90eb3f2673ccfc54879e222aee3803
jczetta <jczetta@gmail.com>
2016-04-13 14:56:01 -0400

    Add id to search in all order ransack locations

--- spec/features/buying/viewing_invoices_spec.rb
+++ spec/features/buying/viewing_invoices_spec.rb
@@ -121,7 +121,7 @@ describe "Buyer invoices" do
       end
 
       it "by order number" do
-        fill_in "q_order_number_or_payment_note_cont", with: invoiced_order.order_number
+        fill_in "q_id_or_order_number_or_payment_note_cont", with: invoiced_order.order_number
         click_button "Filter"
 
         expect(page).to have_content(invoiced_order.order_number)
@@ -130,14 +130,14 @@ describe "Buyer invoices" do
       end
 
       it "by purchase order" do
-        fill_in "q_order_number_or_payment_note_cont", with: "123456"
+        fill_in "q_id_or_order_number_or_payment_note_cont", with: "123456"
         click_button "Filter"
 
         expect(page).to have_content(invoiced_order.order_number)
         expect(page).not_to have_content(invoiced_order2.order_number)
         expect(page).not_to have_content(invoiced_order3.order_number)
 
-        fill_in "q_order_number_or_payment_note_cont", with: "77839"
+        fill_in "q_id_or_order_number_or_payment_note_cont", with: "77839"
         click_button "Filter"
 
         expect(page).not_to have_content(invoiced_order.order_number)
