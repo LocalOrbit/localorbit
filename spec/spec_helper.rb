30e96f18eb420c4d5a96fd7b12cc102f514c19ff
Keith Meisel <keith@localorb.it>
2016-04-13 15:13:42 -0400

    Backing out recent changes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -86,7 +86,6 @@ RSpec.configure do |config|
 
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
-    DatabaseCleaner.strategy = :transaction # KXM
     ImportLegacyTaxonomy.run(Rails.root.join("db/taxonomy_truncated.csv"))
   end
 
--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -15,25 +15,6 @@ RSpec.configure do |config|
     DatabaseCleaner.start
   end
 
-  config.around( :each ) do |spec|
-    if spec.metadata[:js]
-      # JS => doesn't share connections => can't use transactions
-      spec.run
-      DatabaseCleaner.clean_with :deletion
-    else
-      # No JS/Devise => run with Rack::Test => transactions are ok
-      DatabaseCleaner.start
-      spec.run
-      DatabaseCleaner.clean
-
-      # see https://github.com/bmabey/database_cleaner/issues/99
-      begin
-        ActiveRecord::Base.connection.send :rollback_transaction_records, true
-      rescue
-      end
-    end
-  end
-
   config.after(:each) do
     DatabaseCleaner.clean
   end
