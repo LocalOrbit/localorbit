31bcd7e2231ecea392e48d479610d710e1d133e3
Keith Meisel <keith@localorb.it>
2016-05-02 09:27:44 -0400

    Now capturing Stripe card for Bank Account creation

--- app/interactors/create_bank_account.rb
+++ app/interactors/create_bank_account.rb
@@ -3,16 +3,27 @@ class CreateBankAccount
 
   def setup
     # Out of the original context and without this setup, the interactor fails at entity.bank_accounts.create(params)
-    context[:entity] ||= context[:market] || context[:organization]
+    context[:entity]    ||= context[:market] || context[:organization]
+    bank_account_params ||= context[:bank_account_params]
   end
 
   def perform
-    params = bank_account_params.dup
-    # if stripe_cust = context[:stripe_customer]
-    #   params[:notes] = "Stripe customer id: #{stripe_cust.id}"
-    # end
 
-    params.delete(:stripe_tok)
+    if(
+      # If the supplied bank_account_params constitute a Stripe Card...
+      bank_account_params.class == Stripe::Card &&
+      # ...and the card is for this Stripe customer
+      context[:entity].try(:stripe_customer_id) == params.bank_account_params
+    )
+      # ...then use the card to create the bank account.
+      params = extract_stripe_card_attributes(params)
+
+    else
+      # Otherwise, use the supplied hash
+      params = bank_account_params.dup
+      params.delete(:stripe_tok)
+    end
+
     context[:bank_account] = entity.bank_accounts.create(params)
 
     unless context[:bank_account].valid?
@@ -24,5 +35,18 @@ class CreateBankAccount
     if bank_account = context[:bank_account]
       bank_account.destroy
     end
+  end
+
+  def extract_stripe_card_attributes(params)
+    ret_val = {
+      bank_name: params.brand,
+      last_four: params.last4,
+      account_type: params.object,
+      verified: true,
+      bankable_type: 'Market',
+      expiration_month: params.exp_month,
+      expiration_year: params.exp_year,
+      stripe_id: params.id
+    }
   end
 end
--- app/interactors/create_stripe_subscription_for_entity.rb
+++ app/interactors/create_stripe_subscription_for_entity.rb
@@ -1,20 +1,17 @@
 class CreateStripeSubscriptionForEntity
   include Interactor
 
-  # This serves to prepare for perform AND document the expected context elements
-  def setup
+  def perform
+    # Initialize
     token       ||= context[:market_params][:stripe_tok]
     sub_params  ||= context[:subscription_params]
     entity      ||= context[:entity]
     stripe_cust ||= context[:stripe_customer]
-  end
 
-  def perform
-    # Initialize
     customer = PaymentProvider::Stripe.get_stripe_customer(stripe_cust.id)
 
     # Create the subscription
-    subscription = PaymentProvider::Stripe.upsert_subscription(entity, customer, stripe_subscription_info)
+    subscription = PaymentProvider::Stripe.upsert_subscription(entity, customer, stripe_subscription_info(sub_params, entity, token))
     context[:subscription] = subscription
     # Update the entity (if it's a Market)
     entity.set_subscription(subscription) if entity.respond_to?(:set_subscription)
@@ -24,11 +21,14 @@ class CreateStripeSubscriptionForEntity
     # ... and grab the most recent one
     context[:invoice] = invoices.data.first
 
+    # Capture the card data for later bank account creation (if necessary)
+    context[:bank_account_params] = PaymentProvider::Stripe.glean_card(context[:invoice])
+
   rescue => e
     context.fail!(error: e.message)
   end
 
-  def stripe_subscription_info
+  def stripe_subscription_info(sub_params, entity, token)
     ret_val = {
       plan: sub_params[:plan],
       metadata: {
@@ -41,5 +41,8 @@ class CreateStripeSubscriptionForEntity
     
     # Stripe complains if you pass an empty coupon.  Only add it if it exists
     ret_val[:coupon] = sub_params[:coupon] if sub_params[:coupon].present?
+
+    # Return the return value
+    ret_val
   end
 end
