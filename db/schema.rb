07ddba498da23206c8300f3edb32703dcd494620
Andy Bass <andy@localorb.it>
2016-05-05 17:31:47 -0400

    Add buyer/supplier attributes to registration form

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160418132511) do
+ActiveRecord::Schema.define(version: 20160505152336) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -481,11 +481,12 @@ ActiveRecord::Schema.define(version: 20160418132511) do
     t.boolean  "pending",                                                default: false
     t.text     "zpl_logo"
     t.string   "zpl_printer"
-    t.boolean  "self_directed_creation",                                 default: false
     t.boolean  "stripe_standalone"
     t.string   "legacy_stripe_account_id"
+    t.boolean  "self_directed_creation",                                 default: false
     t.boolean  "allow_product_fee"
     t.integer  "number_format_numeric",                                  default: 0
+    t.boolean  "subscribed",                                             default: false
   end
 
   add_index "markets", ["name"], name: "index_markets_on_name", using: :btree
@@ -655,19 +656,29 @@ ActiveRecord::Schema.define(version: 20160418132511) do
     t.text     "how_story"
     t.string   "photo_uid"
     t.string   "balanced_customer_uri"
-    t.boolean  "balanced_underwritten",        default: false, null: false
+    t.boolean  "balanced_underwritten",                                default: false, null: false
     t.string   "facebook"
     t.string   "twitter"
-    t.boolean  "display_facebook",             default: false
-    t.boolean  "display_twitter",              default: false
+    t.boolean  "display_facebook",                                     default: false
+    t.boolean  "display_twitter",                                      default: false
     t.boolean  "allow_purchase_orders"
     t.boolean  "allow_credit_cards"
     t.boolean  "allow_ach"
     t.integer  "legacy_id"
-    t.boolean  "show_profile",                 default: true
-    t.boolean  "active",                       default: false
-    t.boolean  "needs_activated_notification", default: true
+    t.boolean  "show_profile",                                         default: true
+    t.boolean  "active",                                               default: false
+    t.boolean  "needs_activated_notification",                         default: true
     t.string   "stripe_customer_id"
+    t.string   "org_type"
+    t.integer  "plan_id"
+    t.datetime "plan_start_at"
+    t.integer  "plan_interval",                                        default: 1,     null: false
+    t.decimal  "plan_fee",                     precision: 7, scale: 2, default: 0.0,   null: false
+    t.integer  "plan_bank_account_id"
+    t.string   "buyer_org_type"
+    t.string   "ownership_type"
+    t.boolean  "non_profit"
+    t.string   "professional_organizations"
   end
 
   add_index "organizations", ["name"], name: "index_organizations_on_name", using: :btree
@@ -806,6 +817,23 @@ ActiveRecord::Schema.define(version: 20160418132511) do
   add_index "promotions", ["market_id"], name: "index_promotions_on_market_id", using: :btree
   add_index "promotions", ["product_id"], name: "index_promotions_on_product_id", using: :btree
 
+  create_table "role_actions", force: true do |t|
+    t.string "section"
+    t.string "action"
+    t.string "description"
+    t.string "org_type",    default: [], array: true
+    t.string "plan_ids",    default: [], array: true
+  end
+
+  create_table "roles", force: true do |t|
+    t.string   "name"
+    t.string   "activities",      limit: 4096, default: [], array: true
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "org_type"
+    t.integer  "organization_id"
+  end
+
   create_table "sequences", force: true do |t|
     t.string  "name"
     t.integer "value", default: 0, null: false
@@ -896,5 +924,13 @@ ActiveRecord::Schema.define(version: 20160418132511) do
   add_index "users", ["invitations_count"], name: "index_users_on_invitations_count", using: :btree
   add_index "users", ["invited_by_id"], name: "index_users_on_invited_by_id", using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+
+  create_table "users_roles", id: false, force: true do |t|
+    t.integer "user_id"
+    t.integer "role_id"
+  end
+
+  add_index "users_roles", ["role_id"], name: "index_users_roles_on_role_id", using: :btree
+  add_index "users_roles", ["user_id"], name: "index_users_roles_on_user_id", using: :btree
 
 end
