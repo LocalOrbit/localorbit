08f4a82e2a3c378b606ce2189ecd657c3f6b1505
Keith Meisel <keith@localorb.it>
2016-05-10 14:11:51 -0400

    Initial draft of basic scaffold

---
+++ spec/features/market_manager/manage_cross_selling_lists_spec.rb
@@ -0,0 +1,20 @@
+require "spec_helper"
+
+describe "Manage cross selling lists" do
+  context "for a market with no cross sell lists" do
+    it "shows new list button" do
+    end
+  end
+
+  context "for a market with cross selling lists" do
+    it "shows a list of cross selling lists" do
+    end
+    it "saves changes to cross selling markets" do
+    end
+  end
+
+  context "view organization cross sells" do
+    it "allows organization to see their cross sells" do
+    end
+  end
+end
