13fcf291430c4d139ba4da022c899bb49ddff48b
Andy Bass <andy@localorb.it>
2016-06-02 10:28:15 -0400

    Typo

--- db/migrate/20160123195225_create_role_actions_table.rb
+++ db/migrate/20160123195225_create_role_actions_table.rb
@@ -2,7 +2,7 @@ class CreateRoleActionsTable < ActiveRecord::Migration
   def change
     create_table :role_actions do |t|
       t.string :description
-      t.string :org_type, array: true, using: 'gin', default: '{}'
+      t.string :org_types, array: true, using: 'gin', default: '{}'
       t.string :section
       t.string :action
       t.string :plan_ids, array: true, using: 'gin', default: '{}'
