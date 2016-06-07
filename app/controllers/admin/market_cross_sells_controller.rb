f3c54a88440fd0ab02048b589724ace7d3508de4
Keith Meisel <keith@localorb.it>
2016-06-07 13:29:19 -0400

    Revert changes made to obviated code

--- app/controllers/admin/market_cross_sells_controller.rb
+++ app/controllers/admin/market_cross_sells_controller.rb
@@ -2,30 +2,15 @@ class Admin::MarketCrossSellsController < AdminController
   before_action :require_admin_or_market_manager
   before_action :find_market
 
-  # KXM I expect the meat of both this and OrganizationCrossSellsController to be substantially similar - perhaps an opportunity to create a parent controller for both?
   def show
     @cross_selling_markets = current_user.markets.where(allow_cross_sell: true).where.not(id: @market.id).order(:name)
   end
 
-  def new
-  end
-
-  def create
-  end
-
-  # KXM This controller (Admin::MarketCrossSellsController) will need to be beefed up.  For the time being I'll replace the update function
   def update
-    # KXM Original code to HERE
-    # ids = params[:market].try(:[], :cross_sell_ids) || []
-    # @market.cross_sell_ids = ids
-    # HERE
+    ids = params[:market].try(:[], :cross_sell_ids) || []
+    @market.cross_sell_ids = ids
 
-    @market.toggle!(:market_enabled_cross_sell)
-
-    notice = @market.market_enabled_cross_sell? ? "Cross selling is now active" : "Cross selling is now inactive"
-
-    # KXM This is also original
-    redirect_to admin_market_cross_sell_path(@market), notice: notice
+    redirect_to admin_market_cross_sell_path(@market), notice: "Market Updated Successfully"
   end
 
   protected
