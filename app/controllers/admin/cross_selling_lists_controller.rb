a045466569e2c4e422a34e5d6e40dc7a74cf9a84
Keith Meisel <keith@localorb.it>
2016-06-17 16:46:39 -0400

    CRUD for child lists; exposed related list products

--- app/controllers/admin/cross_selling_lists_controller.rb
+++ app/controllers/admin/cross_selling_lists_controller.rb
@@ -10,11 +10,7 @@ class Admin::CrossSellingListsController < AdminController
   end
 
   def show
-    @cross_selling_list = CrossSellingList.find(params[:id])
-  end
-
-  # KXM I don't think this is needed...
-  def edit
+    @cross_selling_list = CrossSellingList.includes(:active_children, :products).find(params[:id])
   end
 
   def new
@@ -23,12 +19,15 @@ class Admin::CrossSellingListsController < AdminController
 
   def create
     @cross_selling_list = @entity.cross_selling_lists.build(cross_selling_list_params)
-    # KXM Some manual assignments will be required here (I'm thinking specifically 'creator').
-    # Further, the subscriber array will have to be addressed after saving the list itself 
-    # (the newly created id inserted as the derived list parent).
-
+    @cross_selling_list.creator = true
 
     if @cross_selling_list.save
+      selected = cross_selling_list_params[:shared_with].select(&:present?).map { |submitted_id| {parent_id: @cross_selling_list.id, entity_id: submitted_id.to_i} }
+
+      selected.each do |id_hash|
+        create_list(@cross_selling_list, id_hash)
+      end
+
       redirect_to [:admin, @entity, @cross_selling_list], notice: "Successfully created #{@cross_selling_list.name}"
     else
       flash.now[:alert] = "Could not create list"
@@ -37,18 +36,35 @@ class Admin::CrossSellingListsController < AdminController
   end
 
   def update
-    @cross_selling_list = CrossSellingList.find(params[:id])
+    @cross_selling_list = CrossSellingList.includes(:children).find(params[:id])
 
     if @cross_selling_list.update_attributes(cross_selling_list_params)
+
+      # If the edits are saved successfully then cascade through the related lists...
+      existing = @cross_selling_list.active_children.map { |l| {parent_id: l.parent_id, entity_id: l.entity_id} }
+      selected = cross_selling_list_params[:shared_with].select(&:present?).map { |submitted_id| {parent_id: @cross_selling_list.id, entity_id: submitted_id.to_i} }
+      overlap  = existing & selected
+
+      # Delete those that exist but aren't selected
+      (existing - selected).each do |id_hash|
+        delete_list(@cross_selling_list, id_hash)
+      end
+
+      # Add those that are selected but don't exist
+      (selected - existing).each do |id_hash|
+        create_list(@cross_selling_list, id_hash)
+      end
+
+      # Update those that appear in both
+      overlap.each do |id_hash|
+        update_list(@cross_selling_list, id_hash)
+      end
+
       redirect_to [:admin, @entity, @cross_selling_list]
     else
       flash.now.alert = "Could not update Cross Selling List"
       render :show
     end
-  end
-
-  # KXM Look into SoftDelete... this is likely not needed
-  def destroy
   end
 
   def cross_selling_list_params
@@ -66,4 +82,37 @@ class Admin::CrossSellingListsController < AdminController
     end
   end
 
+  def create_list(parent, id_hash)
+    target = parent.children.where("parent_id = ? AND entity_id = ?", id_hash[:parent_id], id_hash[:entity_id]).first
+    if target.blank?
+      new_list = parent.dup
+      new_list.entity_id = id_hash[:entity_id]
+      new_list.entity_type = "Market"
+      new_list.creator = false
+      new_list.status = "Pending"
+      new_list.parent_id = id_hash[:parent_id]
+      new_list.save
+    else
+      target.update_attribute(:deleted_at, nil)
+      # KXM This may not be transparent enough to be effective...
+      target.update_attribute(:status, "Active") if target.status = "Revoked"
+    end
+  end
+
+  def update_list(parent, id_hash)
+    target = get_child(parent, id_hash)
+    target.update_attribute(:name, parent.name) if target.pending?
+  end
+
+  def delete_list(parent, id_hash)
+    target = get_child(parent, id_hash)
+    target.update_attribute(:status, "Revoked") if target.status = "Active"
+    target.soft_delete
+  end
+
+  protected
+
+  def get_child(parent, id_hash)
+    parent.children.where("parent_id = ? AND entity_id = ?", id_hash[:parent_id], id_hash[:entity_id]).first
+  end
 end
--- app/models/cross_selling_list.rb
+++ app/models/cross_selling_list.rb
@@ -9,14 +9,19 @@ class CrossSellingList < ActiveRecord::Base
 
   belongs_to :parent, class_name: "CrossSellingList"
   has_many :children, class_name: "CrossSellingList", foreign_key: "parent_id"
+  has_many :active_children, -> { where(deleted_at: nil) }, class_name: "CrossSellingList", foreign_key: "parent_id"
 
   has_many :products, through: :cross_selling_list_products
+  has_many :cross_selling_list_products
 
   # Basic validation
   validates :name, presence: true, length: {maximum: 255}
   validates :entity_id, presence: true, numericality: {only_integer: true}
   validates :entity_type, presence: true, length: {maximum: 255}
   validates :status, presence: true, length: {maximum: 255}
+
+  # KXM flesh this out... may it reference the existing :children relation?
+  # scope :active_children, -> { where("#{self.class.table_name}.") }
 
   # Can this specify only_integer without mandating presence: true?
   # validates :parent_id, numericality: {only_integer: true}
@@ -36,13 +41,19 @@ class CrossSellingList < ActiveRecord::Base
 
   def active?
   	# Master lists are active if published and not deleted
-  	# Sublists are active if published and not deleted AND master list is active
+    return true if parent_id.blank? && deleted_at.blank? && status = "Published"
 
-  	# For now, though, let's just let everything pass...
-  	true 
+  	# Sublists are active if published and not deleted AND master list is active
+    return true if parent_id.present? && deleted_at.blank? && parent.active? && status = "Active"
+
+    false
+  end
+
+  def pending?
+    status = "Pending"
   end
 
   def subscribers
-    children.map{|c| c.entity.name}.join(", ")
+    active_children.includes(:entity).map{|c| c.entity.name}.join(", ")
   end
 end
