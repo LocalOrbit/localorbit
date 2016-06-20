38ee93a3af0146ea7367545fda0942411aeb5acc
jczetta <jczetta@gmail.com>
2016-06-20 16:19:59 -0400

    Begin identifying unique constraints for consol pick lists

--- app/controllers/admin/pick_lists_controller.rb
+++ app/controllers/admin/pick_lists_controller.rb
@@ -1,6 +1,11 @@
 class Admin::PickListsController < AdminController
   def show
     @delivery = Delivery.find(params[:id]).decorate
+    @delivery_schedule = @delivery.delivery_schedule
+    # @delivery = @delivery.decorate 
+    unique = {seller_start:@delivery_schedule.seller_delivery_start, seller_loc:@delivery_schedule.seller_fulfillment_location_id} # fulfillment location id should never be 0 if it comes up on a delivery
+
+
     @very_important_person = current_user.admin? || current_user.managed_market_ids.include?(@delivery.delivery_schedule.market_id)
 
     order_items = OrderItem.where(delivery_status: "pending", orders: {delivery_id: @delivery.id})
@@ -13,6 +18,8 @@ class Admin::PickListsController < AdminController
     unless @very_important_person
       order_items = order_items.where(products: {organization_id: current_user.organization_ids})
     end
+
+    # want this to be grouped by the unique pieces
 
     @pick_lists = order_items.group_by {|item| item.product.organization_id }.map do |_, items|
       PickListPresenter.new(items, @delivery_notes)
