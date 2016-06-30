4f794d5395eff8c1a3289e58dd10407bc02743b1
jczetta <jczetta@gmail.com>
2016-06-30 15:32:05 -0400

    Add fxn to pick lists controller

--- app/controllers/admin/pick_lists_controller.rb
+++ app/controllers/admin/pick_lists_controller.rb
@@ -1,25 +1,36 @@
 class Admin::PickListsController < AdminController
-  def show
-    binding.pry
-    @delivery = Delivery.find(params[:id]).decorate
-    @delivery_schedule = @delivery.delivery_schedule
-    # @delivery = @delivery.decorate 
-    unique = {seller_start:@delivery_schedule.seller_delivery_start, seller_loc:@delivery_schedule.seller_fulfillment_location_id,seller_display_date:@delivery.seller_display_date} # fulfillment location id should never be 0 if it comes up on a delivery
 
-    # must be on that delivery schedule
-    # so, deliveries on that delivery schedule with a delivery id that 
+  # basically want ids of all deliveries with the same delivery_date_heading
+  def get_deliveries(decorated_delivery)
+    delivs = []
+    markd = decorated_delivery.upcoming_delivery_date_heading
+    decorated_delivery.delivery_schedule.deliveries.each do |deliv|
+      deliv = deliv.decorate
+      if deliv.upcoming_delivery_date_heading == markd
+        delivs << deliv
+      end
+    end
+    if delivs.empty?
+      delivs = [decorated_delivery]
+    end
+    delivs.map(&:id)
+  end
+
+  def show
+    # binding.pry
+    @delivery = Delivery.find(params[:id]).decorate
+    # @delivery_schedule = @delivery.delivery_schedule
+
+    # unique = {seller_start:@delivery_schedule.seller_delivery_start, seller_loc:@delivery_schedule.seller_fulfillment_location_id,seller_display_date:@delivery.seller_display_date} # fulfillment location id should never be 0 if it comes up on a delivery
 
     @very_important_person = current_user.admin? || current_user.managed_market_ids.include?(@delivery.delivery_schedule.market_id)
 
-    # need a method to get the SET of deliveries to look through in order to create the order items
+    # need a method / functionality to handle the view such that they're grouped by the day/time uniqueness with location as desired for consolidated (seller display date on decorated delivery) -- how will the view handle it for show, looks to be iterating on deliveries instead of pick lists?
 
-    # also, need a method / functionality to handle the view such that they're grouped by the day/time uniqueness with location as desired for consolidated
-
-    order_items = OrderItem.where(delivery_status: "pending", orders: {delivery_id: @delivery.id})
+    order_items = OrderItem.where(delivery_status: "pending", orders: {delivery_id: get_deliveries(@delivery)})
                     .eager_load(:order, product: :organization)
                     .order("organizations.name, products.name")
                     .preload(order: :organization)
-    # binding.pry
 
     @delivery_notes = DeliveryNote.joins(:order).where(orders: {delivery_id: @delivery.id})
     
@@ -27,8 +38,8 @@ class Admin::PickListsController < AdminController
       order_items = order_items.where(products: {organization_id: current_user.organization_ids})
     end
 
-    # want this to be grouped by the unique pieces
-
+    # want this to be grouped by the unique pieces?
+    # no: should always be grped by org, just consolidated re: deliveries
     @pick_lists = order_items.group_by {|item| item.product.organization_id}.map do |_, items|
       PickListPresenter.new(items, @delivery_notes)
     end
