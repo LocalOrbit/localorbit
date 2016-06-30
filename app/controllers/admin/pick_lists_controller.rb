b1d2b47da34318d10cba5090ecc09cb5cf565ce7
jczetta <jczetta@gmail.com>
2016-06-30 13:26:12 -0400

    Comment clarity

--- app/controllers/admin/pick_lists_controller.rb
+++ app/controllers/admin/pick_lists_controller.rb
@@ -1,12 +1,19 @@
 class Admin::PickListsController < AdminController
   def show
+    binding.pry
     @delivery = Delivery.find(params[:id]).decorate
     @delivery_schedule = @delivery.delivery_schedule
     # @delivery = @delivery.decorate 
     unique = {seller_start:@delivery_schedule.seller_delivery_start, seller_loc:@delivery_schedule.seller_fulfillment_location_id} # fulfillment location id should never be 0 if it comes up on a delivery
 
+    # must be on that delivery schedule
+    # so, deliveries on that delivery schedule with a delivery id that 
 
     @very_important_person = current_user.admin? || current_user.managed_market_ids.include?(@delivery.delivery_schedule.market_id)
+
+    # need a method to get the SET of deliveries to look through in order to create the order items
+
+    # also, need a method / functionality to handle the view such that they're grouped by the day/time uniqueness with location as desired for consolidated
 
     order_items = OrderItem.where(delivery_status: "pending", orders: {delivery_id: @delivery.id})
                     .eager_load(:order, product: :organization)
