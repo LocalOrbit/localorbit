af644f6c14e968646f5fb7936d7a0a57d104bb35
Andy Bass <andy@localorb.it>
2016-07-26 12:13:50 -0400

    Check for nil order

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -14,7 +14,7 @@ class CartItem < ActiveRecord::Base
   validate :quantity_is_available, unless: "errors.has_key? :quantity"
 
   def unit_price
-    Orders::UnitPriceLogic.unit_price(product, cart.market, cart.organization, order.market.add_item_pricing ? order.created_at : Time.current, quantity)
+    Orders::UnitPriceLogic.unit_price(product, cart.market, cart.organization, !order.nil? && order.market.add_item_pricing ? order.created_at : Time.current, quantity)
   end
 
   def total_price
