71e20607e196e2e35ed904322c05b8db817773aa
Andy Bass <andy@localorb.it>
2016-09-03 22:42:38 -0400

    Fix s3 settings

--- config/initializers/direct_s3_upload.rb
+++ config/initializers/direct_s3_upload.rb
@@ -9,7 +9,7 @@ S3DirectUpload.config do |c|
     module UploadHelper
       class S3Uploader
         def url
-          "http#{@options[:ssl] ? 's' : ''}://#{@options[:bucket]}.#{@options[:region]}.amazonaws.com/"
+          "http#{@options[:ssl] ? 's' : ''}://#{@options[:bucket]}.#{@options[:region]}.s3.amazonaws.com/"
         end
       end
     end
