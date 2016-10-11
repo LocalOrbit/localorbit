8577f8a2c5bf7489e4b0faffdee7ab7e53883115
Andy Bass <andy@localorb.it>
2016-10-11 11:15:58 -0400

    Debugging

--- app/mailers/export_mailer.rb
+++ app/mailers/export_mailer.rb
@@ -1,6 +1,7 @@
 class ExportMailer < BaseMailer
   def export_success(recipients, file)
 
+    puts "In Export Mailer"
     attachments['export.csv'] = {mime_type: 'text/csv', content: file}
     mail(
       to: recipients,
