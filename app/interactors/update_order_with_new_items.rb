53ce5ee228e027ee1146e64398d243b7b9167e13
Andy Bass <andy@localorb.it>
2016-11-15 12:29:00 -0500

    Remove sending of update emails for added items to orders

--- app/interactors/update_order_with_new_items.rb
+++ app/interactors/update_order_with_new_items.rb
@@ -1,6 +1,6 @@
 class UpdateOrderWithNewItems
   include Interactor::Organizer
 
-  organize AddItemsToOrder, ApplyDiscountToAddedOrderItems, StoreOrderFees, UpdatePurchase, SendUpdateEmails
+  organize AddItemsToOrder, ApplyDiscountToAddedOrderItems, StoreOrderFees, UpdatePurchase
 
 end
