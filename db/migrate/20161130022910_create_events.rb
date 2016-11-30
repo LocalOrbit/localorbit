a91ec463bef85024ce530276c33dd5b1da9be46f
Keith Meisel <keith@localorb.it>
2016-11-29 21:30:33 -0500

    Initial scaffold for events table

---
+++ db/migrate/20161130022910_create_events.rb
@@ -0,0 +1,12 @@
+class CreateEvents < ActiveRecord::Migration
+  def change
+    create_table :events do |t|
+      t.string :event_id
+      t.text :payload
+      t.timestamp :successful_at
+      t.string :timestamps
+
+      t.timestamps
+    end
+  end
+end
