012fad6980ecf875d72d1268c9556b2ea211a05a
Keith Meisel <keith@localorb.it>
2016-12-01 02:52:44 -0500

    Refactoring in anticipation of failed payment

--- lib/payment_provider/handlers/invoice_handler.rb
+++ lib/payment_provider/handlers/invoice_handler.rb
@@ -46,11 +46,35 @@ module PaymentProvider
         raise "Card not on file" unless bank_account = BankAccount.where(stripe_id: charge.source.id).first
 
         # ...then create a payment record:
+        # Payment.create!(payment_params(subscriber, bank_account, event_params))
         Payment.create!(payment_type: 'service', amount: event_params[:total], created_at: event_params[:date], updated_at: event_params[:date], status: 'paid',payer_id: subscriber.organization_id, payer_type: 'Organization', market_id: subscriber.id, bank_account_id: bank_account.id, stripe_id: event_params[:payment], payment_provider: 'stripe', organization_id: subscriber.organization_id )
       end
 
       def self.event_log_record(event)
         e = Event.where(event_id: event.id).first || Event.create!(event_id: event.id, stripe_customer_id: event.data.object.customer, payload: event.to_json)
+      end
+
+      private
+
+      def payment_params(subscriber, bank_account, event_params)
+        # KXM This ought to handle any payment status (paid, failed, etc)
+        {
+          payment_type: 'service',
+          amount: event_params[:total],
+          created_at: event_params[:date],
+          updated_at: event_params[:date],
+
+          status: 'paid',
+
+          payer_id: subscriber.organization_id,
+          payer_type: 'Organization',
+          market_id: subscriber.id,
+          bank_account_id: bank_account.id,
+          stripe_id: event_params[:payment],
+          payment_provider: 'stripe',
+          organization_id: subscriber.organization_id,
+
+        }
       end
 
     end
