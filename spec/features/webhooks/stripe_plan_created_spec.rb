4c9b6d76fa8932632f71a0f5f6f81453bd63464d
Keith Meisel <keith@localorb.it>
2016-12-05 20:54:58 -0500

    Plan handler now inherits; Add to test spec

--- spec/features/webhooks/stripe_plan_created_spec.rb
+++ spec/features/webhooks/stripe_plan_created_spec.rb
@@ -6,10 +6,14 @@ describe "stripe plan.created event", vcr: true, webhook: true do
 
   it "creates a plan" do
     expect(find_plans.count).to eq 2
-    post '/webhooks/stripe', JSON.parse(File.read('spec/features/webhooks/plan.created.json'))
+    response = post '/webhooks/stripe', JSON.parse(File.read('spec/features/webhooks/plan.created.json'))
+
+    expect(response.status).to eq 200
 
     # See the Plan in the database:
     expect(find_plans.count).to eq 3
+
+    expect(find_plans.last.stripe_id).to eq 'KXM2'
   end
 
   #
