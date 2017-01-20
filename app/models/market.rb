7f74ddbb74153a7daa821e45d51de201b5a4bba8
Andy Bass <andy@localorb.it>
2017-01-19 22:06:21 -0500

    Add custom background upload option on style page Update background logic

--- app/models/market.rb
+++ app/models/market.rb
@@ -54,8 +54,10 @@ class Market < ActiveRecord::Base
 
   dragonfly_accessor :logo
   dragonfly_accessor :photo
+  dragonfly_accessor :background_img
   define_after_upload_resize(:logo, 1200, 1200)
   define_after_upload_resize(:photo, 1800, 1800)
+  define_after_upload_resize(:background_img, 1366, 768)
   validates_property :format, of: :logo,  in: %w(jpg jpeg png gif)
   validates_property :format, of: :photo, in: %w(jpg jpeg png gif)
 
