0cf7d7d61633c0aca80164640753f7e66828c04e
Andy Bass <andy@localorb.it>
2017-01-31 10:44:13 -0500

    Unit Price Changes

--- lib/orders/unit_price_logic.rb
+++ lib/orders/unit_price_logic.rb
@@ -2,27 +2,19 @@ module Orders
   class UnitPriceLogic
     class << self
       def prices(product, market, organization, time)
-        price = Price.for_product_and_market_and_org_at_time(product, market, organization, time).order(:min_quantity)
-        price_specific = Price.for_product_and_market_and_org_at_time_specific(product, market, organization, time).order(:min_quantity)
-        if price.empty? && price_specific.empty?
-          price = Price.for_product_and_market_and_org_at_time(product, market, organization, Time.current).order(:min_quantity)
-          price_specific = Price.for_product_and_market_and_org_at_time_specific(product, market, organization, Time.current).order(:min_quantity)
+        price = Price.for_product_and_market_and_org_at_time(product, market, organization, time).order("min_quantity, organization_id")
+        if price.empty?
+          price = Price.for_product_and_market_and_org_at_time(product, market, organization, Time.current).order(:organization_id, :min_quantity)
         end
-        prices = []
+        temp_min = nil
+        dups = []
         price.each do |p|
-          skip=false
-          price_specific.each do |q|
-            if p.min_quantity == q.min_quantity
-              prices << q
-              skip = true
-            end
+          if p.min_quantity == temp_min && p.min_quantity > 0 && p.organization_id.nil?
+            dups << p
           end
-          if !skip
-            prices << p
-          end
+          temp_min = p.min_quantity
         end
-
-        prices
+        price - dups
       end
 
       def unit_price(product, market, organization, time, quantity)
