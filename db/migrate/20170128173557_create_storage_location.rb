dc95742eede344afd4729ac44e499a2d62f89386
Andy Bass <andy@localorb.it>
2017-01-31 23:52:40 -0500

    Fix migration

--- db/migrate/20170128173557_create_storage_location.rb
+++ db/migrate/20170128173557_create_storage_location.rb
@@ -1,8 +1,8 @@
 class CreateStorageLocation < ActiveRecord::Migration
   def change
-    create_table :storage_locations do |t|
-      t.integer :market_id
-      t.string :name
-    end
+  #  create_table :storage_locations do |t|
+  #    t.integer :market_id
+  #    t.string :name
+  #  end
   end
 end
