2d6f7e61144aaa744331dd7930c581a79f10815b
Andy Bass <andy@localorb.it>
2017-02-02 11:09:22 -0500

    Fix

--- db/migrate/20170202160435_add_gc_model_to_org.rb
+++ db/migrate/20170202160435_add_gc_model_to_org.rb
@@ -1,5 +1,5 @@
 class AddGcModelToOrg < ActiveRecord::Migration
   def change
-    add_column :orgs, :payment_model, :string, default: 'buysell'
+    add_column :organizations, :payment_model, :string, default: 'buysell'
   end
 end
