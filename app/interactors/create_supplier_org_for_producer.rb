df0be8d05aab34643a1789449c7d6dd0960f6882
Andy Bass <andy@localorb.it>
2017-02-04 22:08:24 -0500

    Fix

--- app/interactors/create_supplier_org_for_producer.rb
+++ app/interactors/create_supplier_org_for_producer.rb
@@ -2,7 +2,7 @@ class CreateSupplierOrgForProducer
   include Interactor
 
   def perform
-    if market.organization.plan.stripe_id == "PRODUCER"
+    if !market.organization.plan.nil? && market.organization.plan.stripe_id == "PRODUCER"
       organization_params = context[:organization].as_json
 
       org = Organization.new(organization_params.slice(:id).merge(name: "#{organization_params['name']} Supplier", active: true, allow_purchase_orders: true, allow_credit_cards: true, can_sell: true, org_type: 'S'))
