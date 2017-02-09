5f75f321ea43a4b107a60a3978e34959f3d46f1f
Andy Bass <andy@localorb.it>
2017-02-09 11:13:03 -0500

    Fix delvery tools sort

--- app/controllers/admin/delivery_tools_controller.rb
+++ app/controllers/admin/delivery_tools_controller.rb
@@ -2,6 +2,6 @@ class Admin::DeliveryToolsController < AdminController
   before_action :require_selected_market
 
   def show
-    @upcoming_deliveries = current_user.markets.map{|market| market.upcoming_deliveries_for_user(current_user).decorate}.flatten
+    @upcoming_deliveries = current_user.markets.map{|market| market.upcoming_deliveries_for_user(current_user).order("deliver_on").decorate}.flatten
   end
 end
