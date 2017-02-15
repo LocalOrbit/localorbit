4c977c49ccd39ae3501192b1f826a9c8bb0396d4
Keith Meisel <keith@localorb.it>
2017-02-15 12:23:04 -0500

    Documenting failure

--- app/policies/purchase_order_policy.rb
+++ app/policies/purchase_order_policy.rb
@@ -1,11 +1,32 @@
 class PurchaseOrderPolicy < ApplicationPolicy
 
   def create?
-    # KXM GC: policy PurchaseOrder#create? should confirm that market.organization.try(:payment_model) == 'consignment'
     # binding.pry
 
+    # KXM GC: policy PurchaseOrder#create? checking current_market didn't work.  Other ideas?
+    # current_market.present? && current_market.is_consignment_market?
+
+    # I played with setting a custom user context (defined below) that includes the market, but that nests the current user as an instance variable (along with current_market) of the pundit user, making all calls to 'user' have to be 'user.user'.  Ugly.  Tried assigning user to UserContext self, but that threw an error (as expected, really)
+
+    # Defined somewhere:
+    # -------------------
+    # class UserContext
+    #   attr_reader :user, :current_market
+
+    #   def initialize(user, current_market)
+    #     @user = user
+    #     @current_market   = current_market
+    #   end
+    # end
+
+
+    # Defined in the Application controller:
+    # ---------------------------------------
+    # def pundit_user
+    #   UserContext.new(current_user, request.ip)
+    # end
+
     true
-    # market.organization.try(:payment_model) == 'consignment'
   end
 
 end
