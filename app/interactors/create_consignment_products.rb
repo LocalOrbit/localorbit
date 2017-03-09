c773546f764dcae5ee09db8a662b1a6138c01bbd
Keith Meisel <keith@localorb.it>
2017-03-09 18:23:08 -0500

    WIP

--- app/interactors/create_consignment_products.rb
+++ app/interactors/create_consignment_products.rb
@@ -3,9 +3,7 @@ class CreateConsignmentProducts
 
   def perform
     order = context[:order]
-    binding.pry
     ConsignmentProduct.upsert_items(order.items) if order.purchase_order?
-    binding.pry
   end
 
   def rollback
--- app/models/consignment_product.rb
+++ app/models/consignment_product.rb
@@ -10,19 +10,18 @@ class ConsignmentProduct < ActiveRecord::Base
   validates_uniqueness_of :consignment_organization_id, :scope => [:product_id]
 
   def self.upsert_items(order_items)
-    items.each do |item|
-      upsert(item)
+    order_items.each do |item|
+      cp = upsert(item)
     end
   end
 
   def self.upsert(order_item)
-    binding.pry
     # Recover the relevant consignment product row
-    cons_prod = ConsignmentProduct.where(product_id: order_item.product, consignment_organization_id: order_item.order.organization).first
+    cons_prod = ConsignmentProduct.where(product_id: order_item.product.id, consignment_organization_id: order_item.order.organization.id).first
 
     if cons_prod.present?
       # If found, find the relevant consignment_product lot
-      lot = consignment_product.lots.where('lot.id = ?', order_item.lots.first.lot)
+      lot = consignment_product.lots.where('lot.id = ?', order_item.lots.first.lot.id)
       if lot.present?
         # If found, add to the quantity
         # KXM GC: Lot qty will have to be open for modification on PO delivery, too
@@ -32,18 +31,19 @@ class ConsignmentProduct < ActiveRecord::Base
         consignment_product.lots << order_item.lots.first.lot
       end
     else
-      # binding.pry
       # If row not found, build a new consignment product...
       product = order_item.product.dup
-      product.organization = order.organization
-      product.lots << order_item.lots.first.lot
-      product.skip_validation = true
-      product.save!(:validate => false)
+      product.organization_id = order_item.order.organization_id
 
-      binding.pry
+      product.skip_validation = true
+      product.save(:validate => false)
+
+      # ... , including the relevant lot, ...
+      product.lots << order_item.lots.first.lot
+      product.lots.first.update_attribute(:quantity, order_item.quantity)
+
       # ...and insert it with the original product and organization
-      cons_prod = ConsignmentProduct.create(product_id: order_item.product.id, consignment_product_id: product.id, order_id: order_item.order.id)
-      binding.pry
+      cons_prod = ConsignmentProduct.create(product_id: order_item.product.id, consignment_product_id: product.id, consignment_organization_id: order_item.order.organization_id)
     end
     cons_prod
   end
