5a0f2df5007ab04252fcc57cb08008cb1260bf53
Keith Meisel <keith@localorb.it>
2017-03-08 19:34:01 -0500

    Consolodate upsert code

--- app/models/consignment_product.rb
+++ app/models/consignment_product.rb
@@ -9,9 +9,16 @@ class ConsignmentProduct < ActiveRecord::Base
   # This should assert a single consignment_org/product record
   validates_uniqueness_of :consignment_organization_id, :scope => [:product_id]
 
-  def upsert(order, order_item)
+  def self.upsert_items(order_items)
+    items.each do |item|
+      upsert(item)
+    end
+  end
+
+  def self.upsert(order_item)
+    binding.pry
     # Recover the relevant consignment product row
-    cons_prod = ConsignmentProduct.where(product_id: order_item.product, consignment_organization_id: order.organization).first
+    cons_prod = ConsignmentProduct.where(product_id: order_item.product, consignment_organization_id: order_item.order.organization).first
 
     if cons_prod.present?
       # If found, find the relevant consignment_product lot
@@ -25,14 +32,18 @@ class ConsignmentProduct < ActiveRecord::Base
         consignment_product.lots << order_item.lots.first.lot
       end
     else
+      # binding.pry
       # If row not found, build a new consignment product...
       product = order_item.product.dup
       product.organization = order.organization
       product.lots << order_item.lots.first.lot
-      product.save
+      product.skip_validation = true
+      product.save!(:validate => false)
 
+      binding.pry
       # ...and insert it with the original product and organization
-      cons_prod = ConsignmentProduct.create(product: order_item.product, consignment_product: product, order_id: order.id)
+      cons_prod = ConsignmentProduct.create(product_id: order_item.product.id, consignment_product_id: product.id, order_id: order_item.order.id)
+      binding.pry
     end
     cons_prod
   end
