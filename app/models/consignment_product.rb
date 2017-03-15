654d0433c55742b6b9e921c5cfabdfd694661b2b
Keith Meisel <keith@localorb.it>
2017-03-14 22:42:58 -0400

    Duplicate general product for new consignment product

--- app/models/consignment_product.rb
+++ app/models/consignment_product.rb
@@ -20,6 +20,11 @@ class ConsignmentProduct < ActiveRecord::Base
     cons_prod = ConsignmentProduct.where(product_id: order_item.product.id, consignment_organization_id: order_item.order.organization.id).first
 
     if cons_prod.present?
+      # KXM GC: When a consignment product already exists then add a new lot
+      # There is no need looking for the lot here...  The consignment product
+      # lot will be tied to the PO 'Delivered' text box, directly updating the
+      # consignment product in the process
+
       # If found, find the relevant consignment_product lot
       lot = consignment_product.lots.where('lot.id = ?', order_item.lots.first.lot.id)
       if lot.present?
@@ -32,15 +37,22 @@ class ConsignmentProduct < ActiveRecord::Base
       end
     else
       # If row not found, build a new consignment product...
+      general_product = order_item.product.general_product.dup
+      general_product.skip_validation = true
+      general_product.save(:validate => false)
+
       product = order_item.product.dup
+      product.general_product_id = general_product.id
       product.organization_id = order_item.order.organization_id
 
       product.skip_validation = true
       product.save(:validate => false)
 
       # ... , including the relevant lot, ...
-      product.lots << order_item.lots.first.lot
-      product.lots.first.update_attribute(:quantity, order_item.quantity)
+      lot = order_item.lots.first.lot.dup
+      lot.quantity = order_item.quantity
+      lot.product_id = product.id
+      lot.save(:validate => false)
 
       # ...and insert it with the original product and organization
       cons_prod = ConsignmentProduct.create(product_id: order_item.product.id, consignment_product_id: product.id, consignment_organization_id: order_item.order.organization_id)
