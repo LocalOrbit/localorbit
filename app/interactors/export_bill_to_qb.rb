3cf4fc44a8a775af03aaf9a2ec825cb6a011de38
Andy Bass <andy@localorb.it>
2017-03-24 16:19:36 -0400

    Fixes to use transactions to build Bill details Add select for holdover PO selection

--- app/interactors/export_bill_to_qb.rb
+++ app/interactors/export_bill_to_qb.rb
@@ -3,7 +3,7 @@ class ExportBillToQb
 
   def perform
     @qb_profile = curr_market.organization.qb_profile
-    result = Quickbooks::Bill.create_bill(order, session, @qb_profile)
+    result = Quickbooks::Bill.create_bill(order, po_transactions, child_transactions, session, @qb_profile)
     order.qb_ref_id = Integer(result.id)
     order.save!
   end
