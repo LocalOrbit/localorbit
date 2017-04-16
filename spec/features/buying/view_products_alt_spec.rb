421f97c8d0449b2e46c999a049964512e504e367
Andy Bass <andy@localorb.it>
2017-04-16 00:29:08 -0400

    Fix

--- spec/features/buying/view_products_alt_spec.rb
+++ spec/features/buying/view_products_alt_spec.rb
@@ -1,38 +1,47 @@
 require "spec_helper"
 
 feature "Viewing products" do
-  let!(:market) { create(:market, :with_addresses, alternative_order_page: true) }
+  let(:market_org) { create(:organization, :market, :single_location, payment_model: 'consignment') }
+  let!(:market) { create(:market, :with_addresses, organization: market_org, alternative_order_page: true) }
   let!(:delivery_schedule1) { create(:delivery_schedule, :buyer_pickup,
                                      market: market,
-                                     order_cutoff: 24,
-                                     day: 5,
-                                     buyer_pickup_location_id: 0,
-                                     buyer_pickup_start: "12:00 PM",
-                                     buyer_pickup_end: "2:00 PM") }
+                                     delivery_cycle: 'manual',
+                                     order_cutoff: 0) }
   let!(:delivery_schedule2) { create(:delivery_schedule, market: market, day: 3, deleted_at: Time.zone.parse("2013-03-21")) }
 
-  let!(:org1) { create(:organization, :seller, markets: [market]) }
+  let!(:org1) { create(:organization, :seller, :single_location, markets: [market]) }
   let!(:org1_product) { create(:product, :sellable, name: "celery", organization: org1, delivery_schedules: [delivery_schedule1]) }
-  let!(:promotion) { create(:promotion, :active, product: org1_product, market: market, body: "Big savings!") }
 
-  let!(:org2) { create(:organization, :seller, markets: [market]) }
+  let!(:org2) { create(:organization, :seller, :single_location, markets: [market]) }
   let!(:org2_product) { create(:product, :sellable, organization: org2, delivery_schedules: [delivery_schedule1]) }
   let!(:org2_product_deleted) { create(:product, :sellable, organization: org2, deleted_at: Time.zone.parse("2014-10-01")) }
 
-  let!(:inactive_org) { create(:organization, :seller, active: false, markets: [market]) }
+  let!(:inactive_org) { create(:organization, :seller, :single_location, active: false, markets: [market]) }
   let!(:inactive_org_product) { create(:product, :sellable, organization: inactive_org, delivery_schedules: [delivery_schedule1]) }
 
-  let!(:other_org) { create(:organization, :seller) }
+  let!(:other_org) { create(:organization, :seller, :single_location) }
   let!(:other_products) { create_list(:product, 3, :sellable, organization: other_org) }
 
-  let!(:buyer_org) { create(:organization, :single_location, :buyer, markets: [market]) }
+  let!(:buyer_org) { create(:organization, :buyer, :single_location, markets: [market]) }
   let(:user) { create(:user, :buyer, organizations: [buyer_org]) }
   let(:market_manager) { create(:user, :market_manager, managed_markets: [market]) }
 
   let(:available_products) { [org1_product, org2_product] }
 
+  let(:po_item) { create(:order_item, product: org1_product) }
+  let(:purchase_order) { create(:order, :consignment_po, items:[po_item]) }
+  let(:sales_order) { create(:order, :consignment_so) }
+
+  def consigbnment_celery_item
+    Dom::Cart::POItem.find_by_name("celery")
+  end
+
   def celery_item
-    Dom::Cart::Item.find_by_name("celery")
+    Dom::Cart::ConsignmentItem.find_by_name("celery")
+  end
+
+  def cart_link
+    Dom::CartLink.first
   end
 
   before do
@@ -44,16 +53,51 @@ feature "Viewing products" do
     Timecop.return
   end
 
-  scenario "alternate products page" do
-    sign_in_as(user)
-    click_link("Order", match: :first)
+  scenario "Purchase Order", :js do
+    sign_in_as(market_manager)
+    click_link("Purchase Order", match: :first)
+
+    expect(page).to have_content("Select a Supplier")
+    select "Organization 2", from: "Supplier", visible: false
+    click_button "Select Supplier"
 
     # TODO: Fix alternative order page test to check for items in catalog
 
-    #expect(page).to have_content("celery")
-    #expect(page).to have_content(org2_product.name)
-    #fill_in("app-search", with: "celery")
-    #expect(page).to have_content("celery")
-    #expect(page).not_to have_content(org2_product.name)
+    expect(page).to have_content("Please choose a delivery date")
+    fill_in("buyer_deliver_on", with: "04 Apr 2017")
+    click_button "Start Ordering"
+
+    expect(page).to have_content("celery")
+    celery_item.set_quantity(1)
+    expect(Dom::CartLink.first.count).to have_content("1")
+
+
+
   end
+
+  scenario "Sales Order", :js do
+    sign_in_as(market_manager)
+    click_link("Sales Order", match: :first)
+
+    expect(page).to have_content("Select a Buyer")
+    select buyer_org.name, from: "Buyer", visible: false
+    click_button "Select Buyer"
+
+    # TODO: Fix alternative order page test to check for items in catalog
+
+    expect(page).to have_content("Please choose a delivery date")
+    fill_in("buyer_deliver_on", with: "04 Apr 2017")
+    click_button "Start Ordering"
+
+    expect(page).to have_content("celery")
+    celery_item.set_quantity(1)
+    expect(Dom::CartLink.first.count).to have_content("0")
+
+    celery_item.set_sale_price(4)
+    expect(Dom::CartLink.first.count).to have_content("0")
+
+    celery_item.set_net_price(3)
+    expect(Dom::CartLink.first.count).to have_content("1")
+  end
+
 end
