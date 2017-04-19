4352e414a91421d123685b78ef01d2cf432465eb
Andy Bass <andy@localorb.it>
2017-04-19 11:43:47 -0400

    Order detail display fixes Allow update of delivery fees on cart

--- app/interactors/create_order.rb
+++ app/interactors/create_order.rb
@@ -39,8 +39,8 @@ class CreateOrder
       payment_status: "unpaid",
       payment_method: params[:payment_method],
       payment_note: params[:payment_note],
-      delivery_fees: cart.delivery_fees,
-      total_cost: cart.total,
+      delivery_fees: cart.market.is_consignment_market? && !params[:delivery_fees].nil? ? params[:delivery_fees] : cart.delivery_fees,
+      total_cost: cart.market.is_consignment_market? && !params[:delivery_fees].nil? ? params[:delivery_fees] + cart.total : cart.total,
       placed_at: Time.current,
       order_type: params[:order_type] || 'sales',
     )
