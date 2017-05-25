f87f40b23c7c6c4f39c81dc6048570961bbfb99c
Andy Bass <andy@localorb.it>
2017-05-25 10:50:34 -0400

    Fix purchase history page for buyers Show JE button

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -13,14 +13,14 @@ class OrdersController < ApplicationController
 
   def index
     po_filter = {:q => {"order_type_matches" => 'sales'}}
-    @query_params.merge!(po_filter)
+    @query_params.deep_merge!(po_filter)
 
     order_list
   end
 
   def purchase_orders
     po_filter = {:q => {"order_type_eq" => "purchase"}}
-    @query_params.merge!(po_filter)
+    @query_params.deep_merge!(po_filter)
 
     order_list
 
@@ -28,10 +28,11 @@ class OrdersController < ApplicationController
   end
 
   def order_list
+    #@query_params["order_type_matches"] = 'sales'
     @query_params["placed_at_date_gteq"] ||= 7.days.ago.to_date.to_s
     @query_params["placed_at_date_lteq"] ||= Date.today.to_s
-    @presenter = BuyerOrderPresenter.new(current_user, current_market, request.query_parameters, @query_params)
-    @q = search_and_calculate_totals(@presenter)
+    @search_presenter = BuyerOrderPresenter.new(current_user, current_market, request.query_parameters, @query_params)
+    @q = search_and_calculate_totals(@search_presenter)
 
     @buyer_orders ||= @q.result
     @buyer_orders = @buyer_orders.page(params[:page]).per(@query_params[:per_page])
@@ -171,56 +172,4 @@ class OrdersController < ApplicationController
 
     results
   end
-
-=begin
-  def load_consignment_transactions(order)
-    @child_transactions = []
-    @po_transactions = ConsignmentTransaction.joins("
-      LEFT JOIN lots ON consignment_transactions.lot_id = lots.id
-      LEFT JOIN products ON consignment_transactions.product_id = products.id
-      LEFT JOIN order_items ON consignment_transactions.order_item_id = order_items.id")
-                           .where(order_id: order.id, transaction_type: 'PO')
-                           .where("parent_id IS NULL")
-                           .select("consignment_transactions.id, consignment_transactions.transaction_type, consignment_transactions.product_id, products.name as product_name, lots.number as lot_name, order_items.delivery_status, consignment_transactions.quantity, consignment_transactions.net_price, consignment_transactions.sale_price")
-                           .order("consignment_transactions.id, consignment_transactions.parent_id")
-
-    if !@po_transactions.nil?
-      @po_transactions.each do |po|
-        ct = ConsignmentTransaction.joins("
-        LEFT JOIN orders ON consignment_transactions.order_id = orders.id
-        LEFT JOIN lots ON consignment_transactions.lot_id = lots.id
-        LEFT JOIN organizations ON orders.organization_id = organizations.id")
-                 .where(parent_id: po.id)
-                 .select("consignment_transactions.id, consignment_transactions.transaction_type, consignment_transactions.order_id, consignment_transactions.product_id, consignment_transactions.quantity, lots.number as lot_name, consignment_transactions.net_price, consignment_transactions.sale_price, organizations.name AS buyer_name, orders.delivery_status")
-                 .order("consignment_transactions.product_id, consignment_transactions.created_at")
-
-        @child_transactions << ct.to_a
-      end
-    end
-
-    @parent_transactions = []
-    @so_transactions = ConsignmentTransaction.joins("
-        LEFT JOIN lots ON consignment_transactions.lot_id = lots.id
-        LEFT JOIN products ON consignment_transactions.product_id = products.id
-        LEFT JOIN order_items ON consignment_transactions.order_item_id = order_items.id")
-                           .where(order_id: order.id, transaction_type: 'SO')
-                           .select("consignment_transactions.id, consignment_transactions.transaction_type, consignment_transactions.product_id, products.name as product_name, lots.number as lot_name, lots.quantity as lot_quantity, order_items.delivery_status, consignment_transactions.quantity, consignment_transactions.net_price, consignment_transactions.sale_price, consignment_transactions.parent_id")
-                           .order("consignment_transactions.id, consignment_transactions.parent_id")
-
-
-    if !@so_transactions.nil?
-      @so_transactions.each do |so|
-        ct = ConsignmentTransaction.joins("
-            LEFT JOIN orders ON consignment_transactions.order_id = orders.id
-            LEFT JOIN lots ON consignment_transactions.lot_id = lots.id
-            LEFT JOIN organizations ON orders.organization_id = organizations.id")
-                 .where(id: so.parent_id)
-                 .select("consignment_transactions.id, consignment_transactions.transaction_type, consignment_transactions.order_id, consignment_transactions.product_id, consignment_transactions.quantity, lots.number as lot_name, consignment_transactions.net_price, consignment_transactions.sale_price, organizations.name AS buyer_name, orders.delivery_status")
-                 .order("consignment_transactions.product_id, consignment_transactions.created_at")
-
-        @parent_transactions << ct.to_a
-      end
-    end
-  end
-=end
 end
