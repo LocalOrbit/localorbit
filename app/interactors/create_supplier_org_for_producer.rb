f07c738dd094e282350c7fd3a4411dd1d8a18370
Andy Bass <andy@localorb.it>
2017-07-13 11:21:08 -0400

    Fix RYO create

--- app/interactors/create_supplier_org_for_producer.rb
+++ app/interactors/create_supplier_org_for_producer.rb
@@ -2,13 +2,11 @@ class CreateSupplierOrgForProducer
   include Interactor
 
   def perform
-    if !market.organization.plan.nil? && market.organization.plan.stripe_id == "PRODUCER"
+    if !market.organization.plan.nil? && (market.organization.plan.stripe_id == "PRODUCER" || market.organization.plan.stripe_id == "PRODUCER_2017")
       organization_params = context[:organization].as_json
 
       org = Organization.new(organization_params.slice(:id).merge(name: "#{organization_params['name']} Supplier", active: true, allow_purchase_orders: true, allow_credit_cards: true, can_sell: true, org_type: 'S'))
       market.organizations << org
-    else
-      context.fail!
     end
   end
 end
