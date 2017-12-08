fb5e40a7a45e125419d43dd040ab59beb0812e44
Weston Triemstra <weston@netsign.com>
2017-12-07 16:55:48 -0800

    Fix migrations to conform with production, regenerates schema.rb

--- db/migrate/20170412150843_rename_batch_consignment_receipt_error_to_batch_consignment_printable_error.rb
+++ db/migrate/20170412150843_rename_batch_consignment_receipt_error_to_batch_consignment_printable_error.rb
@@ -1,5 +1,6 @@
 class RenameBatchConsignmentReceiptErrorToBatchConsignmentPrintableError < ActiveRecord::Migration
   def change
     rename_table :batch_consignment_receipt_errors, :batch_consignment_printable_errors
+    rename_column :batch_consignment_printable_errors, :batch_consignment_receipt_id, :batch_consignment_printable_id
   end
 end
