55879b5acf50fba615e1aa8bf43cd2f33c6c55de
Weston Triemstra <weston@netsign.com>
2018-01-16 16:48:26 -0800

    Add some caching to user_activities

--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -7,7 +7,7 @@ class ApplicationPolicy
   end
 
   def user_activities
-    if @user.roles.count > 0
+    @user_activities ||= if @user.roles.count > 0
       all_actions = RoleAction.select('lower(description) AS description').where("org_types @> '{#{@user.primary_user_role}}'::character varying[] AND (plan_ids is null OR plan_ids @> '{#{@user.user_organizations.map(&:organization).compact.map(&:plan_id).compact.join(', ')}}'::character varying[])").distinct.map(&:description).flatten
       user_actions = @user.roles.select(:activities).where(org_type: @user.primary_user_role).distinct.map(&:activities).flatten
       if @user.roles[0].organization_id.nil?
