66662c3fb348642b221e6104302178e351edf5ac
Weston Triemstra <weston@netsign.com>
2018-02-16 00:34:03 -0800

    Improve vendor_payments feature spec to include a second managed market

--- spec/features/admin/financials/vendor_payments_spec.rb
+++ spec/features/admin/financials/vendor_payments_spec.rb
@@ -3,15 +3,24 @@ require 'spec_helper'
 feature 'Record payments to suppliers', :js  do
 
   include_context 'the mini market'
+  include_context 'second market'
+
+  let!(:order3_item1) { create(:order_item, product: sally_product1, unit_price: 9.99) }
+  let!(:order3) { create(:order, items: [order3_item1], market: second_market, organization: buyer2_organization) }
+  let!(:order4_item1) { create(:order_item, product: sally_product2, unit_price: 9.99) }
+  let!(:order4) { create(:order, items: [order4_item1], market: second_market, organization: buyer2_organization) }
 
   before do
-    switch_to_subdomain mini_market.subdomain
-    sign_in_as mary
+    second_market.managers << mary
+    mary.organizations << second_market.organization
+    second_market.organizations << seller_organization
     SetOrderItemsStatus.perform(
       user: mary,
-      order_item_ids: [order1_item1.id, order2_item1.id],
+      order_item_ids: [order1_item1.id, order2_item1.id, order3_item1.id, order4_item1.id],
       delivery_status: 'delivered'
     )
+    switch_to_subdomain mini_market.subdomain
+    sign_in_as mary
   end
 
   it 'displays all sellers to whom a managed market owes payment' do
@@ -21,6 +30,9 @@ feature 'Record payments to suppliers', :js  do
     expect(page).to have_content("Sally's Staples")
     expect(page).to have_content('2 orders from Mini Market')
     expect(page).to have_content('$13.98')
+
+    expect(page).to have_content('2 orders from Second Market')
+    expect(page).to have_content('$19.98')
   end
 
 end
