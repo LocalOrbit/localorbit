fd925f7a1601387b0f43aedb1d73f278e73d0f5d
Rob Barreca <rob@counterculturehawaii.com>
2018-02-28 17:56:27 -1000

    [LO-786] Use cleaner require_in_context in DeleteLocations.

--- app/interactors/delete_locations.rb
+++ app/interactors/delete_locations.rb
@@ -2,8 +2,7 @@ class DeleteLocations
   include Interactor
 
   def perform
-    organization = context[:organization]
-    location_ids = context[:location_ids]
+    require_in_context(:organization, :location_ids)
 
     context[:locations] = organization.locations.where(id: location_ids).each { |loc| loc.soft_delete }
   end
