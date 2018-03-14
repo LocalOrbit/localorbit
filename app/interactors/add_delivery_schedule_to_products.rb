393c0759991d0203b2effdfeaf2d1861436f448b
Weston Triemstra <weston@netsign.com>
2018-03-14 11:09:56 -0700

    [LO-1048] Handle case when there are no products on a new market

--- app/interactors/add_delivery_schedule_to_products.rb
+++ app/interactors/add_delivery_schedule_to_products.rb
@@ -9,19 +9,21 @@ class AddDeliveryScheduleToProducts
       where(market_organizations: {market: market}).
       where(gpt[:use_all_deliveries].eq(true)).uniq.pluck(:id)
 
-    columns = %w(product_id delivery_schedule_id)
+    if product_ids.present?
+      columns = %w(product_id delivery_schedule_id)
 
-    to_insert = product_ids.map do |product_id|
-      vals = [product_id, delivery_schedule.id]
-      ActiveRecord::Base.send(:replace_bind_variables,
-        "(#{vals.length.times.collect {'?'}.join(',')})", vals)
+      to_insert = product_ids.map do |product_id|
+        vals = [product_id, delivery_schedule.id]
+        ActiveRecord::Base.send(:replace_bind_variables,
+          "(#{vals.length.times.collect {'?'}.join(',')})", vals)
+      end
+
+      ActiveRecord::Base.connection.execute(<<-SQL)
+        INSERT INTO
+          product_deliveries
+        (#{columns.join(',')})
+        VALUES #{to_insert.join(",")}
+      SQL
     end
-
-    ActiveRecord::Base.connection.execute(<<-SQL)
-      INSERT INTO
-        product_deliveries
-      (#{columns.join(',')})
-      VALUES #{to_insert.join(",")}
-    SQL
   end
 end
--- spec/interactors/add_delivery_schedule_to_products_spec.rb
+++ spec/interactors/add_delivery_schedule_to_products_spec.rb
@@ -1,24 +1,34 @@
-require "spec_helper"
+require 'spec_helper'
 
 describe AddDeliveryScheduleToProducts do
   let!(:market)        { create(:market) }
   let!(:org1)          { create(:organization, :seller, markets: [market]) }
-  let!(:org1_product1) { create(:product, :sellable, organization: org1) }
-  let!(:org1_product2) { create(:product, :sellable, organization: org1, use_all_deliveries: false) }
   let!(:org2)          { create(:organization, markets: [market]) }
-  let!(:org2_product)  { create(:product, :sellable, organization: org2) }
-
   let!(:delivery_schedule) { create(:delivery_schedule) }
 
-  subject! { AddDeliveryScheduleToProducts.perform(delivery_schedule: delivery_schedule, market: market) }
+  context 'when products exist' do
+    let!(:org1_product1) { create(:product, :sellable, organization: org1) }
+    let!(:org1_product2) { create(:product, :sellable, organization: org1, use_all_deliveries: false) }
+    let!(:org2_product)  { create(:product, :sellable, organization: org2) }
 
-  it "adds the delivery schedule to products that use all deliveries" do
-    expect(org1_product1.reload.delivery_schedules).to include(delivery_schedule)
-    expect(org1_product2.reload.delivery_schedules).to_not include(delivery_schedule)
+    subject! { AddDeliveryScheduleToProducts.perform(delivery_schedule: delivery_schedule, market: market) }
+
+    it 'adds the delivery schedule to products that use all deliveries' do
+      expect(org1_product1.reload.delivery_schedules).to include(delivery_schedule)
+      expect(org1_product2.reload.delivery_schedules).to_not include(delivery_schedule)
+    end
+
+    it 'adds the delivery schedule to products regardless of organization selling status' do
+      expect(org2_product.reload.delivery_schedules).to include(delivery_schedule)
+    end
   end
 
-  it "adds the delivery schedule to products regardless of organization selling status" do
-    expect(org2_product.reload.delivery_schedules).to include(delivery_schedule)
+  context 'when no products exist' do
+    subject(:perform) { AddDeliveryScheduleToProducts.perform(delivery_schedule: delivery_schedule, market: market) }
+
+    it 'returns success' do
+      expect(perform.success?).to be true
+    end
   end
 
 end
