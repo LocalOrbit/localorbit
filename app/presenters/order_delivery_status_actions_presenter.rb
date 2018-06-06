bc2699fb0f600c68852aeb07d16ea129499e8515
Rob Barreca <rob@counterculturehawaii.com>
2018-06-05 22:26:35 -1000

    [LO-1105] Change double quote to single per code review.

--- app/presenters/order_delivery_status_actions_presenter.rb
+++ app/presenters/order_delivery_status_actions_presenter.rb
@@ -8,9 +8,9 @@ class OrderDeliveryStatusActionsPresenter
 
   def render
     if Order::DeliveryStatusPolicy.new(user, order).mark_delivered?
-      vc.button_tag "Mark all delivered", class: 'pull-right btn btn--small btn--primary mobile-block', style: 'margin-left: 15px;', id: 'mark-all-delivered', type: 'button'
+      vc.button_tag 'Mark all delivered', class: 'pull-right btn btn--small btn--primary mobile-block', style: 'margin-left: 15px;', id: 'mark-all-delivered', type: 'button'
     elsif Order::DeliveryStatusPolicy.new(user, order).mark_undelivered?
-      vc.button_tag "Undo mark delivery", class: 'pull-right btn btn--small btn--primary mobile-block' , id: 'undo-delivery', type: 'button'
+      vc.button_tag 'Undo mark delivery', class: 'pull-right btn btn--small btn--primary mobile-block' , id: 'undo-delivery', type: 'button'
     end
   end
 
