470911cae3990dc983345d19e32d7332ef5f6793
Weston Triemstra <weston@netsign.com>
2018-06-13 17:19:05 -0700

    WIP to get memcache configured

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,12 +6,15 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  config.perform_caching = true
+  config.cache_store = :dalli_store, 'localhost:11211'
+
   # Do not eager load code on boot.
   config.eager_load = false
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+  config.action_controller.perform_caching = true
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
