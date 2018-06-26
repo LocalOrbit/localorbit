6a0732641e20ca6246f1279b0d71c2958c7cad11
Weston Triemstra <weston@netsign.com>
2018-06-26 14:17:27 -0700

    Refactor payment mailer to use i18n for subject
    * allow locale files to organized within subdirectories
    * set default locale to be :en

--- app/mailers/payment_mailer.rb
+++ app/mailers/payment_mailer.rb
@@ -1,21 +1,17 @@
 class PaymentMailer < BaseMailer
+
   def payment_made(recipients, payment)
     @payment = payment
     @market  = @payment.market
 
-    mail(
-      to: recipients,
-      subject: "You Have Made a Payment"
-    )
+    mail(to: recipients)
   end
 
   def payment_received(recipients, payment)
     @payment = payment
     @market  = @payment.market
 
-    mail(
-      to: recipients,
-      subject: "You Have Received a Payment"
-    )
+    mail(to: recipients)
   end
+
 end
