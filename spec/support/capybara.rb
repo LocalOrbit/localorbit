78e9783f0529b21fdb87706128bcfafe8ef1c389
Weston Triemstra <weston@netsign.com>
2018-06-27 20:12:58 -0700

    Tune capybara config so don't get Net::ReadTimeout errors
    See https://github.com/SeleniumHQ/selenium/wiki/Ruby-Bindings#internal-timeouts
    and also https://github.com/teamcapybara/capybara/blob/2.18.0/lib/capybara.rb#L489

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -7,6 +7,20 @@ Capybara.default_max_wait_time = (ENV['CAPYBARA_WAIT_TIME'] || 180).to_i
 # hidden elements are ignored by default
 # Capybara.ignore_hidden_elements = true
 
+client = Selenium::WebDriver::Remote::Http::Default.new
+client.read_timeout = 120 # instead of default 60, in seconds
+
+browser_options = ::Selenium::WebDriver::Chrome::Options.new()
+browser_options.args << '--headless'
+browser_options.args << '--disable-gpu'
+
+Capybara.register_driver :selenium_chrome_headless do |app|
+  Capybara::Selenium::Driver.new(app,
+                                 http_client: client,
+                                 browser: :chrome,
+                                 options: browser_options)
+end
+
 Capybara.javascript_driver = :selenium_chrome_headless
 
 #RSpec.configure do |config|
