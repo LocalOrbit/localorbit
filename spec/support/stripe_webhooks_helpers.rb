2a66a2b5dfb3a4a9e57f930a976f1829242c3130
Weston Triemstra <weston@netsign.com>
2018-07-16 19:05:54 -0700

    [LO-1045] Refactor webhook specs as request specs
    Update stripe_event to 2.1.1
    Move the webhook specs from spec/features to sepc/requests
    Refactored webhook specs to more modern request spec style and vcr usage.
    Move the webhook json files into spec/fixtures
    New webhooks helper to bypass signature verification.
    Change the `event_retriever` to `event_filter` for StripeEvent api.Please enter the commit message for your changes. Lines starting

---
+++ spec/support/stripe_webhooks_helpers.rb
@@ -0,0 +1,8 @@
+module StripeWebhooksHelpers
+
+  def bypass_event_signature(payload)
+    event = Stripe::Event.construct_from(JSON.parse(payload, symbolize_names: true))
+    expect(Stripe::Webhook).to receive(:construct_event).and_return(event)
+  end
+
+end
