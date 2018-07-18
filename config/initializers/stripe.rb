100c4e863ca7228f5f4f555656797106bef33133
Weston Triemstra <weston@netsign.com>
2018-07-18 00:01:35 -0700

    [LO-1045] Update configs to include Connect signing secret

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,7 +1,11 @@
 Stripe.api_key = Figaro.env.stripe_secret_key
 Stripe.api_version = '2015-04-07'
 
-StripeEvent.signing_secret = Figaro.env.stripe_signing_secret
+StripeEvent.signing_secrets = [
+  Figaro.env.stripe_account_signing_secret,
+  Figaro.env.stripe_connect_signing_secret
+]
+
 StripeEvent.configure do |events|
   events.all PaymentProvider::Handlers::AsyncHandler.new
 end
