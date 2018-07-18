88b3656bb0b4ce7d123992a358f8e39ca7e31b5d
Weston Triemstra <weston@netsign.com>
2018-07-17 23:59:44 -0700

    [LO-1045] WIP on transfer.paid mystery

--- spec/requests/webhooks/stripe/transfer_events_spec.rb
+++ spec/requests/webhooks/stripe/transfer_events_spec.rb
@@ -5,14 +5,14 @@ RSpec.describe 'Stripe transfer events', :vcr, type: :request do
 
   describe 'transfer.paid event' do                                                                             end
   let(:stripe_account_id) { 'acct_15xJY9HouQbaP1MV' } # matches transfer.paid.json
-  let(:stripe_transfer_id) { 'tr_15xxwkHouQbaP1MV8O0tEg2b' } # matches transfer.paid.json
+  let(:stripe_transfer_id) { 'tr_15xcv9HouQbaP1MVXim58UCZ' } # matches transfer.paid.json
   let!(:market) { create(:market, stripe_account_id: stripe_account_id) }
   let!(:merri) { create(:user, :market_manager, name: 'Merri', managed_markets: [market]) }
   let!(:orders) { create_list(:order, 3, market: market) }
 
   # We need to brow-beat the test orders in the db to have IDs that match the hand-configured metadatain transfer.paid.json:
   let(:replace_order_ids) { [ 1234, 187, 1337 ] }
-  let(:payload) { File.read('spec/features/webhooks/transfer.paid.json') }
+  let(:payload) { File.read('spec/fixtures/webhooks/stripe/transfer.paid.json') }
 
   before do
     orders.zip(replace_order_ids).each do |order, new_id|
@@ -22,7 +22,6 @@ RSpec.describe 'Stripe transfer events', :vcr, type: :request do
 
   # FIXME see LO-1074: when legacy cassette is deleted this fails, event has changed to transfer.created,
   #   and lo.order_id metadata is missing
-  # NOTE: we are not currently subscribed to this event on production!
   it "creates a payment and emails the market's managers" do
     expect(find_payments.count).to eq 0
     post '/webhooks/stripe', payload
