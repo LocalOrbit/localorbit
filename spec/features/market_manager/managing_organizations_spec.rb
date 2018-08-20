f34319f821603d6db6e4afe7d32d4d1e392f8dba
Weston Triemstra <weston@netsign.com>
2018-08-20 12:48:43 -0700

    [LO-970] Use the DropdownHelpers select here also

--- spec/features/market_manager/managing_organizations_spec.rb
+++ spec/features/market_manager/managing_organizations_spec.rb
@@ -113,7 +113,7 @@ describe "A Market Manager", :vcr do
         click_link "Add Organization"
 
         fill_in "Name", with: "Famous Farm"
-        select market2.name, from: "Market", visible: false
+        select_option_on_singleselect('#initial_market_id_chosen', market2.name)
 
         expect(page).to have_content("Allow purchase orders")
         check "Allow purchase orders"
