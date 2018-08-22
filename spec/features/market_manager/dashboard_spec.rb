a5a70d4117295b6bc12ccbd89fc2f9891017531d
Weston Triemstra <weston@netsign.com>
2018-08-22 13:11:20 -0700

    [LO-970] Spec is now more robust and passess consistently

--- spec/features/market_manager/dashboard_spec.rb
+++ spec/features/market_manager/dashboard_spec.rb
@@ -1,12 +1,12 @@
-require "spec_helper"
+require 'spec_helper'
 
-feature "a market manager viewing their dashboard", :js, :shaky do
+feature 'a market manager viewing their dashboard', :js do
   let!(:user) { create(:user, :market_manager) }
   let!(:buyer) { create(:organization, :single_location, :buyer) }
   let!(:market) { create(:market, :with_addresses, organizations: [buyer]) }
   let!(:market_manager) { create(:user, :market_manager, managed_markets: [market]) }
-  let(:zaphod_farms) {create(:organization, :seller, :single_location, name: "Zaphod")}
-  let(:prefect_farms) {create(:organization, :seller, name: "Prefect")}
+  let(:zaphod_farms) {create(:organization, :seller, :single_location, name: 'Zaphod')}
+  let(:prefect_farms) {create(:organization, :seller, name: 'Prefect')}
 
   let(:product1) {create :product, :sellable, organization: zaphod_farms}
   let(:product2) {create :product, :sellable, organization: zaphod_farms}
@@ -16,80 +16,76 @@ feature "a market manager viewing their dashboard", :js, :shaky do
   let!(:delivery)    { delivery_schedule.next_delivery }
 
   before do
-    Timecop.travel("February 15, 2016") do
+    Timecop.travel('February 15, 2016') do
       order_item1 = create(:order_item, unit_price: 7, quantity: 1, product: product1)
       order_item2 = create(:order_item, unit_price: 3, quantity: 1, product: product2)
-      order = create(:order, delivery: delivery, items: [order_item1, order_item2], payment_method: "purchase order", market: market, total_cost: 10)
+      order = create(:order, delivery: delivery, items: [order_item1, order_item2], payment_method: 'purchase order', market: market, total_cost: 10)
       order.save!
     end
 
-    Timecop.travel("February 13, 2016") do
+    Timecop.travel('February 13, 2016') do
       order_item1 = create(:order_item, unit_price: 7, quantity: 1, product: product1)
       order_item2 = create(:order_item, unit_price: 3, quantity: 1, product: product2)
-      order = create(:order, delivery: delivery, items: [order_item1, order_item2], payment_method: "purchase order", market: market, total_cost: 10)
+      order = create(:order, delivery: delivery, items: [order_item1, order_item2], payment_method: 'purchase order', market: market, total_cost: 10)
       order.save!
     end
 
-    Timecop.travel("February 5, 2016") do
+    Timecop.travel('February 5, 2016') do
       order_item1 = create(:order_item, unit_price: 7, quantity: 1, product: product1)
       order_item2 = create(:order_item, unit_price: 3, quantity: 1, product: product2)
-      order = create(:order, delivery: delivery, items: [order_item1, order_item2], payment_method: "purchase order", market: market, total_cost: 10)
+      order = create(:order, delivery: delivery, items: [order_item1, order_item2], payment_method: 'purchase order', market: market, total_cost: 10)
       order.save!
     end
 
-    Timecop.travel("January 5, 2016") do
+    Timecop.travel('January 5, 2016') do
       order_item1 = create(:order_item, unit_price: 7, quantity: 1, product: product1)
       order_item2 = create(:order_item, unit_price: 3, quantity: 1, product: product2)
-      order = create(:order, delivery: delivery, items: [order_item1, order_item2], payment_method: "purchase order", market: market, total_cost: 10)
+      order = create(:order, delivery: delivery, items: [order_item1, order_item2], payment_method: 'purchase order', market: market, total_cost: 10)
       order.save!
     end
   end
 
   def login
-    Timecop.travel("February 15, 2016")
+    Timecop.travel('February 15, 2016')
     switch_to_subdomain(market.subdomain)
     sign_in_as(market_manager)
   end
 
-  context "view various timeframes" do
-
+  context 'view various timeframes' do
     before do
       login
       visit dashboard_path
     end
 
-    describe "viewing dashboard" do
-
-      it "market_manager views dashboard - 7D" do
-        page.execute_script('$("input[type=\'radio\']:checked").prop(\'checked\', false)')
-        page.execute_script('$("#sc-interval1").prop("checked", true).click()')
-        expect(page).to have_selector("#totalSalesAmount", text: '$20')
-        expect(page).to have_selector("#totalOrderCount", text: '2')
-        expect(page).to have_selector("#averageSalesAmount", text: '$10')
+    describe 'viewing dashboard' do
+      it 'market_manager views dashboard - 1D' do
+        expect(page).to have_content('Total Sales')
+        find(:css, 'label[for="sc-interval0"] > div').click
+        expect(page).to have_selector('#totalSalesAmount', text: '$10')
+        expect(page).to have_selector('#totalOrderCount', text: '1')
+        expect(page).to have_selector('#averageSalesAmount', text: '$10')
       end
 
-      #it "market_manager views dashboard - 1D" do
-      #  page.execute_script('$("input[type=\'radio\']:checked").prop(\'checked\', false)')
-      #  page.execute_script('$("#sc-interval0").prop("checked", true).click()')
-      #  expect(page).to have_selector("#totalSalesAmount", text: '$10')
-      #  expect(page).to have_selector("#totalOrderCount", text: '1')
-      #  expect(page).to have_selector("#averageSalesAmount", text: '$10')
-      #end
-
-      it "market_manager views dashboard - MTD" do
-        page.execute_script('$("input[type=\'radio\']:checked").prop(\'checked\', false)')
-        page.execute_script('$("#sc-interval2").prop("checked", true).click()')
-        expect(page).to have_selector("#totalSalesAmount", text: '$30')
-        expect(page).to have_selector("#totalOrderCount", text: '3')
-        expect(page).to have_selector("#averageSalesAmount", text: '$10')
+      it 'market_manager views dashboard - 7D' do
+        expect(page).to have_selector('#totalSalesAmount', text: '$20')
+        expect(page).to have_selector('#totalOrderCount', text: '2')
+        expect(page).to have_selector('#averageSalesAmount', text: '$10')
       end
 
-      it "market_manager views dashboard - YTD" do
-        page.execute_script('$("input[type=\'radio\']:checked").prop(\'checked\', false)')
-        page.execute_script('$("#sc-interval3").prop("checked", true).click()')
-        expect(page).to have_selector("#totalSalesAmount", text: '$40')
-        expect(page).to have_selector("#totalOrderCount", text: '4')
-        expect(page).to have_selector("#averageSalesAmount", text: '$10')
+      it 'market_manager views dashboard - MTD' do
+        expect(page).to have_content('Total Sales')
+        find(:css, 'label[for="sc-interval2"] > div').click
+        expect(page).to have_selector('#totalSalesAmount', text: '$30')
+        expect(page).to have_selector('#totalOrderCount', text: '3')
+        expect(page).to have_selector('#averageSalesAmount', text: '$10')
+      end
+
+      it 'market_manager views dashboard - YTD' do
+        expect(page).to have_content('Total Sales')
+        find(:css, 'label[for="sc-interval3"] > div').click
+        expect(page).to have_selector('#totalSalesAmount', text: '$40')
+        expect(page).to have_selector('#totalOrderCount', text: '4')
+        expect(page).to have_selector('#averageSalesAmount', text: '$10')
       end
     end
   end
