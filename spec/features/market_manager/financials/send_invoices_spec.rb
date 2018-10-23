8f735ef408fa25ff556d33f9fc2a4f797ee5692b
Weston Triemstra <weston@netsign.com>
2018-10-23 12:38:01 -0700

    PDF version now 1.7 (macos to mojave caused this?)

--- spec/features/market_manager/financials/send_invoices_spec.rb
+++ spec/features/market_manager/financials/send_invoices_spec.rb
@@ -128,7 +128,7 @@ feature "sending invoices" do
           expect(batch_invoice).to be
           expect(batch_invoice.pdf).to be
           expect(batch_invoice.pdf.file).to be
-          expect(batch_invoice.pdf.file.readlines.first).to match("%PDF-1.5\n")
+          expect(batch_invoice.pdf.file.readlines.first).to match("%PDF-1.7\n")
         end
       end
     end
