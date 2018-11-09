8c318acd0b6e40e38802f7cc466c0e4b6d115bbd
Weston Triemstra <weston@netsign.com>
2018-11-09 11:48:58 -0800

    Again, more tuning of window size, and better text to wait for

--- spec/features/selling/edit_pricing_spec.rb
+++ spec/features/selling/edit_pricing_spec.rb
@@ -224,7 +224,7 @@ describe 'price estimator', :js do
 
   it 'allows price adding and editing properly in both markets' do
     # Pricing adding tests
-    expect(page).to have_content('Market')
+    expect(page).to have_content('All Markets')
     select_option_on_singleselect('#p1_select_market_id_chosen', market1.name)
 
     fill_in "price[sale_price]", with: '12.90'
