a066090bd37317f22df8036f4563313aa5cd5fd9
Rob Barreca <rob@counterculturehawaii.com>
2020-03-02 10:18:07 -1000

    Attempt to fix flaky CI spec failures

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,5 @@
 require 'simplecov'
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
-    SimpleCov::Formatter::HTMLFormatter
-]
-
-SimpleCov.start do
+SimpleCov.start 'rails' do
   add_filter '/vendor/'
   add_filter '/config/'
   add_filter '/spec/'
@@ -12,6 +8,8 @@ SimpleCov.start do
   add_filter '/app/helpers/'
   add_filter '/app/uploaders/'
 end
+
+SimpleCov.formatter = SimpleCov::Formatter::HTMLFormatter
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= "test"
