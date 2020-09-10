3ec6b0529edfd5ec6e0e4045b8ffafbca86d03d2
pappudas bairagi <pappudas@bitcot.com>
2020-09-10 14:29:11 +0530

    add checkbox issue changes

--- app/decorators/product_decorator.rb
+++ app/decorators/product_decorator.rb
@@ -1,5 +1,47 @@
-class ProductDecorator < OrganizationItemDecorator
+class ProductDecorator < Draper::Decorator
   delegate_all
+  def has_custom_seller_info?
+    self[:location_id].present? || self[:who_story].present? || self[:how_story].present?
+  end
+
+  def location_options_for_select
+    # NOTE: Location options for new products are loaded on demand
+    return [] unless organization
+
+    organization.locations.visible.order("locations.default_shipping DESC").alphabetical_by_name.map do |location|
+      [location.name, location.id]
+    end
+  end
+
+  def who_story
+    self[:who_story].presence
+  end
+
+  def how_story
+    self[:how_story].presence
+  end
+
+  def location
+    return @location if defined?(@location)
+    @location = (location_id && Location.visible.find_by(id: location_id))
+  end
+
+  def location_map(w=300, h=200)
+    # if location && location.geocode
+    #   google_static_map([location.geocode], location.geocode, w, h, 11)
+    # else
+    #   ""
+    # end
+    ""
+  end
+
+  def location_label
+    "#{location.city}, #{location.state}" if location
+  end
+
+  def location_address
+    "<p class='adr'><span class='street-address'>#{location.address}</span> <span class='locality'>#{location.city}</span>, <span class='region'>#{location.state}</span></p>".html_safe if location
+  end
 
   def cart_item
     @cart_item ||= begin
