df3f0cf6a15906f7ec50bea14cfa0b61cb9c33f6
manjula <manjula@bitcot.com>
2020-09-10 04:49:50 -0700

    general products decorator

--- app/decorators/organization_item_decorator.rb
+++ app/decorators/organization_item_decorator.rb
@@ -27,20 +27,20 @@ class OrganizationItemDecorator < Draper::Decorator
     @location = (location_id && Location.visible.find_by(id: location_id)) || organization.shipping_location
   end
 
-  # def location_map(w=300, h=200)
-  #   # if location && location.geocode
-  #   #   google_static_map([location.geocode], location.geocode, w, h, 11)
-  #   # else
-  #   #   ""
-  #   # end
-  #   ""
-  # end
+  def location_map(w=300, h=200)
+    # if location && location.geocode
+    #   google_static_map([location.geocode], location.geocode, w, h, 11)
+    # else
+    #   ""
+    # end
+    ""
+  end
 
-  # def location_label
-  #   "#{location.city}, #{location.state}" if location
-  # end
+  def location_label
+    "#{location.city}, #{location.state}" if location
+  end
 
-  # def location_address
-  #   "<p class='adr'><span class='street-address'>#{location.address}</span> <span class='locality'>#{location.city}</span>, <span class='region'>#{location.state}</span></p>".html_safe if location
-  # end
+  def location_address
+    "<p class='adr'><span class='street-address'>#{location.address}</span> <span class='locality'>#{location.city}</span>, <span class='region'>#{location.state}</span></p>".html_safe if location
+  end
 end
